cb5cd8347d00aea3075649ad134c8fae654bedbb
Sasa Stamenkovic <umpirsky@gmail.com>
2012-01-29 21:03:03 +0100

    Fixed ArrayChoiceList not found error.

--- src/Sylius/Bundle/SalesBundle/Form/ChoiceList/StatusChoiceList.php
+++ src/Sylius/Bundle/SalesBundle/Form/ChoiceList/StatusChoiceList.php
@@ -13,43 +13,43 @@ namespace Sylius\Bundle\SalesBundle\Form\ChoiceList;
 
 use Sylius\Bundle\SalesBundle\Model\StatusManager;
 
-use Symfony\Component\Form\Extension\Core\ChoiceList\ArrayChoiceList;
+use Symfony\Component\Form\Extension\Core\ChoiceList\ChoiceList;
 
 /**
  * Order status choice list.
  *
  * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
  */
-class StatusChoiceList extends ArrayChoiceList
+class StatusChoiceList extends ChoiceList
 {
     /**
      * @var StatusManager
      */
     protected $statusManager;
-    
+
     /**
      * Constructor.
-     * 
+     *
      * @param $pointManager
      */
     public function __construct(StatusManager $statusManager)
     {
         $this->statusManager = $statusManager;
     }
-    
+
     /**
      * {@inheritdoc}
      */
     public function getChoices()
     {
         $choices = array();
-        
+
         foreach($this->statusManager->getStatuses() as $id => $status) {
             $choices[] = $this->statusManager->translateStatus($id);
         }
-        
+
         $this->choices = $choices;
-        
+
         return parent::getChoices();
     }
 }
