ab0a2f9a61598b959c7a2ef088757c7f72d9cfb7
James Haskell <james.haskell@sheknows.com>
2012-03-12 16:22:33 -0700

    Added configurable provider to cart bundle

--- src/Sylius/Bundle/CartBundle/DependencyInjection/Configuration.php
+++ src/Sylius/Bundle/CartBundle/DependencyInjection/Configuration.php
@@ -42,6 +42,7 @@ class Configuration implements ConfigurationInterface
                 ->scalarNode('driver')->cannotBeOverwritten()->isRequired()->cannotBeEmpty()->end()
                 ->scalarNode('engine')->defaultValue('twig')->end()
                 ->scalarNode('operator')->cannotBeOverwritten()->cannotBeEmpty()->end()
+                ->scalarNode('provider')->defaultValue('Sylius\\Bundle\\CartBundle\\Provider\\Provider')->cannotBeEmpty()->end()
             ->end();
         
         $this->addClassesSection($rootNode);
--- src/Sylius/Bundle/CartBundle/DependencyInjection/SyliusCartExtension.php
+++ src/Sylius/Bundle/CartBundle/DependencyInjection/SyliusCartExtension.php
@@ -49,6 +49,7 @@ class SyliusCartExtension extends Extension
         
         $container->setParameter('sylius_cart.driver', $config['driver']);
         $container->setParameter('sylius_cart.engine', $config['engine']);
+        $container->setParameter('sylius_cart.provider', $config['provider']);
         
         $container->setAlias('sylius_cart.operator', $config['operator']);
         
