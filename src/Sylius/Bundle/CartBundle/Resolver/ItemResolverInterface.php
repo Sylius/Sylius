ce6d76f01ab1c00035c34664d81600f789fc5f66
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-03-27 00:01:16 +0200

    added descriptions for item resolver.

--- src/Sylius/Bundle/CartBundle/Resolver/ItemResolverInterface.php
+++ src/Sylius/Bundle/CartBundle/Resolver/ItemResolverInterface.php
@@ -11,10 +11,32 @@
 
 namespace Sylius\Bundle\CartBundle\Resolver;
 
+use Sylius\Bundle\CartBundle\Model\ItemInterface;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Resolver returns cart item that needs to be added based on request.
+ * Should be called only when adding/removing items.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
 interface ItemResolverInterface
 {
+    /**
+     * Returns item to add.
+     *
+     * @param Request $request
+     *
+     * @return ItemInterface
+     */
     function resolveItemToAdd(Request $request);
+
+    /**
+     * Returns item to remove.
+     *
+     * @param Request $request
+     *
+     * @return ItemInterface
+     */
     function resolveItemToRemove(Request $request);
 }
