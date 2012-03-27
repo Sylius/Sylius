56f53ae3c2b1cac917f95a50a932f895a9a82700
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-03-28 00:08:09 +0200

    sensible default abstract CartOperator.

--- src/Sylius/Bundle/CartBundle/Operator/CartOperator.php
+++ src/Sylius/Bundle/CartBundle/Operator/CartOperator.php
@@ -17,6 +17,8 @@ use Sylius\Bundle\CartBundle\Model\ItemInterface;
 
 /**
  * Base class for cart operator.
+ * Sensible defaults for most common cart solutions.
+ * Can be overriden to fit exact needs.
  *
  * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
  */
@@ -37,5 +39,61 @@ abstract class CartOperator implements CartOperatorInterface
     public function __construct(CartManagerInterface $cartManager)
     {
         $this->cartManager = $cartManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addItem(CartInterface $cart, ItemInterface $item)
+    {
+        foreach ($cart->getItems() as $existingItem) {
+            if ($item->equals($existingItem)) {
+                $existingItem->setQuantity($existin->getQuantity() + $item->getQuantity());
+
+                return;
+            }
+        }
+
+        $cart->addItem($item);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function removeItem(CartInterface $cart, ItemInterface $item)
+    {
+        $cart->removeItem($item);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function refresh(CartInterface $cart)
+    {
+        $cart->setTotalItems($cart->countItems());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validate(CartInterface $cart)
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function clear(CartInterface $cart)
+    {
+        $this->cartManager->removeCart($cart);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function save(CartInterface $cart)
+    {
+        $this->cartManager->persistCart($cart);
     }
 }
