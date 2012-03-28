5002b77d6b6a62a838db2f2fed7e9f7163c4ec74
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-03-28 21:20:23 +0200

    add model tests.

---
+++ src/Sylius/Bundle/CartBundle/Tests/Model/CartTest.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\CartBundle\Tests\Model;
+
+/**
+ * Cart model test.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+class CartTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $cart = $this->getCart();
+
+        $this->assertEquals(0, $cart->getTotalItems());
+        $this->assertEquals(false, $cart->isLocked());
+        $this->assertInstanceOf('DateTime', $cart->getExpiresAt());
+    }
+
+    public function testGetSetId()
+    {
+        $cart = $this->getCart();
+        $this->assertNull($cart->getId());
+
+        $cart->setId(5);
+        $this->assertEquals(5, $cart->getId());
+    }
+
+    public function testGetSetTotalItems()
+    {
+        $cart = $this->getCart();
+
+        $cart->setTotalItems(3);
+        $this->assertEquals(3, $cart->getTotalItems());
+    }
+
+    public function testIsSetLocked()
+    {
+        $cart = $this->getCart();
+
+        $cart->setLocked(true);
+        $this->assertTrue($cart->isLocked());
+    }
+
+    public function testGetSetExpiresAt()
+    {
+        $cart = $this->getCart();
+        $expiresAt = $cart->getExpiresAt();
+
+        sleep(1);
+
+        $this->assertEquals($expiresAt, $cart->getExpiresAt());
+    }
+
+    public function testIncrementExpiresAt()
+    {
+        $cart = $this->getCart();
+        $expiresAt = $cart->getExpiresAt();
+
+        sleep(1);
+        $cart->incrementExpiresAt();
+
+        $this->assertGreaterThan($expiresAt, $cart->getExpiresAt());
+    }
+
+    private function getCart()
+    {
+        return $this->getMockForAbstractClass('Sylius\Bundle\CartBundle\Model\Cart');
+    }
+}
---
+++ src/Sylius/Bundle/CartBundle/Tests/Model/ItemTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\CartBundle\Tests\Model;
+
+/**
+ * Item model test.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+class ItemTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $item = $this->getItem();
+
+        $this->assertEquals(0, $item->getQuantity());
+    }
+
+    public function testGetSetId()
+    {
+        $item = $this->getItem();
+        $this->assertNull($item->getId());
+
+        $item->setId(5);
+        $this->assertEquals(5, $item->getId());
+    }
+
+    public function testGetSetCart()
+    {
+        $item = $this->getItem();
+        $this->assertNull($item->getCart());
+
+        $cart = $this->getMockCart();
+
+        $item->setCart($cart);
+        $this->assertEquals($cart, $item->getCart());
+
+    }
+
+    private function getItem()
+    {
+        return $this->getMockForAbstractClass('Sylius\Bundle\CartBundle\Model\Item');
+    }
+
+    private function getMockCart()
+    {
+        return $this->getMock('Sylius\Bundle\CartBundle\Model\CartInterface');
+    }
+}
+
