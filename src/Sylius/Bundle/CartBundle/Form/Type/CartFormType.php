4b2b9b20757bd9caaafa731624565fbea171a304
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-04-12 19:50:14 +0200

    update to catch up with Form api changes.

--- src/Sylius/Bundle/CartBundle/Form/Type/CartFormType.php
+++ src/Sylius/Bundle/CartBundle/Form/Type/CartFormType.php
@@ -51,7 +51,7 @@ class CartFormType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getDefaultOptions(array $options)
+    public function getDefaultOptions()
     {
         return array(
             'data_class' => $this->dataClass
--- src/Sylius/Bundle/CartBundle/Form/Type/ItemType.php
+++ src/Sylius/Bundle/CartBundle/Form/Type/ItemType.php
@@ -49,7 +49,7 @@ class ItemType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getDefaultOptions(array $options)
+    public function getDefaultOptions()
     {
         return array(
             'data_class' => $this->dataClass
