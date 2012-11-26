f1dab9540fe30d794f4b11bd57b18e8d06f978f0
Ivannis Suárez Jérez <ivannis.suarez@gmail.com>
2012-11-26 19:25:04 +0100

    added support for MongoDB  in ServiceGenerator class

--- src/Sylius/Bundle/ResourceBundle/DependencyInjection/ServiceGenerator.php
+++ src/Sylius/Bundle/ResourceBundle/DependencyInjection/ServiceGenerator.php
@@ -56,11 +56,41 @@ class ServiceGenerator
                 ->setPublic(false)
             ;
 
-
             $doctrineRepositoryServiceName = sprintf($pattern, 'doctrine.repository');
             $this->container->setDefinition($doctrineRepositoryServiceName, $doctrineRepository);
 
             $repository = new Definition('Sylius\\Bundle\\ResourceBundle\\Doctrine\\ORM\\ResourceRepository');
+            $repository->setArguments(array(new Reference($doctrineRepositoryServiceName), $class));
+
+            $this->container->setDefinition(sprintf($pattern, 'repository'), $repository);
+        } else if (SyliusResourceBundle::DRIVER_DOCTRINE_MONGODB_ODM === $driver) {
+            $pattern = $prefix.'.%s.'.$resourceName;
+
+            $controller = new Definition('Sylius\\Bundle\\ResourceBundle\\Controller\\ResourceController');
+            $controller
+                ->setArguments(array($prefix, $resourceName, ''))
+                ->addMethodCall('setContainer', array(new Reference('service_container')))
+            ;
+
+            $this->container->setDefinition(sprintf($pattern, 'controller'), $controller);
+
+            $manager = new Definition('Sylius\\Bundle\\ResourceBundle\\Doctrine\\ResourceManager');
+            $manager->setArguments(array(new Reference('doctrine.odm.mongodb.document_manager'), $class));
+
+            $this->container->setDefinition(sprintf($pattern, 'manager'), $manager);
+
+            $doctrineRepository = new Definition('Doctrine\\Common\\Persistence\\ObjectRepository');
+            $doctrineRepository
+                ->setFactoryService('doctrine.odm.mongodb.document_manager')
+                ->setFactoryMethod('getRepository')
+                ->setArguments(array($class))
+                ->setPublic(false)
+            ;
+
+            $doctrineRepositoryServiceName = sprintf($pattern, 'doctrine.repository');
+            $this->container->setDefinition($doctrineRepositoryServiceName, $doctrineRepository);
+
+            $repository = new Definition('Sylius\\Bundle\\ResourceBundle\\Doctrine\\ODM\\MongoDB\\ResourceRepository');
             $repository->setArguments(array(new Reference($doctrineRepositoryServiceName), $class));
 
             $this->container->setDefinition(sprintf($pattern, 'repository'), $repository);
