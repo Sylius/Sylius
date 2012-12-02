dbb04d69f5ca67b852d689543f41a1487989f6a0
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-12-02 23:11:24 +0100

    Make the entity repository more flexible

--- src/Sylius/Bundle/ResourceBundle/Doctrine/ORM/EntityRepository.php
+++ src/Sylius/Bundle/ResourceBundle/Doctrine/ORM/EntityRepository.php
@@ -12,6 +12,7 @@
 namespace Sylius\Bundle\ResourceBundle\Doctrine\ORM;
 
 use Doctrine\ORM\EntityRepository as BaseEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\Pagerfanta;
 
@@ -29,18 +30,105 @@ class EntityRepository extends BaseEntityRepository
         return new $className;
     }
 
-    public function createPaginator(array $criteria = array(), array $sortBy = null)
+    public function find($id)
     {
-        $alias = $this->getAlias();
-        $queryBuilder = $this->createQueryBuilder($alias);
+        return $this
+            ->getQueryBuilder()
+            ->andWhere($this->getAlias().'.id = '.intval($id))
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
 
-        if (null !== $sortBy) {
-            foreach ($sortBy as $property => $order) {
-                $queryBuilder->orderBy($alias.'.'.$property, $order);
-            }
+    public function findAll()
+    {
+        return $this
+            ->getCollectionQueryBuilder()
+            ->getQuery()
+            ->execute()
+        ;
+    }
+
+    public function findOneBy(array $criteria)
+    {
+        $queryBuilder = $this->getQueryBuilder();
+
+        $this->applyCriteria($queryBuilder, $criteria);
+
+        return $queryBuilder
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+    {
+        $queryBuilder = $this->getCollectionQueryBuilder();
+
+        $this->applyCriteria($queryBuilder, $criteria);
+        $this->applySorting($queryBuilder, $orderBy);
+
+        if (null !== $limit) {
+            $queryBuilder->setMaxResults($limit);
         }
 
+        if (null !== $offset) {
+            $queryBuilder->setFirstResult($offset);
+        }
+
+        return $queryBuilder
+            ->getQuery()
+            ->execute()
+        ;
+    }
+
+    public function createPaginator(array $criteria = null, array $orderBy = null)
+    {
+        $queryBuilder = $this->getCollectionQueryBuilder();
+
+        $this->applyCriteria($queryBuilder, $criteria);
+        $this->applySorting($queryBuilder, $orderBy);
+
         return new Pagerfanta(new DoctrineORMAdapter($queryBuilder));
+    }
+
+    protected function getQueryBuilder()
+    {
+        return $this->createQueryBuilder($this->getAlias());
+    }
+
+    protected function getCollectionQueryBuilder()
+    {
+        return $this->createQueryBuilder($this->getAlias());
+    }
+
+    protected function applyCriteria(QueryBuilder $queryBuilder, array $criteria = null)
+    {
+        if (null === $criteria) {
+            return;
+        }
+
+        $alias = $this->getAlias();
+
+        foreach ($criteria as $property => $value) {
+            $queryBuilder
+                ->andWhere($alias.'.'.$property.' = :'.$property)
+                ->setParameter($property, $value)
+            ;
+        }
+    }
+
+    protected function applySorting(QueryBuilder $queryBuilder, array $sorting = null)
+    {
+        if (null === $sorting) {
+            return;
+        }
+
+        $alias = $this->getAlias();
+
+        foreach ($sorting as $property => $order) {
+            $queryBuilder->orderBy($alias.'.'.$property, $order);
+        }
     }
 
     protected function getAlias()
