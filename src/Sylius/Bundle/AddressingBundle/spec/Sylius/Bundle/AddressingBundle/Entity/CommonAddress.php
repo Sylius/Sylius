ae7b3138e2e5bd37a99b0394e3d503b2f7fa7e83
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-12-02 15:03:10 +0100

    Update composer.json and readme, add simple examples

--- src/Sylius/Bundle/AddressingBundle/spec/Sylius/Bundle/AddressingBundle/Entity/CommonAddress.php
+++ src/Sylius/Bundle/AddressingBundle/spec/Sylius/Bundle/AddressingBundle/Entity/CommonAddress.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace spec\Sylius\Bundle\AddressingBundle\Model;
+namespace spec\Sylius\Bundle\AddressingBundle\Entity;
 
 use PHPSpec2\ObjectBehavior;
 
 /**
- * Common address model spec.
+ * Common address entity spec.
  *
  * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
  */
@@ -13,8 +13,7 @@ class CommonAddress extends ObjectBehavior
 {
     function it_should_be_initializable()
     {
-        $this->shouldHaveType('Sylius\Bundle\AddressingBundle\Model\CommonAddress');
-
+        $this->shouldHaveType('Sylius\Bundle\AddressingBundle\Entity\CommonAddress');
     }
 
     function it_should_be_sylius_common_address()
