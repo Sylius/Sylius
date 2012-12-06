eac7c22f7765e1332c3603a8a744c08eda335210
Ivannis Suárez Jérez <ivannis.suarez@gmail.com>
2012-12-06 17:07:56 +0100

    added the last changes in EntityRepository class to MongoDB DocumentRepository class

--- src/Sylius/Bundle/ResourceBundle/Doctrine/ODM/MongoDB/DocumentRepository.php
+++ src/Sylius/Bundle/ResourceBundle/Doctrine/ODM/MongoDB/DocumentRepository.php
@@ -12,11 +12,12 @@
 namespace Sylius\Bundle\ResourceBundle\Doctrine\ODM\MongoDB;
 
 use Doctrine\ODM\MongoDB\DocumentRepository as BaseDocumentRepository;
+use Doctrine\MongoDB\Query\Builder as QueryBuilder;
 use Pagerfanta\Adapter\DoctrineODMMongoDBAdapter;
 use Pagerfanta\Pagerfanta;
 
 /**
- * Doctrine ORM driver resource manager.
+ * Doctrine ODM driver resource manager.
  *
  * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
  */
@@ -29,15 +30,99 @@ class DocumentRepository extends BaseDocumentRepository
         return new $className;
     }
 
+    public function find($id)
+    {
+        return $this
+            ->getQueryBuilder()
+            ->field('id')->equals(new \MongoId($id))
+            ->getQuery()
+            ->getSingleResult()
+        ;
+    }
+    
+    public function findAll()
+    {
+        return $this
+            ->getCollectionQueryBuilder()
+            ->getQuery()
+            ->execute()
+        ;
+    }
+    
+    public function findOneBy(array $criteria)
+    {
+        $queryBuilder = $this->getQueryBuilder();
+    
+        $this->applyCriteria($queryBuilder, $criteria);
+    
+        return $queryBuilder
+            ->getQuery()
+            ->getSingleResult()
+        ;
+    }
+    
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+    {
+        $queryBuilder = $this->getCollectionQueryBuilder();
+    
+        $this->applyCriteria($queryBuilder, $criteria);
+        $this->applySorting($queryBuilder, $orderBy);
+    
+        if (null !== $limit) {
+            $queryBuilder->limit($limit);
+        }
+    
+        if (null !== $offset) {
+            $queryBuilder->skip($offset);
+        }
+                    
+        return $queryBuilder
+            ->getQuery()
+            ->execute()
+        ;
+    }
+    
     public function createPaginator(array $criteria = array(), array $sortBy = null)
     {
-        $queryBuilder = $this->createQueryBuilder();
-        if (null !== $sortBy) {
-            foreach ($sortBy as $property => $order) {
-                $queryBuilder->sort($property, $order);
-            }
-        }
-
+        $queryBuilder = $this->getCollectionQueryBuilder();
+        
+        $this->applyCriteria($queryBuilder, $criteria);
+        $this->applySorting($queryBuilder, $sortBy);
+        
         return new Pagerfanta(new DoctrineODMMongoDBAdapter($queryBuilder));
+    }
+        
+    protected function getQueryBuilder()
+    {
+        return $this->createQueryBuilder();
+    }
+    
+    protected function getCollectionQueryBuilder()
+    {
+        return $this->createQueryBuilder();
+    }
+    
+    protected function applyCriteria(QueryBuilder $queryBuilder, array $criteria = null)
+    {
+        if (null === $criteria) {
+            return;
+        }
+    
+        foreach ($criteria as $property => $value) {
+            $queryBuilder
+                ->field($property)->equals($value)
+            ;
+        }
+    }
+    
+    protected function applySorting(QueryBuilder $queryBuilder, array $sorting = null)
+    {
+        if (null === $sorting) {
+            return;
+        }
+    
+        foreach ($sorting as $property => $order) {
+            $queryBuilder->sort($property, $order);
+        }
     }
 }
