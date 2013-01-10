1c68a707167b5d232d0f56cb0afda68da9911010
umpirsky <umpirsky@gmail.com>
2013-01-10 12:54:37 +0100

    Added InventoryChangeListener

---
+++ EventListener/InventoryChangeListenerInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Sylius sandbox application.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\InventoryBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+/**
+ * Inventory change listeners must implement this interface because {@link SyliusInventoryExtension}
+ * tags events with {@link InventoryChangeListenerInterface::onInventoryChange()} method.
+ *
+ * @author Саша Стаменковић <umpirsky@gmail.com>
+ */
+interface InventoryChangeListenerInterface
+{
+    /**
+     * Fired on inventory change.
+     *
+     * @param GenericEvent $event
+     */
+    public function onInventoryChange(GenericEvent $event);
+}
