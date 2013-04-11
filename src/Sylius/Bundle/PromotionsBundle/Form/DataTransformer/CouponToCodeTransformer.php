fc7e58d1fe05d937ce8e7166d221777739092e01
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2013-04-11 08:33:11 +0200

    Add coupon to code form type

---
+++ src/Sylius/Bundle/PromotionsBundle/Form/DataTransformer/CouponToCodeTransformer.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\PromotionsBundle\Form\DataTransformer;
+
+use Doctrine\Common\Persistence\ObjectRepository;
+use Sylius\Bundle\PromotionsBundle\Model\CouponInterface;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
+
+/**
+ * Coupon to code transformer.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+class CouponToCodeTransformer implements DataTransformerInterface
+{
+    /**
+     * Coupon repository.
+     *
+     * @var ObjectRepository
+     */
+    private $couponRepository;
+
+    /**
+     * Constructor.
+     *
+     * @param ObjectRepository $couponRepository
+     */
+    public function __construct(ObjectRepository $couponRepository)
+    {
+        $this->couponRepository = $couponRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transform($coupon)
+    {
+        if (null === $coupon) {
+            return '';
+        }
+
+        if (!$coupon instanceof CouponInterface) {
+            throw new UnexpectedTypeException($coupon, 'Sylius\Bundle\PromotionsBundle\Model\CouponInterface');
+        }
+
+        return $coupon->getCode();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function reverseTransform($code)
+    {
+        if (!$code) {
+            return null;
+        }
+
+        return $this->couponRepository->findOneBy(array('code' => $code));
+    }
+}
+
