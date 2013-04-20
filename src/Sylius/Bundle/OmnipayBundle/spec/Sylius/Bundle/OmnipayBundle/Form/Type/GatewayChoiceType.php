dc72511b9a3cf9f5a302111e362e7a4432d2b7a0
Alex Demchenko <pilot@pesochin.net>
2013-04-21 00:26:51 +0300

    Update requirements, add specs

---
+++ src/Sylius/Bundle/OmnipayBundle/spec/Sylius/Bundle/OmnipayBundle/Form/Type/GatewayChoiceType.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Sylius\Bundle\OmnipayBundle\Form\Type;
+
+use PHPSpec2\ObjectBehavior;
+
+class GatewayChoiceType extends ObjectBehavior
+{
+    /**
+     * @param array $gateways
+     */
+    function let($gateways)
+    {
+        $this->beConstructedWith($gateways);
+    }
+
+    function it_should_be_a_form_type()
+    {
+        $this->shouldHaveType('Symfony\Component\Form\AbstractType');
+    }
+
+    function it_should_have_custom_field_name()
+    {
+        $this->getName()->shouldReturn('sylius_omnipay_gateway_choice');
+    }
+
+    function its_getParent_should_return_choice()
+    {
+        $this->getParent()->shouldReturn('choice');
+    }
+}
