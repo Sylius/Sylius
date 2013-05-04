49101669a231e15b64d405029cda5e0670bbde2a
Piotr Antosik <mail@piotrantosik.com>
2013-05-04 23:11:00 +0200

    ENCJA produkt posiada wycinek (demo) dostarczony przez autora który może pobrać użytkownik

---
+++ src/Ant/Bundle/CoreBundle/Entity/Product.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ant\Bundle\CoreBundle\Entity;
+
+use Sylius\Bundle\CoreBundle\Entity\Product as BaseProduct;
+
+/**
+ * Product entity.
+ *
+ */
+class Product extends BaseProduct
+{
+
+}
