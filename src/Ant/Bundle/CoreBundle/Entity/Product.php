5f62cdd1ab8c3d1012b36442c8cc913e8e34d975
Piotr Antosik <mail@piotrantosik.com>
2013-05-07 12:48:07 +0200

    encja produktu i usera

--- src/Ant/Bundle/CoreBundle/Entity/Product.php
+++ src/Ant/Bundle/CoreBundle/Entity/Product.php
@@ -2,13 +2,22 @@
 
 namespace Ant\Bundle\CoreBundle\Entity;
 
-use Sylius\Bundle\CoreBundle\Entity\Product as BaseProduct;
+use Sylius\Bundle\AssortmentBundle\Model\Product as BaseProduct;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Product entity.
- *
+ * Produkt bez wariantów, parametrów.
  */
 class Product extends BaseProduct
 {
 
+    private $enabled;
+
+    public function __construct() {
+        parent::__construct();
+
+        $this->enabled = false;
+    }
 }
--- src/Ant/Bundle/CoreBundle/Entity/User.php
+++ src/Ant/Bundle/CoreBundle/Entity/User.php
@@ -6,9 +6,9 @@ use Sylius\Bundle\CoreBundle\Entity\User as BaseUser;
 
 /**
  * User entity.
- *
  */
 class User extends BaseUser
 {
     const ROLE_ADD_PRODUCT = 'ROLE_ADD_PRODUCT';
+
 }
