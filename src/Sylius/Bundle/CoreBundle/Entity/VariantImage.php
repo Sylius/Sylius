a344ddb634ce4f5bc9633107e0f91ae17d6d1cbf
Alexandre Bacco <alexandre.bacco@gmail.com>
2013-05-08 15:48:05 +0300

    Fix PHP Doc

--- src/Sylius/Bundle/CoreBundle/Entity/VariantImage.php
+++ src/Sylius/Bundle/CoreBundle/Entity/VariantImage.php
@@ -18,11 +18,17 @@ class VariantImage extends Image implements VariantImageInterface
 {
     protected $variant;
 
+    /**
+     * {@inheritdoc}
+     */
     public function getVariant()
     {
         return $this->variant;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function setVariant(VariantInterface $variant = null)
     {
         $this->variant = $variant;
