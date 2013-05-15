a2c231f344b72c8880e08a45684e80dfcef76884
winzou <alexandre.bacco@gmail.com>
2013-05-15 18:29:07 +0200

    Get OrderItem back

---
+++ src/Sylius/Bundle/CoreBundle/Model/OrderItemInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\CoreBundle\Model;
+
+use Sylius\Bundle\CartBundle\Model\CartItemInterface;
+
+/**
+ * Sylius core Order model.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+interface OrderItemInterface extends CartItemInterface
+{
+}
