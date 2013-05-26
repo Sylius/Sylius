e46cd152a8477c215bfb455e057d47371443ebd6
jjanvier <j.janvier@gmail.com>
2013-05-26 12:33:39 +0200

    Adding firstName and lastName properties to User entity. Overriding FOS User profile edition form.

--- src/Sylius/Bundle/CoreBundle/Entity/User.php
+++ src/Sylius/Bundle/CoreBundle/Entity/User.php
@@ -21,6 +21,10 @@ use FOS\UserBundle\Entity\User as BaseUser;
  */
 class User extends BaseUser
 {
+    protected $firstName;
+
+    protected $lastName;
+
     protected $orders;
 
     public function __construct()
@@ -33,5 +37,25 @@ class User extends BaseUser
     public function getOrders()
     {
         return $this->orders;
+    }
+
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastName;
     }
 }
