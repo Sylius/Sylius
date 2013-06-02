5557640636f87e753dbc7257fe097b8f8e1320f3
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2013-06-03 00:01:07 +0200

    Initial specs clean up

--- src/Sylius/Bundle/PromotionsBundle/spec/Sylius/Bundle/SyliusPromotionsBundle/Repository/PromotionRepositorySpec.php
+++ src/Sylius/Bundle/PromotionsBundle/spec/Sylius/Bundle/SyliusPromotionsBundle/Repository/PromotionRepositorySpec.php
@@ -1,15 +1,22 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace spec\Sylius\Bundle\PromotionsBundle\Repository;
 
 use PHPSpec2\ObjectBehavior;
 
 /**
- * Promotion repository spec.
- *
  * @author Saša Stamenković <umpirsky@gmail.com>
  */
-class PromotionRepository extends ObjectBehavior
+class PromotionRepositorySpec extends ObjectBehavior
 {
     /**
      * @param Doctrine\ORM\EntityManager         $em
