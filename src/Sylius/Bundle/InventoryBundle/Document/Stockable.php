edc7a471bda86f29c18b1d15fdeae30f4da33bca
Eymen Gunay <eymen@egunay.com>
2013-06-12 19:42:56 +0200

    MongoDB ODM support added

---
+++ src/Sylius/Bundle/InventoryBundle/Document/Stockable.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\InventoryBundle\Document;
+
+use Sylius\Bundle\InventoryBundle\Model\Stockable as BaseStockable;
+
+/**
+ * Stocakble entity.
+ *
+ * @author Eymen Gunay <eymen@egunay.com>
+ */
+class Stockable extends BaseStockable
+{
+}
