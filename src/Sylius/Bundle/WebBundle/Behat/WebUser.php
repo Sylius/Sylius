699941a71db2a7f26898b65ef0c12113f4588ea6
Alexandre Bacco <alexandre.bacco@gmail.com>
2013-06-12 20:42:58 +0200

    Fix WebUser
    This should turn Travis green

--- src/Sylius/Bundle/WebBundle/Behat/WebUser.php
+++ src/Sylius/Bundle/WebBundle/Behat/WebUser.php
@@ -636,10 +636,10 @@ class WebUser extends MinkContext implements KernelAwareInterface
      */
     private function iAmLoggedInAsRole($role)
     {
-        $this->getSubContext('data')->thereIsUser('username', 'password', $role);
+        $this->getSubContext('data')->thereIsUser('email@foo.com', 'password', $role);
         $this->getSession()->visit($this->generatePageUrl('fos_user_security_login'));
 
-        $this->fillField('Email', 'email');
+        $this->fillField('Email', 'email@foo.com');
         $this->fillField('Password', 'password');
         $this->pressButton('login');
     }
