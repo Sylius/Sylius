b0c5d36360a677cfbebf04bfcaf7b5881aa215c4
headrevision <headrevision@gmail.com>
2013-08-13 16:46:42 +0200

    added step to initially grant access for app in OAuth login scenarios

--- src/Sylius/Bundle/WebBundle/Behat/OAuthContext.php
+++ src/Sylius/Bundle/WebBundle/Behat/OAuthContext.php
@@ -21,6 +21,33 @@ use Behat\MinkExtension\Context\RawMinkContext;
  */
 class OAuthContext extends RawMinkContext
 {
+/**
+ *@When /^I wait some time$/
+ */
+public function waitsometime() {
+$this->getSession('selenium2')->wait(30000, 'false');
+}
+    /**
+     * @When /^I allow the use of my (.+) account \(if I am still on the (.+) website\)$/
+     */
+    public function iAllowTheUseOfMyAccount($providerName, $domain)
+    {
+        if ($this->currentUrlContains($domain)) {
+
+            $submitButtons = $this->getSession('selenium2')->getPage()->findAll('xpath', '//form//button[@type="submit"]');
+            if (count($submitButtons) != 2) {
+                throw new ExpectationException('Page should contain a form with 2 buttons.', $this->getSession('selenium2'));
+            }
+
+            if ($providerName == 'Google') {
+                $submitButtons[0]->click();
+            } else {
+                $submitButtons[1]->click();
+            }
+
+        }
+    }
+
     /**
      * @Given /^I am not logged in$/
      */
