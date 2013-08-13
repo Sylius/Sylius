f354c2b486d382a91380656e7e3e4c1702698d3c
headrevision <headrevision@gmail.com>
2013-08-13 16:46:27 +0200

    reference implementation of Sylius user login/registration by OAuth sign-in provider account (incomplete)

---
+++ src/Sylius/Bundle/CoreBundle/OAuth/UserProvider.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+* This file is part of the Sylius package.
+*
+* (c) Paweł Jędrzejewski
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Sylius\Bundle\CoreBundle\OAuth;
+
+use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
+use HWI\Bundle\OAuthBundle\Security\Core\User\FOSUBUserProvider;
+use Sylius\Bundle\CoreBundle\Model\User;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+
+/**
+ * Loading and ad-hoc creation of a user by an OAuth sign-in provider account.
+ *
+ * @author Fabian Kiss <fabian.kiss@ymc.ch>
+ */
+class UserProvider extends FOSUBUserProvider
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function loadUserByOAuthUserResponse(UserResponseInterface $response)
+    {
+        try {
+            return parent::loadUserByOAuthUserResponse($response);
+        } catch (UsernameNotFoundException $e) {
+            if (null === $user = $this->userManager->findUserByEmail($response->getEmail())) {
+                return $this->createUserByOAuthUserResponse($response);
+            } else {
+                return $this->updateUserByOAuthUserResponse($user, $response);
+            }
+        }
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function connect($user, UserResponseInterface $response)
+    {
+        $providerName = $response->getResourceOwner()->getName();
+        $uniqueId = $response->getUsername();
+        $user->addOAuthAccount($providerName, $uniqueId);
+
+        $this->userManager->updateUser($user);
+    }
+
+    /**
+     * Ad-hoc creation of user
+     *
+     * @param UserResponseInterface $response
+     *
+     * @return User
+     */
+    protected function createUserByOAuthUserResponse(UserResponseInterface $response)
+    {
+        $user = $this->userManager->createUser();
+        $this->updateUserByOAuthUserResponse($user, $response);
+
+        // set default values taken from OAuth sign-in provider account
+        if (null !== $email = $response->getEmail()) {
+            $user->setEmail($email);
+        }
+        if (null === $this->userManager->findUserByUsername($response->getNickname())) {
+            $user->setUsername($response->getNickname());
+        }
+
+        $user->setEnabled(true);
+
+        return $user;
+    }
+
+    /**
+     * Attach OAuth sign-in provider account to existing user
+     *
+     * @param User $user
+     * @param UserResponseInterface $response
+     *
+     * @return User
+     */
+    protected function updateUserByOAuthUserResponse(User $user, UserResponseInterface $response)
+    {
+        $providerName = $response->getResourceOwner()->getName();
+        $providerNameSetter = 'set'.ucfirst($providerName).'Id';
+        $user->$providerNameSetter($response->getUsername());
+
+        return $user;
+    }
+}
---
+++ src/Sylius/Bundle/WebBundle/Behat/OAuthContext.php
@@ -0,0 +1,104 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\WebBundle\Behat;
+
+use Behat\Mink\Exception\ExpectationException;
+use Behat\MinkExtension\Context\RawMinkContext;
+
+/**
+ * OAuth context.
+ *
+ * @author Fabian Kiss <fabian.kiss@ymc.ch>
+ */
+class OAuthContext extends RawMinkContext
+{
+    /**
+     * @Given /^I am not logged in$/
+     */
+    public function iAmNotLoggedIn()
+    {
+        $this->getSession('selenium2')->restart();
+    }
+
+    /**
+     * @When /^I allow the use of my (.+) account \(if I am still on the (.+) website\)$/
+     */
+    public function iAllowTheUseOfMyAccount($providerName, $domain)
+    {
+        if ($this->currentUrlContains($domain)) {
+
+            $submitButtons = $this->getSession('selenium2')->getPage()->findAll('xpath', '//form//button[@type="submit"]');
+            if (count($submitButtons) != 2) {
+                throw new ExpectationException('Page should contain a form with 2 buttons.', $this->getSession('selenium2'));
+            }
+
+            if ($providerName == 'Google') {
+                $submitButtons[0]->click();
+            } else {
+                $submitButtons[1]->click();
+            }
+
+        }
+    }
+
+    /**
+     * @Then /^I should be on the (.+) website$/
+     * @Then /^I should still be on the (.+) website$/
+     */
+    public function iShouldBeOnTheWebsite($domain)
+    {
+        if (!$this->currentUrlContains($domain)) {
+            $message = sprintf('Current URL should contain "%s".', $domain);
+            throw new ExpectationException($message, $this->getSession('selenium2'));
+        }
+    }
+
+    /**
+     * @Then /^I should not be on the (.+) website anymore$/
+     */
+    public function iShouldNotBeOnTheWebsiteAnymore($domain)
+    {
+        if ($this->currentUrlContains($domain)) {
+            $message = sprintf('Current URL should not contain "%s".', $domain);
+            throw new ExpectationException($message, $this->getSession('selenium2'));
+        }
+
+        // Re-set default session
+        $currentUrl = $this->getSession()->getCurrentUrl();
+        $this->getMink()->setDefaultSessionName('symfony2');
+        $this->getSession()->visit($currentUrl);
+    }
+
+    /**
+     * @Then /^I should see the .+ login form$/
+     */
+    public function iShouldSeeTheLoginForm()
+    {
+        $loginForm = $this->getLoginForm();
+    }
+
+    protected function currentUrlContains($domain)
+    {
+        $currentUrl = $this->getSession('selenium2')->getCurrentUrl();
+        return strpos($currentUrl, $domain) !== false;
+    }
+
+    protected function getLoginForm()
+    {
+        return $this->assertSession('selenium2')->elementExists('xpath',
+            '//form//input[@type="email"]' .
+            '/ancestor::form//input[@type="password"]' .
+            '/ancestor::form//*[@type="submit"]' .
+            '/ancestor::form'
+        );
+    }
+}
