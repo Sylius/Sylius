dceea8b95e807e26b30c8808d12295ab7602063b
amenophis <jeremy@leherpeur.net>
2013-08-30 19:42:58 +0200

    Add parsing expression in container

--- src/Sylius/Bundle/ResourceBundle/Controller/ParametersParser.php
+++ src/Sylius/Bundle/ResourceBundle/Controller/ParametersParser.php
@@ -11,27 +11,67 @@
 
 namespace Sylius\Bundle\ResourceBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\DependencyInjection\Container;
 
 /**
  * Configuration parameters parser.
  *
  * @author Paweł Jędrzejewski <pjedrzejewski@sylius.pl>
+ * @author Jérémy Leherpeur <jeremy@leherpeur.net>
  */
 class ParametersParser
 {
-    public function parse(array $parameters, Request $request)
+    /**
+     * @var Container
+     */
+    protected $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    public function parse(array $parameters = array())
     {
         foreach ($parameters as $key => $value) {
             if (is_array($value)) {
-                $parameters[$key] = $this->parse($value, $request);
+                $parameters[$key] = $this->parse($value);
             }
 
             if (is_string($value) && 0 === strpos($value, '$')) {
                 $parameters[$key] = $request->get(substr($value, 1));
             }
+
+            if (is_string($value) && $result = $this->getServiceAndExpression($value)) {
+                $accessor = PropertyAccess::createPropertyAccessor();
+                try{
+                    $value = $accessor->getValue($result['service'], $result['expression']);
+                    $parameters[$key] = $value;
+                } catch (\Exception $e) {
+                    return false;
+                }
+            }
         }
 
         return $parameters;
+    }
+
+    protected function getServiceAndExpression($expression)
+    {
+        $expressionParts = explode('.', $expression);
+
+        $serviceName = "";
+        $serviceExists = false;
+        while (count($expressionParts) > 0 && !$serviceExists = $this->container->has($serviceName))
+        {
+            $serviceName .= ($serviceName === "") ? array_shift($expressionParts) : '.'.array_shift($expressionParts);
+        }
+
+        if (!$serviceExists) return false;
+
+        return array(
+            'service' => $this->container->get($serviceName),
+            'expression' => implode('.', $expressionParts)
+        );
     }
 }
