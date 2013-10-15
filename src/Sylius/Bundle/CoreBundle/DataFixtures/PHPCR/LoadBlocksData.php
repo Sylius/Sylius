28f4da1f0232c1eeb6302688b9b44c577b9f4d83
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2013-10-15 03:52:19 +0200

    POC of CMF block bundle integration

---
+++ src/Sylius/Bundle/CoreBundle/DataFixtures/PHPCR/LoadBlocksData.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\CoreBundle\DataFixtures\PHPCR;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use PHPCR\Util\NodeHelper;
+
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\Yaml\Parser;
+
+use Symfony\Cmf\Bundle\BlockBundle\Doctrine\Phpcr\SimpleBlock;
+
+class LoadBlocksData extends ContainerAware implements FixtureInterface, OrderedFixtureInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $session = $manager->getPhpcrSession();
+
+        $basepath = $this->container->getParameter('cmf_block.persistence.phpcr.block_basepath');;
+        NodeHelper::createPath($session, $basepath);
+
+        $parent = $manager->find(null, $basepath);
+
+        $contactBlock = new SimpleBlock();
+        $contactBlock->setParentDocument($parent);
+        $contactBlock->setName('contact');
+        $contactBlock->setTitle('Contact us');
+        $contactBlock->setBody('Call (8) 888 333 421!');
+
+        $manager->persist($contactBlock);
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrder()
+    {
+        return 1;
+    }
+}
