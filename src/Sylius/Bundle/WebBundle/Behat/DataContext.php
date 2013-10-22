cac22e07f2214623852a3737a6b1802df32d7b84
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2013-10-22 21:31:13 +0200

    Fix the shipping charges processor

--- src/Sylius/Bundle/WebBundle/Behat/DataContext.php
+++ src/Sylius/Bundle/WebBundle/Behat/DataContext.php
@@ -697,7 +697,10 @@ class DataContext extends BehatContext implements KernelAwareInterface
     {
         foreach ($table->getHash() as $data) {
             $category = array_key_exists('category', $data) ? $data['category'] : null;
-            $method = $this->thereIsShippingMethod($data['name'], $data['zone'], $category);
+            $calculator = array_key_exists('calculator', $data) ? str_replace(' ', '_', strtolower($data['calculator'])) : null;
+            $configuration = array_key_exists('configuration', $data) ? $this->getConfiguration($data['configuration']) : null;
+
+            $method = $this->thereIsShippingMethod($data['name'], $data['zone'], $category, $calculator, $configuration);
         }
     }
 
@@ -728,7 +731,7 @@ class DataContext extends BehatContext implements KernelAwareInterface
      * @Given /^I created shipping method "([^""]*)" within zone "([^""]*)"$/
      * @Given /^There is shipping method "([^""]*)" within zone "([^""]*)"$/
      */
-    public function thereIsShippingMethod($name, $zoneName)
+    public function thereIsShippingMethod($name, $zoneName, $category = null, $calculator = DefaultCalculators::PER_ITEM_RATE, array $configuration = array('amount' => 2500))
     {
         $method = $this
             ->getRepository('shipping_method')
@@ -737,8 +740,8 @@ class DataContext extends BehatContext implements KernelAwareInterface
 
         $method->setName($name);
         $method->setZone($this->findOneByName('zone', $zoneName));
-        $method->setCalculator(DefaultCalculators::PER_ITEM_RATE);
-        $method->setConfiguration(array('amount' => 25.00));
+        $method->setCalculator($calculator);
+        $method->setConfiguration($configuration);
 
         $manager = $this->getEntityManager();
 
