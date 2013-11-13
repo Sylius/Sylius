cf2f7df0adef66eb90048adf05730097464859c7
Julien Janvier <j.janvier@gmail.com>
2013-11-13 13:18:35 +0100

    Type hinting specs

--- src/Sylius/Bundle/PaymentsBundle/spec/Sylius/Bundle/PaymentsBundle/Form/Type/PaymentMethodTypeSpec.php
+++ src/Sylius/Bundle/PaymentsBundle/spec/Sylius/Bundle/PaymentsBundle/Form/Type/PaymentMethodTypeSpec.php
@@ -13,6 +13,8 @@ namespace spec\Sylius\Bundle\PaymentsBundle\Form\Type;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
  * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
@@ -29,10 +31,7 @@ class PaymentMethodTypeSpec extends ObjectBehavior
         $this->shouldImplement('Symfony\Component\Form\FormTypeInterface');
     }
 
-    /**
-     * @param Symfony\Component\Form\FormBuilder $builder
-     */
-    function it_builds_form_with_proper_fields($builder)
+    function it_builds_form_with_proper_fields(FormBuilder $builder)
     {
         $builder
             ->add('name', 'text', Argument::any())
@@ -61,10 +60,7 @@ class PaymentMethodTypeSpec extends ObjectBehavior
         $this->buildForm($builder, array());
     }
 
-    /**
-     * @param Symfony\Component\OptionsResolver\OptionsResolverInterface $resolver
-     */
-    function it_defines_assigned_data_class($resolver)
+    function it_defines_assigned_data_class(OptionsResolverInterface $resolver)
     {
         $resolver
             ->setDefaults(array(
