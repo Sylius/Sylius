017734bb70228dc923921ef5571c5c70dfc3dc13
Marco Zirino <marco@krewmedia.com>
2013-12-05 12:17:01 -0800

    added province fields

---
+++ src/Sylius/Bundle/AddressingBundle/Model/Province.php
@@ -0,0 +1,115 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\AddressingBundle\Model;
+
+/**
+ * Default province model.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@sylius.pl>
+ */
+class Province implements ProvinceInterface
+{
+    /**
+     * Province id.
+     *
+     * @var mixed
+     */
+    protected $id;
+
+    /**
+     * Name.
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Name.
+     *
+     * @var string
+     */
+    protected $isoName;
+
+    /**
+     * Country.
+     *
+     * @var CountryInterface
+     */
+    protected $country;
+
+    public function __toString()
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIsoName()
+    {
+        return $this->isoName;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setIsoName($isoName)
+    {
+        $this->isoName = $isoName;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCountry(CountryInterface $country = null)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+}
