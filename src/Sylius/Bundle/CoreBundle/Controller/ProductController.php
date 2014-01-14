4ffb065e4b3038ad11e8b2bb4130a37ed72732a8
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2014-01-14 12:04:07 +0100

    Fix product controller & delete action

--- src/Sylius/Bundle/CoreBundle/Controller/ProductController.php
+++ src/Sylius/Bundle/CoreBundle/Controller/ProductController.php
@@ -63,17 +63,15 @@ class ProductController extends ResourceController
             throw new NotFoundHttpException('Requested taxon does not exist.');
         }
 
-        $config = $this->getConfiguration();
-
         $paginator = $this
             ->getRepository()
             ->createByTaxonPaginator($taxon)
         ;
 
-        $paginator->setMaxPerPage($config->getPaginationMaxPerPage());
+        $paginator->setMaxPerPage($this->config->getPaginationMaxPerPage());
         $paginator->setCurrentPage($request->query->get('page', 1));
 
-        return $this->renderResponse($config->getTemplate('productIndex.html'), array(
+        return $this->render($this->config->getTemplate('productIndex.html'), array(
             'taxon'    => $taxon,
             'products' => $paginator,
         ));
