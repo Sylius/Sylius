fe2310b91cec2ed0766e49e5fb2164314d79c82f
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-01-18 20:01:32 +0200

    Added input options to support --no-interaction flag for sylius:install

--- src/Sylius/Bundle/InstallerBundle/Command/InstallCommand.php
+++ src/Sylius/Bundle/InstallerBundle/Command/InstallCommand.php
@@ -3,6 +3,8 @@
 namespace Sylius\Bundle\InstallerBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use RuntimeException;
@@ -14,6 +16,15 @@ class InstallCommand extends ContainerAwareCommand
         $this
             ->setName('sylius:install')
             ->setDescription('Sylius installer.')
+            ->addOption(
+                'with-fixtures',
+                null,
+                InputOption::VALUE_NONE,
+                'Install fixtures without asking (used with --no-interaction)'
+            )
+            ->addOption('admin-name', 'an', InputOption::VALUE_OPTIONAL, 'Administrator name')
+            ->addOption('admin-password', 'ap', InputOption::VALUE_OPTIONAL, 'Administrator password')
+            ->addOption('admin-email', 'ae', InputOption::VALUE_OPTIONAL, 'Administrator email')
         ;
     }
 
@@ -67,33 +78,17 @@ class InstallCommand extends ContainerAwareCommand
     {
         $output->writeln('<info>Setting up database.</info>');
 
-        $dialog = $this->getHelperSet()->get('dialog');
-
         $this
-            ->runCommand('doctrine:database:create', $input, $output)
-            ->runCommand('doctrine:schema:create', $input, $output)
-            ->runCommand('assetic:dump', $input, $output)
+            ->runCommand('doctrine:database:create', new ArrayInput(['--no-interaction']), $output)
+            ->runCommand('doctrine:schema:create', new ArrayInput([['--no-interaction']]), $output)
+            ->runCommand('assetic:dump', new ArrayInput(['--no-interaction']), $output)
         ;
 
-        if ($dialog->askConfirmation($output, '<question>Load fixtures (Y/N)?</question>', false)) {
-            $this->runCommand('doctrine:fixtures:load', $input, $output);
-        }
-
         $output->writeln('');
-        $output->writeln('<info>Administration setup.</info>');
 
-        $userClass = $this->getContainer()->getParameter('sylius.model.user.class');
-        $user = new $userClass;
+        $this->installFixtures($input, $output);
 
-        $user->setUsername($dialog->ask($output, '<question>Username:</question>'));
-        $user->setPlainPassword($dialog->ask($output, '<question>Password:</question>'));
-        $user->setEmail($dialog->ask($output, '<question>Email:</question>'));
-        $user->setEnabled(true);
-        $user->setRoles(array('ROLE_SYLIUS_ADMIN'));
-
-        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
-        $em->persist($user);
-        $em->flush();
+        $this->createAdminUser($input, $output);
 
         $output->writeln('');
 
@@ -102,12 +97,79 @@ class InstallCommand extends ContainerAwareCommand
 
     private function runCommand($command, InputInterface $input, OutputInterface $output)
     {
-        $this
+        $input->setInteractive(false);
+        $returnCode = $this
             ->getApplication()
             ->find($command)
             ->run($input, $output)
         ;
 
+        if ($returnCode !== 0) {
+            throw new RuntimeException(sprintf(
+                "Command '%s %s' failed during setup. See output for details.",
+                $command,
+                $input
+            ));
+        }
+
         return $this;
+    }
+
+    protected function installFixtures(InputInterface $input, OutputInterface $output)
+    {
+        $dialog = $this->getHelperSet()->get('dialog');
+
+        if ($input->isInteractive()) {
+            if (!$dialog->askConfirmation($output, '<question>Load fixtures (Y/N)?</question>', false)) {
+                return;
+            }
+        } else {
+            if (!$input->getOption('with-fixtures')) {
+                return;
+            }
+        }
+
+        $output->writeln('<info>Loading sample data.</info>');
+        $this->runCommand(
+            'doctrine:fixtures:load',
+            new ArrayInput(['--no-interaction']),
+            $output
+        );
+
+        $output->writeln('');
+    }
+
+    protected function createAdminUser(InputInterface $input, OutputInterface $output)
+    {
+        $dialog = $this->getHelperSet()->get('dialog');
+
+        $userClass = $this->getContainer()->getParameter('sylius.model.user.class');
+        $user      = new $userClass;
+
+        $output->writeln('<info>Creating administrator account.</info>');
+
+        if ($input->isInteractive()) {
+            $username = $dialog->ask($output, '<question>Username:</question>');
+            $password = $dialog->ask($output, '<question>Password:</question>');
+            $email    = $dialog->ask($output, '<question>Email:</question>');
+        } else {
+            $username = $input->getOption('admin-name');
+            $password = $input->getOption('admin-password');
+            $email    = $input->getOption('admin-email');
+        }
+
+        $output->writeln("<info>Username:</info> " . $username);
+        $output->writeln("<info>Password:</info> " . $password);
+        $output->writeln("<info>Email:</info>    " . $email);
+
+        $user->setUsername($username);
+        $user->setPlainPassword($password);
+        $user->setEmail($email);
+        $user->setEnabled(true);
+        $user->setRoles(array('ROLE_SYLIUS_ADMIN'));
+
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        $em->persist($user);
+        $em->flush();
     }
 }
