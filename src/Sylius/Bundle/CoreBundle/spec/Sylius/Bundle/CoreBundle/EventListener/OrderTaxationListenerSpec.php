ac4dae1f85a9dfaf48d2fa165085c77eac6fee5d
Alexandre <alexandre.bacco@gmail.com>
2014-01-22 22:56:22 +0800

    Rename OrderTaxationListener::applyTaxes

--- src/Sylius/Bundle/CoreBundle/spec/Sylius/Bundle/CoreBundle/EventListener/OrderTaxationListenerSpec.php
+++ src/Sylius/Bundle/CoreBundle/spec/Sylius/Bundle/CoreBundle/EventListener/OrderTaxationListenerSpec.php
@@ -12,16 +12,16 @@
 namespace spec\Sylius\Bundle\CoreBundle\EventListener;
 
 use PhpSpec\ObjectBehavior;
+use Sylius\Bundle\CoreBundle\Model\OrderInterface;
+use Sylius\Bundle\CoreBundle\OrderProcessing\TaxationProcessorInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
 
 /**
  * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
  */
 class OrderTaxationListenerSpec extends ObjectBehavior
 {
-    /**
-     * @param Sylius\Bundle\CoreBundle\OrderProcessing\TaxationProcessorInterface $taxationProcessor
-     */
-    function let($taxationProcessor)
+    function let(TaxationProcessorInterface $taxationProcessor)
     {
         $this->beConstructedWith($taxationProcessor);
     }
@@ -31,29 +31,22 @@ class OrderTaxationListenerSpec extends ObjectBehavior
         $this->shouldHaveType('Sylius\Bundle\CoreBundle\EventListener\OrderTaxationListener');
     }
 
-    /**
-     * @param Symfony\Component\EventDispatcher\GenericEvent $event
-     * @param \stdClass                                      $invalidSubject
-     */
-    function it_throws_exception_if_event_has_non_order_subject($event, $invalidSubject)
+    function it_throws_exception_if_event_has_non_order_subject(GenericEvent $event, \stdClass $invalidSubject)
     {
         $event->getSubject()->willReturn($invalidSubject);
 
         $this
             ->shouldThrow('InvalidArgumentException')
-            ->duringOnCartChange($event)
+            ->duringApplyTaxes($event)
         ;
     }
 
-    /**
-     * @param Symfony\Component\EventDispatcher\GenericEvent $event
-     * @param Sylius\Bundle\CoreBundle\Model\OrderInterface  $order
-     */
-    function it_calls_taxation_processor_on_order($taxationProcessor, $event, $order)
+    function it_calls_taxation_processor_on_order(TaxationProcessorInterface $taxationProcessor, GenericEvent $event, OrderInterface $order)
     {
         $event->getSubject()->willReturn($order);
         $taxationProcessor->applyTaxes($order)->shouldBeCalled();
+        $order->calculateTotal()->shouldBeCalled();
 
-        $this->onCartChange($event);
+        $this->applyTaxes($event);
     }
 }
