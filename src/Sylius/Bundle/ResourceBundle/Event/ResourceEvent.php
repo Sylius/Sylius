a495ef775bbfe7dd83bed70af8fe200451beb60a
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2014-01-29 13:32:34 +0100

    Fix missed conflicts

--- src/Sylius/Bundle/ResourceBundle/Event/ResourceEvent.php
+++ src/Sylius/Bundle/ResourceBundle/Event/ResourceEvent.php
@@ -25,32 +25,20 @@ class ResourceEvent extends GenericEvent
     const TYPE_INFO     = 'info';
     const TYPE_SUCCESS  = 'success';
 
-<<<<<<< HEAD
-    public function stop($message, $type = self::TYPE_ERROR, $params = array())
-=======
     public function stop($message, $type = self::TYPE_ERROR, $parameters = array())
->>>>>>> Introduce domain manager and restore the ability to cancel action
     {
         $this->error = true;
         $this->messageType = $type;
         $this->message = $message;
-<<<<<<< HEAD
-        $this->messageParams = $params;
-=======
         $this->messageParameters = $parameters;
 
->>>>>>> Introduce domain manager and restore the ability to cancel action
         $this->stopPropagation();
     }
 
     /**
      * Indicate if an error has been detected
      *
-<<<<<<< HEAD
      * @var Boolean
-=======
-     * @var boolean
->>>>>>> Introduce domain manager and restore the ability to cancel action
      */
     protected $error = false;
 
@@ -73,20 +61,12 @@ class ResourceEvent extends GenericEvent
      *
      * @var array
      */
-<<<<<<< HEAD
-    protected $messageParams = array();
-=======
     protected $messageParameters = array();
->>>>>>> Introduce domain manager and restore the ability to cancel action
 
     /**
      * Get error property
      *
-<<<<<<< HEAD
      * @return Boolean $error
-=======
-     * @return boolean $error
->>>>>>> Introduce domain manager and restore the ability to cancel action
      */
     public function isStopped()
     {
@@ -114,15 +94,6 @@ class ResourceEvent extends GenericEvent
     }
 
     /**
-<<<<<<< HEAD
-     * Get messageParams property
-     *
-     * @return string $messageParams
-     */
-    public function getMessageParams()
-    {
-        return $this->messageParams;
-=======
      * Get messageParameters property
      *
      * @return string $messageParameters
@@ -130,6 +101,5 @@ class ResourceEvent extends GenericEvent
     public function getMessageParameters()
     {
         return $this->messageParameters;
->>>>>>> Introduce domain manager and restore the ability to cancel action
     }
 }
