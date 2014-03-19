a9cc7aa0013bdc3e8498b22baa1a72004977ed9f
jjanvier <j.janvier@gmail.com>
2014-03-19 13:34:12 +0100

    Fixing references to TaxationBundle classes

--- src/Sylius/Bundle/CoreBundle/OrderProcessing/TaxationProcessor.php
+++ src/Sylius/Bundle/CoreBundle/OrderProcessing/TaxationProcessor.php
@@ -15,8 +15,8 @@ use Sylius\Component\Addressing\Matcher\ZoneMatcherInterface;
 use Sylius\Bundle\CoreBundle\Model\OrderInterface;
 use Sylius\Bundle\ResourceBundle\Model\RepositoryInterface;
 use Sylius\Bundle\SettingsBundle\Model\Settings;
-use Sylius\Bundle\TaxationBundle\Calculator\CalculatorInterface;
-use Sylius\Bundle\TaxationBundle\Resolver\TaxRateResolverInterface;
+use Sylius\Component\Taxation\Calculator\CalculatorInterface;
+use Sylius\Component\Taxation\Resolver\TaxRateResolverInterface;
 
 /**
  * Taxation processor.
