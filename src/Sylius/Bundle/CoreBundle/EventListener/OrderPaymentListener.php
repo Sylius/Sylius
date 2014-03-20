9d0d0d003f530cb2871f16ca7d63a349099b37ae
kayue <kayuey@gmail.com>
2014-03-20 23:11:11 +0800

    Update order payment amount before paid.

--- src/Sylius/Bundle/CoreBundle/EventListener/OrderPaymentListener.php
+++ src/Sylius/Bundle/CoreBundle/EventListener/OrderPaymentListener.php
@@ -61,18 +61,33 @@ class OrderPaymentListener
      * Get the order from event and create payment.
      *
      * @param GenericEvent $event
+     * @throws \InvalidArgumentException
      */
     public function createOrderPayment(GenericEvent $event)
     {
-        $order = $event->getSubject();
+        $this->paymentProcessor->createPayment($this->getOrder($event));
+    }
 
-        if (!$order instanceof OrderInterface) {
-            throw new \InvalidArgumentException(
-                'Order payment listener requires event subject to be instance of "Sylius\Bundle\CoreBundle\Model\OrderInterface"'
-            );
+    /**
+     * Update order's payment.
+     *
+     * @param GenericEvent $event
+     * @throws \InvalidArgumentException
+     */
+    public function updateOrderPayment(GenericEvent $event)
+    {
+        $order = $this->getOrder($event);
+        $payment = $order->getPayment();
+
+        if (!$payment instanceof PaymentInterface) {
+            throw new \InvalidArgumentException(sprintf(
+                'Order\'s payment has to be instance of "Sylius\Bundle\PaymentsBundle\Model\PaymentInterface", %s given.',
+                is_object($payment) ? get_class($payment) : gettype($payment)
+            ));
         }
 
-        $this->paymentProcessor->createPayment($order);
+        $payment->setCurrency($order->getCurrency());
+        $payment->setAmount($order->getTotal());
     }
 
     public function updateOrderOnPayment(GenericEvent $event)
@@ -96,5 +111,23 @@ class OrderPaymentListener
             $this->dispatcher->dispatch(SyliusOrderEvents::PRE_PAY, new GenericEvent($order, $event->getArguments()));
             $this->dispatcher->dispatch(SyliusOrderEvents::POST_PAY, new GenericEvent($order, $event->getArguments()));
         }
+    }
+
+    /**
+     * @param GenericEvent $event
+     * @return OrderInterface
+     * @throws \InvalidArgumentException
+     */
+    protected function getOrder(GenericEvent $event)
+    {
+        $order = $event->getSubject();
+
+        if (!$order instanceof OrderInterface) {
+            throw new \InvalidArgumentException(
+                'Order payment listener requires event subject to be instance of "Sylius\Bundle\CoreBundle\Model\OrderInterface"'
+            );
+        }
+
+        return $order;
     }
 }
