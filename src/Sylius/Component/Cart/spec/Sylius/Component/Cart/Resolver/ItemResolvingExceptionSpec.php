7114172f2c125be2cee27663724a13c3a47de60d
jjanvier <j.janvier@gmail.com>
2014-03-24 22:08:44 +0100

    Misc specs missing

---
+++ src/Sylius/Component/Cart/spec/Sylius/Component/Cart/Resolver/ItemResolvingExceptionSpec.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Sylius\Component\Cart\Resolver;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+/**
+ * @author Julien Janvier <j.janvier@gmail.com>
+ */
+class ItemResolvingExceptionSpec  extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Sylius\Component\Cart\Resolver\ItemResolvingException');
+    }
+
+    function is_is_an_invalid_argument_exception()
+    {
+        $this->shouldHaveType('\InvalidArgumentException');
+    }
+}
