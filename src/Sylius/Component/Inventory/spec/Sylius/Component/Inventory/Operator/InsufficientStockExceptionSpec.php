657d5cfd9bad887b4e8e5cc65c3753cc04a0f9a4
jjanvier <j.janvier@gmail.com>
2014-03-24 22:08:44 +0100

    Misc specs missing

---
+++ src/Sylius/Component/Inventory/spec/Sylius/Component/Inventory/Operator/InsufficientStockExceptionSpec.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Sylius\Component\Inventory\Operator;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Sylius\Component\Inventory\Model\StockableInterface;
+
+/**
+ * @author Julien Janvier <j.janvier@gmail.com>
+ */
+class InsufficientStockExceptionSpec extends ObjectBehavior
+{
+    function let(StockableInterface $stockable)
+    {
+        $this->beConstructedWith($stockable, 10);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Sylius\Component\Inventory\Operator\InsufficientStockException');
+    }
+
+    function it_is_an_uderflow_exception()
+    {
+        $this->shouldHaveType('\UnderflowException');
+    }
+
+    function it_returns_its_stockable()
+    {
+        $this->getStockable()->shouldHaveType('Sylius\Component\Inventory\Model\StockableInterface');
+    }
+}
