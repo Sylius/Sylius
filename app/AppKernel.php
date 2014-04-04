3b84d2898555a2e77d7270da1d02b15da5562122
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2014-04-04 12:25:50 +0200

    Example of external kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -9,144 +9,17 @@
  * file that was distributed with this source code.
  */
 
-use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\HttpKernel\Kernel;
+use Sylius\Bundle\CoreBundle\Kernel\SyliusKernel;
 
 /**
  * Sylius kernel.
  *
  * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
  */
-class AppKernel extends Kernel
+class AppKernel extends SyliusKernel
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function registerBundles()
+    public function getDir()
     {
-        $bundles = array(
-            // Sylius bundles.
-            new Sylius\Bundle\InstallerBundle\SyliusInstallerBundle(),
-            new Sylius\Bundle\OrderBundle\SyliusOrderBundle(),
-            new Sylius\Bundle\MoneyBundle\SyliusMoneyBundle(),
-            new Sylius\Bundle\SettingsBundle\SyliusSettingsBundle(),
-            new Sylius\Bundle\CartBundle\SyliusCartBundle(),
-            new Sylius\Bundle\ProductBundle\SyliusProductBundle(),
-            new Sylius\Bundle\VariableProductBundle\SyliusVariableProductBundle(),
-            new Sylius\Bundle\TaxationBundle\SyliusTaxationBundle(),
-            new Sylius\Bundle\ShippingBundle\SyliusShippingBundle(),
-            new Sylius\Bundle\PaymentsBundle\SyliusPaymentsBundle(),
-            new Sylius\Bundle\PayumBundle\SyliusPayumBundle(),
-            new Sylius\Bundle\PromotionsBundle\SyliusPromotionsBundle(),
-            new Sylius\Bundle\AddressingBundle\SyliusAddressingBundle(),
-            new Sylius\Bundle\InventoryBundle\SyliusInventoryBundle(),
-            new Sylius\Bundle\TaxonomiesBundle\SyliusTaxonomiesBundle(),
-            new Sylius\Bundle\FlowBundle\SyliusFlowBundle(),
-
-            new Sylius\Bundle\CoreBundle\SyliusCoreBundle(),
-            new Sylius\Bundle\WebBundle\SyliusWebBundle(),
-            new Sylius\Bundle\ResourceBundle\SyliusResourceBundle(),
-
-            // CMF bundles.
-            new Sonata\BlockBundle\SonataBlockBundle(),
-            new Symfony\Cmf\Bundle\CoreBundle\CmfCoreBundle(),
-            new Symfony\Cmf\Bundle\BlockBundle\CmfBlockBundle(),
-            new Symfony\Cmf\Bundle\ContentBundle\CmfContentBundle(),
-            new Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle(),
-            new Symfony\Cmf\Bundle\MenuBundle\CmfMenuBundle(),
-
-            // Core bundles.
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-
-            // Third party bundles.
-            new Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle(),
-            new Liip\ImagineBundle\LiipImagineBundle(),
-            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-            new Knp\Bundle\GaufretteBundle\KnpGaufretteBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle($this),
-            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
-            new FOS\RestBundle\FOSRestBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
-            new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
-            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-            new JMS\TranslationBundle\JMSTranslationBundle(),
-            new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
-            new Payum\Bundle\PayumBundle\PayumBundle(),
-        );
-
-        if ('dev' === $this->environment) {
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-        }
-
-        $bundles = $this->addFixturesBundle($bundles);
-
-        return $bundles;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load(__DIR__.'/config/config_'.$this->environment.'.yml');
-
-        if (is_file($file = __DIR__.'/config/config_'.$this->environment.'.local.yml')) {
-            $loader->load($file);
-        }
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getCacheDir()
-    {
-        if ($this->isVagrantEnvironment()) {
-            return '/dev/shm/sylius/cache/'.$this->environment;
-        }
-
-        return parent::getCacheDir();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getLogDir()
-    {
-        if ($this->isVagrantEnvironment()) {
-            return '/dev/shm/sylius/logs';
-        }
-
-        return parent::getLogDir();
-    }
-
-    /**
-     * @return boolean
-     */
-    private function isVagrantEnvironment()
-    {
-        return (getenv('HOME') === '/home/vagrant' || getenv('VAGRANT') === 'VAGRANT') && is_dir('/dev/shm');
-    }
-
-    /**
-     * @param array $bundles
-     * @param array $environments
-     *
-     * @return array
-     */
-    private function addFixturesBundle(array $bundles, array $environments = array('dev', 'test'))
-    {
-        if (in_array($this->environment, $environments) && class_exists('Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle')) {
-            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
-            $bundles[] = new Sylius\Bundle\FixturesBundle\SyliusFixturesBundle();
-        }
-
-        return $bundles;
+        return __DIR__;
     }
 }
