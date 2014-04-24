0e4d31a7fc933f76eda6a16f464971bde5eeab8b
Alexandre <alexandre.bacco@gmail.com>
2014-04-24 16:01:02 +0800

    Add Finite dependency

--- src/Sylius/Bundle/CoreBundle/Kernel/Kernel.php
+++ src/Sylius/Bundle/CoreBundle/Kernel/Kernel.php
@@ -86,6 +86,7 @@ abstract class Kernel extends BaseKernel
             new \HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
             new \Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new \WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
+            new \Finite\Bundle\FiniteBundle\FiniteFiniteBundle(),
         );
 
         if (in_array($this->environment, array('dev', 'test'))) {
