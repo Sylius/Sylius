6c5d1b60561d1420d6053ea57d1523184335c2bf
Alexandre <alexandre.bacco@gmail.com>
2014-05-01 17:17:34 +0800

    Use transition on Order creation

--- src/Sylius/Component/Order/OrderTransitions.php
+++ src/Sylius/Component/Order/OrderTransitions.php
@@ -15,7 +15,7 @@ class OrderTransitions
 {
     const GRAPH = 'sylius_order';
 
-    const SYLIUS_CHECKOUT = 'checkout';
+    const SYLIUS_CREATE   = 'create';
     const SYLIUS_CONFIRM  = 'confirm';
     const SYLIUS_SHIP     = 'ship';
     const SYLIUS_ABANDON  = 'abandon';
