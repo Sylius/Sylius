84d1c3d378bb4fc88c2bb1e989db5f85ae147608
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-05-26 13:10:28 +0000

    remove custom service files.

--- src/Sylius/Bundle/PayumBundle/DependencyInjection/SyliusPayumExtension.php
+++ src/Sylius/Bundle/PayumBundle/DependencyInjection/SyliusPayumExtension.php
@@ -21,24 +21,6 @@ class SyliusPayumExtension extends AbstractResourceExtension
      */
     public function load(array $config, ContainerBuilder $container)
     {
-        list($config, $loader) = $this->configure($config, new Configuration(), $container, self::CONFIGURE_LOADER | self::CONFIGURE_DATABASE | self::CONFIGURE_PARAMETERS);
-
-        $loader->load('payment/generic.xml');
-
-        if (class_exists('Payum\Be2Bill\PaymentFactory')) {
-            $loader->load('payment/be2bill.xml');
-        }
-
-        if (class_exists('Payum\Paypal\ExpressCheckout\Nvp\PaymentFactory')) {
-            $loader->load('payment/paypal_express_checkout_nvp.xml');
-        }
-
-        if (class_exists('Payum\Paypal\ExpressCheckout\Nvp\PaymentFactory')) {
-            $loader->load('payment/paypal_express_checkout_nvp.xml');
-        }
-
-        if (class_exists('Payum\OmnipayBridge\PaymentFactory')) {
-            $loader->load('payment/omnipay_bridge.xml');
-        }
+        $this->configure($config, new Configuration(), $container, self::CONFIGURE_LOADER | self::CONFIGURE_DATABASE | self::CONFIGURE_PARAMETERS);
     }
 }
