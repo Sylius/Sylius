4d6b6bd42d312612f73c12db1337f980e96b88b9
stloyd <stloyd@gmail.com>
2014-09-09 16:49:47 +0200

    [CoreBundle] Add ability to information how many products were sold

---
+++ app/migrations/Version20140909144526.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Sylius\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20140909144526 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE sylius_product_variant ADD sold INT NOT NULL');
+        $this->addSql('CREATE INDEX IDX_A29B52398D2DD99 ON sylius_product_variant (sold)');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX IDX_A29B52398D2DD99 ON sylius_product_variant');
+        $this->addSql('ALTER TABLE sylius_product_variant DROP sold');
+    }
+}
