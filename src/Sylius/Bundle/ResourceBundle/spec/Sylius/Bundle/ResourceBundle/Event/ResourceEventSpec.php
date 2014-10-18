eb0d24e9dbf7ec3c5f217426e91012adae86c1d7
Arnaud Langlade <alanglade@clever-age.com>
2014-10-18 15:14:30 +0200

    Missing specs

---
+++ src/Sylius/Bundle/ResourceBundle/spec/Sylius/Bundle/ResourceBundle/Event/ResourceEventSpec.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace spec\Sylius\Bundle\ResourceBundle\Event;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Sylius\Bundle\ResourceBundle\Event\ResourceEvent;
+
+class ResourceEventSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith('message');
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Sylius\Bundle\ResourceBundle\Event\ResourceEvent');
+    }
+
+    function it_stops_event_propagation()
+    {
+        $this->stop('message', ResourceEvent::TYPE_SUCCESS, array('parameter'));
+        $this->getMessageType()->shouldReturn(ResourceEvent::TYPE_SUCCESS);
+        $this->getMessageParameters()->shouldReturn(array('parameter'));
+        $this->getMessage()->shouldReturn('message');
+        $this->isPropagationStopped()->shouldReturn(true);
+    }
+
+    function it_check_if_an_error_has_been_detected()
+    {
+        $this->isStopped()->shouldReturn(false);
+        $this->stop('message');
+        $this->isStopped()->shouldReturn(true);
+    }
+
+    function it_has_not_message_type_by_default()
+    {
+        $this->getMessageType()->shouldReturn('');
+    }
+
+    function its_message_type_is_mutable()
+    {
+        $this->setMessageType(ResourceEvent::TYPE_SUCCESS)->shouldReturn($this);
+        $this->getMessageType()->shouldReturn(ResourceEvent::TYPE_SUCCESS);
+    }
+
+    function it_has_not_message_by_default()
+    {
+        $this->getMessage()->shouldReturn('');
+    }
+
+    function its_message_is_mutable()
+    {
+        $this->setMessage('message')->shouldReturn($this);
+        $this->getMessage()->shouldReturn('message');
+    }
+
+    function it_has_not_message_parameter_by_default()
+    {
+        $this->getMessageParameters()->shouldReturn(array());
+    }
+
+    function its_message_parameter_is_mutable()
+    {
+        $this->setMessageParameters(array('parameters'))->shouldReturn($this);
+        $this->getMessageParameters()->shouldReturn(array('parameters'));
+    }
+
+}
---
+++ src/Sylius/Bundle/ResourceBundle/spec/Sylius/Bundle/ResourceBundle/Form/Type/ObjectToIdentifierTypeSpec.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Sylius\Bundle\ResourceBundle\Form\Type;
+
+use Doctrine\Common\Persistence\ObjectRepository;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Symfony\Component\Form\Test\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ObjectToIdentifierTypeSpec extends ObjectBehavior
+{
+    function let(ManagerRegistry $manager)
+    {
+        $this->beConstructedWith($manager, 'name');
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Sylius\Bundle\ResourceBundle\Form\Type\ObjectToIdentifierType');
+    }
+
+    function it_build_a_form($manager, FormBuilderInterface $builder, ObjectRepository $repository)
+    {
+        $manager->getRepository('class')->willReturn($repository);
+
+        $builder->addModelTransformer(
+            Argument::type('Sylius\Bundle\ResourceBundle\Form\DataTransformer\ObjectToIdentifierTransformer')
+        )->shouldBeCalled();
+
+        $this->buildForm($builder, array(
+            'class' => 'class',
+            'identifier' => 'identifier',
+        ));
+    }
+
+    function it_has_options(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'identifier' => 'id'
+        ))->willReturn($resolver);
+
+        $resolver->setAllowedTypes(array(
+            'identifier' => array('string')
+        ))->willReturn($resolver);
+
+        $this->setDefaultOptions($resolver);
+    }
+
+    function it_has_a_name()
+    {
+        $this->getParent()->shouldReturn('entity');
+    }
+
+    function it_has_a_parent()
+    {
+        $this->getName()->shouldReturn('name');
+    }
+}
