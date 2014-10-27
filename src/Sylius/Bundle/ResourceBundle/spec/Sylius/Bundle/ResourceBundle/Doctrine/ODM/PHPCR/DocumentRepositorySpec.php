c80dcb961e9fb2a7ae4a9e6af4af4218895093ea
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2014-10-27 20:46:49 +0100

    Remove bugged spec

--- src/Sylius/Bundle/ResourceBundle/spec/Sylius/Bundle/ResourceBundle/Doctrine/ODM/PHPCR/DocumentRepositorySpec.php
+++ src/Sylius/Bundle/ResourceBundle/spec/Sylius/Bundle/ResourceBundle/Doctrine/ODM/PHPCR/DocumentRepositorySpec.php
@@ -2,17 +2,19 @@
 
 namespace spec\Sylius\Bundle\ResourceBundle\Doctrine\ODM\PHPCR;
 
+use Doctrine\ODM\PHPCR\DocumentManager;
+use Doctrine\ODM\PHPCR\Mapping\ClassMetadata;
+use Doctrine\ODM\PHPCR\Query\Builder\QueryBuilder;
+use Doctrine\ODM\PHPCR\UnitOfWork;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
-use Doctrine\ODM\PHPCR\Mapping\ClassMetadata;
-use Doctrine\ODM\PHPCR\DocumentManager;
-use Doctrine\ODM\PHPCR\Query\Builder\QueryBuilder;
 
 class DocumentRepositorySpec extends ObjectBehavior
 {
-    function let(DocumentManager $dm, ClassMetadata $class)
+    function let(DocumentManager $dm, ClassMetadata $class, UnitOfWork $uow)
     {
         $this->beConstructedWith($dm, $class);
+        $dm->getUnitOfWork()->shouldBeCalled()->willReturn($uow);
 
         $class->name = 'Sylius\Component\Core\Model\Product';
     }
@@ -25,15 +27,6 @@ class DocumentRepositorySpec extends ObjectBehavior
     function it_creates_instance()
     {
         $this->createNew()->shouldHaveType('Sylius\Component\Core\Model\Product');
-    }
-
-    function it_creates_a_paginator($dm, QueryBuilder $queryBuilder)
-    {
-        $dm->createQueryBuilder()->willReturn($queryBuilder);
-        $queryBuilder->from('o')->willReturn($queryBuilder);
-        $queryBuilder->document('Sylius\Component\Core\Model\Product', 'o')->willReturn($queryBuilder);
-
-        $this->createPaginator()->shouldHaveType('Pagerfanta\Pagerfanta');
     }
 
     function it_has_a_paginator(QueryBuilder $queryBuilder)
