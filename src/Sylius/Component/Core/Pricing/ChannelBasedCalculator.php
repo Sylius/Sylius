4f58bb9854d7d9c167a183e3ede309101de1651f
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2014-11-19 22:07:49 +0100

    Per channel pricing calculator

---
+++ src/Sylius/Component/Core/Pricing/ChannelBasedCalculator.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Core\Pricing;
+
+use Sylius\Component\Pricing\Calculator\CalculatorInterface;
+
+/**
+ * Channel based calculator.
+ *
+ * @author Paweł Jędrzejewski <pawel@sylius.org>
+ */
+class GroupBasedCalculator extends AbstractCalculator implements CalculatorInterface
+{
+    protected $parameterName = 'channel';
+    protected $className     = 'Sylius\Component\Core\Model\ChannelInterface';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getType()
+    {
+        return Calculators::GROUP_BASED;
+    }
+}
