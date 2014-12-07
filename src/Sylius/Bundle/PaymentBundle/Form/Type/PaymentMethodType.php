fa49c5f50f5bd7559a05112d3a7c41c2f70d730e
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-12-07 10:16:32 +0000

    allow configure credentials based on gateway.

--- src/Sylius/Bundle/PaymentBundle/Form/Type/PaymentMethodType.php
+++ src/Sylius/Bundle/PaymentBundle/Form/Type/PaymentMethodType.php
@@ -13,8 +13,9 @@ namespace Sylius\Bundle\PaymentBundle\Form\Type;
 
 use Payum\Paypal\ExpressCheckout\Nvp\PaymentBuilder;
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Sylius\Component\Payment\Model\PaymentMethod;
+use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -46,45 +47,44 @@ class PaymentMethodType extends AbstractResourceType
                 'required' => false,
                 'label'    => 'sylius.form.payment_method.enabled'
             ))
+            ->add('credentials', 'form');
         ;
 
-        $formFactory = $builder->getFormFactory();
+        $that = $this;
 
-        $credentialsMustBeSent = false;
-        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) use (&$credentialsMustBeSent, $formFactory) {
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function(FormEvent $event) use ($that) {
+            /** @var  array $data */
             $data = $event->getData();
-            $form = $event->getForm();
 
-            if (false == isset($data['gateway'])) {
-                return;
-            }
-
-
-
-            $credentialsMustBeSent = true;
-            $paypalPaymentBuilder = new PaymentBuilder();
-
-            $credentialsBuilder = $formFactory->createBuilder('form');
-            foreach ($paypalPaymentBuilder->get('payum.options') as $name => $value) {
-                $credentialsBuilder->add($name, is_bool($value) ? 'checkbox' : 'text', array(
-                    'constraints' => array_filter(array(
-                        $paypalPaymentBuilder->get('payum.required_options', $name) ? new NotBlank : null
-                    )),
-                    'data' => $value,
-                    'required' => (bool)$paypalPaymentBuilder->get('payum.required_options', $name),
-                ));
-            }
-
-            $form->add('credentials', $credentialsBuilder->getForm());
-        });
-
-        $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) use ($builder, &$credentialsMustBeSent) {
-            $form = $event->getForm();
-
-            if ($form->has('credentials') && $credentialsMustBeSent) {
-                $form->get('credentials')->addError(new FormError('Credentials must be set'));
+            if (false == empty($data['gateway'])) {
+                $that->buildCredentials($event->getForm());
             }
         });
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) use ($that) {
+            /** @var  PaymentMethod $paymentMethod */
+            $paymentMethod = $event->getData();
+
+            if ($paymentMethod->getGateway()) {
+                $that->buildCredentials($event->getForm(), $paymentMethod->getCredentials());
+            }
+        });
+    }
+
+    public function buildCredentials(Form $form)
+    {
+        $paypalPaymentBuilder = new PaymentBuilder();
+
+        foreach ($paypalPaymentBuilder->get('payum.options') as $name => $value) {
+            $isRequired = (bool) $paypalPaymentBuilder->get('payum.required_options', $name);
+            $form->get('credentials')->add($name, is_bool($value) ? 'checkbox' : 'text', array(
+                'constraints' => array_filter(array(
+                    $isRequired ? new NotBlank : null
+                )),
+                'empty_data' => $value,
+                'required' => $isRequired,
+            ));
+        }
     }
 
     /**
