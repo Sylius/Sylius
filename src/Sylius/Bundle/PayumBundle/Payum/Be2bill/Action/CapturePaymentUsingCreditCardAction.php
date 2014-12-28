f47a2924d78be9eb763b5b13300d7595af63002f
Kotlyar Maksim <kotlyar.maksim@gmail.com>
2014-12-28 16:10:06 +0000

    upgrade payum to 0.13

--- src/Sylius/Bundle/PayumBundle/Payum/Be2bill/Action/CapturePaymentUsingCreditCardAction.php
+++ src/Sylius/Bundle/PayumBundle/Payum/Be2bill/Action/CapturePaymentUsingCreditCardAction.php
@@ -11,30 +11,16 @@
 
 namespace Sylius\Bundle\PayumBundle\Payum\Be2bill\Action;
 
-use Payum\Core\Exception\LogicException;
+use Payum\Core\Request\GetHttpRequest;
 use Payum\Core\Security\TokenInterface;
 use Sylius\Bundle\PayumBundle\Payum\Action\AbstractCapturePaymentAction;
 use Sylius\Component\Payment\Model\PaymentInterface;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @author Alexandre Bacco <alexandre.bacco@gmail.com>
  */
 class CapturePaymentUsingCreditCardAction extends AbstractCapturePaymentAction
 {
-    /**
-     * @var Request
-     */
-    protected $httpRequest;
-
-    /**
-     * @param Request $request
-     */
-    public function setRequest(Request $request = null)
-    {
-        $this->httpRequest = $request;
-    }
-
     /**
      * {@inheritDoc}
      */
@@ -44,17 +30,15 @@ class CapturePaymentUsingCreditCardAction extends AbstractCapturePaymentAction
             return;
         }
 
-        if (!$this->httpRequest) {
-            throw new LogicException('The action can be run only when http request is set.');
-        }
+        $this->payment->execute($httpRequest = new GetHttpRequest());
 
         $order = $payment->getOrder();
 
         $details = array();
         $details['AMOUNT'] = $order->getTotal();
         $details['CLIENTEMAIL'] = $order->getEmail();
-        $details['CLIENTUSERAGENT'] = $this->httpRequest->headers->get('User-Agent', 'Unknown');
-        $details['CLIENTIP'] = $this->httpRequest->getClientIp();
+        $details['CLIENTUSERAGENT'] = $httpRequest->userAgent ?: 'Unknown';
+        $details['CLIENTIP'] = $httpRequest->clientIp;
         $details['CLIENTIDENT'] = $order->getUser() ? $order->getUser()->getId() : $order->getEmail();
         $details['DESCRIPTION'] = sprintf('Order containing %d items for a total of %01.2f', $order->getItems()->count(), $order->getTotal() / 100);
         $details['ORDERID'] = $payment->getId();
