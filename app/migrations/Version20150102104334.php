cfb429970973f86d6732c6adeb4ca5172cb3dfd5
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2015-01-02 12:09:31 +0100

    Make promotions soft-deleteable as well

--- app/migrations/Version20150102104334.php
+++ app/migrations/Version20150102104334.php
@@ -17,14 +17,14 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20141226173325 extends AbstractMigration
+class Version20150102104334 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE sylius_promotion_coupon ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME DEFAULT NULL, ADD deleted_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE sylius_promotion ADD deleted_at DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema)
@@ -32,6 +32,6 @@ class Version20141226173325 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE sylius_promotion_coupon DROP created_at, DROP updated_at, DROP deleted_at');
+        $this->addSql('ALTER TABLE sylius_promotion DROP deleted_at');
     }
 }
