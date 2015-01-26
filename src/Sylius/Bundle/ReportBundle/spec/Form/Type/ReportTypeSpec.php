6d2c473081e15b3d65878e767d35c7d130157f9d
Mateusz Zalewski <zaleslaw@gmail.com>
2015-01-26 15:57:13 +0100

    Finished spec for ReportType.

--- src/Sylius/Bundle/ReportBundle/spec/Form/Type/ReportTypeSpec.php
+++ src/Sylius/Bundle/ReportBundle/spec/Form/Type/ReportTypeSpec.php
@@ -12,15 +12,15 @@
 namespace spec\Sylius\Bundle\ReportBundle\Form\Type;
 
 use PhpSpec\ObjectBehavior;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormConfigInterface;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormView;
-use Symfony\Component\Form\Form;
 use Prophecy\Argument;
 use Sylius\Component\Registry\ServiceRegistryInterface;
-use Sylius\Bundle\ReportBundle\Renderer\TableRenderer;
-use Sylius\Bundle\ReportBundle\Renderer\ChartRenderer;
+use Sylius\Component\Report\DataFetcher\DataFetcherInterface;
+use Sylius\Component\Report\Renderer\RendererInterface;
 
 /**
  * @author Mateusz Zalewski <mateusz.zalewski@lakion.com>
@@ -42,34 +42,68 @@ class ReportTypeSpec extends ObjectBehavior
         $this->shouldHaveType('Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType');   
     }
 
-    // function it_build_form_with_proper_fields(FormBuilder $builder, $dataFetcherRegistry, $rendererRegistry, TableRenderer $tableRenderer, ChartRenderer $chartRenderer)
-    // {
-    //     $builder->addEventSubscriber(Argument::any())->willReturn($builder);
+    function it_build_form_with_proper_fields(
+        FormBuilderInterface $builder,
+        FormFactoryInterface $factory,
+        $dataFetcherRegistry,
+        $rendererRegistry,
+        RendererInterface $renderer,
+        DataFetcherInterface $dataFetcher
+    )
+    {
+        $builder->getFormFactory()->willReturn($factory);
 
-    //     $builder->add('name', 'text', Argument::any())->willReturn($builder);
-    //     $builder->add('description', 'textarea', Argument::any())->willReturn($builder);
-    //     $builder->add('renderer', 'sylius_renderer_choice', Argument::any())->willReturn($builder);
-    //     $builder->add('dataFetcher', 'sylius_data_fetcher_choice', Argument::any())->willReturn($builder);
+        $builder->add('name', 'text', Argument::any())->shouldBeCalled()->willReturn($builder);
+        $builder->add('description', 'textarea', Argument::any())->shouldBeCalled()->willReturn($builder);
+        $builder->add('renderer', 'sylius_renderer_choice', Argument::any())->shouldBeCalled()->willReturn($builder);
+        $builder->add('dataFetcher', 'sylius_data_fetcher_choice', Argument::any())->shouldBeCalled()->willReturn($builder);
 
-    //     $rendererRegistry->all()->willReturn(array('table' => $tableRenderer, 'chart' => $chartRenderer));
-    //     $builder->create('rendererConfiguration')
-    // }
+        $builder->addEventSubscriber(Argument::type('Sylius\Bundle\ReportBundle\Form\EventListener\BuildReportRendererFormListener'))->shouldBeCalled()->willReturn($builder);
+        $builder->addEventSubscriber(Argument::type('Sylius\Bundle\ReportBundle\Form\EventListener\BuildReportDataFetcherFormListener'))->shouldBeCalled()->willReturn($builder);
 
-    // function it_builds_view(FormView $view, FormView $tableRendererView, FormView $chartRendererView, FormView $userRegistrationDataFetcherView, FormInterface $form, FormConfigInterface $config, FormInterface $formInterface, Form $formTable, Form $formChart, Form $formUserRegistration)
-    // {
-    //     $form->getConfig()->willReturn($config);
-    //     $prototypes = array(
-    //         'renderers' => array('table' => $formTable, 'chart' => $formChart),
-    //         'dataFetchers' => array('user_registration' => $formUserRegistration)
-    //     );
-    //     $config->getAttributes('prototypes')->willReturn($prototypes);
+        $renderer->getType()->willReturn('test_renderer');
+        $rendererRegistry->all()->willReturn(array('test_renderer' => $renderer));
+        $builder->create('rendererConfiguration', 'sylius_renderer_test_renderer')->willReturn($builder);
+        $builder->getForm()->shouldBeCalled()->willReturn(Argument::type('Symfony\Component\Form\Form'));
 
-    //     $formTable->createView($view)->shouldReturn($formTable);
-    //     $formChart->createView($view)->shouldReturn($formChart);
-    //     $formUserRegistration->createView($view)->shouldReturn($formUserRegistration);
+        $dataFetcher->getType()->willReturn('test_data_fetcher');
+        $dataFetcherRegistry->all()->willReturn(array('test_data_fetcher' => $dataFetcher));
+        $builder->create('dataFetcherConfiguration', 'sylius_data_fetcher_test_data_fetcher')->willReturn($builder);
+        $builder->getForm()->shouldBeCalled()->willReturn(Argument::type('Symfony\Component\Form\Form'));
 
-    //     $this->buildView($view, $formInterface, array());
-    // }
+        $prototypes = array(
+            'renderers' => array(
+                'test_renderer' => Argument::type('Symfony\Component\Form\Form')
+                ),
+            'dataFetchers' => array(
+                'test_data_fetcher' => Argument::type('Symfony\Component\Form\Form')
+                )
+            );
+        $builder->setAttribute('prototypes', $prototypes)->shouldBeCalled();
+
+        $this->buildForm($builder, array());
+    }
+
+    function it_builds_view(
+        FormConfigInterface $config,
+        FormView $view,
+        FormInterface $form,
+        FormInterface $formTable,
+        FormInterface $formUserRegistration
+    )
+    {
+        $prototypes = array(
+            'dataFetchers' => array('user_registration' => $formUserRegistration),
+            'renderers' => array('table' => $formTable)
+        );
+        $config->getAttribute('prototypes')->willReturn($prototypes);
+        $form->getConfig()->willReturn($config);
+
+        $formTable->createView($view)->shouldBeCalled();
+        $formUserRegistration->createView($view)->shouldBeCalled();
+
+        $this->buildView($view, $form, array());
+    }
 
     function it_has_name()
     {
