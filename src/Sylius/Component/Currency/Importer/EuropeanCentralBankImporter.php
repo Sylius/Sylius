8dd9343def2fd49e3fe8b356040e8fe7bf7443ce
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2015-02-25 12:07:34 +0000

    [currency][ecb importer] added base currency.

--- src/Sylius/Component/Currency/Importer/EuropeanCentralBankImporter.php
+++ src/Sylius/Component/Currency/Importer/EuropeanCentralBankImporter.php
@@ -14,13 +14,18 @@ namespace Sylius\Component\Currency\Importer;
 class EuropeanCentralBankImporter extends AbstractImporter
 {
     private $url = 'http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml';
+    private $baseCurrency;
 
     /**
      * {@inheritdoc}
      */
     public function configure(array $options = array())
     {
+        if (!isset($options['base_currency'])) {
+            throw new \InvalidArgumentException('"base_currency" must be set in order to use EuropeanCentralBankImporter.');
+        }
 
+        $this->baseCurrency = $options['base_currency'];
     }
 
     /**
@@ -30,6 +35,9 @@ class EuropeanCentralBankImporter extends AbstractImporter
     {
         $xml = @simplexml_load_file($this->url);
         if ($xml instanceof \SimpleXMLElement) {
+            // base currency: euro
+            $this->updateOrCreate($managedCurrencies, $this->baseCurrency, 1.00);
+
             $data = $xml->xpath('//gesmes:Envelope/*[3]/*');
             foreach ($data[0]->children() as $child) {
                 $this->updateOrCreate($managedCurrencies, (string) $child->attributes()->currency, (float) $child->attributes()->rate);
