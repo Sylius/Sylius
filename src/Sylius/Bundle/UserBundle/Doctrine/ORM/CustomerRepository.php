16e6e7c4699cc3cd8b2de4005aac144fc84f6890
michalmarcinkowski <michal.marcinkowski@lakion.com>
2015-05-21 10:48:56 +0200

    Customer backend management

--- src/Sylius/Bundle/UserBundle/Doctrine/ORM/CustomerRepository.php
+++ src/Sylius/Bundle/UserBundle/Doctrine/ORM/CustomerRepository.php
@@ -44,4 +44,48 @@ class CustomerRepository extends EntityRepository
 
         return $result;
     }
+
+    /**
+     * @param array $criteria
+     * @param array $sorting
+     * @param bool  $deleted
+     *
+     * @return PagerfantaInterface
+     */
+    public function createFilterPaginator($criteria = array(), $sorting = array(), $deleted = false)
+    {
+        $queryBuilder = parent::getCollectionQueryBuilder();
+
+        if ($deleted) {
+            $this->_em->getFilters()->disable('softdeleteable');
+        }
+
+        if (isset($criteria['query'])) {
+            $queryBuilder
+                ->leftJoin($this->getAlias().'.user', 'user')
+                ->where($queryBuilder->expr()->like($this->getAlias().'.emailCanonical', ':query'))
+                ->orWhere($queryBuilder->expr()->like($this->getAlias().'.firstName', ':query'))
+                ->orWhere($queryBuilder->expr()->like($this->getAlias().'.lastName', ':query'))
+                ->orWhere($queryBuilder->expr()->like('user.username', ':query'))
+                ->setParameter('query', '%'.$criteria['query'].'%')
+            ;
+        }
+        if (isset($criteria['enabled'])) {
+            $queryBuilder
+                ->andWhere($queryBuilder->expr()->eq($this->getAlias().'.enabled', ':enabled'))
+                ->setParameter('enabled', $criteria['enabled'])
+            ;
+        }
+
+        if (empty($sorting)) {
+            if (!is_array($sorting)) {
+                $sorting = array();
+            }
+            $sorting['updatedAt'] = 'desc';
+        }
+
+        $this->applySorting($queryBuilder, $sorting);
+
+        return $this->getPaginator($queryBuilder);
+    }
 }
