b1ebaad9fdae729a8cde88c144f635713363d61a
Kamil Kokot <kamkok512@gmail.com>
2015-05-27 14:10:17 +0200

    [ThemeBundle] Introduced Translator::transChoice() support

--- src/Sylius/Bundle/ThemeBundle/Translation/Translator.php
+++ src/Sylius/Bundle/ThemeBundle/Translation/Translator.php
@@ -9,6 +9,7 @@ use Sylius\Bundle\ThemeBundle\Model\ThemeInterface;
 use Sylius\Bundle\ThemeBundle\Translation\Loader\Loader;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Translation\Loader\LoaderInterface;
+use Symfony\Component\Translation\MessageCatalogueInterface;
 use Symfony\Component\Translation\MessageSelector;
 use Symfony\Bundle\FrameworkBundle\Translation\Translator as BaseTranslator;
 
@@ -18,9 +19,16 @@ use Symfony\Bundle\FrameworkBundle\Translation\Translator as BaseTranslator;
 class Translator extends BaseTranslator
 {
     /**
+     * @var MessageSelector
+     */
+    protected $selector;
+
+    /**
+     * List of all found themes indexed by absoulte paths of them.
+     *
      * @var ThemeInterface[]
      */
-    protected $themes;
+    protected $themesPathsToThemes;
 
     /**
      * @var ThemeContextInterface
@@ -37,7 +45,13 @@ class Translator extends BaseTranslator
      */
     public function __construct(ContainerInterface $container, MessageSelector $selector, $loaderIds = array(), array $options = array())
     {
-        $this->themes = $container->get('sylius.repository.theme')->findAll();
+        $this->selector = $selector;
+
+        $themes = $container->get('sylius.repository.theme')->findAll();
+        foreach ($themes as $theme) {
+            $this->themesPathsToThemes[realpath($theme->getPath())] = $theme;
+        }
+
         $this->themeContext = $container->get('sylius.context.theme');
         $this->resourcesToThemes = new ArrayCollection();
 
@@ -67,16 +81,84 @@ class Translator extends BaseTranslator
      */
     public function trans($id, array $parameters = array(), $domain = null, $locale = null)
     {
-        foreach ($this->themeContext->getThemesSortedByPriorityInDescendingOrder() as $theme) {
-            $themedMessageId = $id . '|' . $theme->getLogicalName();
-            $translatedMessage = parent::trans($themedMessageId, $parameters, $domain, $locale);
+        return $this->doTranslate(
+            $id,
+            $parameters,
+            $domain,
+            $locale,
+            null,
+            $this->themeContext->getThemesSortedByPriorityInDescendingOrder()
+        );
+    }
 
-            if ($themedMessageId !== $translatedMessage) {
-                return $translatedMessage;
+    /**
+     * {@inheritdoc}
+     */
+    public function transChoice($id, $number, array $parameters = array(), $domain = null, $locale = null)
+    {
+        return $this->doTranslate(
+            $id,
+            $parameters,
+            $domain,
+            $locale,
+            $number,
+            $this->themeContext->getThemesSortedByPriorityInDescendingOrder()
+        );
+    }
+
+
+    /**
+     * @param mixed $id
+     * @param array $parameters
+     * @param string $domain
+     * @param string $locale
+     * @param integer $number
+     * @param ThemeInterface[] $themes
+     *
+     * @return null|MessageCatalogueInterface
+     */
+    protected function doTranslate($id, array $parameters, $domain, $locale, $number, array $themes = array())
+    {
+        $id = (string) $id;
+        $domain = $domain ?: 'messages';
+
+        $catalogue = null;
+        foreach ($themes as $theme) {
+            $themedMessageId = $id . '|' . $theme->getLogicalName();
+
+            if ($catalogue = $this->getCatalogueHavingTranslation($themedMessageId, $domain, $locale)) {
+                $id = $themedMessageId;
+                break;
             }
         }
 
-        return parent::trans($id, $parameters, $domain, $locale);
+        if (null === $catalogue) {
+            $catalogue = $this->getCatalogueHavingTranslation($id, $domain, $locale);
+        }
+
+        $translatedMessage = $catalogue ? $catalogue->get($id, $domain) : $id;
+
+        if (null !== $number) {
+            $translatedMessage = $this->selector->choose($translatedMessage, $number, $catalogue->getLocale());
+        }
+
+        return strtr($translatedMessage, $parameters);
+    }
+
+    /**
+     * @param string $id
+     * @param string $domain
+     * @param string $locale
+     * @return MessageCatalogueInterface|null
+     */
+    protected function getCatalogueHavingTranslation($id, $domain, $locale)
+    {
+        $catalogue = $this->getCatalogue($locale);
+        while (null !== $catalogue && !$catalogue->defines($id, $domain)) {
+            $catalogue = $catalogue->getFallbackCatalogue();
+        }
+
+        return $catalogue;
     }
 
     /**
@@ -86,8 +168,8 @@ class Translator extends BaseTranslator
     {
         $resource = realpath($resource);
 
-        foreach ($this->themes as $theme) {
-            if (false !== strpos($resource, realpath($theme->getPath()))) {
+        foreach ($this->themesPathsToThemes as $themePath => $theme) {
+            if (false !== strpos($resource, $themePath)) {
                 $this->resourcesToThemes->set($resource, $theme);
                 return;
             }
