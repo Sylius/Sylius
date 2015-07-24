44a577dc5415155eceadeea48b4858e7f567f77b
msumme <msumme@gmail.com>
2015-07-24 13:24:03 -0500

    Fixing Type Error when Order has ShippingMethod
    This fixes the error "The form's view data is expected to be of type
    scalar, array or an instance of \ArrayAccess, but is an instance of
    class Proxies\__CG__\Sylius\Component\Core\Model\ShippingMethod" that
    occurs when viewing the shipping form with either only one option
    available, or when going back from checkout.
    
    
    
    Fixes issue #2979

---
+++ src/Sylius/Bundle/ShippingBundle/Form/Transformer/ObjectToIdentifierTransformer.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\ShippingBundle\Form\Transformer;
+
+use Sylius\Bundle\ResourceBundle\Form\DataTransformer\ObjectToIdentifierTransformer as BaseTransformer;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+/**
+ * Object to identifier transformer.
+ *
+ * @author Paweł Jędrzejewski <pawel@sylius.org>
+ */
+class ObjectToIdentifierTransformer extends BaseTransformer
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function transform($value)
+    {
+        if (!is_object($value)) {
+            return null;
+        }
+
+        $accessor = PropertyAccess::createPropertyAccessor();
+
+        return $accessor->getValue($value, $this->identifier);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function reverseTransform($value)
+    {
+        if (empty($value)) {
+            return null;
+        }
+
+        return $this->repository->findOneBy(array($this->identifier => $value));
+    }
+}
