97750dc73a8ada50ecd24574bf7e35c4c5ec11d5
Arminek <arkadiusz.k.e@gmail.com>
2015-09-17 15:10:30 +0200

    [Inventory] Removed fluent interfaces

--- src/Sylius/Component/Inventory/Model/InventoryUnitInterface.php
+++ src/Sylius/Component/Inventory/Model/InventoryUnitInterface.php
@@ -14,8 +14,6 @@ namespace Sylius\Component\Inventory\Model;
 use Sylius\Component\Resource\Model\TimestampableInterface;
 
 /**
- * Inventory unit interface.
- *
  * @author Paweł Jędrzejewski <pawel@sylius.org>
  */
 interface InventoryUnitInterface extends TimestampableInterface
@@ -30,57 +28,41 @@ interface InventoryUnitInterface extends TimestampableInterface
     const STATE_RETURNED    = 'returned';
 
     /**
-     * Get related stockable object.
-     *
      * @return StockableInterface
      */
     public function getStockable();
 
     /**
-     * Set stockable object.
-     *
      * @param $stockable StockableInterface
      */
     public function setStockable(StockableInterface $stockable);
 
     /**
-     * Get the SKU of stockable.
-     *
      * @return string
      */
     public function getSku();
 
     /**
-     * Get displayed inventory name
-     *
      * @return string
      */
     public function getInventoryName();
 
     /**
-     * Get inventory unit state.
-     *
      * @return string
      */
     public function getInventoryState();
 
     /**
-     * Set inventory unit state.
-     *
      * @param string $state
      */
     public function setInventoryState($state);
 
     /**
-     * Is in "sold" state?
-     *
      * @return bool
      */
     public function isSold();
 
     /**
-     * Is a backordered inventory unit?
-     *
      * @return bool
      */
     public function isBackordered();
