7a0ca545e6c0bf7915d4035524fe8a23fe4e5034
Joseph Bielawski <stloyd+gh@gmail.com>
2015-09-24 11:10:39 +0200

    Remove old closures compatibility code

--- src/Sylius/Bundle/ShippingBundle/Form/Type/ShippingMethodChoiceType.php
+++ src/Sylius/Bundle/ShippingBundle/Form/Type/ShippingMethodChoiceType.php
@@ -83,14 +83,11 @@ class ShippingMethodChoiceType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $methodsResolver = $this->resolver;
-        $repository = $this->repository;
-
-        $choiceList = function (Options $options) use ($methodsResolver, $repository) {
+        $choiceList = function (Options $options) {
             if (isset($options['subject'])) {
-                $methods = $methodsResolver->getSupportedMethods($options['subject'], $options['criteria']);
+                $methods = $this->resolver->getSupportedMethods($options['subject'], $options['criteria']);
             } else {
-                $methods = $repository->findBy($options['criteria']);
+                $methods = $this->repository->findBy($options['criteria']);
             }
 
             return new ObjectChoiceList($methods, null, array(), null, 'id');
