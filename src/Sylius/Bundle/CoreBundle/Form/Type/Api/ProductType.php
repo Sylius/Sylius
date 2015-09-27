2d9c82590f055478af65455898cac3fc8f8767a5
umpirsky <umpirsky@gmail.com>
2015-09-27 13:17:48 +0200

    Revert variant on hand form

--- src/Sylius/Bundle/CoreBundle/Form/Type/Api/ProductType.php
+++ src/Sylius/Bundle/CoreBundle/Form/Type/Api/ProductType.php
@@ -34,6 +34,9 @@ class ProductType extends BaseProductType
             ->add('price', 'sylius_money', array(
                 'property_path' => 'masterVariant.price'
             ))
+            ->add('onHand', 'integer', array(
+                'property_path' => 'masterVariant.onHand'
+            ))
             ->add('sku', 'text', array(
                 'property_path' => 'masterVariant.sku'
             ))
