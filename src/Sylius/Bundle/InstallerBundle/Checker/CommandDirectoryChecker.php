42f2d38db5ba2fe12e1412852e2197e02a599cdb
Mateusz Zalewski <zaleslaw@gmail.com>
2015-10-06 10:57:23 +0200

    [Installer] PR review fixes

--- src/Sylius/Bundle/InstallerBundle/Checker/CommandDirectoryChecker.php
+++ src/Sylius/Bundle/InstallerBundle/Checker/CommandDirectoryChecker.php
@@ -13,18 +13,33 @@ namespace Sylius\Bundle\InstallerBundle\Checker;
 
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Debug\Exception\ContextErrorException;
+use Symfony\Component\Filesystem\Exception\IOException;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Iterator\RecursiveDirectoryIterator;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
  * @author Mateusz Zalewski <mateusz.zalewski@lakion.com>
  */
-class CommandDirectoryChecker implements CommandDirectoryCheckerInterface
+class CommandDirectoryChecker
 {
     /**
      * @var string
      */
     private $name;
+
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * @param Filesystem $filesystem
+     */
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
 
     /**
      * {@inheritdoc}
@@ -64,7 +79,9 @@ class CommandDirectoryChecker implements CommandDirectoryCheckerInterface
      */
     private function createDirectory($directory, OutputInterface $output)
     {
-        if (!mkdir($directory, 0755, true)) {
+        try {
+            $this->filesystem->mkdir($directory, 755);
+        } catch (IOException $exception) {
             $output->writeln($this->createUnexistingDirectoryMessage(getcwd() . '/' . $directory));
 
             throw new \RuntimeException("Failed while trying to create directory.");
@@ -85,6 +102,8 @@ class CommandDirectoryChecker implements CommandDirectoryCheckerInterface
 
         if (!is_writable($directory)) {
             $this->changePermissions($directory, $output);
+
+            return;
         }
 
         foreach (new RecursiveDirectoryIterator($directory, \FilesystemIterator::CURRENT_AS_FILEINFO) as $subdirectory) {
@@ -103,10 +122,10 @@ class CommandDirectoryChecker implements CommandDirectoryCheckerInterface
     private function changePermissions($directory, OutputInterface $output)
     {
         try {
-            chmod($directory, 0755);
+            $this->filesystem->chmod($directory, 0755, 0000, true);
 
             $output->writeln(sprintf('<comment>Changed "%s" permissions to 0755.</comment>', $directory));
-        } catch (ContextErrorException $exception) {
+        } catch (IOException $exception) {
             throw new AccessDeniedException(dirname($directory));
         }
     }
