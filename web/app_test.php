f245326b107ad1ef5dd135a2a4f996b90056389e
Stefan Doorn <stefan@efectos.nl>
2015-11-06 11:58:12 +0100

    Allow vagrant users to access app_dev.php from host IP

--- web/app_test.php
+++ web/app_test.php
@@ -18,7 +18,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '113.0.0.1'))
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '113.0.0.1', '10.0.0.1'))
         || php_sapi_name() === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
