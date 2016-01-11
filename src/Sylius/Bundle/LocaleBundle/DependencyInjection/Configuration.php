d5b267408fbd841b018e23ac8f73f83df07b5519
Kamil Kokot <kamil@kokot.me>
2016-01-11 21:46:17 +0100

    [LocaleBundle] Rewritten configuration to use resource configuration builder

--- src/Sylius/Bundle/LocaleBundle/DependencyInjection/Configuration.php
+++ src/Sylius/Bundle/LocaleBundle/DependencyInjection/Configuration.php
@@ -14,10 +14,11 @@ namespace Sylius\Bundle\LocaleBundle\DependencyInjection;
 use Sylius\Bundle\LocaleBundle\Controller\LocaleController;
 use Sylius\Bundle\LocaleBundle\Form\Type\LocaleChoiceType;
 use Sylius\Bundle\LocaleBundle\Form\Type\LocaleType;
+use Sylius\Bundle\ResourceBundle\DependencyInjection\Configuration\ResourceConfigurationBuilder;
+use Sylius\Bundle\ResourceBundle\DependencyInjection\Configuration\SyliusResource;
 use Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
 use Sylius\Component\Locale\Model\Locale;
 use Sylius\Component\Locale\Model\LocaleInterface;
-use Sylius\Component\Resource\Factory\Factory;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
@@ -53,50 +54,21 @@ class Configuration implements ConfigurationInterface
     }
 
     /**
-     * @param ArrayNodeDefinition $node
+     * @param ArrayNodeDefinition $rootDefinition
      */
-    private function addResourcesSection(ArrayNodeDefinition $node)
+    private function addResourcesSection(ArrayNodeDefinition $rootDefinition)
     {
-        $node
-            ->children()
-                ->arrayNode('resources')
-                    ->addDefaultsIfNotSet()
-                    ->children()
-                        ->arrayNode('locale')
-                            ->addDefaultsIfNotSet()
-                            ->children()
-                                ->variableNode('options')->end()
-                                ->arrayNode('classes')
-                                    ->addDefaultsIfNotSet()
-                                    ->children()
-                                        ->scalarNode('model')->defaultValue(Locale::class)->cannotBeEmpty()->end()
-                                        ->scalarNode('interface')->defaultValue(LocaleInterface::class)->cannotBeEmpty()->end()
-                                        ->scalarNode('controller')->defaultValue(LocaleController::class)->cannotBeEmpty()->end()
-                                        ->scalarNode('repository')->cannotBeEmpty()->end()
-                                        ->scalarNode('factory')->defaultValue(Factory::class)->end()
-                                        ->arrayNode('form')
-                                            ->addDefaultsIfNotSet()
-                                            ->children()
-                                                ->scalarNode('default')->defaultValue(LocaleType::class)->cannotBeEmpty()->end()
-                                                ->scalarNode('choice')->defaultValue(LocaleChoiceType::class)->cannotBeEmpty()->end()
-                                            ->end()
-                                        ->end()
-                                    ->end()
-                                ->end()
-                                ->arrayNode('validation_groups')
-                                    ->addDefaultsIfNotSet()
-                                    ->children()
-                                        ->arrayNode('default')
-                                            ->prototype('scalar')->end()
-                                            ->defaultValue(array('sylius'))
-                                        ->end()
-                                    ->end()
-                                ->end()
-                            ->end()
-                        ->end()
-                    ->end()
-                ->end()
-            ->end()
-        ;
+        $resourceConfigurationGenerator = new ResourceConfigurationBuilder();
+
+        $resourcesDefinition = $resourceConfigurationGenerator->initResourcesConfiguration($rootDefinition);
+
+        $localeResource = new SyliusResource('locale', Locale::class, LocaleInterface::class);
+        $localeResource->useController(LocaleController::class);
+        $localeResource->useDefaultRepository();
+        $localeResource->useDefaultFactory();
+        $localeResource->addForm('default', LocaleType::class, ['sylius']);
+        $localeResource->addForm('choice', LocaleChoiceType::class);
+
+        $resourceConfigurationGenerator->addSyliusResource($resourcesDefinition, $localeResource);
     }
 }
