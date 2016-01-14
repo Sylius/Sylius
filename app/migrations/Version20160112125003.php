6c9dd5591f120833cb6dc18a9af034490dedcdcd
Krzysztof Wędrowicz <krzysztof.wedrowicz@wp.pl>
2016-01-14 13:32:12 +0100

    resized length of additionalInformation to 1000

--- app/migrations/Version20160112125003.php
+++ app/migrations/Version20160112125003.php
@@ -18,7 +18,7 @@ class Version20160112125003 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE sylius_order ADD additional_information VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE sylius_order ADD additional_information VARCHAR(1000) DEFAULT NULL');
     }
 
     /**
