37c1daa7714f4005420e13ff6e55401ccf116163
Paweł Jędrzejewski <pawel@sylius.org>
2016-01-17 17:36:57 +0100

    Use NPM to manage frontend libs

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,18 +3,30 @@ var gulpif = require('gulp-if');
 var uglify = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
 var concat = require('gulp-concat');
+var less = require('gulp-less');
 var sourcemaps = require('gulp-sourcemaps');
-
 var debug = require('gulp-debug');
 
 var env = process.env.GULP_ENV;
 
+var paths = {
+    js: [
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/admin-lte/dist/js/app.min.js'
+    ],
+    'less': [
+        'src/Sylius/Bundle/UiBundle/Resources/assets/less/**'
+    ],
+    css: [
+        'node_modules/bootstrap/dist/css/bootstrap.min.css',
+        'node_modules/admin-lte/dist/css/AdminLTE.min.css',
+        'web/test/*'
+    ]
+};
+
 gulp.task('js', function () {
-    return gulp.src([
-        'vendor/bower_components/AdminLTE/plugins/jQuery/jQuery-2.1.4.min.js',
-        'vendor/bower_components/AdminLTE/bootstrap/js/bootstrap.min.js',
-        'vendor/bower_components/AdminLTE/dist/js/app.min.js'
-    ])
+    return gulp.src(paths.js)
         .pipe(concat('javascript.js'))
         .pipe(gulpif('prod' === env, uglify()))
         .pipe(sourcemaps.write('./'))
@@ -22,12 +34,17 @@ gulp.task('js', function () {
     ;
 });
 
+gulp.task('less', function () {
+    return gulp.src(paths.less)
+        .pipe(less())
+        .pipe(concat('less-compiled.css'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('web/test'))
+    ;
+});
+
 gulp.task('css', function () {
-    return gulp.src([
-        'vendor/bower_components/AdminLTE/bootstrap/css/bootstrap.min.css',
-        'vendor/bower_components/AdminLTE/dist/css/AdminLTE.min.css',
-        'src/Sylius/Bundle/UiBundle/Resources/assets/css/**'
-    ])
+    return gulp.src(paths.css)
         .pipe(concat('style.css'))
         .pipe(gulpif('prod' === env, uglifycss()))
         .pipe(sourcemaps.write('./'))
@@ -35,4 +52,10 @@ gulp.task('css', function () {
     ;
 });
 
-gulp.task('default', ['js', 'css']);
+gulp.task('watch', function() {
+    gulp.watch(paths.js, ['js']);
+    gulp.watch(paths.less, ['less', 'css']);
+    gulp.watch(paths.css, ['css']);
+});
+
+gulp.task('default', ['watch', 'js', 'less', 'css']);
