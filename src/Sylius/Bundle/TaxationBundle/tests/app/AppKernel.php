8f6a5fecca87637b67a8728f603fcbf754eb36c0
Paweł Jędrzejewski <pawel@sylius.org>
2016-01-20 16:44:17 +0100

    [TaxationBundle] Integration tests for bundles - POC

---
+++ src/Sylius/Bundle/TaxationBundle/tests/app/AppKernel.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+/**
+ * @author Paweł Jędrzejewski <pawel@sylius.org>
+ */
+class AppKernel extends Kernel
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles()
+    {
+        return array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Sylius\Bundle\TaxationBundle\SyliusTaxationBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new \Sylius\Bundle\ResourceBundle\SyliusResourceBundle(),
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config.yml');
+    }
+}
