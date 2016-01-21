e70d2b82bcc5cc233ea1a16c40eb57fc76eabd11
Kamil Kokot <kamil@kokot.me>
2016-01-21 16:02:04 +0100

    [ThemeBundle] Added decorator for TranslationFilesFinderInterface, that ensures the correct order of files

---
+++ src/Sylius/Bundle/ThemeBundle/Translation/Finder/OrderingTranslationFilesFinder.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\ThemeBundle\Translation\Finder;
+
+use Sylius\Bundle\ThemeBundle\Model\ThemeInterface;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+final class OrderingTranslationFilesFinder implements TranslationFilesFinderInterface
+{
+    /**
+     * @var TranslationFilesFinderInterface
+     */
+    private $translationFilesFinder;
+
+    /**
+     * @param TranslationFilesFinderInterface $translationFilesFinder
+     */
+    public function __construct(TranslationFilesFinderInterface $translationFilesFinder)
+    {
+        $this->translationFilesFinder = $translationFilesFinder;
+    }
+
+    public function findTranslationFiles(ThemeInterface $theme)
+    {
+        $files = $this->translationFilesFinder->findTranslationFiles($theme);
+
+        usort($files, function ($firstFile, $secondFile) use ($theme) {
+            $firstFile = str_replace($theme->getPath(), '', $firstFile);
+            $secondFile = str_replace($theme->getPath(), '', $secondFile);
+
+            return strpos($secondFile, 'translations') - strpos($firstFile, 'translations');
+        });
+
+        return $files;
+    }
+}
