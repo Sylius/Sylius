d750fecacf129b8d7ae516e6e77d8e6c78a21735
Loic Fremont <loic@mobizel.com>
2016-01-27 15:20:46 +0100

    Add command to change the password of a user

---
+++ src/Sylius/Bundle/UserBundle/Command/ChangePasswordCommand.php
@@ -0,0 +1,132 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\UserBundle\Command;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Sylius\Component\User\Model\UserInterface;
+use Sylius\Component\User\Repository\UserRepositoryInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @author Loïc Frémont <loic@mobizel.com>
+ */
+class ChangePasswordCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('sylius:user:change-password')
+            ->setDescription('Change the password of a user.')
+            ->setDefinition(array(
+                new InputArgument('email', InputArgument::REQUIRED, 'The email'),
+                new InputArgument('password', InputArgument::REQUIRED, 'The password'),
+            ))
+            ->setHelp(<<<EOT
+The <info>sylius:user:change-password</info> command changes the password of a user:
+  <info>php app/console sylius:user:change-password matthieu</info>
+This interactive shell will first ask you for a password.
+You can alternatively specify the password as a second argument:
+  <info>php app/console fos:user:change-password matthieu mypassword</info>
+EOT
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+
+        /** @var UserInterface $user */
+        $user = $this->getUserRepository()->findOneByEmail($email);
+
+        if (null === $user) {
+            throw new \InvalidArgumentException(sprintf('Could not find user by email "%s"', $email));
+        }
+
+        $this->changePassword($user, $password);
+        $this->getEntityManager()->persist($user);
+        $this->getEntityManager()->flush();
+
+        $output->writeln(sprintf('Change password user <comment>%s</comment>', $email));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        if (!$input->getArgument('email')) {
+            $email = $this->getHelper('dialog')->askAndValidate(
+                $output,
+                'Please enter an email:',
+                function($username) {
+                    if (empty($username)) {
+                        throw new \Exception('Email can not be empty');
+                    }
+                    return $username;
+                }
+            );
+
+            $input->setArgument('email', $email);
+        }
+
+        if (!$input->getArgument('password')) {
+            $password = $this->getHelper('dialog')->askHiddenResponseAndValidate(
+                $output,
+                'Please choose a password:',
+                function($password) {
+                    if (empty($password)) {
+                        throw new \Exception('Password can not be empty');
+                    }
+                    return $password;
+                }
+            );
+
+            $input->setArgument('password', $password);
+        }
+    }
+
+    /**
+     * @param UserInterface $user
+     * @param string $password
+     */
+    protected function changePassword(UserInterface $user, $password)
+    {
+        $user->setPlainPassword($password);
+        $this->getContainer()->get('sylius.user.password_updater')->updatePassword($user);
+    }
+
+    /**
+     * @return EntityManagerInterface
+     */
+    protected function getEntityManager()
+    {
+        return $this->getContainer()->get('sylius.manager.user');
+    }
+
+    /**
+     * @return UserRepositoryInterface
+     */
+    protected function getUserRepository()
+    {
+        return $this->getContainer()->get('sylius.repository.user');
+    }
+}
