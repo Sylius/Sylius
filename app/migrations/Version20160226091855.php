4a0fb3da6bd4630ccefcc60a99e76677d19e1cda
Łukasz Chruściel <lchrusciel@gmail.com>
2016-02-24 15:14:58 +0100

    [Order] Remove softdeletable from Order entity mapping

---
+++ app/migrations/Version20160226091855.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Sylius\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160226091855 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE sylius_order DROP INDEX IDX_6196A1F94D4CFF2B, ADD UNIQUE INDEX UNIQ_6196A1F94D4CFF2B (shipping_address_id)');
+        $this->addSql('ALTER TABLE sylius_order DROP INDEX IDX_6196A1F979D0C0E4, ADD UNIQUE INDEX UNIQ_6196A1F979D0C0E4 (billing_address_id)');
+        $this->addSql('ALTER TABLE sylius_order DROP deleted_at');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE sylius_order DROP INDEX UNIQ_6196A1F94D4CFF2B, ADD INDEX IDX_6196A1F94D4CFF2B (shipping_address_id)');
+        $this->addSql('ALTER TABLE sylius_order DROP INDEX UNIQ_6196A1F979D0C0E4, ADD INDEX IDX_6196A1F979D0C0E4 (billing_address_id)');
+        $this->addSql('ALTER TABLE sylius_order ADD deleted_at DATETIME DEFAULT NULL');
+    }
+}
