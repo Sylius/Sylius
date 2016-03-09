8e9a358ddb2b0581ca046c18bb73ba34edd5da6d
Paweł Jędrzejewski <pawel@sylius.org>
2016-03-09 22:58:36 +0100

    Add API tests for tax categories on grids

--- src/Sylius/Bundle/CoreBundle/Tests/Controller/TaxCategoryApiTest.php
+++ src/Sylius/Bundle/CoreBundle/Tests/Controller/TaxCategoryApiTest.php
@@ -16,86 +16,126 @@ use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @author Łukasz Chruściel <lukasz.chrusciel@lakion.com>
+ * @author Paweł Jędrzejewski <pawel@sylius.org>
  */
-class ShippingCategoryApiTest extends JsonApiTestCase
+class TaxCategoryApiTest extends JsonApiTestCase
 {
-    public function testCreateShippingCategoryAccessDeniedResponse()
+    public function testCreateTaxCategoryAccessDeniedResponse()
     {
-        $this->client->request('POST', '/api/shipping-categories/');
+        $this->client->request('POST', '/api/tax-categories/');
 
         $response = $this->client->getResponse();
         $this->assertResponse($response, 'authentication/access_denied_response', Response::HTTP_UNAUTHORIZED);
     }
 
-    public function testCreateShippingCategoryValidationFailResponse()
+    public function testCreateTaxCategoryValidationFailResponse()
     {
         $this->loadFixturesFromFile('authentication/api_administrator.yml');
 
-        $this->client->request('POST', '/api/shipping-categories/', [], [], [
+        $this->client->request('POST', '/api/tax-categories/', [], [], [
             'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
             'CONTENT_TYPE' => 'application/json',
         ]);
 
         $response = $this->client->getResponse();
-        $this->assertResponse($response, 'shipping_category/create_validation_fail_response', Response::HTTP_BAD_REQUEST);
+        $this->assertResponse($response, 'tax_category/create_validation_fail_response', Response::HTTP_BAD_REQUEST);
     }
 
-    public function testCreateShippingCategoryResponse()
+    public function testCreateTaxCategoryResponse()
     {
         $this->loadFixturesFromFile('authentication/api_administrator.yml');
 
         $data =
 <<<EOT
         {
-            "code": "reg",
-            "name": "Regular",
-            "description": "Regular weight items"
+            "code": "clothing",
+            "name": "Clothing",
+            "description": "All items classified as clothing."
         }
 EOT;
 
-        $this->client->request('POST', '/api/shipping-categories/', [], [], [
+        $this->client->request('POST', '/api/tax-categories/', [], [], [
             'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
             'CONTENT_TYPE' => 'application/json',
         ], $data);
 
         $response = $this->client->getResponse();
-        $this->assertResponse($response, 'shipping_category/create_response', Response::HTTP_CREATED);
+        $this->assertResponse($response, 'tax_category/create_response', Response::HTTP_CREATED);
     }
 
-    public function testGetShippingCategoriesListAccessDeniedResponse()
+    public function testGetTaxCategoriesListAccessDeniedResponse()
     {
-        $this->client->request('GET', '/api/shipping-categories/');
+        $this->client->request('GET', '/api/tax-categories/');
 
         $response = $this->client->getResponse();
         $this->assertResponse($response, 'authentication/access_denied_response', Response::HTTP_UNAUTHORIZED);
     }
 
-    public function testGetShippingCategoriesListResponse()
+    public function testGetTaxCategoriesListResponse()
     {
         $this->loadFixturesFromFile('authentication/api_administrator.yml');
-        $this->loadFixturesFromFile('resources/shipping_categories.yml');
+        $this->loadFixturesFromFile('resources/tax_categories.yml');
 
-        $this->client->request('GET', '/api/shipping-categories/', [], [], [
+        $this->client->request('GET', '/api/tax-categories/', [], [], [
             'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
         ]);
 
         $response = $this->client->getResponse();
-        $this->assertResponse($response, 'shipping_category/index_response', Response::HTTP_OK);
+        $this->assertResponse($response, 'tax_category/index_response', Response::HTTP_OK);
     }
 
-    public function testGetShippingCategoryAccessDeniedResponse()
+    public function testGetTaxCategoriesSortedListResponse()
     {
-        $this->client->request('GET', '/api/shipping-categories/1');
+        $this->loadFixturesFromFile('authentication/api_administrator.yml');
+        $this->loadFixturesFromFile('resources/tax_categories.yml');
+
+        $this->client->request('GET', '/api/tax-categories/', ['sorting' => ['name' => 'asc']], [], [
+            'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
+        ]);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'tax_category/sorted_index_response', Response::HTTP_OK);
+    }
+
+    public function testGetTaxCategoriesFilteredListByNameResponse()
+    {
+        $this->loadFixturesFromFile('authentication/api_administrator.yml');
+        $this->loadFixturesFromFile('resources/tax_categories_for_filtering.yml');
+
+        $this->client->request('GET', '/api/tax-categories/', ['criteria' => ['name' => 'clothing']], [], [
+            'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
+        ]);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'tax_category/filtered_by_name_index_response', Response::HTTP_OK);
+    }
+
+    public function testGetTaxCategoriesFilteredListByCodeResponse()
+    {
+        $this->loadFixturesFromFile('authentication/api_administrator.yml');
+        $this->loadFixturesFromFile('resources/tax_categories_for_filtering.yml');
+
+        $this->client->request('GET', '/api/tax-categories/', ['criteria' => ['code' => 'TC1']], [], [
+            'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
+        ]);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'tax_category/filtered_by_code_index_response', Response::HTTP_OK);
+    }
+
+    public function testGetTaxCategoryAccessDeniedResponse()
+    {
+        $this->client->request('GET', '/api/tax-categories/1');
 
         $response = $this->client->getResponse();
         $this->assertResponse($response, 'authentication/access_denied_response', Response::HTTP_UNAUTHORIZED);
     }
 
-    public function testGetShippingCategoryWhichDoesNotExistResponse()
+    public function testGetTaxCategoryWhichDoesNotExistResponse()
     {
         $this->loadFixturesFromFile('authentication/api_administrator.yml');
 
-        $this->client->request('GET', '/api/shipping-categories/-1', [], [], [
+        $this->client->request('GET', '/api/tax-categories/-1', [], [], [
             'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
             'ACCEPT' => 'application/json',
         ]);
@@ -104,33 +144,33 @@ EOT;
         $this->assertResponse($response, 'error/not_found_response', Response::HTTP_NOT_FOUND);
     }
 
-    public function testGetShippingCategoryResponse()
+    public function testGetTaxCategoryResponse()
     {
         $this->loadFixturesFromFile('authentication/api_administrator.yml');
-        $shippingCategories = $this->loadFixturesFromFile('resources/shipping_categories.yml');
+        $taxCategories = $this->loadFixturesFromFile('resources/tax_categories.yml');
 
-        $this->client->request('GET', '/api/shipping-categories/'.$shippingCategories['shipping_category_1']->getId(), [], [], [
+        $this->client->request('GET', '/api/tax-categories/'.$taxCategories['tax_category_1']->getId(), [], [], [
             'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
             'ACCEPT' => 'application/json',
         ]);
 
         $response = $this->client->getResponse();
-        $this->assertResponse($response, 'shipping_category/show_response', Response::HTTP_OK);
+        $this->assertResponse($response, 'tax_category/show_response', Response::HTTP_OK);
     }
 
-    public function testFullUpdateShippingCategoryAccessDeniedResponse()
+    public function testFullUpdateTaxCategoryAccessDeniedResponse()
     {
-        $this->client->request('PUT', '/api/shipping-categories/1');
+        $this->client->request('PUT', '/api/tax-categories/1');
 
         $response = $this->client->getResponse();
         $this->assertResponse($response, 'authentication/access_denied_response', Response::HTTP_UNAUTHORIZED);
     }
 
-    public function testFullUpdateShippingCategoryWhichDoesNotExistResponse()
+    public function testFullUpdateTaxCategoryWhichDoesNotExistResponse()
     {
         $this->loadFixturesFromFile('authentication/api_administrator.yml');
 
-        $this->client->request('PUT', '/api/shipping-categories/-1', [], [], [
+        $this->client->request('PUT', '/api/tax-categories/-1', [], [], [
             'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
             'ACCEPT' => 'application/json',
         ]);
@@ -139,77 +179,34 @@ EOT;
         $this->assertResponse($response, 'error/not_found_response', Response::HTTP_NOT_FOUND);
     }
 
-    public function testFullUpdateShippingCategoryValidationFailResponse()
+    public function testFullUpdateTaxCategoryValidationFailResponse()
     {
         $this->loadFixturesFromFile('authentication/api_administrator.yml');
-        $shippingCategories = $this->loadFixturesFromFile('resources/shipping_categories.yml');
+        $taxCategories = $this->loadFixturesFromFile('resources/tax_categories.yml');
 
-        $this->client->request('PUT', '/api/shipping-categories/'.$shippingCategories['shipping_category_1']->getId(), [], [], [
+        $this->client->request('PUT', '/api/tax-categories/'.$taxCategories['tax_category_1']->getId(), [], [], [
             'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
             'CONTENT_TYPE' => 'application/json',
         ]);
 
         $response = $this->client->getResponse();
-        $this->assertResponse($response, 'shipping_category/update_validation_fail_response', Response::HTTP_BAD_REQUEST);
+        $this->assertResponse($response, 'tax_category/update_validation_fail_response', Response::HTTP_BAD_REQUEST);
     }
 
-    public function testFullUpdateShippingCategoryResponse()
+    public function testFullUpdateTaxCategoryResponse()
     {
         $this->loadFixturesFromFile('authentication/api_administrator.yml');
-        $shippingCategories = $this->loadFixturesFromFile('resources/shipping_categories.yml');
+        $taxCategories = $this->loadFixturesFromFile('resources/tax_categories.yml');
 
         $data =
 <<<EOT
         {
-            "name": "Light",
-            "description": "Light weight items"
+            "name": "Clothing & Accessories",
+            "description": "All items classified as clothing with accessories."
         }
 EOT;
 
-        $this->client->request('PUT', '/api/shipping-categories/'.$shippingCategories['shipping_category_1']->getId(), [], [], [
-            'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
-            'CONTENT_TYPE' => 'application/json',
-        ], $data);
-
-        $response = $this->client->getResponse();
-        $this->assertResponseCode($response, Response::HTTP_NO_CONTENT);
-
-        $this->client->request('GET', '/api/shipping-categories/'.$shippingCategories['shipping_category_1']->getId(), [], [], [
-            'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
-            'ACCEPT' => 'application/json',
-        ]);
-
-        $response = $this->client->getResponse();
-        $this->assertResponse($response, 'shipping_category/update_response', Response::HTTP_OK);
-    }
-
-    public function testPartialUpdateShippingCategoryWhichDoesNotExistResponse()
-    {
-        $this->loadFixturesFromFile('authentication/api_administrator.yml');
-
-        $this->client->request('PATCH', '/api/shipping-categories/-1', [], [], [
-            'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
-            'ACCEPT' => 'application/json',
-        ]);
-
-        $response = $this->client->getResponse();
-        $this->assertResponse($response, 'error/not_found_response', Response::HTTP_NOT_FOUND);
-    }
-
-    public function testPartialUpdateShippingCategoryResponse()
-    {
-        $this->loadFixturesFromFile('authentication/api_administrator.yml');
-        $shippingCategories = $this->loadFixturesFromFile('resources/shipping_categories.yml');
-
-        $data =
-<<<EOT
-        {
-            "name": "Light",
-            "description": "Light weight items"
-        }
-EOT;
-
-        $this->client->request('PATCH', '/api/shipping-categories/'.$shippingCategories['shipping_category_1']->getId(), [], [], [
+        $this->client->request('PUT', '/api/tax-categories/'.$taxCategories['tax_category_1']->getId(), [], [], [
             'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
             'CONTENT_TYPE' => 'application/json',
         ], $data);
@@ -217,20 +214,20 @@ EOT;
         $response = $this->client->getResponse();
         $this->assertResponseCode($response, Response::HTTP_NO_CONTENT);
 
-        $this->client->request('GET', '/api/shipping-categories/'.$shippingCategories['shipping_category_1']->getId(), [], [], [
+        $this->client->request('GET', '/api/tax-categories/'.$taxCategories['tax_category_1']->getId(), [], [], [
             'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
             'ACCEPT' => 'application/json',
         ]);
 
         $response = $this->client->getResponse();
-        $this->assertResponse($response, 'shipping_category/update_response', Response::HTTP_OK);
+        $this->assertResponse($response, 'tax_category/update_response', Response::HTTP_OK);
     }
 
-    public function testDeleteShippingCategoryWhichDoesNotExistResponse()
+    public function testPartialUpdateTaxCategoryWhichDoesNotExistResponse()
     {
         $this->loadFixturesFromFile('authentication/api_administrator.yml');
 
-        $this->client->request('DELETE', '/api/shipping-categories/-1', [], [], [
+        $this->client->request('PATCH', '/api/tax-categories/-1', [], [], [
             'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
             'ACCEPT' => 'application/json',
         ]);
@@ -239,12 +236,54 @@ EOT;
         $this->assertResponse($response, 'error/not_found_response', Response::HTTP_NOT_FOUND);
     }
 
-    public function testDeleteShippingCategoryResponse()
+    public function testPartialUpdateTaxCategoryResponse()
     {
         $this->loadFixturesFromFile('authentication/api_administrator.yml');
-        $shippingCategories = $this->loadFixturesFromFile('resources/shipping_categories.yml');
+        $taxCategories = $this->loadFixturesFromFile('resources/tax_categories.yml');
 
-        $this->client->request('DELETE', '/api/shipping-categories/'.$shippingCategories['shipping_category_1']->getId(), [], [], [
+        $data =
+<<<EOT
+        {
+            "name": "Clothing & Accessories"
+        }
+EOT;
+
+        $this->client->request('PATCH', '/api/tax-categories/'.$taxCategories['tax_category_1']->getId(), [], [], [
+            'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
+            'CONTENT_TYPE' => 'application/json',
+        ], $data);
+
+        $response = $this->client->getResponse();
+        $this->assertResponseCode($response, Response::HTTP_NO_CONTENT);
+
+        $this->client->request('GET', '/api/tax-categories/'.$taxCategories['tax_category_1']->getId(), [], [], [
+            'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
+            'ACCEPT' => 'application/json',
+        ]);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'tax_category/partial_update_response', Response::HTTP_OK);
+    }
+
+    public function testDeleteTaxCategoryWhichDoesNotExistResponse()
+    {
+        $this->loadFixturesFromFile('authentication/api_administrator.yml');
+
+        $this->client->request('DELETE', '/api/tax-categories/-1', [], [], [
+            'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
+            'ACCEPT' => 'application/json',
+        ]);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'error/not_found_response', Response::HTTP_NOT_FOUND);
+    }
+
+    public function testDeleteTaxCategoryResponse()
+    {
+        $this->loadFixturesFromFile('authentication/api_administrator.yml');
+        $taxCategories = $this->loadFixturesFromFile('resources/tax_categories.yml');
+
+        $this->client->request('DELETE', '/api/tax-categories/'.$taxCategories['tax_category_1']->getId(), [], [], [
             'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
             'CONTENT_TYPE' => 'application/json',
         ], []);
@@ -252,7 +291,7 @@ EOT;
         $response = $this->client->getResponse();
         $this->assertResponseCode($response, Response::HTTP_NO_CONTENT);
 
-        $this->client->request('GET', '/api/shipping-categories/'.$shippingCategories['shipping_category_1']->getId(), [], [], [
+        $this->client->request('GET', '/api/tax-categories/'.$taxCategories['tax_category_1']->getId(), [], [], [
             'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
             'ACCEPT' => 'application/json',
         ]);
