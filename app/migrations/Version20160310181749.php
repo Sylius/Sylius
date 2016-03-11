26031fc2caa2cf4e2b2835b954abe29791a4b5cd
Hussein Jafferjee <hussein@jafferjee.ca>
2016-03-11 09:46:52 -0800

    Add migration for coupon fix

---
+++ app/migrations/Version20160310181749.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Sylius\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160310181749 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE sylius_order DROP INDEX UNIQ_6196A1F917B24436, ADD INDEX IDX_6196A1F917B24436 (promotion_coupon_id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE sylius_order DROP INDEX IDX_6196A1F917B24436, ADD UNIQUE INDEX UNIQ_6196A1F917B24436 (promotion_coupon_id)');
+    }
+}
