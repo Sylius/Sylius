8630afc05fbcd9e5c281e28719f6dec937420e03
Kamil Kokot <kamil@kokot.me>
2016-03-22 11:25:18 +0100

    [Resource] Made DoctrineTargetEntitiesResolver the CompilerPass, added tests for it, fixed for cases that does not use Doctrine ORM

--- src/Sylius/Bundle/ResourceBundle/DependencyInjection/Compiler/RegisterResourcesPass.php
+++ src/Sylius/Bundle/ResourceBundle/DependencyInjection/Compiler/RegisterResourcesPass.php
@@ -11,9 +11,9 @@
 
 namespace Sylius\Bundle\ResourceBundle\DependencyInjection\Compiler;
 
-use Sylius\Bundle\ResourceBundle\DependencyInjection\DoctrineTargetEntitiesResolver;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 
 /**
  * Adds all resources to the registry.
@@ -27,30 +27,15 @@ class RegisterResourcesPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        if (!$container->hasParameter('sylius.resources')) {
+        try {
+            $resources = $container->getParameter('sylius.resources');
+            $registry = $container->findDefinition('sylius.resource_registry');
+        } catch (InvalidArgumentException $exception) {
             return;
         }
 
-        $resources = $container->getParameter('sylius.resources');
-        $registry = $container->getDefinition('sylius.resource_registry');
-
-        $interfaces = [];
-
         foreach ($resources as $alias => $configuration) {
             $registry->addMethodCall('addFromAliasAndConfiguration', [$alias, $configuration]);
-
-            if (isset($configuration['classes']['interface'])) {
-                $alias = explode('.', $alias);
-
-                if (!isset($alias[0]) || !isset($alias[1])) {
-                    throw new \RuntimeException(sprintf('Error configuring "%s" resource. The resource alias should follow the "prefix.name" format.', $alias[0]));
-                }
-
-                $interfaces[$configuration['classes']['interface']] = sprintf('%s.model.%s.class', $alias[0], $alias[1]);
-            }
         }
-
-        $resolver = new DoctrineTargetEntitiesResolver();
-        $resolver->resolve($container, $interfaces);
     }
 }
