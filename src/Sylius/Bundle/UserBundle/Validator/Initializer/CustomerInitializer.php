9c5d94410c87d594dc781dd6c31017753697bfa6
Steffen Brem <steffenbrem@gmail.com>
2016-04-11 15:53:57 +0200

    Fix issue #4733. Added customer validator initializer that sets email canonical on initialisation. Customer object is now also validating against `emailCanonical` and uses `email` as it's error path.

---
+++ src/Sylius/Bundle/UserBundle/Validator/Initializer/CustomerInitializer.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\UserBundle\Validator\Initializer;
+
+use Sylius\Component\User\Canonicalizer\CanonicalizerInterface;
+use Sylius\Component\User\Model\CustomerInterface;
+use Symfony\Component\Validator\ObjectInitializerInterface;
+
+/**
+ * @author Steffen Brem <steffenbrem@gmail.com>
+ */
+final class CustomerInitializer implements ObjectInitializerInterface
+{
+    /**
+     * @var CanonicalizerInterface
+     */
+    private $canonicalizer;
+
+    /**
+     * @param CanonicalizerInterface $canonicalizer
+     */
+    public function __construct(CanonicalizerInterface $canonicalizer)
+    {
+        $this->canonicalizer = $canonicalizer;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function initialize($object)
+    {
+        if ($object instanceof CustomerInterface) {
+            $emailCanonical = $this->canonicalizer->canonicalize($object->getEmail());
+            $object->setEmailCanonical($emailCanonical);
+        }
+    }
+}
