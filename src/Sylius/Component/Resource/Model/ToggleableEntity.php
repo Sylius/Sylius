a5c70ceb2d91667c0a8cb7ee7b7cea18953888cf
Gustavo Perdomo <gperdomor@gmail.com>
2016-04-18 16:54:25 -0430

    Add TimestampableEntity and ToggleableEntity traits with annotation support

---
+++ src/Sylius/Component/Resource/Model/ToggleableEntity.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Resource\Model;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * @author Gustavo Perdomo <gperdomor@gmail.com>
+ */
+trait ToggleableEntity
+{
+    /**
+     * @var bool
+     * 
+     * @ORM\Column(name="enabled", type="boolean")
+     */
+    protected $enabled = true;
+
+    /**
+     * @return bool
+     */
+    public function isEnabled()
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @param bool $enabled
+     */
+    public function setEnabled($enabled)
+    {
+        $this->enabled = (bool) $enabled;
+    }
+
+    public function enable()
+    {
+        $this->enabled = true;
+    }
+
+    public function disable()
+    {
+        $this->enabled = false;
+    }
+}
