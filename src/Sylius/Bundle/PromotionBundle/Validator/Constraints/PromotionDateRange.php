72ab953a3f9ae55da2e5d1b065a0e6423ce10d14
Łukasz Chruściel <lchrusciel@gmail.com>
2016-04-26 09:20:04 +0200

    [Promotion] Add Promotion data range validator

---
+++ src/Sylius/Bundle/PromotionBundle/Validator/Constraints/PromotionDateRange.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\PromotionBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @author Łukasz Chruściel <lukasz.chrusciel@lakion.com>
+ */
+class PromotionDateRange extends Constraint
+{
+    public $message = 'sylius.promotion.startsAt.end_date_cannot_be_set_prior_start_date';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTargets()
+    {
+        return [self::CLASS_CONSTRAINT];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validatedBy()
+    {
+        return 'sylius_promotion_date_range_validator';
+    }
+}
