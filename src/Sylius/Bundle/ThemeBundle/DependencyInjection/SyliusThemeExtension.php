463ad0039d4aaf08d8bd2825b18ac493d0509d64
Kamil Kokot <kamil@kokot.me>
2016-04-26 13:39:04 +0200

    [Theme] Decouple from SettingsBundle and cleanup container configuration

--- src/Sylius/Bundle/ThemeBundle/DependencyInjection/SyliusThemeExtension.php
+++ src/Sylius/Bundle/ThemeBundle/DependencyInjection/SyliusThemeExtension.php
@@ -13,13 +13,15 @@ namespace Sylius\Bundle\ThemeBundle\DependencyInjection;
 
 use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 /**
  * @author Kamil Kokot <kamil.kokot@lakion.com>
  */
-class SyliusThemeExtension extends AbstractResourceExtension
+final class SyliusThemeExtension extends AbstractResourceExtension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -29,27 +31,53 @@ class SyliusThemeExtension extends AbstractResourceExtension
         $config = $this->processConfiguration(new Configuration(), $configs);
 
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/services'));
-        $loader->load('assets.xml');
-        $loader->load('configuration.xml');
-        $loader->load('resource_locators.xml');
         $loader->load('services.xml');
-        $loader->load('settings.xml');
-        $loader->load('templating.xml');
-        $loader->load('translations.xml');
+        $loader->load(sprintf('driver/%s.xml', $config['driver']));
 
+        $this->loadFilesystemConfiguration($container, $loader, $config);
+
+        $container->setParameter('sylius.interface.theme.class', $config['resources']['theme']['classes']['interface']);
         $container->setAlias('sylius.context.theme', $config['context']);
 
-        // TODO: Interfaces ready for filesystem decoupling, configuration not ready yet
-        $loader->load('filesystem_configuration.xml');
-        $container->setAlias('sylius.theme.configuration.loader', 'sylius.theme.configuration.loader.json_file');
-        $container->setAlias('sylius.theme.configuration.provider', 'sylius.theme.configuration.provider.filesystem');
-        $container->setParameter('sylius.theme.configuration.filesystem.locations', $config['sources']['filesystem']['locations']);
-
-        $loader->load(sprintf('driver/%s.xml', $config['driver']));
         $this->registerResources('sylius', $config['driver'], $config['resources'], $container);
 
         $container->getDefinition('sylius.repository.theme')->setLazy(true);
+    }
 
-        $container->setParameter('sylius.interface.theme.class', $config['resources']['theme']['classes']['interface']);
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/services'));
+
+        $this->prependSyliusSettings($container, $loader);
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     * @param LoaderInterface $loader
+     * @param array $config
+     */
+    private function loadFilesystemConfiguration(ContainerBuilder $container, LoaderInterface $loader, array $config)
+    {
+        $loader->load('configuration/filesystem.xml');
+
+        $container->setAlias('sylius.theme.configuration.loader', 'sylius.theme.configuration.loader.json_file');
+        $container->setAlias('sylius.theme.configuration.provider', 'sylius.theme.configuration.provider.filesystem');
+        $container->setParameter('sylius.theme.configuration.filesystem.locations', $config['sources']['filesystem']['locations']);
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     * @param LoaderInterface $loader
+     */
+    private function prependSyliusSettings(ContainerBuilder $container, LoaderInterface $loader)
+    {
+        if (!$container->hasExtension('sylius_settings')) {
+            return;
+        }
+
+        $loader->load('integration/settings.xml');
     }
 }
