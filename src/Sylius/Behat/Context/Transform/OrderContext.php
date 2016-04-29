6a40a76cdf76ede5be4282e0976f93e184a15636
Grzegorz Sadowski <sadowskigp@gmail.com>
2016-04-29 10:00:01 +0200

    [Admin][Order] Added order transformer

---
+++ src/Sylius/Behat/Context/Transform/OrderContext.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Behat\Context\Transform;
+
+use Behat\Behat\Context\Context;
+use Sylius\Component\Core\Repository\OrderRepositoryInterface;
+use Webmozart\Assert\Assert;
+
+/**
+ * @author Grzegorz Sadowski <grzegorz.sadowski@lakion.com>
+ */
+final class OrderContext implements Context
+{
+    /**
+     * @var OrderRepositoryInterface
+     */
+    private $orderRepository;
+
+    /**
+     * @param OrderRepositoryInterface $orderRepository
+     */
+    public function __construct(OrderRepositoryInterface $orderRepository)
+    {
+        $this->orderRepository = $orderRepository;
+    }
+
+    /**
+     * @Transform :order
+     */
+    public function getOrderByNumber($orderNumber)
+    {
+        $order = $this->orderRepository->findOneBy(['number' => $orderNumber]);
+
+        Assert::notNull($order, sprintf('Cannot find order with number %s', $orderNumber));
+
+        return $order;
+    }
+}
