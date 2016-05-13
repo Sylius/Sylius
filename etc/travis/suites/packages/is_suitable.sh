1bcd39ae9e0b7b0ceae97d49cf0e4fb3cf6f6578
Kamil Kokot <kamil@kokot.me>
2016-05-13 11:25:35 +0200

    [Travis] Innovational, intelligent suite executing :camel:

---
+++ etc/travis/suites/packages/is_suitable.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/common.lib.sh"
+
+if [ $(git diff --name-only HEAD origin/master | grep -c -e ^src/Sylius/Bundle -e ^src/Sylius/Component) -eq 0 ]; then
+    print_header "Skipped suite" "Packages"
+    print_warning "No changes detected in src/Sylius/Bundle/* or src/Sylius/Component/*"
+    exit 1
+fi
+
+exit 0
