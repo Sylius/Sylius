e4ca3a5b93b5341e48cfde20c56124eaa1a1ce85
Kamil Kokot <kamil@kokot.me>
2016-05-13 09:36:10 +0200

    [Travis] More verbose docs build

--- etc/travis/suites/docs/script.sh
+++ etc/travis/suites/docs/script.sh
@@ -2,5 +2,14 @@
 
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/common.lib.sh"
 
+code=0
+
 print_header "Building" "Documentation"
-run_command "sphinx-build -nW -b html docs docs/build"
+run_command "sphinx-build -nW -b html docs docs/build" || code=$?
+
+if [[ ${code} != 0 ]]; then
+    print_warning "Build failed, rerunning to show all the warnings and errors"
+    run_command "sphinx-build -n -b html docs docs/build"
+fi
+
+exit ${code}
