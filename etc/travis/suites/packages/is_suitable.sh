c16e56be414c941696de5cb73cef332e52489b74
Kamil Kokot <kamil@kokot.me>
2016-05-13 13:50:19 +0200

    [Travis] Always run full test suite on master branch

--- etc/travis/suites/packages/is_suitable.sh
+++ etc/travis/suites/packages/is_suitable.sh
@@ -2,6 +2,10 @@
 
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/common.lib.sh"
 
+if [ $(git rev-parse --abbrev-ref HEAD) = "master" ]; then
+    exit 0 # Always execute full suite on master
+fi
+
 if [ $(git diff --name-only HEAD origin/master | grep -c -e ^src/Sylius/Bundle -e ^src/Sylius/Component) -eq 0 ]; then
     print_header "Skipped suite" "Packages"
     print_warning "No changes detected in src/Sylius/Bundle/* or src/Sylius/Component/*"
