500a1b6c3707c992fb2768f8d7b890f07da0a87b
Kamil Kokot <kamil@kokot.me>
2016-05-20 11:17:07 +0200

    [Travis] Ensure full suite is always run on branch builds

--- etc/travis/suites/packages/is_suitable.sh
+++ etc/travis/suites/packages/is_suitable.sh
@@ -2,8 +2,8 @@
 
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/common.lib.sh"
 
-if [ $(git rev-parse --abbrev-ref HEAD) = "master" ]; then
-    exit 0 # Always execute full suite on master
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    exit 0 # Always execute full suite on branch builds
 fi
 
 if [ $(git diff --name-only HEAD origin/master | grep -c -e ^src/Sylius/Bundle -e ^src/Sylius/Component) -eq 0 ]; then
