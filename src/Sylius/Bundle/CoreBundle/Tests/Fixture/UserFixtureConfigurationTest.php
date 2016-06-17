6cdc9b62e8c15ad2b276bc892941f4b84f9d2c51
Kamil Kokot <kamil@kokot.me>
2016-06-17 14:00:15 +0200

    [Core] Add user fixtures (manual or random generation)

---
+++ src/Sylius/Bundle/CoreBundle/Tests/Fixture/UserFixtureConfigurationTest.php
@@ -0,0 +1,139 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\CoreBundle\Tests\Fixture;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Matthias\SymfonyConfigTest\Partial\PartialProcessor;
+use Matthias\SymfonyConfigTest\PhpUnit\ConfigurationTestCaseTrait;
+use Sylius\Bundle\CoreBundle\Fixture\ShippingMethodFixture;
+use Sylius\Bundle\CoreBundle\Fixture\UserFixture;
+use Sylius\Component\Addressing\Model\ZoneInterface;
+use Sylius\Component\Addressing\Repository\ZoneRepositoryInterface;
+use Sylius\Component\Resource\Factory\FactoryInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Sylius\Component\Shipping\Model\ShippingCategoryInterface;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+final class UserFixtureConfigurationTest extends \PHPUnit_Framework_TestCase
+{
+    use ConfigurationTestCaseTrait;
+
+    /**
+     * @test
+     */
+    public function users_must_be_set_and_not_empty()
+    {
+        $this->assertPartialConfigurationIsInvalid([[]], 'users');
+        $this->assertPartialConfigurationIsInvalid([['users' => null]], 'users');
+        $this->assertPartialConfigurationIsInvalid([['users' => []]], 'users');
+    }
+
+    /**
+     * @test
+     */
+    public function if_users_contains_a_number_then_it_is_amount_of_randomly_generated_resources()
+    {
+        $processedConfiguration = (new PartialProcessor())->processConfiguration(
+            $this->getConfiguration(),
+            'users',
+            [['users' => 3]]
+        );
+
+        static::assertCount(3, $processedConfiguration['users']);
+
+        $processedConfiguration = (new PartialProcessor())->processConfiguration(
+            $this->getConfiguration(),
+            'users',
+            [['users' => '2']]
+        );
+
+        static::assertCount(2, $processedConfiguration['users']);
+    }
+
+    /**
+     * @test
+     */
+    public function users_can_be_defined_as_array_of_strings()
+    {
+        $this->assertProcessedConfigurationEquals(
+            [['users' => ['one@example.com', 'two@example.com']]],
+            ['users' => [['email' => 'one@example.com'], ['email' => 'two@example.com']]],
+            'users'
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function user_email_is_required()
+    {
+        $this->assertPartialConfigurationIsInvalid([['users' => [null]]], 'users');
+        $this->assertPartialConfigurationIsInvalid([['users' => [['email' => null]]]], 'users');
+
+        $this->assertConfigurationIsValid([['users' => [['email' => 'john.doe@example.com']]]], 'users');
+    }
+
+    /**
+     * @test
+     */
+    public function user_first_name_is_optional()
+    {
+        $this->assertConfigurationIsValid([['users' => [['first_name' => 'John']]]], 'users.*.first_name');
+    }
+
+    /**
+     * @test
+     */
+    public function user_last_name_is_optional()
+    {
+        $this->assertConfigurationIsValid([['users' => [['last_name' => 'Doe']]]], 'users.*.last_name');
+    }
+
+    /**
+     * @test
+     */
+    public function user_currency_code_is_optional()
+    {
+        $this->assertConfigurationIsValid([['users' => [['currency_code' => 'USD']]]], 'users.*.currency_code');
+    }
+
+    /**
+     * @test
+     */
+    public function user_may_be_toggled()
+    {
+        $this->assertConfigurationIsValid([['users' => [['enabled' => false]]]], 'users.*.enabled');
+    }
+
+    /**
+     * @test
+     */
+    public function user_password_code_is_optional()
+    {
+        $this->assertConfigurationIsValid([['users' => [['password' => 'I.<3.Krzysztof.Krawczyk']]]], 'users.*.password');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getConfiguration()
+    {
+        return new UserFixture(
+            $this->getMockBuilder(FactoryInterface::class)->getMock(),
+            $this->getMockBuilder(ObjectManager::class)->getMock(),
+            $this->getMockBuilder(FactoryInterface::class)->getMock(),
+            $this->getMockBuilder(RepositoryInterface::class)->getMock()
+        );
+    }
+}
