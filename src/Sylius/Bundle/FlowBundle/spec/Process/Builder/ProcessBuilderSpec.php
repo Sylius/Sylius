b223e8bfb662b413514e780d614765fed9222f0e
dantleech <dan.t.leech@gmail.com>
2016-07-05 12:16:02 +0100

    Only add steps that are active

--- src/Sylius/Bundle/FlowBundle/spec/Process/Builder/ProcessBuilderSpec.php
+++ src/Sylius/Bundle/FlowBundle/spec/Process/Builder/ProcessBuilderSpec.php
@@ -14,6 +14,9 @@ namespace spec\Sylius\Bundle\FlowBundle\Process\Builder;
 use PhpSpec\ObjectBehavior;
 use Sylius\Bundle\FlowBundle\Process\Builder\ProcessBuilderInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Sylius\Bundle\FlowBundle\Process\Step\StepInterface;
+use Sylius\Bundle\FlowBundle\Process\Scenario\ProcessScenarioInterface;
+use Prophecy\Argument;
 
 class ProcessBuilderSpec extends ObjectBehavior
 {
@@ -30,5 +33,28 @@ class ProcessBuilderSpec extends ObjectBehavior
     function it_is_process_builder()
     {
         $this->shouldImplement(ProcessBuilderInterface::class);
+    }
+
+    function it_should_not_add_inactive_steps(
+        StepInterface $step,
+        ProcessScenarioInterface $scenario
+    ) {
+        $this->build($scenario);
+        $step->isActive()->willReturn(false);
+        $step->setName(Argument::any())->shouldNotBeCalled();
+
+        $this->add('foobar', $step);
+    }
+
+    function it_should_add_active_steps(
+        StepInterface $step,
+        ProcessScenarioInterface $scenario
+    ) {
+        $step->getName()->willReturn(null);
+        $this->build($scenario);
+        $step->isActive()->willReturn(true);
+        $step->setName('foobar')->shouldBeCalled();
+
+        $this->add('foobar', $step);
     }
 }
