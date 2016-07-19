0113ac50c7639762f59fc29e7639d9297829fc0a
Kamil Kokot <kamil@kokot.me>
2016-07-19 10:49:53 +0200

    [Currency] Add composite context

---
+++ src/Sylius/Component/Currency/Context/CompositeCurrencyContext.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Currency\Context;
+
+use Zend\Stdlib\PriorityQueue;
+
+/**
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+final class CompositeCurrencyContext implements CurrencyContextInterface
+{
+    /**
+     * @var PriorityQueue|CurrencyContextInterface[]
+     */
+    private $currencyContexts;
+
+    public function __construct()
+    {
+        $this->currencyContexts = new PriorityQueue();
+    }
+
+    /**
+     * @param CurrencyContextInterface $currencyContext
+     * @param int $priority
+     */
+    public function addContext(CurrencyContextInterface $currencyContext, $priority = 0)
+    {
+        $this->currencyContexts->insert($currencyContext, $priority);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCurrency()
+    {
+        foreach ($this->currencyContexts as $currencyContext) {
+            try {
+                return $currencyContext->getCurrency();
+            } catch (CurrencyNotFoundException $exception) {
+                continue;
+            }
+        }
+
+        throw new CurrencyNotFoundException();
+    }
+}
