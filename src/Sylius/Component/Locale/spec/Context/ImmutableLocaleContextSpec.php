db7951848afa203da87f35cbee783b8822bf03ad
Kamil Kokot <kamil@kokot.me>
2016-07-25 13:53:28 +0200

    [Locale] Refactor basic locale context interface and provide immutable implementation

---
+++ src/Sylius/Component/Locale/spec/Context/ImmutableLocaleContextSpec.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Sylius\Component\Locale\Context;
+
+use PhpSpec\ObjectBehavior;
+use Sylius\Component\Locale\Context\ImmutableLocaleContext;
+use Sylius\Component\Locale\Context\LocaleContextInterface;
+
+/**
+ * @mixin ImmutableLocaleContext
+ *
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+final class ImmutableLocaleContextSpec extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith('pl_PL');
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(ImmutableLocaleContext::class);
+    }
+
+    function it_is_Sylius_locale_context()
+    {
+        $this->shouldImplement(LocaleContextInterface::class);
+    }
+
+    function it_gets_a_locale_code()
+    {
+        $this->getLocaleCode()->shouldReturn('pl_PL');
+    }
+}
