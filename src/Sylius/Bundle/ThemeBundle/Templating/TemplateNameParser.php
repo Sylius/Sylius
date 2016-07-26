3b5f72c1033d11da79e2beb863f77e39d7150f7f
Kamil Kokot <kamil@kokot.me>
2016-07-26 14:26:06 +0200

    [Theme] Support bundle namespaced template paths

---
+++ src/Sylius/Bundle/ThemeBundle/Templating/TemplateNameParser.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\ThemeBundle\Templating;
+
+use Symfony\Bundle\FrameworkBundle\Templating\TemplateReference;
+use Symfony\Component\Templating\TemplateNameParserInterface;
+use Symfony\Component\Templating\TemplateReferenceInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+/**
+ * TemplateNameParser converts template names from the short notation
+ * "@Bundle/Section/template.format.engine" to TemplateReferenceInterface instances.
+ *
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+final class TemplateNameParser implements TemplateNameParserInterface
+{
+    /**
+     * @var TemplateNameParserInterface
+     */
+    private $decoratedParser;
+
+    /**
+     * @var KernelInterface
+     */
+    private $kernel;
+
+    /**
+     * @var TemplateReferenceInterface[]
+     */
+    private $cache = [];
+
+    /**
+     * @param TemplateNameParserInterface $decoratedParser
+     * @param KernelInterface $kernel
+     */
+    public function __construct(TemplateNameParserInterface $decoratedParser, KernelInterface $kernel)
+    {
+        $this->decoratedParser = $decoratedParser;
+        $this->kernel = $kernel;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function parse($name)
+    {
+        if ($name instanceof TemplateReferenceInterface) {
+            return $name;
+        } elseif (isset($this->cache[$name])) {
+            return $this->cache[$name];
+        }
+
+        if (!preg_match('/^@([^\/]*)(?:\/(.+))?\/(.+)\.([^\.]+)\.([^\.]+)$/', $name, $matches)) {
+            return $this->decoratedParser->parse($name);
+        }
+
+        $template = new TemplateReference($matches[1] . 'Bundle', $matches[2], $matches[3], $matches[4], $matches[5]);
+
+        if ($template->get('bundle')) {
+            try {
+                $this->kernel->getBundle($template->get('bundle'));
+            } catch (\Exception $e) {
+                throw new \InvalidArgumentException(sprintf('Template name "%s" is not valid.', $name), 0, $e);
+            }
+        }
+
+        return $this->cache[$name] = $template;
+    }
+}
---
+++ src/Sylius/Bundle/ThemeBundle/spec/Templating/TemplateNameParserSpec.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace spec\Sylius\Bundle\ThemeBundle\Templating;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Sylius\Bundle\ThemeBundle\Templating\TemplateNameParser;
+use Symfony\Bundle\FrameworkBundle\Templating\TemplateReference;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Templating\TemplateNameParserInterface;
+use Symfony\Component\Templating\TemplateReferenceInterface;
+
+/**
+ * @mixin TemplateNameParser
+ *
+ * @author Kamil Kokot <kamil.kokot@lakion.com>
+ */
+final class TemplateNameParserSpec extends ObjectBehavior
+{
+    function let(TemplateNameParserInterface $decoratedParser, KernelInterface $kernel)
+    {
+        $this->beConstructedWith($decoratedParser, $kernel);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(TemplateNameParser::class);
+    }
+
+    function it_is_a_template_name_parser()
+    {
+        $this->shouldImplement(TemplateNameParserInterface::class);
+    }
+
+    function it_returns_template_reference_if_passed_as_name(TemplateReferenceInterface $templateReference)
+    {
+        $this->parse($templateReference)->shouldReturn($templateReference);
+    }
+
+    function it_delegates_not_namespaced_paths_to_decorated_parser(
+        TemplateNameParserInterface $decoratedParser,
+        TemplateReferenceInterface $templateReference
+    ) {
+        $decoratedParser->parse('Bundle:Not:namespaced.html.twig')->willReturn($templateReference);
+        $decoratedParser->parse('/Not/namespaced.html.twig')->willReturn($templateReference);
+
+        $this->parse('Bundle:Not:namespaced.html.twig')->shouldReturn($templateReference);
+        $this->parse('/Not/namespaced.html.twig')->shouldReturn($templateReference);
+    }
+
+    function it_generates_template_references_from_namespaced_paths(KernelInterface $kernel)
+    {
+        $kernel->getBundle('AcmeBundle')->willReturn(null); // just do not throw an exception
+
+        $this->parse('@Acme/app.html.twig')->shouldBeLike(new TemplateReference('AcmeBundle', '', 'app', 'html', 'twig'));
+        $this->parse('@Acme/Directory/app.html.twig')->shouldBeLike(new TemplateReference('AcmeBundle', 'Directory', 'app', 'html', 'twig'));
+        $this->parse('@Acme/Directory.WithDot/app.html.twig')->shouldBeLike(new TemplateReference('AcmeBundle', 'Directory.WithDot', 'app', 'html', 'twig'));
+        $this->parse('@Acme/Directory/app.with.dots.html.twig')->shouldBeLike(new TemplateReference('AcmeBundle', 'Directory', 'app.with.dots', 'html', 'twig'));
+        $this->parse('@Acme/Nested/Directory/app.html.twig')->shouldBeLike(new TemplateReference('AcmeBundle', 'Nested/Directory', 'app', 'html', 'twig'));
+    }
+
+    function it_throws_an_exception_if_namespaced_path_references_not_existing_bundle(KernelInterface $kernel)
+    {
+        $kernel->getBundle('AcmeBundle')->willThrow(\Exception::class);
+
+        $this->shouldThrow(\InvalidArgumentException::class)->during('parse', ['@Acme/app.html.twig']);
+    }
+}
