56e425f51d278c6a9c057cea6c5bccd62d755e1d
Kamil Kokot <kamil@kokot.me>
2016-07-26 15:08:08 +0200

    [Theme] Clean up theme filesystem loader

--- src/Sylius/Bundle/ThemeBundle/Twig/ThemeFilesystemLoader.php
+++ src/Sylius/Bundle/ThemeBundle/Twig/ThemeFilesystemLoader.php
@@ -52,24 +52,8 @@ final class ThemeFilesystemLoader extends \Twig_Loader_Filesystem
             return $this->cache[$logicalName];
         }
 
-//        $file = null;
-//        $previous = null;
-//        try {
-//            $file = parent::findTemplate($logicalName);
-//        } catch (\Twig_Error_Loader $e) {
-//            $twigLoaderException = $e;
-//
-//            // for BC
-//            try {
-                $template = $this->parser->parse($template);
-                $file = $this->locator->locate($template);
-//            } catch (\Exception $e) {
-//            }
-//        }
-
-//        if (false === $file || null === $file) {
-//            throw $twigLoaderException;
-//        }
+        $template = $this->parser->parse($template);
+        $file = $this->locator->locate($template);
 
         return $this->cache[$logicalName] = $file;
     }
