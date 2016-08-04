f2dcbf087c5c3650541152b1c79399a9c159914e
Łukasz <lchrusciel@gmail.com>
2016-08-04 10:12:41 +0200

    [Promotion] Decouple CouponFactory from repository

--- src/Sylius/Component/Promotion/Factory/CouponFactoryInterface.php
+++ src/Sylius/Component/Promotion/Factory/CouponFactoryInterface.php
@@ -12,6 +12,7 @@
 namespace Sylius\Component\Promotion\Factory;
 
 use Sylius\Component\Promotion\Model\CouponInterface;
+use Sylius\Component\Promotion\Model\PromotionInterface;
 use Sylius\Component\Resource\Factory\FactoryInterface;
 
 /**
@@ -20,11 +21,11 @@ use Sylius\Component\Resource\Factory\FactoryInterface;
 interface CouponFactoryInterface extends FactoryInterface
 {
     /**
-     * @param mixed $promotionId
+     * @param PromotionInterface $promotionId
      *
      * @return CouponInterface
      *
      * @throws \InvalidArgumentException
      */
-    public function createForPromotion($promotionId);
+    public function createForPromotion(PromotionInterface $promotionId);
 }
