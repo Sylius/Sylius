534e6f36a12491dd3c090063be76e3ac56be6a17
Mateusz Zalewski <zaleslaw@gmail.com>
2016-08-05 09:45:33 +0200

    [API][Zone] Create zone API tests

--- tests/Controller/ZoneApiTest.php
+++ tests/Controller/ZoneApiTest.php
@@ -22,10 +22,63 @@ class ZoneApiTest extends JsonApiTestCase
     /**
      * @var array
      */
+    private static $authorizedHeaderWithContentType = [
+        'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
+        'CONTENT_TYPE' => 'application/json',
+    ];
+
+    /**
+     * @var array
+     */
     private static $authorizedHeaderWithAccept = [
         'HTTP_Authorization' => 'Bearer SampleTokenNjZkNjY2MDEwMTAzMDkxMGE0OTlhYzU3NzYyMTE0ZGQ3ODcyMDAwM2EwMDZjNDI5NDlhMDdlMQ',
         'ACCEPT' => 'application/json',
     ];
+
+    public function testCreateZoneAccessDeniedResponse()
+    {
+        $this->client->request('POST', '/api/zones/country');
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'authentication/access_denied_response', Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function testCreateZoneValidationFailResponse()
+    {
+        $this->loadFixturesFromFile('authentication/api_administrator.yml');
+
+        $this->client->request('POST', '/api/zones/country', [], [], static::$authorizedHeaderWithContentType);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'zone/create_validation_fail_response', Response::HTTP_BAD_REQUEST);
+    }
+
+    public function testCreateZoneResponse()
+    {
+        $this->loadFixturesFromFile('authentication/api_administrator.yml');
+        $this->loadFixturesFromFile('resources/countries.yml');
+
+        $data =
+<<<EOT
+        {
+            "code": "EU",
+            "name": "European Union",
+            "members": [
+                {
+                    "code": "NL"
+                },
+                {
+                    "code": "BE"
+                }
+            ]
+        }
+EOT;
+
+        $this->client->request('POST', '/api/zones/country', [], [], static::$authorizedHeaderWithContentType, $data);
+
+        $response = $this->client->getResponse();
+        $this->assertResponse($response, 'zone/create_response', Response::HTTP_CREATED);
+    }
 
     public function testGetZonesListResponse()
     {
