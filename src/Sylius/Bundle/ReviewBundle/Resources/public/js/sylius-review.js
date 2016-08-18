aa2f700501012e70f695f6a773117f9efdc08912
Nicolas ADLER <nicolas.adler@openizi.com>
2016-08-18 17:14:07 +0200

    [Review][Product] Add reviews on product show page

--- src/Sylius/Bundle/ReviewBundle/Resources/public/js/sylius-review.js
+++ src/Sylius/Bundle/ReviewBundle/Resources/public/js/sylius-review.js
@@ -7,7 +7,7 @@
  * file that was distributed with this source code.
  */
 
-function initializeAjaxForm(successMessage, messageHolderSelector) {
+function initializeAjaxForm(successMessage, successHeader, messageHolderSelector) {
     $("#reviewForm").submit(function(e) {
         e.preventDefault();
         var form = $(this);
@@ -20,7 +20,7 @@ function initializeAjaxForm(successMessage, messageHolderSelector) {
             accept: "application/json",
             success: function(data, textStatus, xhr) {
                 completeRequest(form);
-                appendFlash(successMessage, messageHolderSelector);
+                appendFlash(successMessage, successHeader, messageHolderSelector);
             },
             error: function(xhr, textStatus, errorThrown) {
                 renderErrors(xhr, form);
@@ -32,14 +32,32 @@ function initializeAjaxForm(successMessage, messageHolderSelector) {
 function renderErrors(xhr, form) {
     clearErrors(form);
 
-    $.each(xhr.responseJSON.errors.errors, function(key, value) {
-        $('div.panel-body').addClass('has-error').prepend('<span class="help-block form-error">' + value + '</span>');
+    $.each(xhr.responseJSON.errors.children, function(key, value) {
+        var error = getError(value);
+        if (error != null) {
+            $('#reviewForm').addClass('error').prepend('<div class="ui error message"><p>' + error +'</p></div>');
+        }
     });
 }
 
+function getError(value)
+{
+    if ('errors' in value) {
+        return value.errors.join(', ');
+    } else if ('children' in value) {
+        var error = null;
+        for (child in value.children) {
+            error = getError(value.children[child]);
+        }
+        return error;
+    }
+
+    return null;
+}
+
 function clearErrors(form) {
-    form.find('.form-error').remove();
-    form.find('.has-error').removeClass('has-error');
+    form.find('.error.message').remove();
+    form.find('.error').removeClass('error');
 }
 
 function parseFormToJson(form) {
@@ -52,10 +70,20 @@ function parseFormToJson(form) {
     return formJson;
 }
 
-function appendFlash(successMessage, messageHolderSelector) {
-    messageHolderSelector = messageHolderSelector ? messageHolderSelector : '#flashes-container';
+function appendFlash(successMessage, successHeader, messageHolderSelector) {
+    messageHolderSelector = messageHolderSelector ? messageHolderSelector : '.flashes';
+    var html = '<div class="ui icon positive message">' +
+                    '<i class="close icon"></i>' +
+                    '<i class="checkmark" icon"></i>' +
+                    '<div class="content">' +
+                        '<div class="header">' +
+                            successHeader +
+                        '</div>' +
+                        '<p>' + successMessage + '</p>' +
+                    '</div>' +
+                '</div>';
 
-    $(messageHolderSelector).html('<div class="alert alert-success"><a class="close" data-dismiss="alert" href="#">×</a>' + successMessage + '</div>');
+    $(messageHolderSelector).html(html);
 }
 
 function completeRequest(form) {
