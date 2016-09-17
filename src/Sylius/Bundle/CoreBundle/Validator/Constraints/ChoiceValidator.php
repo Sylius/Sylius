5d7a593b40ac7ceca6dcf7f2439cb2b0a1ae4116
Patrick McDougle <pmcdougle@popsugar.com>
2016-09-17 10:54:23 -0700

    PR Fixups

--- src/Sylius/Bundle/CoreBundle/Validator/Constraints/ChoiceValidator.php
+++ src/Sylius/Bundle/CoreBundle/Validator/Constraints/ChoiceValidator.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 * This file is part of the Sylius package.
 *
@@ -13,6 +14,7 @@ namespace Sylius\Bundle\CoreBundle\Validator\Constraints;
 use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\Constraints\ChoiceValidator as BaseChoiceValidator;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Symfony\Component\Validator\Exception\ConstraintDefinitionException;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 
@@ -21,25 +23,23 @@ use Symfony\Component\Validator\Exception\UnexpectedTypeException;
 */
 class ChoiceValidator extends BaseChoiceValidator
 {
+    /**
+     * {@inheritdoc}
+     */
     public function validate($value, Constraint $constraint)
     {
         if (!$constraint instanceof Choice) {
-            throw new UnexpectedTypeException($constraint, __NAMESPACE__.'\Choice');
-        }
-
-        if (!is_array($constraint->choices) && !$constraint->callback) {
-            throw new ConstraintDefinitionException('Either "choices" or "callback" must be specified on constraint Choice');
+            throw new UnexpectedTypeException($constraint, Choice::class);
         }
 
         if (null === $value) {
             return;
         }
 
-        if ($constraint->callback) {
+        if ($this->context instanceof ExecutionContextInterface && $constraint->callback) {
             $object = $this->context->getObject();
-            if (isset($object)
-                && !is_callable($choices = array($object, $constraint->callback))
-                && !is_callable($choices = $constraint->callback)
+            if (!isset($object)
+                || !is_callable($choices = [$object, $constraint->callback])
             ) {
                 throw new ConstraintDefinitionException('The Choice constraint expects a valid callback');
             }
@@ -50,6 +50,7 @@ class ChoiceValidator extends BaseChoiceValidator
             $constraint->choices = $choices;
             $constraint->callback = null;
         }
+
         return parent::validate($value, $constraint);
     }
 }
