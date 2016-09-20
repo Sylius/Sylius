f7baf9e8b9121eaa6b1e36bcb4be7abedc384b8b
Mateusz Zalewski <zaleslaw@gmail.com>
2016-09-20 12:16:35 +0200

    [Core][Shop] Change product price on options change

---
+++ src/Sylius/Bundle/CoreBundle/Controller/ProductController.php
@@ -0,0 +1,106 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\CoreBundle\Controller;
+
+use FOS\RestBundle\View\View;
+use Sylius\Bundle\ResourceBundle\Controller\RequestConfiguration;
+use Sylius\Bundle\ResourceBundle\Controller\ResourceController;
+use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Component\Core\Model\ProductVariantInterface;
+use Sylius\Component\Resource\ResourceActions;
+use Sylius\Component\Variation\Model\OptionValueInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @author Mateusz Zalewski <mateusz.zalewski@lakion.com>
+ */
+class ProductController extends ResourceController
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function showAction(Request $request)
+    {
+        $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
+
+        $this->isGrantedOr403($configuration, ResourceActions::SHOW);
+        /** @var ProductInterface $resource */
+        $resource = $this->findOr404($configuration);
+
+        $this->eventDispatcher->dispatch(ResourceActions::SHOW, $configuration, $resource);
+
+        $view = View::create($resource);
+
+        if ($configuration->isHtmlRequest()) {
+            $view = $this->prepareHtmlRequestView($resource, $configuration, $view);
+        }
+
+        return $this->viewHandler->handle($configuration, $view);
+    }
+
+    /**
+     * @param ProductInterface $resource
+     * @param RequestConfiguration $configuration
+     * @param View $view
+     *
+     * @return View
+     */
+    private function prepareHtmlRequestView(
+        ProductInterface $resource,
+        RequestConfiguration $configuration,
+        View $view
+    ) {
+        $templateData = [
+            'configuration' => $configuration,
+            'metadata' => $this->metadata,
+            'resource' => $resource,
+            $this->metadata->getName() => $resource,
+        ];
+
+        if (!$resource->isSimple()) {
+            $templateData['variantsPrices'] = $this->getVariantsPrices($resource);
+        }
+
+        return $view
+            ->setTemplate($configuration->getTemplate(ResourceActions::SHOW . '.html'))
+            ->setTemplateVar($this->metadata->getName())
+            ->setData($templateData)
+        ;
+    }
+
+    /**
+     * @param ProductInterface $product
+     *
+     * @return array
+     */
+    private function getVariantsPrices(ProductInterface $product)
+    {
+        $variantsPrices = [];
+
+        if (ProductInterface::VARIANT_SELECTION_MATCH === $product->getVariantSelectionMethod()) {
+            /** @var ProductVariantInterface $variant */
+            foreach ($product->getVariants() as $variant) {
+                $optionMap = [];
+                /** @var OptionValueInterface $option */
+                foreach ($variant->getOptions() as $option) {
+                    $optionMap[$option->getOption()->getCode()] = $option->getValue();
+                }
+
+                $optionMap['value'] = $variant->getPrice();
+
+                $variantsPrices[] = $optionMap;
+            }
+        }
+
+        return $variantsPrices;
+    }
+}
