bacfac685576521c9e8e9d89ecbd7f01e4588707
Mateusz Zalewski <zaleslaw@gmail.com>
2016-10-03 15:10:21 +0200

    [Core] Increment promotions applied on order after its completion

---
+++ src/Sylius/Component/Core/Promotion/Modifier/OrderPromotionsUsageModifier.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Core\Promotion\Modifier;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Sylius\Component\Core\Model\OrderInterface;
+
+/**
+ * @author Mateusz Zalewski <mateusz.zalewski@lakion.com>
+ */
+final class OrderPromotionsUsageModifier implements OrderPromotionsUsageModifierInterface
+{
+    /**
+     * @var ObjectManager
+     */
+    private $promotionManager;
+
+    /**
+     * @param ObjectManager $promotionManager
+     */
+    public function __construct(ObjectManager $promotionManager)
+    {
+        $this->promotionManager = $promotionManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function increment(OrderInterface $order)
+    {
+        foreach ($order->getPromotions() as $promotion) {
+            $promotion->incrementUsed();
+        }
+
+        $this->promotionManager->flush();
+    }
+}
