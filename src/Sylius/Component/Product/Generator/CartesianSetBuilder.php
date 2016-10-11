006e0aa60a349547e80974def71f3e5a9a828f30
Kamil Kokot <kamil@kokot.me>
2016-10-11 15:33:50 +0200

    [Product] Improve CartesianSetBuilder

--- src/Sylius/Component/Product/Generator/CartesianSetBuilder.php
+++ src/Sylius/Component/Product/Generator/CartesianSetBuilder.php
@@ -11,68 +11,34 @@
 
 namespace Sylius\Component\Product\Generator;
 
+use Webmozart\Assert\Assert;
+
 /**
- * Builds the Cartesian product set from one or more given sets.
- *
  * @author Paweł Jędrzejewski <pawel@sylius.org>
  */
 final class CartesianSetBuilder
 {
     /**
-     * @param array $setTuples
-     *
-     * @return array
-     *
-     * @throws \InvalidArgumentException If the array is empty.
-     * @throws \InvalidArgumentException If the array does not contain arrays of set tuples.
-     */
-    public function build(array $setTuples)
-    {
-        return $this->doBuild($setTuples, false);
-    }
-
-    /**
-     * @param array $setTuples
-     * @param bool $isRecursiveStep
+     * @param array[] $set
      *
      * @return array
      */
-    private function doBuild(array $setTuples, $isRecursiveStep)
+    public function build(array $set)
     {
-        $countTuples = count($setTuples);
+        Assert::isArray($set);
+        Assert::allIsArray($set);
 
-        if (1 === $countTuples) {
-            return reset($setTuples);
+        if (!$set) {
+            return [[]];
         }
 
-        if (0 === $countTuples) {
-            throw new \InvalidArgumentException('The set builder requires a single array of one or more array sets.');
-        }
-
-        foreach ($setTuples as $tuple) {
-            if (!is_array($tuple)) {
-                throw new \InvalidArgumentException('The set builder requires a single array of one or more array sets.');
-            }
-        }
-
-        $keys = array_keys($setTuples);
-
-        $a = array_shift($setTuples);
-        $k = array_shift($keys);
-
-        $b = $this->doBuild($setTuples, true);
+        $subset = (array) array_shift($set);
+        $cartesianSubset = $this->build($set);
 
         $result = [];
-
-        foreach ($a as $valueA) {
-            if ($valueA) {
-                foreach ($b as $valueB) {
-                    if ($isRecursiveStep) {
-                        $result[] = array_merge([$valueA], (array) $valueB);
-                    } else {
-                        $result[] = [$k => $valueA] + array_combine($keys, (array) $valueB);
-                    }
-                }
+        foreach ($subset as $value) {
+            foreach ($cartesianSubset as $cartesianValue) {
+                $result[] = array_merge([$value], $cartesianValue);
             }
         }
 
