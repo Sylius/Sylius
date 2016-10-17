3ad64076ec468ecb534a857b48459ffa224ba264
Grzegorz Sadowski <sadowskigp@gmail.com>
2016-10-17 15:48:51 +0200

    [Admin][ProductReview] Implement scenario for browsing product reviews

--- src/Sylius/Behat/Context/Setup/ProductReviewContext.php
+++ src/Sylius/Behat/Context/Setup/ProductReviewContext.php
@@ -13,9 +13,9 @@ namespace Sylius\Behat\Context\Setup;
 
 use Behat\Behat\Context\Context;
 use SM\Factory\FactoryInterface as StateMachineFactoryInterface;
-use Sylius\Behat\Service\SharedStorageInterface;
 use Sylius\Component\Core\Model\CustomerInterface;
 use Sylius\Component\Core\Model\ProductInterface;
+use Sylius\Behat\Service\SharedStorageInterface;
 use Sylius\Component\Core\ProductReviewTransitions;
 use Sylius\Component\Resource\Factory\FactoryInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
