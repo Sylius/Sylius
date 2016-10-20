235e714cdb7f66f163d1268c45d7852131bd8f7f
Kamil Kokot <kamil@kokot.me>
2016-10-20 17:09:02 +0200

    Improve discount rendering

--- src/Sylius/Bundle/MoneyBundle/Formatter/MoneyFormatter.php
+++ src/Sylius/Bundle/MoneyBundle/Formatter/MoneyFormatter.php
@@ -25,13 +25,13 @@ final class MoneyFormatter implements MoneyFormatterInterface
     {
         $formatter = new \NumberFormatter($locale, \NumberFormatter::CURRENCY);
 
-        $result = $formatter->formatCurrency($amount / 100, $currency);
+        $result = $formatter->formatCurrency(abs($amount / 100), $currency);
         Assert::notSame(
             false,
             $result,
             sprintf('The amount "%s" of type %s cannot be formatted to currency "%s".', $amount, gettype($amount), $currency)
         );
 
-        return $result;
+        return $amount >= 0 ? $result : '-' . $result;
     }
 }
--- src/Sylius/Bundle/MoneyBundle/spec/Formatter/MoneyFormatterSpec.php
+++ src/Sylius/Bundle/MoneyBundle/spec/Formatter/MoneyFormatterSpec.php
@@ -32,15 +32,38 @@ final class MoneyFormatterSpec extends ObjectBehavior
         $this->shouldImplement(MoneyFormatterInterface::class);
     }
 
-    function it_formats_money_using_given_currency_and_locale()
+    function it_formats_positive_money_using_given_currency_and_locale()
     {
         $this->format(15, 'USD', 'en')->shouldReturn('$0.15');
         $this->format(2500, 'USD', 'en')->shouldReturn('$25.00');
         $this->format(312, 'EUR', 'en')->shouldReturn('€3.12');
     }
 
-    function it_formats_money_using_default_locale_if_not_given()
+    function it_formats_positive_money_using_default_locale_if_not_given()
     {
         $this->format(500, 'USD')->shouldReturn('$5.00');
+    }
+
+    function it_formats_negative_money_using_given_currency_and_locale()
+    {
+        $this->format(-15, 'USD', 'en')->shouldReturn('-$0.15');
+        $this->format(-2500, 'USD', 'en')->shouldReturn('-$25.00');
+        $this->format(-312, 'EUR', 'en')->shouldReturn('-€3.12');
+    }
+
+    function it_formats_negative_money_using_default_locale_if_not_given()
+    {
+        $this->format(-500, 'USD')->shouldReturn('-$5.00');
+    }
+
+    function it_formats_zero_money_using_given_currency_and_locale()
+    {
+        $this->format(0, 'USD', 'en')->shouldReturn('$0.00');
+        $this->format(0, 'EUR', 'en')->shouldReturn('€0.00');
+    }
+
+    function it_formats_zero_money_using_default_locale_if_not_given()
+    {
+        $this->format(0, 'USD')->shouldReturn('$0.00');
     }
 }
