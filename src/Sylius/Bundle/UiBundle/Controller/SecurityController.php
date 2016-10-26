9f65e533500197ba748a22be8daeed80bc778fe2
Joeri Timmermans <joeri.timmermans@intracto.com>
2016-10-26 13:33:57 +0200

    [UiBundle][UserBundle] #3448 prevent logged in users from visiting login

--- src/Sylius/Bundle/UiBundle/Controller/SecurityController.php
+++ src/Sylius/Bundle/UiBundle/Controller/SecurityController.php
@@ -11,10 +11,14 @@
 
 namespace Sylius\Bundle\UiBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 /**
@@ -22,6 +26,11 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
  */
 final class SecurityController
 {
+    private static $DEFAULT_TARGET_PATH = [
+        'admin' => 'sylius_admin_dashboard',
+        'shop' => 'sylius_shop_homepage',
+    ];
+
     /**
      * @var AuthenticationUtils
      */
@@ -38,18 +47,42 @@ final class SecurityController
     private $templatingEngine;
 
     /**
+     * @var AuthorizationChecker
+     */
+    private $authorizationChecker;
+
+    /**
+     * @var TokenStorage
+     */
+    private $tokenStorage;
+
+    /**
+     * @var Router
+     */
+    private $router;
+
+    /**
      * @param AuthenticationUtils $authenticationUtils
      * @param FormFactoryInterface $formFactory
      * @param EngineInterface $templatingEngine
+     * @param AuthorizationChecker $authorizationChecker
+     * @param TokenStorage $tokenStorage
+     * @param Router $router
      */
     public function __construct(
         AuthenticationUtils $authenticationUtils,
         FormFactoryInterface $formFactory,
-        EngineInterface $templatingEngine
+        EngineInterface $templatingEngine,
+        AuthorizationChecker $authorizationChecker,
+        TokenStorage $tokenStorage,
+        Router $router
     ) {
         $this->authenticationUtils = $authenticationUtils;
         $this->formFactory = $formFactory;
         $this->templatingEngine = $templatingEngine;
+        $this->authorizationChecker = $authorizationChecker;
+        $this->tokenStorage = $tokenStorage;
+        $this->router = $router;
     }
 
     /**
@@ -59,6 +92,10 @@ final class SecurityController
      */
     public function loginAction(Request $request)
     {
+        if ($redirect = $this->redirectIfLoggedIn()) {
+            return $redirect;
+        }
+
         $lastError = $this->authenticationUtils->getLastAuthenticationError();
         $lastUsername = $this->authenticationUtils->getLastUsername();
 
@@ -87,5 +124,28 @@ final class SecurityController
     public function logoutAction(Request $request)
     {
         throw new \RuntimeException('You must configure the logout path to be handled by the firewall.');
+    }
+
+    /**
+     * This will prevent users from going to the login page when they are already logged in.
+     *
+     * @return false|RedirectResponse
+     */
+    private function redirectIfLoggedIn()
+    {
+        if (!$this->authorizationChecker->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            return false;
+        }
+
+        $token = $this->tokenStorage->getToken();
+        $providerKey = $token->getProviderKey();
+
+        if (!array_key_exists($providerKey, self::$DEFAULT_TARGET_PATH)) {
+            return false;
+        }
+
+        $route = self::$DEFAULT_TARGET_PATH[$providerKey];
+
+        return new RedirectResponse($this->router->generate($route));
     }
 }
