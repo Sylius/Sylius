2ae3b013d521873a7a77a1ff2aa4962d6189cda9
tuka217 <a.walasek@gazeta.pl>
2016-11-02 14:43:03 +0100

    [Core] Create ProductTaxon, add collection of ProductTaxon to Product, remove collection of taxons from product

---
+++ src/Sylius/Component/Core/Model/ProductTaxon.php
@@ -0,0 +1,94 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Core\Model;
+
+/**
+ * @author Anna Walasek <anna.walasek@lakion.com>
+ */
+class ProductTaxon implements ProductTaxonInterface
+{
+    /**
+     * @var mixed
+     */
+    protected $id;
+    
+    /**
+     * @var ProductInterface
+     */
+    protected $product;
+
+    /**
+     * @var TaxonInterface
+     */
+    protected $taxon;
+
+    /**
+     * @var int
+     */
+    protected $position;
+ 
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setProduct(ProductInterface $product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTaxon()
+    {
+        return $this->taxon;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setTaxon(TaxonInterface $taxon)
+    {
+        $this->taxon = $taxon;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+    }
+}
