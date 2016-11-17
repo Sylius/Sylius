290d7668599057b988f1b4586fd448351103e621
Łukasz <lchrusciel@gmail.com>
2016-11-17 22:37:54 +0100

    [Product] Custom validator for code

---
+++ src/Sylius/Bundle/ProductBundle/spec/Validator/Constraint/UniqueSimpleProductCodeSpec.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Sylius\Bundle\ProductBundle\Validator\Constraint;
+
+use PhpSpec\ObjectBehavior;
+use Sylius\Bundle\ProductBundle\Validator\Constraint\UniqueSimpleProductCode;
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @author Łukasz Chruściel <lukasz.chrusciel@lakion.org>
+ */
+final class UniqueSimpleProductCodeSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(UniqueSimpleProductCode::class);
+    }
+
+    function it_is_a_validation_constraint()
+    {
+        $this->shouldHaveType(Constraint::class);
+    }
+
+    function it_is_class_constraint()
+    {
+        $this->getTargets()->shouldReturn(Constraint::CLASS_CONSTRAINT);
+    }
+}
