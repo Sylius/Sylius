138715861e244f97f87807a57da33993fc7f9261
Łukasz <lchrusciel@gmail.com>
2016-11-18 13:41:05 +0100

    [Order] Remove exchange rate

--- src/Sylius/Behat/Context/Setup/OrderContext.php
+++ src/Sylius/Behat/Context/Setup/OrderContext.php
@@ -573,7 +573,6 @@ final class OrderContext implements Context
         $currency = $this->currencyRepository->findOneBy(['code' => $currencyCode]);
 
         $order->setCurrencyCode($currency->getCode());
-        $order->setExchangeRate($currency->getExchangeRate());
 
         return $order;
     }
