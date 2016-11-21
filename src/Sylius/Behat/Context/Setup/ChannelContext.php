1d53b0be0695ca0c4170ee7c3792da46875bbe5b
Mateusz Zalewski <zaleslaw@gmail.com>
2016-11-21 14:33:48 +0100

    [Behat] Merge base currency step with channel creation step

--- src/Sylius/Behat/Context/Setup/ChannelContext.php
+++ src/Sylius/Behat/Context/Setup/ChannelContext.php
@@ -114,7 +114,11 @@ final class ChannelContext implements Context
 
     /**
      * @Given the store operates on a single channel
+<<<<<<< HEAD
      * @Given the store operates on a single channel in :currencyCode currency
+=======
+     * @Given the store operates on a single channel in currency :currencyCode
+>>>>>>> [Behat] Merge base currency step with channel creation step
      */
     public function storeOperatesOnASingleChannel($currencyCode = null)
     {
