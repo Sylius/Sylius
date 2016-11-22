7d687a14c9a932a86093680265980cc241cb2f7c
Mateusz Zalewski <zaleslaw@gmail.com>
2016-11-22 23:54:09 +0100

    [Core] Improve fixtures and implement calculator for price per channel

---
+++ src/Sylius/Component/Core/Calculator/ProductVariantPriceCalculator.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Core\Calculator;
+
+use Sylius\Component\Core\Model\ChannelInterface;
+use Sylius\Component\Core\Model\ProductVariantInterface;
+use Webmozart\Assert\Assert;
+
+/**
+ * @author Mateusz Zalewski <mateusz.zalewski@lakion.com>
+ */
+final class ProductVariantPriceCalculator implements ProductVariantPriceCalculatorInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function calculate(ProductVariantInterface $productVariant, ChannelInterface $channel)
+    {
+        $channelPricing = $productVariant->getChannelPricingForChannel($channel);
+        Assert::notNull($channelPricing);
+
+        return $channelPricing->getPrice();
+    }
+}
