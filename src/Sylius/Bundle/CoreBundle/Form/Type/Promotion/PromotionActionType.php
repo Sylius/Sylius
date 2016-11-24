d24c6a851868e8e367b9b866445473b7c8a27f2e
Mateusz Zalewski <zaleslaw@gmail.com>
2016-11-24 15:50:22 +0100

    [Core] Render action configuration per channel

---
+++ src/Sylius/Bundle/CoreBundle/Form/Type/Promotion/PromotionActionType.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\CoreBundle\Form\Type\Promotion;
+
+use Sylius\Bundle\CoreBundle\Form\EventSubscriber\BuildChannelAwarePromotionActionFormSubscriber;
+use Sylius\Bundle\PromotionBundle\Form\Type\Core\AbstractConfigurationType;
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
+use Sylius\Component\Registry\ServiceRegistryInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * {@inheritdoc}
+ */
+class PromotionActionType extends AbstractConfigurationType
+{
+    /**
+     * @var ChannelRepositoryInterface
+     */
+    private $channelRepository;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param ChannelRepositoryInterface $channelRepository
+     */
+    public function __construct($dataClass, ServiceRegistryInterface $registry, ChannelRepositoryInterface $channelRepository)
+    {
+        parent::__construct($dataClass, $registry);
+
+        $this->channelRepository = $channelRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options = [])
+    {
+        $builder
+            ->add('type', 'sylius_promotion_action_choice', [
+                'label' => 'sylius.form.promotion_action.type',
+                'attr' => [
+                    'data-form-collection' => 'update',
+                ],
+            ])
+            ->addEventSubscriber(
+                new BuildChannelAwarePromotionActionFormSubscriber(
+                    $this->registry,
+                    $builder->getFormFactory(),
+                    (isset($options['configuration_type'])) ? $options['configuration_type'] : null,
+                    $this->channelRepository
+                )
+            )
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'sylius_promotion_action';
+    }
+}
