8ac89569a846a3527d7cdd84c1c56f0689522bdc
Łukasz <lchrusciel@gmail.com>
2016-11-24 14:50:42 +0100

    [Core] Remove currency exchange rate from promotion action

--- src/Sylius/Component/Core/Promotion/Action/UnitFixedDiscountPromotionActionCommand.php
+++ src/Sylius/Component/Core/Promotion/Action/UnitFixedDiscountPromotionActionCommand.php
@@ -45,30 +45,22 @@ final class UnitFixedDiscountPromotionActionCommand extends UnitDiscountPromotio
     private $productFilter;
 
     /**
-     * @var CurrencyConverterInterface
-     */
-    private $currencyConverter;
-
-    /**
      * @param FactoryInterface $adjustmentFactory
      * @param FilterInterface $priceRangeFilter
      * @param FilterInterface $taxonFilter
      * @param FilterInterface $productFilter
-     * @param CurrencyConverterInterface $currencyConverter
      */
     public function __construct(
         FactoryInterface $adjustmentFactory,
         FilterInterface $priceRangeFilter,
         FilterInterface $taxonFilter,
-        FilterInterface $productFilter,
-        CurrencyConverterInterface $currencyConverter
+        FilterInterface $productFilter
     ) {
         parent::__construct($adjustmentFactory);
 
         $this->priceRangeFilter = $priceRangeFilter;
         $this->taxonFilter = $taxonFilter;
         $this->productFilter = $productFilter;
-        $this->currencyConverter = $currencyConverter;
     }
 
     /**
@@ -133,9 +125,6 @@ final class UnitFixedDiscountPromotionActionCommand extends UnitDiscountPromotio
             return $configuration['base_amount'];
         }
 
-        return $this->currencyConverter->convertToBase(
-            $configuration['amounts'][$currencyCode],
-            $currencyCode
-        );
+        return $configuration['amounts'][$currencyCode];
     }
 }
