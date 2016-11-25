9caf2fc9c838daf864ab3dea6fee4f20379c8a6b
Mateusz Zalewski <zaleslaw@gmail.com>
2016-11-25 12:02:51 +0100

    [Core][Behat] Take into account channel in fixed promotions

--- src/Sylius/Component/Core/Promotion/Action/UnitFixedDiscountPromotionActionCommand.php
+++ src/Sylius/Component/Core/Promotion/Action/UnitFixedDiscountPromotionActionCommand.php
@@ -71,7 +71,13 @@ final class UnitFixedDiscountPromotionActionCommand extends UnitDiscountPromotio
             throw new UnexpectedTypeException($subject, OrderInterface::class);
         }
 
-        $amount = $this->getAmountByCurrencyCode($configuration, $subject->getCurrencyCode());
+        $channelCode = $subject->getChannel()->getCode();
+        if (!isset($configuration[$channelCode])) {
+            return;
+        }
+
+        $amount = $configuration[$channelCode]['amount'];
+
         if (0 === $amount) {
             return;
         }
@@ -110,20 +116,5 @@ final class UnitFixedDiscountPromotionActionCommand extends UnitDiscountPromotio
                 $promotion
             );
         }
-    }
-
-    /**
-     * @param array $configuration
-     * @param string $currencyCode
-     *
-     * @return int
-     */
-    private function getAmountByCurrencyCode(array $configuration, $currencyCode)
-    {
-        if (!isset($configuration['amounts'][$currencyCode])) {
-            return $configuration['base_amount'];
-        }
-
-        return $configuration['amounts'][$currencyCode];
     }
 }
