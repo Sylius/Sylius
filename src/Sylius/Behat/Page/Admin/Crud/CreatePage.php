ce53ca204b87f58580ce4b9a9a15f33816336916
Kamil Kokot <kamil@kokot.me>
2016-12-08 16:47:05 +0100

    [Behat] Use full route names

--- src/Sylius/Behat/Page/Admin/Crud/CreatePage.php
+++ src/Sylius/Behat/Page/Admin/Crud/CreatePage.php
@@ -24,19 +24,19 @@ class CreatePage extends SymfonyPage implements CreatePageInterface
     /**
      * @var string
      */
-    private $resourceName;
+    private $routeName;
 
     /**
      * @param Session $session
      * @param array $parameters
      * @param RouterInterface $router
-     * @param string $resourceName
+     * @param string $routeName
      */
-    public function __construct(Session $session, array $parameters, RouterInterface $router, $resourceName)
+    public function __construct(Session $session, array $parameters, RouterInterface $router, $routeName)
     {
         parent::__construct($session, $parameters, $router);
 
-        $this->resourceName = strtolower($resourceName);
+        $this->routeName = $routeName;
     }
 
     /**
@@ -70,7 +70,7 @@ class CreatePage extends SymfonyPage implements CreatePageInterface
      */
     public function getRouteName()
     {
-        return sprintf('sylius_admin_%s_create', $this->resourceName);
+        return $this->routeName;
     }
 
     /**
--- src/Sylius/Behat/Page/Admin/Crud/IndexPage.php
+++ src/Sylius/Behat/Page/Admin/Crud/IndexPage.php
@@ -31,26 +31,26 @@ class IndexPage extends SymfonyPage implements IndexPageInterface
     /**
      * @var string
      */
-    private $resourceName;
+    private $routeName;
 
     /**
      * @param Session $session
      * @param array $parameters
      * @param RouterInterface $router
      * @param TableAccessorInterface $tableAccessor
-     * @param string $resourceName
+     * @param string $routeName
      */
     public function __construct(
         Session $session,
         array $parameters,
         RouterInterface $router,
         TableAccessorInterface $tableAccessor,
-        $resourceName
+        $routeName
     ) {
         parent::__construct($session, $parameters, $router);
 
         $this->tableAccessor = $tableAccessor;
-        $this->resourceName = strtolower($resourceName);
+        $this->routeName = $routeName;
     }
 
     /**
@@ -144,7 +144,7 @@ class IndexPage extends SymfonyPage implements IndexPageInterface
      */
     public function getRouteName()
     {
-        return sprintf('sylius_admin_%s_index', $this->resourceName);
+        return $this->routeName;
     }
 
     /**
