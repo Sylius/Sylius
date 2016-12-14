fd0c246028360daff95bd305a743ac6ddae501a6
Jan Goralski <jan.wojciech.goralski@gmail.com>
2016-12-14 12:01:42 +0100

    [GridBundle][AdminBundle] Added MoneyFilterType

---
+++ src/Sylius/Bundle/GridBundle/Form/Type/Filter/MoneyFilterType.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\GridBundle\Form\Type\Filter;
+
+use Sylius\Bundle\CurrencyBundle\Form\Type\CurrencyChoiceType;
+use Sylius\Component\Grid\Filter\MoneyFilter;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @author Jan Góralski <jan.goralski@lakion.com>
+ */
+final class MoneyFilterType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('greaterThan', NumberType::class, [
+                'label' => 'sylius.ui.greater_than',
+                'required' => false,
+                'scale' => $options['scale'],
+            ])
+            ->add('lessThan', NumberType::class, [
+                'label' => 'sylius.ui.less_than',
+                'required' => false,
+                'scale' => $options['scale'],
+            ])
+            ->add('currency', CurrencyChoiceType::class, [
+                'label' => 'sylius.ui.currency',
+                'placeholder' => '---',
+                'required' => false,
+            ])
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver
+            ->setDefaults([
+                'data_class' => null,
+                'scale' => MoneyFilter::DEFAULT_SCALE,
+            ])
+            ->setAllowedTypes('scale', ['string', 'int'])
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'sylius_grid_filter_money';
+    }
+}
