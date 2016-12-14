6dfb47f601f5a5771b088a25eb84b14ac75f0776
Grzegorz Sadowski <sadowskigp@gmail.com>
2016-12-14 10:32:21 +0100

    [Admin][Inventory] Add inventory index page

---
+++ src/Sylius/Bundle/CoreBundle/Doctrine/ORM/ProductVariantRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\CoreBundle\Doctrine\ORM;
+
+use Sylius\Bundle\ProductBundle\Doctrine\ORM\ProductVariantRepository as BaseProductVariantRepository;
+use Sylius\Component\Core\Repository\ProductVariantRepositoryInterface;
+
+/**
+ * @author Grzegorz Sadowski <grzegorz.sadowski@lakion.com>
+ */
+class ProductVariantRepository extends BaseProductVariantRepository implements ProductVariantRepositoryInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function createQueryBuilderByTracked($tracked = true)
+    {
+        return $this
+            ->createQueryBuilder('o')
+            ->where('o.tracked = :tracked')
+            ->setParameter('tracked', $tracked)
+        ;
+    }
+}
