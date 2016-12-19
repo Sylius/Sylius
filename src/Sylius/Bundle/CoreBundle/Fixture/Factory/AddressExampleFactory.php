b7964df6a24b9c5b855b30101b8a8fe3edda99f8
Łukasz <lchrusciel@gmail.com>
2016-12-19 16:20:42 +0100

    [Fixtures] Assign address province based on name even if country has it defined

--- src/Sylius/Bundle/CoreBundle/Fixture/Factory/AddressExampleFactory.php
+++ src/Sylius/Bundle/CoreBundle/Fixture/Factory/AddressExampleFactory.php
@@ -11,10 +11,10 @@
 
 namespace Sylius\Bundle\CoreBundle\Fixture\Factory;
 
-use Sylius\Bundle\CoreBundle\Fixture\OptionsResolver\LazyOption;
+use Doctrine\Common\Collections\Collection;
+use Sylius\Component\Addressing\Model\Country;
 use Sylius\Component\Addressing\Model\ProvinceInterface;
 use Sylius\Component\Core\Model\AddressInterface;
-use Sylius\Component\Core\Model\CustomerInterface;
 use Sylius\Component\Resource\Factory\FactoryInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Symfony\Component\OptionsResolver\Options;
@@ -24,7 +24,7 @@ use Webmozart\Assert\Assert;
 /**
  * @author Jan Góralski <jan.goralski@lakion.com>
  */
-class AddressExampleFactory extends AbstractExampleFactory implements ExampleFactoryInterface
+class AddressExampleFactory extends AbstractExampleFactory
 {
     /**
      * @var FactoryInterface
@@ -135,13 +135,7 @@ class AddressExampleFactory extends AbstractExampleFactory implements ExampleFac
         $this->assertCountryCodeIsValid($options['country_code']);
         $address->setCountryCode($options['country_code']);
 
-        if (null !== $options['province_code']) {
-            $this->assertProvinceCodeIsValid($options['province_code'], $options['country_code']);
-            $address->setProvinceCode($options['province_code']);
-        } else {
-            $this->assertCountryHasNoProvinces($options['country_code']);
-            $address->setProvinceName($options['province_name']);
-        }
+        $this->resolveCountryProvince($options, $address);
 
         $options['customer']->addAddress($address);
 
@@ -175,11 +169,58 @@ class AddressExampleFactory extends AbstractExampleFactory implements ExampleFac
     }
 
     /**
-     * @param string $countryCode
+     * @param array $options
+     * @param AddressInterface $address
+     *
+     * @return string
      */
-    private function assertCountryHasNoProvinces($countryCode)
+    private function provideProvince(array $options, AddressInterface $address)
     {
-        $country = $this->countryRepository->findOneBy(['code' => $countryCode]);
-        Assert::allIsEmpty($country->getProvinces(), 'Country should not have any provinces if you want to specify province name');
+        /** @var Country $country */
+        $country = $this->countryRepository->findOneBy(['code' => $options['country_code']]);
+
+        if ($country->hasProvinces()) {
+            $address->setProvinceCode($this->getProvinceCode($country->getProvinces(), $options['province_name']));
+
+            return;
+        }
+
+        $address->setProvinceName($options['province_name']);
+    }
+
+    /**
+     * @param Collection $provinces
+     * @param string $provinceName
+     *
+     * @return string
+     */
+    private function getProvinceCode(Collection $provinces, $provinceName)
+    {
+        /** @var ProvinceInterface $province */
+        foreach ($provinces as $province) {
+            if ($province->getName() === $provinceName) {
+                return $province->getCode();
+            }
+        }
+
+        throw new \InvalidArgumentException(sprintf('Country has defined provinces, but %s is not one of them', $provinceName));
+    }
+
+    /**
+     * @param array $options
+     * @param AddressInterface $address
+     */
+    private function resolveCountryProvince(array $options, AddressInterface $address)
+    {
+        if (null !== $options['province_code']) {
+            $this->assertProvinceCodeIsValid($options['province_code'], $options['country_code']);
+            $address->setProvinceCode($options['province_code']);
+
+            return;
+        }
+
+        if (null !== $options['province_name']){
+            $this->provideProvince($options, $address);
+        }
     }
 }
