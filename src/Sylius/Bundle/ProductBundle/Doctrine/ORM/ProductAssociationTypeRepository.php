cf54366cf1991de5a1ec2851e423d07242e536f4
Grzegorz Sadowski <sadowskigp@gmail.com>
2016-12-20 15:23:20 +0100

    [ProductAssociationType] Add product association type repository

---
+++ src/Sylius/Bundle/ProductBundle/Doctrine/ORM/ProductAssociationTypeRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\ProductBundle\Doctrine\ORM;
+
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+use Sylius\Component\Product\Repository\ProductAssociationTypeRepositoryInterface;
+
+/**
+ * @author Grzegorz Sadowski <grzegorz.sadowski@lakion.com>
+ */
+class ProductAssociationTypeRepository extends EntityRepository implements ProductAssociationTypeRepositoryInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function createListQueryBuilder($locale)
+    {
+        return $this->createQueryBuilder('o')
+            ->leftJoin('o.translations', 'translation')
+            ->andWhere('translation.locale = :locale')
+            ->setParameter('locale', $locale)
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findByName($name, $locale)
+    {
+        return $this->createQueryBuilder('o')
+            ->leftJoin('o.translations', 'translation')
+            ->andWhere('translation.name = :name')
+            ->andWhere('translation.locale = :locale')
+            ->setParameter('name', $name)
+            ->setParameter('locale', $locale)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+}
