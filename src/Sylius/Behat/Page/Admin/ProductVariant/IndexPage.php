93199e710667693e0431fdf5b40dd41d4915a8ab
Grzegorz Sadowski <sadowskigp@gmail.com>
2016-12-22 16:35:02 +0100

    [Behat][Admin][ProductVariant] Implement scenarios for sorting variants by position

--- src/Sylius/Behat/Page/Admin/ProductVariant/IndexPage.php
+++ src/Sylius/Behat/Page/Admin/ProductVariant/IndexPage.php
@@ -11,9 +11,11 @@
 
 namespace Sylius\Behat\Page\Admin\ProductVariant;
 
+use Behat\Mink\Element\NodeElement;
 use Behat\Mink\Exception\ElementNotFoundException;
 use Sylius\Behat\Page\Admin\Crud\IndexPage as BaseIndexPage;
 use Sylius\Component\Core\Model\ProductVariantInterface;
+use Webmozart\Assert\Assert;
 
 /**
  * @author Arkadiusz Krakowiak <arkadiusz.krakowiak@lakion.com>
@@ -43,11 +45,36 @@ final class IndexPage extends BaseIndexPage implements IndexPageInterface
     /**
      * {@inheritdoc}
      */
+    public function setPosition($name, $position)
+    {
+        /** @var NodeElement $productVariantsRow */
+        $productVariantsRow = $this->getElement('table')->find('css', sprintf('tbody > tr:contains("%s")', $name));
+        Assert::notNull($productVariantsRow, 'There are no row with given product variant\'s name!');
+
+        $productVariantPosition = $productVariantsRow->find('css', '.sylius-product-variant-position');
+        Assert::notNull($productVariantPosition, 'There are no position field in given row!');
+        $productVariantPosition->setValue($position);
+    }
+
+    public function savePositions()
+    {
+        $this->getElement('save_configuration_button')->press();
+
+        $this->getDocument()->waitFor(5, function () {
+            return false === $this->getElement('save_configuration_button')->hasClass('loading');
+        });
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
     public function getDefinedElements()
     {
         return array_merge(parent::getDefinedElements(), [
             'on_hand_quantity' => '.onHand[data-product-variant-id="%id%"]',
             'on_hold_quantity' => '.onHold[data-product-variant-id="%id%"]',
+            'save_configuration_button' => '.sylius-save-position',
         ]);
     }
 }
