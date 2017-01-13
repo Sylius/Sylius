86a656d62466d5edcf2b559e3b33ef2fdadda573
Łukasz <lchrusciel@gmail.com>
2017-01-13 15:21:43 +0100

    [Review] Register recalculator dynamically

--- src/Sylius/Bundle/ReviewBundle/DependencyInjection/SyliusReviewExtension.php
+++ src/Sylius/Bundle/ReviewBundle/DependencyInjection/SyliusReviewExtension.php
@@ -12,9 +12,13 @@
 namespace Sylius\Bundle\ReviewBundle\DependencyInjection;
 
 use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
+use Sylius\Bundle\ReviewBundle\EventListener\ReviewChangeListener;
+use Sylius\Bundle\ReviewBundle\Updater\AverageRatingUpdater;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * @author Mateusz Zalewski <mateusz.zalewski@lakion.com>
@@ -42,6 +46,8 @@ final class SyliusReviewExtension extends AbstractResourceExtension
     {
         $container->setParameter('sylius.review.subjects', $resources);
 
+        $this->createReviewListeners(array_keys($resources), $container);
+
         $resolvedResources = [];
         foreach ($resources as $subjectName => $subjectConfig) {
             foreach ($subjectConfig as $resourceName => $resourceConfig) {
@@ -52,5 +58,35 @@ final class SyliusReviewExtension extends AbstractResourceExtension
         }
 
         return $resolvedResources;
+    }
+
+    /**
+     * @param array $reviewSubjects
+     * @param ContainerBuilder $container
+     */
+    private function createReviewListeners(array $reviewSubjects, ContainerBuilder $container)
+    {
+        foreach ($reviewSubjects as $reviewSubject) {
+            $reviewChangeListener = new Definition(ReviewChangeListener::class, [
+                new Reference(sprintf('sylius.%s_review.average_rating_updater', $reviewSubject)),
+            ]);
+
+            $reviewChangeListener->addTag('kernel.event_listener', [
+                'event' => sprintf('sylius.%s_review.post_update', $reviewSubject),
+                'method' => 'recalculateSubjectRating',
+            ]);
+            $reviewChangeListener->addTag('kernel.event_listener', [
+                'event' => sprintf('sylius.%s_review.post_delete', $reviewSubject),
+                'method' => 'recalculateSubjectRating',
+            ]);
+
+            $container->addDefinitions([
+                sprintf('sylius.%s_review.average_rating_updater', $reviewSubject) => new Definition(AverageRatingUpdater::class, [
+                    new Reference('sylius.average_rating_calculator'),
+                    new Reference(sprintf('sylius.manager.%s_review', $reviewSubject)),
+                ]),
+                sprintf('sylius.listener.%s_review_change', $reviewSubject) => $reviewChangeListener,
+            ]);
+        }
     }
 }
