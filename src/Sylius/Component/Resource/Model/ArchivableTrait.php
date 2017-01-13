9840a74ed745154db5ac813ab89dc8387ec67d81
Jan Goralski <jan.wojciech.goralski@gmail.com>
2017-01-13 13:47:43 +0100

    [Grid][Admin][Resource] Added archiving functionality

---
+++ src/Sylius/Component/Resource/Model/ArchivableTrait.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Resource\Model;
+
+/**
+ * @see ArchivableInterface
+ *
+ * @author Jan Góralski <jan.goralski@lakion.com>
+ */
+trait ArchivableTrait
+{
+    /**
+     * @var \DateTime|null
+     */
+    protected $archivedAt;
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getArchivedAt()
+    {
+        return $this->archivedAt;
+    }
+
+    /**
+     * @param \DateTime|null $archivedAt
+     */
+    public function setArchivedAt(\DateTime $archivedAt = null)
+    {
+        $this->archivedAt = $archivedAt;
+    }
+}
