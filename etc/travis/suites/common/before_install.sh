f757ee8f36d702e15593ac660e552cce2fd17826
Kamil Kokot <kamil@kokot.me>
2017-01-25 14:47:17 +0100

    [Travis] Remove unnecessary commands

--- etc/travis/suites/common/before_install.sh
+++ etc/travis/suites/common/before_install.sh
@@ -7,7 +7,3 @@ run_command "git fetch origin master:refs/remotes/origin/master" || exit $? # Ma
 run_command "phpenv config-rm xdebug.ini" # Disable XDebug
 run_command "echo \"memory_limit=4096M\" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini" || exit $? # Increase memory limit to 4GB
 run_command "mkdir -p \"${SYLIUS_CACHE_DIR}\"" || exit $? # Create Sylius cache directory
-
-print_header "Preparing Composer" "Sylius"
-run_command "composer config -g github-oauth.github.com f89d08114c80d1a1b019b09ce60a38f75a5c9480" || exit $? # Please do not use this key outside the Sylius project
-run_command "composer self-update" || exit $?
