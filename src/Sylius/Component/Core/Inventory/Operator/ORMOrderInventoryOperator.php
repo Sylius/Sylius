ea563b804ec4e0f73bb43a19f784de2f14ef7e06
Grzegorz Sadowski <sadowskigp@gmail.com>
2017-01-30 15:32:15 +0100

    [Inventory] Decorate order inventory operator

---
+++ src/Sylius/Component/Core/Inventory/Operator/ORMOrderInventoryOperator.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Core\Inventory\Operator;
+
+use Doctrine\DBAL\LockMode;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\OptimisticLockException;
+use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\Model\OrderItemInterface;
+
+/**
+ * @author Grzegorz Sadowski <grzegorz.sadowski@lakion.com>
+ */
+final class ORMOrderInventoryOperator implements OrderInventoryOperatorInterface
+{
+    /**
+     * @var OrderInventoryOperatorInterface
+     */
+    private $decoratedOperator;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $productVariantManager;
+
+    /**
+     * @param OrderInventoryOperatorInterface $decoratedOperator
+     * @param EntityManagerInterface $productVariantManager
+     */
+    public function __construct(
+        OrderInventoryOperatorInterface $decoratedOperator,
+        EntityManagerInterface $productVariantManager
+    ) {
+        $this->decoratedOperator = $decoratedOperator;
+        $this->productVariantManager = $productVariantManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function cancel(OrderInterface $order)
+    {
+        $this->decoratedOperator->cancel($order);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws OptimisticLockException
+     */
+    public function hold(OrderInterface $order)
+    {
+        $this->lockProductVariants($order);
+
+        $this->decoratedOperator->hold($order);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function sell(OrderInterface $order)
+    {
+        $this->decoratedOperator->sell($order);
+    }
+
+    /**
+     * @param OrderInterface $order
+     *
+     * @throws OptimisticLockException
+     */
+    private function lockProductVariants(OrderInterface $order)
+    {
+        /** @var OrderItemInterface $orderItem */
+        foreach ($order->getItems() as $orderItem) {
+            $variant = $orderItem->getVariant();
+
+            if (!$variant->isTracked()) {
+                continue;
+            }
+
+            $this->productVariantManager->lock($variant, LockMode::OPTIMISTIC, $variant->getVersion());
+        }
+    }
+}
---
+++ src/Sylius/Component/Core/spec/Inventory/Operator/ORMOrderInventoryOperatorSpec.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Sylius\Component\Core\Inventory\Operator;
+
+use Doctrine\DBAL\LockMode;
+use Doctrine\ORM\EntityManagerInterface;
+use PhpSpec\ObjectBehavior;
+use Sylius\Component\Core\Inventory\Operator\OrderInventoryOperatorInterface;
+use Sylius\Component\Core\Inventory\Operator\ORMOrderInventoryOperator;
+use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\Model\OrderItemInterface;
+use Sylius\Component\Core\Model\ProductVariantInterface;
+
+/**
+ * @author Grzegorz Sadowski <grzegorz.sadowski@lakion.com>
+ */
+final class ORMOrderInventoryOperatorSpec extends ObjectBehavior
+{
+    function let(OrderInventoryOperatorInterface $decoratedOperator, EntityManagerInterface $productVariantManager)
+    {
+        $this->beConstructedWith($decoratedOperator, $productVariantManager);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(ORMOrderInventoryOperator::class);
+    }
+
+    function it_implements_an_order_inventory_operator_interface()
+    {
+        $this->shouldImplement(OrderInventoryOperatorInterface::class);
+    }
+
+    function it_locks_tracked_variants_during_holding(
+        OrderInventoryOperatorInterface $decoratedOperator,
+        EntityManagerInterface $productVariantManager,
+        OrderInterface $order,
+        OrderItemInterface $orderItem,
+        ProductVariantInterface $variant
+    ) {
+        $order->getItems()->willReturn([$orderItem]);
+        $orderItem->getVariant()->willReturn($variant);
+        $variant->isTracked()->willReturn(true);
+        $variant->getVersion()->willReturn('7');
+
+        $productVariantManager->lock($variant, LockMode::OPTIMISTIC, '7')->shouldBeCalled();
+
+        $decoratedOperator->hold($order)->shouldBeCalled();
+
+        $this->hold($order);
+    }
+}
