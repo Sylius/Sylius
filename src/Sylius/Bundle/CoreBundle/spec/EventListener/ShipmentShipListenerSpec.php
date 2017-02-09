8372cd620c8a867632613a2b7d203fb3010b75ac
Grzegorz Sadowski <sadowskigp@gmail.com>
2017-02-09 16:35:08 +0100

    [Shipment] Move sending confirmation email from state machine to listener on post ship

---
+++ src/Sylius/Bundle/CoreBundle/spec/EventListener/ShipmentShipListenerSpec.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Sylius\Bundle\CoreBundle\EventListener;
+
+use PhpSpec\ObjectBehavior;
+use Sylius\Bundle\CoreBundle\EmailManager\ShipmentEmailManager;
+use Sylius\Bundle\CoreBundle\EventListener\ShipmentShipListener;
+use Sylius\Component\Core\Model\ShipmentInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+/**
+ * @author Grzegorz Sadowski <grzegorz.sadowski@lakion.com>
+ */
+final class ShipmentShipListenerSpec extends ObjectBehavior
+{
+    function let(ShipmentEmailManager $shipmentEmailManager)
+    {
+        $this->beConstructedWith($shipmentEmailManager);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(ShipmentShipListener::class);
+    }
+
+    function it_sends_a_confirmation_email(
+        ShipmentEmailManager $shipmentEmailManager,
+        GenericEvent $event,
+        ShipmentInterface $shipment
+    ) {
+        $event->getSubject()->willReturn($shipment);
+
+        $shipmentEmailManager->sendConfirmationEmail($shipment)->shouldBeCalled();
+
+        $this->sendConfirmationEmail($event);
+    }
+
+    function it_throws_an_invalid_argument_exception_if_an_event_subject_is_not_a_shipment_instance(
+        GenericEvent $event,
+        \stdClass $shipment
+    ) {
+        $event->getSubject()->willReturn($shipment);
+
+        $this->shouldThrow(\InvalidArgumentException::class)->during('sendConfirmationEmail', [$event]);
+    }
+}
