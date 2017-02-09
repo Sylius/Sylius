8bae9a18cee15e310e281ab5cd2265288fc4d779
Grzegorz Sadowski <sadowskigp@gmail.com>
2017-02-09 10:37:03 +0100

    [Admin][Customer] Add knp menu builder for menu on a customer show page

---
+++ src/Sylius/Bundle/AdminBundle/Menu/CustomerShowMenuBuilder.php
@@ -0,0 +1,124 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\AdminBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+use Sylius\Component\Core\Model\CustomerInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * @author Grzegorz Sadowski <grzegorz.sadowski@lakion.com>
+ */
+final class CustomerShowMenuBuilder
+{
+    const EVENT_NAME = 'sylius.menu.admin.customer.show';
+
+    /**
+     * @var FactoryInterface
+     */
+    private $factory;
+
+    /**
+     * @var EventDispatcherInterface
+     */
+    private $eventDispatcher;
+
+    /**
+     * @param FactoryInterface $factory
+     * @param EventDispatcherInterface $eventDispatcher
+     */
+    public function __construct(
+        FactoryInterface $factory,
+        EventDispatcherInterface $eventDispatcher
+    ) {
+        $this->factory = $factory;
+        $this->eventDispatcher = $eventDispatcher;
+    }
+
+    /**
+     * @param array $options
+     *
+     * @return ItemInterface
+     */
+    public function createMenu(array $options)
+    {
+        $menu = $this->factory->createItem('root');
+
+        if (!isset($options['customer'])) {
+            return $menu;
+        }
+
+        $customer = $options['customer'];
+        $this->addChilds($menu, $customer);
+
+        $this->eventDispatcher->dispatch(self::EVENT_NAME, new MenuBuilderEvent($this->factory, $menu));
+
+        return $menu;
+    }
+
+    /**
+     * @param ItemInterface $menu
+     */
+    private function addChilds(ItemInterface $menu, CustomerInterface $customer)
+    {
+        if (null !== $customer->getUser()) {
+            $menu->setExtra('column_id', 'actions');
+
+            $menu
+                ->addChild('update', [
+                    'route' => 'sylius_admin_customer_update',
+                    'routeParameters' => ['id' => $customer->getId()]
+                ])
+                ->setAttribute('type', 'edit')
+            ;
+
+            $menu
+                ->addChild('order_index', [
+                    'route' => 'sylius_admin_customer_order_index',
+                    'routeParameters' => ['id' => $customer->getId()]
+                ])
+                ->setAttribute('type', 'show')
+                ->setLabel('sylius.ui.show_orders')
+            ;
+
+            $menu
+                ->addChild('user_delete', [
+                    'route' => 'sylius_admin_shop_user_delete',
+                    'routeParameters' => ['id' => $customer->getUser()->getId()]
+                ])
+                ->setAttribute('type', 'delete')
+                ->setAttribute('customer_id', $customer->getId())
+            ;
+
+            return;
+        }
+
+        $menu->setExtra('column_id', 'no-account');
+
+        $menu
+            ->addChild('guest')
+            ->setLabel('sylius.ui.guest')
+            ->setLabelAttribute('icon', 'spy')
+        ;
+
+        $menu
+            ->addChild('order_index', [
+                'route' => 'sylius_admin_customer_order_index',
+                'routeParameters' => ['id' => $customer->getId()]
+            ])
+            ->setAttribute('type', 'show')
+            ->setLabel('sylius.ui.show_orders')
+        ;
+    }
+}
---
+++ src/Sylius/Bundle/AdminBundle/spec/Menu/CustomerShowMenuBuilderSpec.php
@@ -0,0 +1,141 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\Sylius\Bundle\AdminBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Sylius\Bundle\AdminBundle\Menu\CustomerShowMenuBuilder;
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+use Sylius\Component\Core\Model\CustomerInterface;
+use Sylius\Component\User\Model\UserInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+/**
+ * @author Grzegorz Sadowski <grzegorz.sadowski@lakion.com>
+ */
+final class CustomerShowMenuBuilderSpec extends ObjectBehavior
+{
+    function let(
+        FactoryInterface $factory,
+        EventDispatcherInterface $eventDispatcher
+    ) {
+        $this->beConstructedWith($factory, $eventDispatcher);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(CustomerShowMenuBuilder::class);
+    }
+
+    function it_creates_a_customer_show_menu_for_customer_with_user(
+        FactoryInterface $factory,
+        EventDispatcherInterface $eventDispatcher,
+        ItemInterface $menu,
+        CustomerInterface $customer,
+        UserInterface $user
+    ) {
+        $factory->createItem('root')->willReturn($menu);
+
+        $customer->getId()->willReturn(7);
+        $customer->getUser()->willReturn($user);
+        $user->getId()->willReturn(4);
+
+        $menu->setExtra('column_id', 'actions')->shouldBeCalled()->willReturn($menu);
+
+        $menu
+            ->addChild('update', [
+                'route' => 'sylius_admin_customer_update',
+                'routeParameters' => ['id' => 7]
+            ])
+            ->shouldBeCalled()
+            ->willReturn($menu)
+        ;
+        $menu->setAttribute('type', 'edit')->shouldBeCalled()->willReturn($menu);
+
+        $menu
+            ->addChild('order_index', [
+                'route' => 'sylius_admin_customer_order_index',
+                'routeParameters' => ['id' => 7]
+            ])
+            ->shouldBeCalled()
+            ->willReturn($menu)
+        ;
+        $menu->setAttribute('type', 'show')->shouldBeCalled()->willReturn($menu);
+        $menu->setLabel('sylius.ui.show_orders')->shouldBeCalled()->willReturn($menu);
+
+        $menu
+            ->addChild('user_delete', [
+                'route' => 'sylius_admin_shop_user_delete',
+                'routeParameters' => ['id' => 4]
+            ])
+            ->shouldBeCalled()
+            ->willReturn($menu)
+        ;
+        $menu->setAttribute('type', 'delete')->shouldBeCalled()->willReturn($menu);
+        $menu->setAttribute('customer_id', 7)->shouldBeCalled()->willReturn($menu);
+
+        $eventDispatcher
+            ->dispatch('sylius.menu.admin.customer.show', Argument::type(MenuBuilderEvent::class))
+            ->shouldBeCalled()
+        ;
+
+        $this->createMenu(['customer' => $customer])->shouldReturn($menu);
+    }
+
+    function it_creates_a_customer_show_menu_for_customer_without_user(
+        FactoryInterface $factory,
+        EventDispatcherInterface $eventDispatcher,
+        ItemInterface $menu,
+        CustomerInterface $customer
+    ) {
+        $factory->createItem('root')->willReturn($menu);
+
+        $customer->getId()->willReturn(7);
+        $customer->getUser()->willReturn(null);
+
+        $menu->setExtra('column_id', 'no-account')->shouldBeCalled()->willReturn($menu);
+
+        $menu->addChild('guest')->shouldBeCalled()->willReturn($menu);
+        $menu->setLabel('sylius.ui.guest')->shouldBeCalled()->willReturn($menu);
+        $menu->setLabelAttribute('icon', 'spy')->shouldBeCalled()->willReturn($menu);
+
+        $menu
+            ->addChild('order_index', [
+                'route' => 'sylius_admin_customer_order_index',
+                'routeParameters' => ['id' => 7]
+            ])
+            ->shouldBeCalled()
+            ->willReturn($menu)
+        ;
+        $menu->setAttribute('type', 'show')->shouldBeCalled()->willReturn($menu);
+        $menu->setLabel('sylius.ui.show_orders')->shouldBeCalled()->willReturn($menu);
+
+        $eventDispatcher
+            ->dispatch('sylius.menu.admin.customer.show', Argument::type(MenuBuilderEvent::class))
+            ->shouldBeCalled()
+        ;
+
+        $this->createMenu(['customer' => $customer])->shouldReturn($menu);
+    }
+
+    function it_returns_an_empty_customer_show_menu_when_there_is_no_customer_in_options(
+        FactoryInterface $factory,
+        ItemInterface $menu
+    ) {
+
+        $factory->createItem('root')->willReturn($menu);
+
+        $this->createMenu([])->shouldReturn($menu);
+    }
+}
