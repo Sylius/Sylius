58ce3f4edd88bb896896b13cf215f7088edbb678
Grzegorz Sadowski <sadowskigp@gmail.com>
2017-02-14 14:33:36 +0100

    [Core] Remove unnecessary custom controllers and controller's actions

--- src/Sylius/Bundle/CoreBundle/Controller/OrderController.php
+++ src/Sylius/Bundle/CoreBundle/Controller/OrderController.php
@@ -11,92 +11,14 @@
 
 namespace Sylius\Bundle\CoreBundle\Controller;
 
-use Doctrine\ORM\OptimisticLockException;
 use FOS\RestBundle\View\View;
 use Sylius\Bundle\OrderBundle\Controller\OrderController as BaseOrderController;
-use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
-use Sylius\Component\Resource\ResourceActions;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Webmozart\Assert\Assert;
 
 class OrderController extends BaseOrderController
 {
-    /**
-     * @param Request $request
-     *
-     * @return Response
-     */
-    public function completeAction(Request $request)
-    {
-        $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
-
-        $this->isGrantedOr403($configuration, ResourceActions::UPDATE);
-        $order = $this->findOr404($configuration);
-
-        $form = $this->resourceFormFactory->create($configuration, $order);
-
-        if (in_array($request->getMethod(), ['POST', 'PUT', 'PATCH'], true) && $form->handleRequest($request)->isValid()) {
-            $order = $form->getData();
-
-            /** @var ResourceControllerEvent $event */
-            $event = $this->eventDispatcher->dispatchPreEvent(ResourceActions::UPDATE, $configuration, $order);
-
-            if ($event->isStopped() && !$configuration->isHtmlRequest()) {
-                throw new HttpException($event->getErrorCode(), $event->getMessage());
-            }
-            if ($event->isStopped()) {
-                $this->flashHelper->addFlashFromEvent($configuration, $event);
-
-                if ($event->hasResponse()) {
-                    return $event->getResponse();
-                }
-
-                return $this->redirectHandler->redirectToResource($configuration, $order);
-            }
-
-            try {
-                if ($configuration->hasStateMachine()) {
-                    $this->stateMachine->apply($configuration, $order);
-                }
-
-                $this->manager->flush();
-            } catch (OptimisticLockException $exception) {
-                $this->addFlash('error', 'sylius.checkout.complete_error');
-
-                return $this->redirectHandler->redirectToRoute($configuration, 'sylius_shop_checkout_complete');
-            }
-
-            $this->eventDispatcher->dispatchPostEvent(ResourceActions::UPDATE, $configuration, $order);
-
-            if (!$configuration->isHtmlRequest()) {
-                return $this->viewHandler->handle($configuration, View::create(null, Response::HTTP_NO_CONTENT));
-            }
-
-            $this->flashHelper->addSuccessFlash($configuration, ResourceActions::UPDATE, $order);
-
-            return $this->redirectHandler->redirectToResource($configuration, $order);
-        }
-
-        if (!$configuration->isHtmlRequest()) {
-            return $this->viewHandler->handle($configuration, View::create($form, Response::HTTP_BAD_REQUEST));
-        }
-
-        $view = View::create()
-            ->setData([
-                'configuration' => $configuration,
-                'metadata' => $this->metadata,
-                'order' => $order,
-                'form' => $form->createView(),
-            ])
-            ->setTemplate($configuration->getTemplate(ResourceActions::UPDATE . '.html'))
-        ;
-
-        return $this->viewHandler->handle($configuration, $view);
-    }
-
     /**
      * @param Request $request
      *
@@ -130,50 +52,5 @@ class OrderController extends BaseOrderController
         ;
 
         return $this->viewHandler->handle($configuration, $view);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function applyStateMachineTransitionAction(Request $request)
-    {
-        $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
-
-        $this->isGrantedOr403($configuration, ResourceActions::UPDATE);
-        $order = $this->findOr404($configuration);
-
-        $event = $this->eventDispatcher->dispatchPreEvent(ResourceActions::UPDATE, $configuration, $order);
-
-        if ($event->isStopped() && !$configuration->isHtmlRequest()) {
-            throw new HttpException($event->getErrorCode(), $event->getMessage());
-        }
-        if ($event->isStopped()) {
-            $this->flashHelper->addFlashFromEvent($configuration, $event);
-
-            return $this->redirectHandler->redirectToResource($configuration, $order);
-        }
-
-        if (!$this->stateMachine->can($configuration, $order)) {
-            throw new BadRequestHttpException();
-        }
-
-        try {
-            $this->stateMachine->apply($configuration, $order);
-            $this->manager->flush();
-        } catch (OptimisticLockException $exception) {
-            $this->addFlash('error', 'sylius.order.apply_state_machine_transition_error');
-
-            return $this->redirectHandler->redirectToResource($configuration, $order);
-        }
-
-        $this->eventDispatcher->dispatchPostEvent(ResourceActions::UPDATE, $configuration, $order);
-
-        if (!$configuration->isHtmlRequest()) {
-            return $this->viewHandler->handle($configuration, View::create($order, Response::HTTP_OK));
-        }
-
-        $this->flashHelper->addSuccessFlash($configuration, ResourceActions::UPDATE, $order);
-
-        return $this->redirectHandler->redirectToResource($configuration, $order);
     }
 }
