ee01ea8d6bf99f5419fa23a30c1b57059ace4ad2
Steffen Brem <steffenbrem@gmail.com>
2017-03-01 14:05:11 +0100

    Accidental change, revert.

--- src/Sylius/Bundle/CoreBundle/Fixture/TshirtProductFixture.php
+++ src/Sylius/Bundle/CoreBundle/Fixture/TshirtProductFixture.php
@@ -162,8 +162,8 @@ class TshirtProductFixture extends AbstractFixture
                 ],
                 'product_options' => ['t_shirt_color', 't_shirt_size'],
                 'images' => [
-                    'main' => sprintf('%s/../Resources/fixtures/%s', __DIR__, 't-shirts.jpg'),
-                    'thumbnail' => sprintf('%s/../Resources/fixtures/%s', __DIR__, 't-shirts.jpg'),
+                    [sprintf('%s/../Resources/fixtures/%s', __DIR__, 't-shirts.jpg'), 'main'],
+                    [sprintf('%s/../Resources/fixtures/%s', __DIR__, 't-shirts.jpg'), 'thumbnail'],
                 ],
             ];
         }
