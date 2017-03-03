642b1c7b0fe04a2257b1f3a39623e82707f9ef6f
Gorka Laucirica <gorka.lauzirika@gmail.com>
2017-03-03 17:03:06 +0100

    Renamed migration and change "original price" column name

--- app/migrations/Version20170303170201.php
+++ app/migrations/Version20170303170201.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20161214101054 extends AbstractMigration
+class Version20170303170201 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,7 +18,7 @@ class Version20161214101054 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE sylius_channel_pricing ADD originalPrice INT');
+        $this->addSql('ALTER TABLE sylius_channel_pricing ADD original_price INT');
     }
 
     /**
@@ -29,6 +29,6 @@ class Version20161214101054 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE sylius_channel_pricing DROP originalPrice');
+        $this->addSql('ALTER TABLE sylius_channel_pricing DROP original_price');
     }
 }
