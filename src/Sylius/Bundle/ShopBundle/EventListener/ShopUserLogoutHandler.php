281de5a947863c2704310508b135a01d665f51e4
Łukasz <lchrusciel@gmail.com>
2017-04-26 10:42:22 +0200

    [Core] [Shop] Move shop session depend listeners

--- src/Sylius/Bundle/ShopBundle/EventListener/ShopUserLogoutHandler.php
+++ src/Sylius/Bundle/ShopBundle/EventListener/ShopUserLogoutHandler.php
@@ -11,7 +11,7 @@
 
 declare(strict_types=1);
 
-namespace Sylius\Bundle\CoreBundle\EventListener;
+namespace Sylius\Bundle\ShopBundle\EventListener;
 
 use Sylius\Component\Channel\Context\ChannelContextInterface;
 use Symfony\Component\HttpFoundation\Request;
