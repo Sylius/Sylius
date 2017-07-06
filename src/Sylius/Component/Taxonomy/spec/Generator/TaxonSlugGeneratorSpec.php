554f7498114b6603fe11b204fc7e7af8dc76a683
Theodor Manolescu <manolescu.theodor@yahoo.com>
2017-07-06 21:54:12 +0300

    8213 add phpspec test

--- src/Sylius/Component/Taxonomy/spec/Generator/TaxonSlugGeneratorSpec.php
+++ src/Sylius/Component/Taxonomy/spec/Generator/TaxonSlugGeneratorSpec.php
@@ -47,6 +47,19 @@ final class TaxonSlugGeneratorSpec extends ObjectBehavior
         $this->generate('Battle games', 1)->shouldReturn('board-games/battle-games');;
     }
 
+    function it_generates_slug_based_on_new_taxon_name_and_parent_taxon_slug_for_polish_locale(
+        TaxonInterface $parent,
+        TaxonRepositoryInterface $taxonRepository
+    ) {
+        $taxonRepository->find(1)->willReturn($parent);
+        $parent->getSlug()->willReturn('bronie-sredniowieczne');
+        $parent->getTranslation('pl_PL')->willReturn($parent);
+
+        $this->generate('Silniki oblężnicze', 1, 'pl_PL')
+            ->shouldReturn('bronie-sredniowieczne/silniki-obleznicze')
+        ;
+    }
+
     function it_generates_slug_based_on_new_taxon_name_if_this_taxon_has_no_parent()
     {
         $this->generate('Board games')->shouldReturn('board-games');;
