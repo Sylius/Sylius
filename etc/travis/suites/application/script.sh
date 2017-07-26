17cc54a57c1c7894b822df1845216daf58a59c60
Kamil Kokot <kamil@kokot.me>
2017-07-26 16:48:21 +0200

    REMOVE THIS COMMIT: Run only JS scenarios on Travis

--- etc/travis/suites/application/script.sh
+++ etc/travis/suites/application/script.sh
@@ -4,16 +4,16 @@ source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/common.lib.s
 
 code=0
 commands=(
-    validate-composer
-    validate-behat-features
-    validate-doctrine-schema
-    validate-twig
-    test-phpspec
-    test-phpunit
-    test-fixtures
-    test-behat-without-javascript
+#    validate-composer
+#    validate-behat-features
+#    validate-doctrine-schema
+#    validate-twig
+#    test-phpspec
+#    test-phpunit
+#    test-fixtures
+#    test-behat-without-javascript
     test-behat-with-javascript
-    test-behat-with-cli
+#    test-behat-with-cli
 )
 
 for command in ${commands[@]}; do
