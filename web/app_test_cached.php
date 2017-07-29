41a67cbb6ed9848a60760fa1d152e49961545655
Danny van Wijk <info@vwmedia.nl>
2017-07-29 10:08:42 +0000

    Don't load class cache and bootstrap file on php7. Remove app/autoload.php

--- web/app_test_cached.php
+++ web/app_test_cached.php
@@ -26,12 +26,9 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-/** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
+require __DIR__.'/../vendor/autoload.php';
 
 $kernel = new AppKernel('test_cached', false);
-$kernel->loadClassCache();
 
 $request = Request::createFromGlobals();
 
