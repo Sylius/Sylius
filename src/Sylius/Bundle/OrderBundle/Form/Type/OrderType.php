394ae25361ad8abd18c6b8f7b9a83fcb7d4a9d9a
Grzegorz Sadowski <sadowskigp@gmail.com>
2017-09-08 16:17:50 +0200

    [Order] Remove unnecessary fields from OrderType

--- src/Sylius/Bundle/OrderBundle/Form/Type/OrderType.php
+++ src/Sylius/Bundle/OrderBundle/Form/Type/OrderType.php
@@ -14,34 +14,12 @@ declare(strict_types=1);
 namespace Sylius\Bundle\OrderBundle\Form\Type;
 
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\FormBuilderInterface;
 
 /**
  * @author Paweł Jędrzejewski <pawel@sylius.org>
  */
 final class OrderType extends AbstractResourceType
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('items', CollectionType::class, [
-                'entry_type' => OrderItemType::class,
-                'allow_add' => true,
-                'allow_delete' => true,
-                'by_reference' => false,
-                'label' => 'sylius.form.order.items',
-            ])
-            ->add('notes', TextareaType::class, [
-                'label' => 'sylius.ui.notes',
-            ])
-        ;
-    }
-
     /**
      * {@inheritdoc}
      */
