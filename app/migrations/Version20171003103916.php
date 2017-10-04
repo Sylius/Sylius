ed496e0229ac884d7fd1fe5cacb2a768804d0d13
Grzegorz Sadowski <sadowskigp@gmail.com>
2017-10-04 11:03:42 +0200

    [Migrations][ProductAttribute] Refactor migrations for translatable select product attribute

--- app/migrations/Version20171003103916.php
+++ app/migrations/Version20171003103916.php
@@ -4,13 +4,15 @@ namespace Sylius\Migrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Sylius\Component\Attribute\AttributeType\SelectAttributeType;
+use Sylius\Component\Product\Model\ProductAttributeInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20170626083012 extends AbstractMigration implements ContainerAwareInterface
+class Version20171003103916 extends AbstractMigration implements ContainerAwareInterface
 {
     /**
      * @var ContainerInterface
@@ -32,27 +34,31 @@ class Version20170626083012 extends AbstractMigration implements ContainerAwareI
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        $defaultLocale = $this->container->getParameter('locale');
-        $conn = $this->container->get('doctrine.dbal.default_connection');
 
-        $attributes = $conn->executeQuery('SELECT id, configuration FROM sylius_product_attribute WHERE `type` = "select"')->fetchAll();
-        foreach ($attributes as $attribute) {
-            $updatedConfig = ['choices' => []];
-            $configuration = unserialize($attribute['configuration']);
+        $defaultLocale = $this->container->getParameter('locale');
+        $productAttributeRepository = $this->container->get('sylius.repository.product_attribute');
+
+        $productAttributes = $productAttributeRepository->findBy(['type' => SelectAttributeType::TYPE]);
+        /** @var ProductAttributeInterface $productAttribute */
+        foreach ($productAttributes as $productAttribute) {
+            $configuration = $productAttribute->getConfiguration();
+            $upgradedConfiguration = [];
 
             foreach ($configuration as $configurationKey => $value) {
-                if ($configurationKey === 'choices') {
+                if ('choices' === $configurationKey) {
                     foreach ($value as $key => $choice) {
-                        $updatedConfig[$configurationKey][$key][$defaultLocale] = $choice;
+                        $upgradedConfiguration[$configurationKey][$key][$defaultLocale] = $choice;
                     }
-                } else {
-                    $updatedConfig[$configurationKey] = $value;
+
+                    continue;
                 }
+
+                $upgradedConfiguration[$configurationKey] = $value;
             }
 
             $this->addSql('UPDATE sylius_product_attribute SET configuration = :configuration WHERE id = :id', [
-                'id' => $attribute['id'],
-                'configuration' => serialize($updatedConfig),
+                'id' => $productAttribute->getId(),
+                'configuration' => serialize($upgradedConfiguration),
             ]);
         }
     }
@@ -64,31 +70,33 @@ class Version20170626083012 extends AbstractMigration implements ContainerAwareI
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        $defaultLocale = $this->container->getParameter('locale');
-        $conn = $this->container->get('doctrine.dbal.default_connection');
 
-        $attributes = $conn->executeQuery('SELECT id, configuration FROM sylius_product_attribute WHERE `type` = "select"')->fetchAll();
-        foreach ($attributes as $attribute) {
-            $newConfig = ['choices' => []];
-            $configuration = unserialize($attribute['configuration']);
+        $defaultLocale = $this->container->getParameter('locale');
+        $productAttributeRepository = $this->container->get('sylius.repository.product_attribute');
+
+        $productAttributes = $productAttributeRepository->findBy(['type' => SelectAttributeType::TYPE]);
+        /** @var ProductAttributeInterface $productAttribute */
+        foreach ($productAttributes as $productAttribute) {
+            $configuration = $productAttribute->getConfiguration();
+            $downgradedConfiguration = [];
 
             foreach ($configuration as $configurationKey => $value) {
-                if ($configurationKey === 'choices') {
+                if ('choices' === $configurationKey) {
                     foreach ($value as $key => $choice) {
-                        foreach ($choice as $locale => $translate) {
-                            if ($locale === $defaultLocale) {
-                                $newConfig[$configurationKey][$key] = $translate;
-                            }
+                        if (array_key_exists($defaultLocale, $choice)) {
+                            $downgradedConfiguration[$configurationKey][$key] = $choice[$defaultLocale];
                         }
                     }
-                } else {
-                    $newConfig[$configurationKey] = $value;
+
+                    continue;
                 }
+
+                $downgradedConfiguration[$configurationKey] = $value;
             }
 
             $this->addSql('UPDATE sylius_product_attribute SET configuration = :configuration WHERE id = :id', [
-                'id' => $attribute['id'],
-                'configuration' => serialize($newConfig),
+                'id' => $productAttribute->getId(),
+                'configuration' => serialize($downgradedConfiguration),
             ]);
         }
     }
