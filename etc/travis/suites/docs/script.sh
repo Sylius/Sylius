cb73a9bd6afabbd6dc7621e6d3ceddf5805d900f
Kamil Kokot <kamil@kokot.me>
2017-10-17 09:55:36 +0200

    Do not check links while building documentation

--- etc/travis/suites/docs/script.sh
+++ etc/travis/suites/docs/script.sh
@@ -5,16 +5,15 @@ source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/common.lib.s
 code=0
 
 print_header "Building" "Documentation"
-run_command "sphinx-build -nWT -b linkcheck docs docs/build" || code=$?
+run_command "sphinx-build -nWT docs docs/build" || code=$?
 # Flags used here
 #  -n   Run in nit-picky mode. Currently, this generates warnings for all missing references.
 #  -W   Turn warnings into errors. This means that the build stops at the first warning and sphinx-build exits with exit status 1.
 #  -T   Displays the full stack trace if an unhandled exception occurs.
-#  -b   Linkcheck builder checks for broken links.
 
 if [[ ${code} != 0 ]]; then
     print_warning "Build failed, rerunning to show all the warnings and errors"
-    run_command "sphinx-build -n -b html docs docs/build"
+    run_command "sphinx-build -nT docs docs/build"
 fi
 
 exit ${code}
