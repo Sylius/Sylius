1534c528bedd79f541d74ea591dccdb7b82e843c
mamazu <maxo.pech@gmail.com>
2018-02-05 13:53:44 +0100

    Cleaned up the fixtures

--- app/migrations/Version20180205125123.php
+++ app/migrations/Version20180205125123.php
@@ -8,14 +8,15 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180201112957 extends AbstractMigration
+class Version20180205125123 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE sylius_url_redirects');
+        $this->addSql('CREATE TABLE sylius_postcodes (id INT AUTO_INCREMENT NOT NULL, country_id INT NOT NULL, name VARCHAR(255) NOT NULL, postCode VARCHAR(255) NOT NULL, INDEX IDX_419792EFF92F3E70 (country_id), UNIQUE INDEX UNIQ_419792EFF92F3E70C30B0B2F (country_id, postCode), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE sylius_postcodes ADD CONSTRAINT FK_419792EFF92F3E70 FOREIGN KEY (country_id) REFERENCES sylius_country (id)');
     }
 
     public function down(Schema $schema)
@@ -23,6 +24,6 @@ class Version20180201112957 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE sylius_url_redirects (id INT AUTO_INCREMENT NOT NULL, oldRoute VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, newRoute VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, enabled TINYINT(1) NOT NULL, type VARCHAR(9) NOT NULL COLLATE utf8_unicode_ci, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('DROP TABLE sylius_postcodes');
     }
 }
