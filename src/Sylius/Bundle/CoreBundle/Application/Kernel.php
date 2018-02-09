453e58530df4cd58d905865a2d5c86f92c9ae8c7
Kamil Kokot <kamil@kokot.me>
2018-02-09 16:13:21 +0100

    Update version to 1.1.1-DEV

--- src/Sylius/Bundle/CoreBundle/Application/Kernel.php
+++ src/Sylius/Bundle/CoreBundle/Application/Kernel.php
@@ -31,12 +31,12 @@ use Webmozart\Assert\Assert;
 
 class Kernel extends HttpKernel
 {
-    public const VERSION = '1.1.0';
-    public const VERSION_ID = '10100';
+    public const VERSION = '1.1.1-DEV';
+    public const VERSION_ID = '10101';
     public const MAJOR_VERSION = '1';
     public const MINOR_VERSION = '1';
-    public const RELEASE_VERSION = '0';
-    public const EXTRA_VERSION = '';
+    public const RELEASE_VERSION = '1';
+    public const EXTRA_VERSION = 'DEV';
 
     /**
      * {@inheritdoc}
