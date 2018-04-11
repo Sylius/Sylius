2ae06f2a57ae7ccdc48707fb22c36c0c221fc155
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2018-04-11 11:22:16 +0200

    ChannelNotFoundException updated to other exceptions' format without BC break

--- src/Sylius/Component/Channel/Context/ChannelNotFoundException.php
+++ src/Sylius/Component/Channel/Context/ChannelNotFoundException.php
@@ -15,11 +15,23 @@ namespace Sylius\Component\Channel\Context;
 
 class ChannelNotFoundException extends \RuntimeException
 {
+    private static const EXCEPTION_MESSAGE = 'Channel could not be found! Tip: You can use the Web Debug Toolbar to switch between channels in development.';
+
     /**
      * {@inheritdoc}
      */
-    public function __construct(?\Exception $previousException = null)
+    public function __construct($messageOrPreviousException = null, ?\Throwable $previousException = null)
     {
-        parent::__construct('Channel could not be found! Tip: You can use the Web Debug Toolbar to switch between channels in development.', 0, $previousException);
+        if ($messageOrPreviousException instanceof \Throwable) {
+            parent::__construct(self::EXCEPTION_MESSAGE, 0, $messageOrPreviousException);
+        }
+
+        else if (is_string($messageOrPreviousException)) {
+            parent::__construct($messageOrPreviousException, 0, $previousException);
+        }
+
+        else {
+            parent::__construct(self::EXCEPTION_MESSAGE, 0, $previousException);
+        }
     }
 }
