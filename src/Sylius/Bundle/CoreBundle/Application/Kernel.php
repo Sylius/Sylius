83587c46ac41386b049a4dd8909f8ba5b4ab1233
Kamil Kokot <kamil@kokot.me>
2018-04-12 14:14:57 +0200

    Set version to v1.0.14

--- src/Sylius/Bundle/CoreBundle/Application/Kernel.php
+++ src/Sylius/Bundle/CoreBundle/Application/Kernel.php
@@ -31,12 +31,12 @@ use Webmozart\Assert\Assert;
 
 class Kernel extends HttpKernel
 {
-    public const VERSION = '1.0.14-DEV';
+    public const VERSION = '1.0.14';
     public const VERSION_ID = '10014';
     public const MAJOR_VERSION = '1';
     public const MINOR_VERSION = '0';
     public const RELEASE_VERSION = '14';
-    public const EXTRA_VERSION = 'DEV';
+    public const EXTRA_VERSION = '';
 
     /**
      * {@inheritdoc}
