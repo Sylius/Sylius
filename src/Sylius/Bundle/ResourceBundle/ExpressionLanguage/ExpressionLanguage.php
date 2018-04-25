302a43769144a30718f2245c72bb6c28d4e8816f
Kamil Kokot <kamil@kokot.me>
2018-04-25 13:14:10 +0200

    Add BC layer for ExpressionLanguage

--- src/Sylius/Bundle/ResourceBundle/ExpressionLanguage/ExpressionLanguage.php
+++ src/Sylius/Bundle/ResourceBundle/ExpressionLanguage/ExpressionLanguage.php
@@ -13,7 +13,9 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\ResourceBundle\ExpressionLanguage;
 
+use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Component\DependencyInjection\ExpressionLanguage as BaseExpressionLanguage;
+use Symfony\Component\ExpressionLanguage\ParserCache\ParserCacheAdapter;
 use Symfony\Component\ExpressionLanguage\ParserCache\ParserCacheInterface;
 
 final class ExpressionLanguage extends BaseExpressionLanguage
@@ -21,10 +23,20 @@ final class ExpressionLanguage extends BaseExpressionLanguage
     /**
      * {@inheritdoc}
      */
-    public function __construct(?ParserCacheInterface $parser = null, array $providers = [])
+    public function __construct($cache = null, array $providers = [])
     {
+        if (null !== $cache) {
+            if ($cache instanceof ParserCacheInterface) {
+                @trigger_error(sprintf('Passing an instance of %s as constructor argument for %s is deprecated as of Sylius 1.2 and will be removed in 2.0. Pass an instance of %s instead.', ParserCacheInterface::class, self::class, CacheItemPoolInterface::class), E_USER_DEPRECATED);
+
+                $cache = new ParserCacheAdapter($cache);
+            } elseif (!$cache instanceof CacheItemPoolInterface) {
+                throw new \InvalidArgumentException(sprintf('Cache argument has to implement %s.', CacheItemPoolInterface::class));
+            }
+        }
+
         array_unshift($providers, new NotNullExpressionFunctionProvider());
 
-        parent::__construct($parser, $providers);
+        parent::__construct($cache, $providers);
     }
 }
