c94ba633239676b6812b94f1e13b6cf278371d12
Adrien LUCAS <adrien.lucas@smile.fr>
2018-10-07 22:54:15 +0200

    Add autoconfiguration for cart contexts and order processor services.

--- src/Sylius/Bundle/OrderBundle/DependencyInjection/SyliusOrderExtension.php
+++ src/Sylius/Bundle/OrderBundle/DependencyInjection/SyliusOrderExtension.php
@@ -13,7 +13,11 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\OrderBundle\DependencyInjection;
 
+use Sylius\Bundle\OrderBundle\DependencyInjection\Compiler\RegisterCartContextsPass;
+use Sylius\Bundle\OrderBundle\DependencyInjection\Compiler\RegisterProcessorsPass;
 use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
+use Sylius\Component\Order\Context\CartContextInterface;
+use Sylius\Component\Order\Processor\OrderProcessorInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
@@ -36,5 +40,14 @@ final class SyliusOrderExtension extends AbstractResourceExtension
 
         $container->setParameter('sylius_order.cart_expiration_period', $config['expiration']['cart']);
         $container->setParameter('sylius_order.order_expiration_period', $config['expiration']['order']);
+
+        $container
+            ->registerForAutoconfiguration(CartContextInterface::class)
+            ->addTag(RegisterCartContextsPass::CART_CONTEXT_SERVICE_TAG)
+        ;
+        $container
+            ->registerForAutoconfiguration(OrderProcessorInterface::class)
+            ->addTag(RegisterProcessorsPass::PROCESSOR_SERVICE_TAG)
+        ;
     }
 }
---
+++ src/Sylius/Bundle/OrderBundle/Tests/DependencyInjection/SyliusOrderExtensionTest.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\OrderBundle\Tests\DependencyInjection;
+
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractExtensionTestCase;
+use Sylius\Bundle\OrderBundle\DependencyInjection\Compiler\RegisterCartContextsPass;
+use Sylius\Bundle\OrderBundle\DependencyInjection\Compiler\RegisterProcessorsPass;
+use Sylius\Bundle\OrderBundle\DependencyInjection\SyliusOrderExtension;
+use Sylius\Component\Order\Context\CartContextInterface;
+use Sylius\Component\Order\Processor\OrderProcessorInterface;
+use Symfony\Component\DependencyInjection\Definition;
+
+final class SyliusOrderExtensionTest extends AbstractExtensionTestCase
+{
+    /**
+     * @test
+     */
+    public function it_autoconfigures_cart_contexts_and_order_processors(): void
+    {
+        $this->container->setDefinition(
+            'acme.cart_context_autoconfigured',
+            (new Definition())
+                ->setClass(self::getMockClass(CartContextInterface::class))
+                ->setAutoconfigured(true)
+        );
+
+        $this->container->setDefinition(
+            'acme.processor_autoconfigured',
+            (new Definition())
+                ->setClass(self::getMockClass(OrderProcessorInterface::class))
+                ->setAutoconfigured(true)
+        );
+
+        $this->load();
+        $this->compile();
+
+        $this->assertContainerBuilderHasServiceDefinitionWithTag(
+            'acme.cart_context_autoconfigured',
+            RegisterCartContextsPass::CART_CONTEXT_SERVICE_TAG
+        );
+
+        $this->assertContainerBuilderHasServiceDefinitionWithTag(
+            'acme.processor_autoconfigured',
+            RegisterProcessorsPass::PROCESSOR_SERVICE_TAG
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getContainerExtensions(): array
+    {
+        return [new SyliusOrderExtension()];
+    }
+}
