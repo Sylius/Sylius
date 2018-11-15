4ef4380281e1bdf9e1de3fbc0683c44dad1152a5
Laurent Clouet <laurent.clouet@cyclad.fr>
2018-11-15 22:31:32 +0100

    PHPDoc added about transformation

--- src/Sylius/Bundle/PromotionBundle/Form/Type/Action/PercentFloatType.php
+++ src/Sylius/Bundle/PromotionBundle/Form/Type/Action/PercentFloatType.php
@@ -1,13 +1,23 @@
 <?php
 
-namespace Sylius\Bundle\PromotionBundle\Form\Type\Action;
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
+declare(strict_types=1);
+
+namespace Sylius\Bundle\PromotionBundle\Form\Type\Action;
 
 use Sylius\Bundle\PromotionBundle\Form\DataTransformer\PercentFloatToLocalizedStringTransformer;
 use Symfony\Component\Form\Extension\Core\Type\PercentType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class PercentFloatType extends PercentType
+final class PercentFloatType extends PercentType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
