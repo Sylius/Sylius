c2af9ea1914be9fd69923098ed99591b13d8e368
Laurent Clouet <laurent.clouet@cyclad.fr>
2018-11-18 15:26:58 +0100

    #9858 Directly change view transformer to percent float when building the form for promotion

--- src/Sylius/Bundle/PromotionBundle/Form/Type/Action/PercentageDiscountConfigurationType.php
+++ src/Sylius/Bundle/PromotionBundle/Form/Type/Action/PercentageDiscountConfigurationType.php
@@ -13,7 +13,9 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\PromotionBundle\Form\Type\Action;
 
+use Sylius\Bundle\PromotionBundle\Form\DataTransformer\PercentFloatToLocalizedStringTransformer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PercentType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Range;
@@ -27,7 +29,7 @@ final class PercentageDiscountConfigurationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('percentage', PercentFloatType::class, [
+            ->add('percentage', PercentType::class, [
                 'label' => 'sylius.form.promotion_action.percentage_discount_configuration.percentage',
                 'constraints' => [
                     new NotBlank(['groups' => ['sylius']]),
@@ -42,6 +44,7 @@ final class PercentageDiscountConfigurationType extends AbstractType
                 ],
             ])
         ;
+        $builder->get('percentage')->resetViewTransformers()->addViewTransformer(new PercentFloatToLocalizedStringTransformer());
     }
 
     /**
