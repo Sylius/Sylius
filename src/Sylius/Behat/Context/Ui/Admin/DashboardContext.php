88ff580e3aedf825dc221db5640e403d010d1ede
Loïc Frémont <lc.fremont@gmail.com>
2018-11-22 16:27:50 +0100

    fix dashboard context

--- src/Sylius/Behat/Context/Ui/Admin/DashboardContext.php
+++ src/Sylius/Behat/Context/Ui/Admin/DashboardContext.php
@@ -14,8 +14,8 @@ declare(strict_types=1);
 namespace Sylius\Behat\Context\Ui\Admin;
 
 use Behat\Behat\Context\Context;
+use FriendsOfBehat\PageObjectExtension\Page\UnexpectedPageException;
 use Sylius\Behat\Page\Admin\DashboardPageInterface;
-use Sylius\Behat\Page\UnexpectedPageException;
 use Sylius\Component\Core\Formatter\StringInflector;
 use Webmozart\Assert\Assert;
 
