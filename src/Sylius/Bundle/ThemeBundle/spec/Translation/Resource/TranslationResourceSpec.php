6b9357d467d3e9d7fb32b8d3feee1851937342d8
Kamil Kokot <kamil@kokot.me>
2018-12-31 13:57:05 +0100

    Fix spec classnames / filenames mismatches

--- src/Sylius/Bundle/ThemeBundle/spec/Translation/Resource/TranslationResourceSpec.php
+++ src/Sylius/Bundle/ThemeBundle/spec/Translation/Resource/TranslationResourceSpec.php
@@ -11,7 +11,7 @@
 
 declare(strict_types=1);
 
-namespace spec\Sylius\Bundle\ThemeBundle\Translation\Provider;
+namespace spec\Sylius\Bundle\ThemeBundle\Translation\Resource;
 
 use PhpSpec\ObjectBehavior;
 use Sylius\Bundle\ThemeBundle\Translation\Resource\TranslationResourceInterface;
