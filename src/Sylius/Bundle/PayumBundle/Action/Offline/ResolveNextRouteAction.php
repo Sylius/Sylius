bd64b8dbf6076596e1c579471da2faa2f2bc3263
Jais Djurhuus-Kempel <jdk@combine.dk>
2019-02-22 13:44:16 +0200

    trying something

--- src/Sylius/Bundle/PayumBundle/Action/Offline/ResolveNextRouteAction.php
+++ src/Sylius/Bundle/PayumBundle/Action/Offline/ResolveNextRouteAction.php
@@ -15,6 +15,7 @@ namespace Sylius\Bundle\PayumBundle\Action\Offline;
 
 use Payum\Core\Action\ActionInterface;
 use Sylius\Bundle\PayumBundle\Request\ResolveNextRoute;
+use Sylius\Component\Core\Model\PaymentMethodInterface;
 use Sylius\Component\Payment\Model\PaymentInterface;
 
 final class ResolveNextRouteAction implements ActionInterface
@@ -26,6 +27,20 @@ final class ResolveNextRouteAction implements ActionInterface
      */
     public function execute($request): void
     {
+        /** @var PaymentInterface $payment */
+        $payment = $request->getFirstModel();
+
+        /** @var PaymentMethodInterface $paymentMethod */
+        $paymentMethod = $payment->getMethod();
+
+        $gatewayConfig = $paymentMethod->getGatewayConfig()->getConfig();
+
+        if (isset($gatewayConfig['use_authorize']) && $gatewayConfig['use_authorize'] == 1) {
+            $request->setRouteName('sylius_shop_order_show');
+            $request->setRouteParameters(['tokenValue' => $payment->getOrder()->getTokenValue()]);
+            return;
+        }
+
         $request->setRouteName('sylius_shop_order_thank_you');
     }
 
