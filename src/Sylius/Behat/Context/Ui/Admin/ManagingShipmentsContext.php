8be7b0ce320ad3222d7368fbf327e5ba8bf4d271
Adam Kasperczak <adamkasperczak123@gmail.com>
2019-03-20 12:05:56 +0100

    Added services for show shipment page in behat

--- src/Sylius/Behat/Context/Ui/Admin/ManagingShipmentsContext.php
+++ src/Sylius/Behat/Context/Ui/Admin/ManagingShipmentsContext.php
@@ -5,24 +5,28 @@ declare(strict_types=1);
 namespace Sylius\Behat\Context\Ui\Admin;
 
 use Behat\Behat\Context\Context;
+use Sylius\Behat\Page\Admin\Crud\IndexPageInterface;
 
 final class ManagingShipmentsContext implements Context
 {
-
     /** @var IndexPageInterface */
     private $indexPage;
 
+    public function __construct(IndexPageInterface $indexPage)
+    {
+        $this->indexPage = $indexPage;
+    }
 
     /**
      * @When I browse shipments
      */
-    public function BrowseShipments()
+    public function BrowseShipments(): void
     {
-
+        $this->indexPage->open();
     }
 
     /**
-     * @Then I should see two shipments in the list
+     * @Then I should see single shipment in the list
      */
     public function iShouldSeeTwoShipmentsInTheList()
     {
@@ -30,17 +34,9 @@ final class ManagingShipmentsContext implements Context
     }
 
     /**
-     * @Given the shipment of the \#00000001 order should be "([^"]*)"
+     * @Then the shipment of the :orderNumber order should be :shippingState
      */
     public function theShipmentOfTheOrderShouldBe($arg1, $arg2)
-    {
-        throw new PendingException();
-    }
-
-    /**
-     * @Given the shipment of the \#00000002 order should be "([^"]*)"
-     */
-    public function theShipmentOfTheOrderShouldBe1($arg1, $arg2)
     {
         throw new PendingException();
     }
