320cf977b8acf9385d666db1d1c9384f44eaef9d
mamazu <14860264+mamazu@users.noreply.github.com>
2019-05-03 16:07:17 +0200

    Adding a coupon code generator command

---
+++ src/Sylius/Bundle/PromotionBundle/Command/GenerateCouponCommand.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\PromotionBundle\Command;
+
+use Sylius\Bundle\CoreBundle\Doctrine\ORM\PromotionRepository;
+use Sylius\Component\Core\Model\PromotionInterface;
+use Sylius\Component\Promotion\Generator\PromotionCouponGeneratorInstruction;
+use Sylius\Component\Promotion\Generator\PromotionCouponGeneratorInstructionInterface;
+use Sylius\Component\Promotion\Generator\PromotionCouponGeneratorInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class GenerateCouponsCommand extends Command
+{
+    /** @var PromotionRepository */
+    private $promotionRepository;
+
+    /** @var PromotionCouponGeneratorInterface */
+    private $couponGenerator;
+
+    /**
+     * GenerateCouponsCommand constructor.
+     *
+     * @param PromotionRepository               $promotionRepository
+     * @param PromotionCouponGeneratorInterface $couponGenerator
+     */
+    public function __construct(
+        PromotionRepository $promotionRepository,
+        PromotionCouponGeneratorInterface $couponGenerator
+    ) {
+        parent::__construct(null);
+        $this->promotionRepository = $promotionRepository;
+        $this->couponGenerator     = $couponGenerator;
+    }
+
+    /**  {@inheritdoc} */
+    protected function configure(): void
+    {
+        $this
+            ->setName('sylius:promotion:generate-coupons')
+            ->setDescription('Generates coupons for a given promotion');
+        $this->addArgument('promotionCode', InputArgument::REQUIRED, 'Code of the promotion');
+        $this->addArgument('count', InputArgument::REQUIRED, 'Amount of coupons to generate');
+				$this->addOption('length', 'len', InputOption::VALUE_OPTIONAL, 'Length of the coupon code (default 10)', '10')
+				;
+    }
+
+    /**  {@inheritdoc} */
+    public function execute(InputInterface $input, OutputInterface $output): void
+    {
+        /** @var string $promotionCode */
+        $promotionCode = $input->getArgument('promotionCode');
+
+        /** @var PromotionInterface|null $promotion */
+        $promotion = $this->promotionRepository->findOneBy(['code' => $promotionCode]);
+        if ($promotion === null) {
+            $output->writeln('<error>No promotion found with this code</error>');
+
+            return;
+        }
+
+        if (!$promotion->isCouponBased()) {
+            $output->writeln('<error>This promotion is not Coupon based</error>');
+
+            return;
+        }
+
+        $instruction = $this->getGeneratorInstructions(
+					(int) $input->getArgument('count'),
+					(int) $input->getArgument('length')
+			);
+        // Generates the promotions
+        $this->couponGenerator->generate($promotion, $instruction);
+    }
+
+    /**
+     * Creates a instruction object for the coupon generator
+     *
+     * @param int $count   How many coupons should be generated
+     * @param int $codeLength
+     *
+     * @return PromotionCouponGeneratorInstructionInterface
+     */
+    public function getGeneratorInstructions(int $count, int $codeLength): PromotionCouponGeneratorInstructionInterface
+    {
+        $instruction = new PromotionCouponGeneratorInstruction();
+        $instruction->setAmount($count);
+        $instruction->setCodeLength($codeLength);
+
+        return $instruction;
+    }
+}
---
+++ src/Sylius/Bundle/PromotionBundle/spec/Command/GenerateCouponCommandSpec.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Sylius\Bundle\PromotionBundle\Command;
+
+use Brille24\Core\Command\GenerateCouponsCommand;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Sylius\Bundle\CoreBundle\Doctrine\ORM\PromotionRepository;
+use Sylius\Component\Core\Model\PromotionInterface;
+use Sylius\Component\Promotion\Generator\PromotionCouponGeneratorInstruction;
+use Sylius\Component\Promotion\Generator\PromotionCouponGeneratorInstructionInterface;
+use Sylius\Component\Promotion\Generator\PromotionCouponGeneratorInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class GenerateCouponsCommandSpec extends ObjectBehavior
+{
+    public function let(
+        PromotionRepository $promotionRepository,
+        PromotionCouponGeneratorInterface $couponGenerator
+    ): void {
+        $this->beConstructedWith($promotionRepository, $couponGenerator);
+    }
+
+    public function it_initializes(): void
+    {
+        $this->shouldImplement(GenerateCouponsCommand::class);
+    }
+
+    public function it_throws_an_error_if_the_promotion_does_not_exist(
+        InputInterface $input,
+        OutputInterface $output,
+        PromotionRepository $promotionRepository,
+        PromotionCouponGeneratorInterface $couponGenerator
+    ): void {
+        $input->getArgument('promotionCode')->willReturn('SOME_CODE');
+        $promotionRepository->findOneBy(['code' => 'SOME_CODE'])->willReturn(null);
+
+        $output->writeln(Argument::containingString('error'))->shouldBeCalled();
+        $couponGenerator->generate(Argument::any())->shouldNotBeCalled();
+
+        $this->execute($input, $output);
+    }
+
+    public function it_throws_an_error_if_the_promotion_is_not_coupon_based(
+        InputInterface $input,
+        OutputInterface $output,
+        PromotionRepository $promotionRepository,
+        PromotionInterface $promotion,
+        PromotionCouponGeneratorInterface $couponGenerator
+    ): void {
+        $input->getArgument('promotionCode')->willReturn('SOME_CODE');
+        $promotionRepository->findOneBy(['code' => 'SOME_CODE'])->willReturn($promotion);
+
+        $promotion->isCouponBased()->willReturn(false);
+        $output->writeln(Argument::containingString('error'))->shouldBeCalled();
+        $couponGenerator->generate(Argument::any())->shouldNotBeCalled();
+
+        $this->execute($input, $output);
+    }
+
+    public function it_creates_a_coupon_generator_instruction(
+        InputInterface $input,
+        OutputInterface $output,
+        PromotionRepository $promotionRepository,
+        PromotionInterface $promotion,
+        PromotionCouponGeneratorInterface $couponGenerator
+    ): void {
+        $input->getArgument('promotionCode')->willReturn('SOME_CODE');
+        $input->getArgument('count')->willReturn('10');
+        $input->getArgument('length')->willReturn('20');
+        $promotionRepository->findOneBy(['code' => 'SOME_CODE'])->willReturn($promotion);
+
+        $promotion->isCouponBased()->willReturn(true);
+
+        // Testing the instruction generation
+        $expectedInstructions = new PromotionCouponGeneratorInstruction();
+        $expectedInstructions->setAmount(10);
+        $expectedInstructions->setCodeLength(20);
+
+        $this->getGeneratorInstructions(10, 20)
+            ->shouldBeLike($expectedInstructions);
+
+        $couponGenerator->generate(
+            $promotion,
+            Argument::type(PromotionCouponGeneratorInstructionInterface::class)
+        )->shouldBeCalled();
+
+        $this->execute($input, $output);
+    }
+}
