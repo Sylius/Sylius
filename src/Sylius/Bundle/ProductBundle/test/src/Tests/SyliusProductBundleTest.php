aeb89644719711a83763fc8f8d3d3f16cae3dbd4
Loïc Frémont <lc.fremont@gmail.com>
2019-05-13 11:27:19 +0200

    Fix default repository for sylius product association type

--- src/Sylius/Bundle/ProductBundle/test/src/Tests/SyliusProductBundleTest.php
+++ src/Sylius/Bundle/ProductBundle/test/src/Tests/SyliusProductBundleTest.php
@@ -14,6 +14,11 @@ declare(strict_types=1);
 namespace Sylius\Bundle\ProductBundle\Tests;
 
 use PHPUnit\Framework\Assert;
+use Sylius\Bundle\ProductBundle\Doctrine\ORM\ProductAssociationTypeRepository;
+use Sylius\Bundle\ProductBundle\Doctrine\ORM\ProductOptionRepository;
+use Sylius\Bundle\ProductBundle\Doctrine\ORM\ProductRepository;
+use Sylius\Bundle\ProductBundle\Doctrine\ORM\ProductVariantRepository;
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -36,6 +41,28 @@ final class SyliusProductBundleTest extends WebTestCase
 
         foreach ($services as $id) {
             Assert::assertNotNull($container->get($id, ContainerInterface::NULL_ON_INVALID_REFERENCE));
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function its_initializes_default_doctrine_repositories(): void
+    {
+        /** @var ContainerBuilder $container */
+        $container = self::createClient()->getContainer();
+
+        $repositories = [
+            'sylius.repository.product' => ProductRepository::class,
+            'sylius.repository.product_association_type' => ProductAssociationTypeRepository::class,
+            'sylius.repository.product_variant' => ProductVariantRepository::class,
+            'sylius.repository.product_attribute' => EntityRepository::class,
+            'sylius.repository.product_option' => ProductOptionRepository::class,
+        ];
+
+        foreach ($repositories as $id => $class) {
+            $service = $container->get($id, ContainerInterface::NULL_ON_INVALID_REFERENCE);
+            Assert::assertEquals(get_class($service), $repositories[$id]);
         }
     }
 }
