f712071c78911cef6fefef0be784e5862d6c9e5f
mamazu <14860264+mamazu@users.noreply.github.com>
2019-05-13 19:32:39 +0200

    Implemented suggestions

--- src/Sylius/Bundle/PromotionBundle/Command/GenerateCouponsCommand.php
+++ src/Sylius/Bundle/PromotionBundle/Command/GenerateCouponsCommand.php
@@ -1,11 +1,20 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace Sylius\Bundle\PromotionBundle\Command;
 
-use Sylius\Bundle\CoreBundle\Doctrine\ORM\PromotionRepository;
 use Sylius\Component\Core\Model\PromotionInterface;
+use Sylius\Component\Core\Repository\PromotionRepositoryInterface;
 use Sylius\Component\Promotion\Generator\PromotionCouponGeneratorInstruction;
 use Sylius\Component\Promotion\Generator\PromotionCouponGeneratorInstructionInterface;
 use Sylius\Component\Promotion\Generator\PromotionCouponGeneratorInterface;
@@ -17,44 +26,37 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 final class GenerateCouponsCommand extends Command
 {
-    /** @var PromotionRepository */
+    /** @var PromotionRepositoryInterface */
     private $promotionRepository;
 
     /** @var PromotionCouponGeneratorInterface */
     private $couponGenerator;
 
-    /**
-     * GenerateCouponsCommand constructor.
-     *
-     * @param PromotionRepository               $promotionRepository
-     * @param PromotionCouponGeneratorInterface $couponGenerator
-     */
     public function __construct(
-        PromotionRepository $promotionRepository,
+        PromotionRepositoryInterface $promotionRepository,
         PromotionCouponGeneratorInterface $couponGenerator
     ) {
-        parent::__construct(null);
+        parent::__construct();
+
         $this->promotionRepository = $promotionRepository;
-        $this->couponGenerator     = $couponGenerator;
+        $this->couponGenerator = $couponGenerator;
     }
 
-    /**  {@inheritdoc} */
     protected function configure(): void
     {
         $this
             ->setName('sylius:promotion:generate-coupons')
             ->setDescription('Generates coupons for a given promotion')
-            ->addArgument('promotionCode', InputArgument::REQUIRED, 'Code of the promotion')
+            ->addArgument('promotion-code', InputArgument::REQUIRED, 'Code of the promotion')
             ->addArgument('count', InputArgument::REQUIRED, 'Amount of coupons to generate')
-            ->addOption('length', 'len', InputOption::VALUE_OPTIONAL, 'Length of the coupon code (default 10)', '10')
+            ->addOption('length', 'len', InputOption::VALUE_OPTIONAL, 'Length of the coupon code (default 10)', 10)
         ;
     }
 
-    /**  {@inheritdoc} */
     public function execute(InputInterface $input, OutputInterface $output): void
     {
         /** @var string $promotionCode */
-        $promotionCode = $input->getArgument('promotionCode');
+        $promotionCode = $input->getArgument('promotion-code');
 
         /** @var PromotionInterface|null $promotion */
         $promotion = $this->promotionRepository->findOneBy(['code' => $promotionCode]);
@@ -65,7 +67,7 @@ final class GenerateCouponsCommand extends Command
         }
 
         if (!$promotion->isCouponBased()) {
-            $output->writeln('<error>This promotion is not Coupon based</error>');
+            $output->writeln('<error>This promotion is not coupon based</error>');
 
             return;
         }
@@ -74,8 +76,10 @@ final class GenerateCouponsCommand extends Command
             (int) $input->getArgument('count'),
             (int) $input->getArgument('length')
         );
-        // Generates the promotions
+
         $this->couponGenerator->generate($promotion, $instruction);
+
+        $output->writeln('<info>Coupons have been generated</info>');
     }
 
     /**
