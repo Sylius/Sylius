21f89bf130f47e2e03e8b3a99f1ed5e6ecfa95d9
Olivier ALLAIN <olivier.allain@synolia.com>
2019-05-27 13:38:44 +0200

    fix multi-line sprintf

--- src/Sylius/Bundle/CoreBundle/Fixture/Factory/ProductExampleFactory.php
+++ src/Sylius/Bundle/CoreBundle/Fixture/Factory/ProductExampleFactory.php
@@ -297,8 +297,8 @@ class ProductExampleFactory extends AbstractExampleFactory implements ExampleFac
         foreach ($options['images'] as $image) {
             if (!array_key_exists('path', $image)) {
                 @trigger_error(
-                    'It is deprecated since Sylius 1.3 to pass indexed array as an image definition.
-                    Please use associative array with "path" and "type" keys instead.',
+                    'It is deprecated since Sylius 1.3 to pass indexed array as an image definition. ' .
+                    'Please use associative array with "path" and "type" keys instead.',
                     \E_USER_DEPRECATED
                 );
 
