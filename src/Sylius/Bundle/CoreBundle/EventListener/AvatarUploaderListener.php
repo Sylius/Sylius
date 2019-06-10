1f7736b52a1116bd8b68f5cd314987c037880a6d
Tomanhez <tomek.sut@gmail.com>
2019-06-10 16:29:06 +0200

    Add UploaderInterface and service

---
+++ src/Sylius/Bundle/CoreBundle/EventListener/AvatarUploaderListener.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\EventListener;
+
+use Sylius\Component\Core\Model\AvatarAwareInterface;
+use Sylius\Component\Core\Uploader\ImageUploaderInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Webmozart\Assert\Assert;
+
+final class AvatarUploaderListener
+{
+    /** @var ImageUploaderInterface */
+    private $uploader;
+
+    public function __construct(ImageUploaderInterface $uploader)
+    {
+        $this->uploader = $uploader;
+    }
+
+    public function uploadAvatar(GenericEvent $event): void
+    {
+        $subject = $event->getSubject();
+        Assert::isInstanceOf($subject, AvatarAwareInterface::class);
+
+        $this->uploader->upload($subject->getAvatarImage());
+    }
+
+}
