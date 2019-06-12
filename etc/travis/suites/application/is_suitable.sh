97dfb6e74c3f5b4285f0afb9f2301744191d78a1
Kamil Kokot <kamil@kokot.me>
2019-06-12 22:30:39 +0800

    Do not run application tests if SECURITY.md is changed

--- etc/travis/suites/application/is_suitable.sh
+++ etc/travis/suites/application/is_suitable.sh
@@ -6,7 +6,7 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     exit 0 # Always execute full suite on branch builds
 fi
 
-if [ $(git diff --name-only HEAD origin/$TRAVIS_BRANCH | grep -c -v -e ^docs -e ^.github -e ^CHANGELOG -e ^LICENSE -e ^README -e ^UPGRADE) -eq 0 ]; then
+if [ $(git diff --name-only HEAD origin/$TRAVIS_BRANCH | grep -c -v -e ^docs -e ^.github -e ^CHANGELOG -e ^LICENSE -e ^README -e ^UPGRADE -e ^SECURITY) -eq 0 ]; then
     print_header "Skipped suite" "Application"
     print_warning "No other changes than those in docs/* were found"
     exit 1
