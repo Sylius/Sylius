c94006a68b2ab507d044e0db606a6087066fdae0
Kamil Kokot <kamil@kokot.me>
2019-06-20 18:47:31 +0800

    Apply coding standards fixes

--- src/Sylius/Bundle/CoreBundle/spec/Fixture/LocaleFixtureSpec.php
+++ src/Sylius/Bundle/CoreBundle/spec/Fixture/LocaleFixtureSpec.php
@@ -1,13 +1,21 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace spec\Sylius\Bundle\CoreBundle\Fixture;
 
 use Doctrine\Common\Persistence\ObjectManager;
-use Sylius\Bundle\CoreBundle\Fixture\LocaleFixture;
 use PhpSpec\ObjectBehavior;
-use Prophecy\Argument;
 use Sylius\Bundle\FixturesBundle\Fixture\FixtureInterface;
-use Sylius\Component\Locale\Model\Locale;
 use Sylius\Component\Locale\Model\LocaleInterface;
 use Sylius\Component\Resource\Factory\FactoryInterface;
 
