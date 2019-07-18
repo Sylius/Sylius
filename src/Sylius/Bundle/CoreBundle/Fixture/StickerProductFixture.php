6ed4ffa9900d894df8cb754eceb31dbef319e587
Adam Kasperczak <adamkasperczak123@gmail.com>
2019-07-18 11:00:42 +0200

    Jeans and dresses fixtures

--- src/Sylius/Bundle/CoreBundle/Fixture/StickerProductFixture.php
+++ src/Sylius/Bundle/CoreBundle/Fixture/StickerProductFixture.php
@@ -13,6 +13,8 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\CoreBundle\Fixture;
 
+@trigger_error('The "StickerProductFixture" class is deprecated since Sylius 1.5 Use new product fixtures class located at "src/Sylius/Bundle/CoreBundle/Fixture/" instead.', \E_USER_DEPRECATED);
+
 use Sylius\Bundle\FixturesBundle\Fixture\AbstractFixture;
 use Sylius\Component\Attribute\AttributeType\TextAttributeType;
 use Sylius\Component\Core\Model\ProductInterface;
