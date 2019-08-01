04e2cad916c81e6b9a53dcfadb8cb8c8a5ab0370
Kamil Kokot <kamil@kokot.me>
2019-08-01 09:50:51 +0200

    PR review fixes

--- src/Sylius/Component/Core/Generator/ImagePathGeneratorInterface.php
+++ src/Sylius/Component/Core/Generator/ImagePathGeneratorInterface.php
@@ -17,5 +17,8 @@ use Sylius\Component\Core\Model\ImageInterface;
 
 interface ImagePathGeneratorInterface
 {
+    /**
+     * It needs to return a different value on each call, so that consumers don't end up in an infinite loop.
+     */
     public function generate(ImageInterface $image): string;
 }
