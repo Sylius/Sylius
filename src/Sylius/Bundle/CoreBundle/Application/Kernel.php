ede39cd333fadca773b284e4f0af0fab4e03aab9
Kamil Kokot <kamil@kokot.me>
2019-08-27 15:53:52 +0200

    Change application's version to v1.4.8

--- src/Sylius/Bundle/CoreBundle/Application/Kernel.php
+++ src/Sylius/Bundle/CoreBundle/Application/Kernel.php
@@ -31,12 +31,12 @@ use Webmozart\Assert\Assert;
 
 class Kernel extends HttpKernel
 {
-    public const VERSION = '1.4.8-DEV';
+    public const VERSION = '1.4.8';
     public const VERSION_ID = '10408';
     public const MAJOR_VERSION = '1';
     public const MINOR_VERSION = '4';
     public const RELEASE_VERSION = '8';
-    public const EXTRA_VERSION = 'DEV';
+    public const EXTRA_VERSION = '';
 
     public function __construct(string $environment, bool $debug)
     {
