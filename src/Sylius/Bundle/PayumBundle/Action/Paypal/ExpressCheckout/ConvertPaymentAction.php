ab3bcc45a1801d1b65a8ac9b61a3e4b2599b93d7
Mateusz Zalewski <zaleslaw@gmail.com>
2019-11-05 15:56:35 +0100

    [Payum][Checkout] Pass address data to PayPal after checkout

--- src/Sylius/Bundle/PayumBundle/Action/Paypal/ExpressCheckout/ConvertPaymentAction.php
+++ src/Sylius/Bundle/PayumBundle/Action/Paypal/ExpressCheckout/ConvertPaymentAction.php
@@ -51,6 +51,8 @@ final class ConvertPaymentAction implements ActionInterface
         $details['PAYMENTREQUEST_0_AMT'] = $this->formatPrice($order->getTotal());
         $details['PAYMENTREQUEST_0_ITEMAMT'] = $this->formatPrice($order->getTotal());
 
+        $details = $this->prepareAddressData($order, $details);
+
         $m = 0;
         foreach ($order->getItems() as $item) {
             $details['L_PAYMENTREQUEST_0_NAME' . $m] = $item->getVariant()->getProduct()->getName();
@@ -100,5 +102,28 @@ final class ConvertPaymentAction implements ActionInterface
     private function formatPrice(int $price): float
     {
         return round($price / 100, 2);
+    }
+
+    private function prepareAddressData(OrderInterface $order, array $details): array
+    {
+        $details['EMAIL'] = $order->getCustomer()->getEmail();
+        $billingAddress = $order->getBillingAddress();
+        $details['LOCALECODE'] = $billingAddress->getCountryCode();
+        $details['PAYMENTREQUEST_0_SHIPTONAME'] = $billingAddress->getFullName();
+        $details['PAYMENTREQUEST_0_SHIPTOSTREET'] = $billingAddress->getStreet();
+        $details['PAYMENTREQUEST_0_SHIPTOCITY'] = $billingAddress->getCity();
+        $details['PAYMENTREQUEST_0_SHIPTOZIP'] = $billingAddress->getPostcode();
+        $details['PAYMENTREQUEST_0_SHIPTOCOUNTRYCODE'] = $billingAddress->getCountryCode();
+
+        if ($billingAddress->getPhoneNumber() !== null) {
+            $details['PAYMENTREQUEST_0_SHIPTOPHONENUM'] = $billingAddress->getPhoneNumber();
+        }
+
+        $province = $billingAddress->getProvinceCode() ?? $billingAddress->getProvinceName();
+        if ($province !== null) {
+            $details['PAYMENTREQUEST_0_SHIPTOSTATE'] = $province;
+        }
+
+        return $details;
     }
 }
