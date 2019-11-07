0acde3ae2cd6572e02b04cdd80f095d1d4ddf044
kulczy <sz.kulczy@gmail.com>
2019-11-07 14:02:31 +0100

    New Webpack approach

--- config/bundles.php
+++ config/bundles.php
@@ -70,5 +70,4 @@ return [
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true, 'test_cached' => true],
     Sylius\Behat\Application\SyliusTestPlugin\SyliusTestPlugin::class => ['test' => true, 'test_cached' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
