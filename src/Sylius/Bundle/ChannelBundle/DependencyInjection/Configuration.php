a6b93bd35f223f9eb785a44a1955ac539be32c1c
Kamil Kokot <kamil@kokot.me>
2019-11-08 12:47:42 +0100

    Remove usage of deprecated "Symfony\Component\Config\Definition\Builder\TreeBuilder::root()"

--- src/Sylius/Bundle/ChannelBundle/DependencyInjection/Configuration.php
+++ src/Sylius/Bundle/ChannelBundle/DependencyInjection/Configuration.php
@@ -30,14 +30,8 @@ final class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        if (method_exists(TreeBuilder::class, 'getRootNode')) {
-            $treeBuilder = new TreeBuilder('sylius_channel');
-            $rootNode = $treeBuilder->getRootNode();
-        } else {
-            // BC layer for symfony/config 4.1 and older
-            $treeBuilder = new TreeBuilder();
-            $rootNode = $treeBuilder->root('sylius_channel');
-        }
+        $treeBuilder = new TreeBuilder('sylius_channel');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
--- src/Sylius/Bundle/LocaleBundle/DependencyInjection/Configuration.php
+++ src/Sylius/Bundle/LocaleBundle/DependencyInjection/Configuration.php
@@ -30,14 +30,8 @@ final class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        if (method_exists(TreeBuilder::class, 'getRootNode')) {
-            $treeBuilder = new TreeBuilder('sylius_locale');
-            $rootNode = $treeBuilder->getRootNode();
-        } else {
-            // BC layer for symfony/config 4.1 and older
-            $treeBuilder = new TreeBuilder();
-            $rootNode = $treeBuilder->root('sylius_locale');
-        }
+        $treeBuilder = new TreeBuilder('sylius_locale');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
