af829bd3dd8890c343b0f2f5622b02a9d5fa4fc3
Adam Kasperczak <adamkasperczak123@gmail.com>
2020-01-12 14:44:18 +0100

    Rework behats - home page

--- src/Sylius/Behat/Page/Shop/HomePage.php
+++ src/Sylius/Behat/Page/Shop/HomePage.php
@@ -41,12 +41,16 @@ class HomePage extends SymfonyPage implements HomePageInterface
 
     public function getFullName(): string
     {
-        return $this->getElement('full_name')->getText();
+        if ($this->hasElement('full_name')) {
+            return $this->getElement('full_name')->getText();
+        }
+
+        return '';
     }
 
     public function getActiveCurrency(): string
     {
-        return $this->getElement('currency_selector')->find('css', '.sylius-active-currency')->getText();
+        return $this->getElement('currency_selector')->find('css', '[data-test-active-currency]')->getText();
     }
 
     public function getAvailableCurrencies(): array
@@ -55,7 +59,7 @@ class HomePage extends SymfonyPage implements HomePageInterface
             function (NodeElement $element) {
                 return $element->getText();
             },
-            $this->getElement('currency_selector')->findAll('css', '.sylius-available-currency')
+            $this->getElement('currency_selector')->findAll('css', '[data-test-available-currency]')
         );
     }
 
@@ -71,7 +75,7 @@ class HomePage extends SymfonyPage implements HomePageInterface
 
     public function getActiveLocale(): string
     {
-        return $this->getElement('locale_selector')->find('css', '.sylius-active-locale')->getText();
+        return $this->getElement('locale_selector')->find('css', '[data-test-active-locale]')->getText();
     }
 
     public function getAvailableLocales(): array
@@ -80,7 +84,7 @@ class HomePage extends SymfonyPage implements HomePageInterface
             function (NodeElement $element) {
                 return $element->getText();
             },
-            $this->getElement('locale_selector')->findAll('css', '.sylius-available-locale')
+            $this->getElement('locale_selector')->findAll('css', '[data-test-available-locale]')
         );
     }
 
@@ -95,17 +99,17 @@ class HomePage extends SymfonyPage implements HomePageInterface
             function (NodeElement $element) {
                 return $element->getText();
             },
-            $this->getElement('latest_products')->findAll('css', '.sylius-product-name')
+            $this->getElement('latest_products')->findAll('css', '[data-test-product-name]')
         );
     }
 
     protected function getDefinedElements(): array
     {
         return array_merge(parent::getDefinedElements(), [
-            'currency_selector' => '#sylius-currency-selector',
-            'full_name' => '.right.menu .item',
+            'currency_selector' => '[data-test-currency-selector]',
+            'full_name' => '[data-test-full-name]',
             'latest_products' => '[data-test-latest-products]',
-            'locale_selector' => '#sylius-locale-selector',
+            'locale_selector' => '[data-test-locale-selector]',
             'logout_button' => '[data-test-logout-button]',
         ]);
     }
