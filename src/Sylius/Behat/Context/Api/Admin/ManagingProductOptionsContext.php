bd9bafc3f5ac05161278d459d609d032cdb7f503
Mateusz Zalewski <zaleslaw@gmail.com>
2020-02-19 11:23:23 +0100

    [Behat][Api] Browsing product options scenario implementation

---
+++ src/Sylius/Behat/Context/Api/Admin/ManagingProductOptionsContext.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Behat\Context\Api\Admin;
+
+use Behat\Behat\Context\Context;
+use Sylius\Behat\Client\ApiClientInterface;
+use Webmozart\Assert\Assert;
+
+final class ManagingProductOptionsContext implements Context
+{
+    /** @var ApiClientInterface */
+    private $client;
+
+    public function __construct(ApiClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @When I browse product options
+     */
+    public function iBrowseProductOptions(): void
+    {
+        $this->client->index('product-options');
+    }
+
+    /**
+     * @Then I should see :count product options in the list
+     */
+    public function iShouldSeeProductOptionsInTheList(int $count): void
+    {
+        $itemsCount = $this->client->countCollectionItems();
+
+        Assert::eq($count, $itemsCount, sprintf('Expected %d product options, but got %d', $count, $itemsCount));
+    }
+
+    /**
+     * @Then the product option :productOptionName should be in the registry
+     */
+    public function theProductOptionShouldAppearInTheRegistry(string $productOptionName): void
+    {
+        $this->client->index('product-options');
+        $this->assertProductOptionWithData('name', $productOptionName);
+    }
+
+    private function assertProductOptionWithData(string $element, string $currencyName): void
+    {
+        $currencies = $this->client->getCollection();
+
+        foreach ($currencies as $currency) {
+            if ($currency[$element] === $currencyName) {
+                return;
+            }
+        }
+
+        throw new \Exception(sprintf('There is no product option with %s "%s" in the list', $element, $currencyName));
+    }
+}
