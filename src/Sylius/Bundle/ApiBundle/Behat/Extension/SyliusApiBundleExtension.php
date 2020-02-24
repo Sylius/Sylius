2cd5e94689e7474f33518e24f99f5a5b0b77adb4
Mateusz Zalewski <zaleslaw@gmail.com>
2020-02-24 13:32:12 +0100

    [API] Simplify serialization groups

--- src/Sylius/Bundle/ApiBundle/Behat/Extension/SyliusApiBundleExtension.php
+++ src/Sylius/Bundle/ApiBundle/Behat/Extension/SyliusApiBundleExtension.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace Sylius\Bundle\ApiBundle\Behat\Extension;
@@ -13,6 +22,9 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
 
+/**
+ * This extension disables javascript session when running api scenarios
+ */
 final class SyliusApiBundleExtension implements Extension
 {
     public function process(ContainerBuilder $container): void
