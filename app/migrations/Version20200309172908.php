35bfe8c9346426adc083f5a40438f87bb7bc0c91
Manuele Menozzi <manuele.menozzi@gmail.com>
2020-03-22 14:52:52 +0100

    Add default value to variant enabled column migration

--- app/migrations/Version20200309172908.php
+++ app/migrations/Version20200309172908.php
@@ -22,7 +22,7 @@ final class Version20200309172908 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE sylius_product_variant ADD enabled TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE sylius_product_variant ADD enabled TINYINT(1) DEFAULT 1 NOT NULL');
     }
 
     public function down(Schema $schema) : void
