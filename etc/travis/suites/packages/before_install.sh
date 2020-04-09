0f64d069094c8aa85cb23cf3296f44b52088f2bf
Youri Westerman <tetracon@gmail.com>
2020-04-09 10:17:43 +0200

    Attempting to create swap space to accomodate composer's memory issues

--- etc/travis/suites/packages/before_install.sh
+++ etc/travis/suites/packages/before_install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+# Make swap
+#https://getcomposer.org/doc/articles/troubleshooting.md#proc-open-fork-failed-errors
+sudo fallocate -l 4G /var/swap.1
+sudo /sbin/mkswap /var/swap.1
+sudo /sbin/swapon /var/swap.1
+
 source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/common.lib.sh"
 
 # To be able to install sylius/* packages by symlinking
