f2234ede207ee78a2f4cf525bb725272649eeb23
Łukasz Chruściel <lchrusciel@gmail.com>
2020-04-10 19:54:45 +0200

    [Maintenance][Travis] Upgrade php memory to 4G

--- etc/travis/suites/application/before_install.sh
+++ etc/travis/suites/application/before_install.sh
@@ -5,6 +5,7 @@ source "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../../bash/application.
 
 print_header "Activating memcached extension" "Sylius"
 run_command "echo \"extension = memcached.so\" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini" || exit $?
+run_command "echo \"memory_limit=4096M\" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini" || exit $?
 
 print_header "Updating Composer" "Sylius"
 run_command "composer self-update --preview"
