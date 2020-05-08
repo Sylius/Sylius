951145fe8166156fdff4f8f2329df363a88eb39e
Tomanhez <tomek.sut@gmail.com>
2020-05-08 13:06:33 +0200

    Add models

---
+++ src/Sylius/Bundle/ApiBundle/Model/Client.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\ApiBundle\Model;
+
+use FOS\OAuthServerBundle\Entity\Client as BaseClient;
+
+class Client extends BaseClient implements ClientInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getPublicId(): string
+    {
+        return $this->getRandomId();
+    }
+}
