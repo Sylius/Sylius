dfea1bcf03238db116d50968c4f518c30cece252
Łukasz Chruściel <lchrusciel@gmail.com>
2020-07-15 15:48:21 +0200

    [API][Behat] Usage of proper tense in background

--- src/Sylius/Behat/Context/Setup/CartContext.php
+++ src/Sylius/Behat/Context/Setup/CartContext.php
@@ -14,61 +14,25 @@ declare(strict_types=1);
 namespace Sylius\Behat\Context\Setup;
 
 use Behat\Behat\Context\Context;
-use Sylius\Behat\Service\SharedStorageInterface;
 use Sylius\Bundle\ApiBundle\Command\Cart\AddItemToCart;
-use Sylius\Bundle\ApiBundle\Command\PickupCart;
-use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Core\Model\ProductInterface;
-use Sylius\Component\Order\Repository\OrderRepositoryInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
-use Webmozart\Assert\Assert;
 
 final class CartContext implements Context
 {
     /** @var MessageBusInterface */
     private $commandBus;
 
-    /** @var OrderRepositoryInterface */
-    private $orderRepository;
-
-    /** @var SharedStorageInterface */
-    private $sharedStorage;
-
-    public function __construct(
-        MessageBusInterface $commandBus,
-        OrderRepositoryInterface $orderRepository,
-        SharedStorageInterface $sharedStorage
-    ) {
+    public function __construct(MessageBusInterface $commandBus)
+    {
         $this->commandBus = $commandBus;
-        $this->orderRepository = $orderRepository;
-        $this->sharedStorage = $sharedStorage;
     }
 
     /**
-     * @Given I added product :product to the cart
+     * @Given /^I added (product "[^"]+") to the (cart)$/
      */
-    public function iAddedProductToTheCart(ProductInterface $product): void
+    public function iAddedProductToTheCart(ProductInterface $product, string $tokenValue): void
     {
-        /** @var OrderInterface|null $order */
-        $order = $this->orderRepository->findLatestCart();
-
-        if ($order === null) {
-            $this->commandBus->dispatch(new PickupCart());
-
-            /** @var OrderInterface|null $order */
-            $order = $this->orderRepository->findLatestCart();
-        }
-
-        Assert::notNull($order);
-
-        $cartToken = $order->getTokenValue();
-
-        $this->commandBus->dispatch(AddItemToCart::createFromData(
-            $cartToken,
-            $product->getCode(),
-            1
-        ));
-
-        $this->sharedStorage->set('cart_token', $cartToken);
+        $this->commandBus->dispatch(AddItemToCart::createFromData($tokenValue, $product->getCode(), 1));
     }
 }
