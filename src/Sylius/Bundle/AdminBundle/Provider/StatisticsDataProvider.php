30b6d187e2a70b1008e1a1c8df1f8fc20804e842
Alan Poulain <contact@alanpoulain.eu>
2020-08-28 17:58:05 +0200

    Introduce `DashboardStatisticsInterface`
    To allow modifying the `DashboardStatistics` model, for instance to add
    more statistics.
    The interface was missing to do so.

--- src/Sylius/Bundle/AdminBundle/Provider/StatisticsDataProvider.php
+++ src/Sylius/Bundle/AdminBundle/Provider/StatisticsDataProvider.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
 namespace Sylius\Bundle\AdminBundle\Provider;
 
 use Sylius\Bundle\MoneyBundle\Formatter\MoneyFormatterInterface;
-use Sylius\Component\Core\Dashboard\DashboardStatistics;
+use Sylius\Component\Core\Dashboard\DashboardStatisticsInterface;
 use Sylius\Component\Core\Dashboard\DashboardStatisticsProviderInterface;
 use Sylius\Component\Core\Dashboard\Interval;
 use Sylius\Component\Core\Dashboard\SalesDataProviderInterface;
@@ -43,7 +43,7 @@ class StatisticsDataProvider implements StatisticsDataProviderInterface
 
     public function getRawData(ChannelInterface $channel, \DateTimeInterface $startDate, \DateTimeInterface $endDate, string $interval): array
     {
-        /** @var DashboardStatistics $statistics */
+        /** @var DashboardStatisticsInterface $statistics */
         $statistics = $this->statisticsProvider->getStatisticsForChannelInPeriod($channel, $startDate, $endDate);
 
         $salesSummary = $this->salesDataProvider->getSalesSummary(
---
+++ src/Sylius/Component/Core/Dashboard/DashboardStatisticsInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Component\Core\Dashboard;
+
+use Sylius\Component\Core\Model\ChannelInterface;
+
+interface DashboardStatisticsInterface
+{
+    public function getChannel(): ?ChannelInterface;
+
+    public function getTotalSales(): int;
+
+    public function getNumberOfNewOrders(): int;
+
+    public function getNumberOfNewCustomers(): int;
+
+    public function getAverageOrderValue(): int;
+}
