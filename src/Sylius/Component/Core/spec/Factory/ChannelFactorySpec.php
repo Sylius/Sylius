4fe2f3c4ccc821bacda197c9b19f5747091d4bfe
Łukasz Chruściel <lchrusciel@gmail.com>
2020-09-07 15:18:08 +0200

    [Channel] Fix bc-breaking changes in ChannelFactory

--- src/Sylius/Component/Core/spec/Factory/ChannelFactorySpec.php
+++ src/Sylius/Component/Core/spec/Factory/ChannelFactorySpec.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
 namespace spec\Sylius\Component\Core\Factory;
 
 use PhpSpec\ObjectBehavior;
-use Sylius\Component\Core\Factory\ChannelFactoryInterface;
+use Sylius\Component\Channel\Factory\ChannelFactoryInterface;
 use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Component\Resource\Factory\FactoryInterface;
 
