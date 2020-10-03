b8b94171686dfa0ffc869280f64568cc3948befc
hatem20 <hatem.said50@gmail.com>
2020-10-03 02:14:34 +0200

    [API][Tax rate] Implement tax_rate_validation feature

--- src/Sylius/Behat/Context/Api/Admin/ManagingTaxRateContext.php
+++ src/Sylius/Behat/Context/Api/Admin/ManagingTaxRateContext.php
@@ -59,42 +59,57 @@ class ManagingTaxRateContext implements Context
 
     /**
      * @When I specify its code as :code
+     * @When I do not specify its code
      */
-    public function iSpecifyItsCodeAs(string $code): void
+    public function iSpecifyItsCodeAs(?string $code = null): void
     {
-        $this->client->addRequestData('code', $code);
+        if ($code) {
+            $this->client->addRequestData('code', $code);
+        }
     }
 
     /**
      * @When I name it :name
+     * @When I do not name it
      */
-    public function iNameIt(string $name): void
+    public function iNameIt(?string $name = null): void
     {
-        $this->client->addRequestData('name', $name);
+        if ($name) {
+            $this->client->addRequestData('name', $name);
+        }
     }
 
     /**
      * @When I define it for the :zone zone
+     * @When I do not specify its zone
      */
-    public function iDefineItForTheZone(ZoneInterface $zone): void
+    public function iDefineItForTheZone(?ZoneInterface $zone = null): void
     {
-        $this->client->addRequestData('zone', $this->iriConverter->getIriFromItem($zone));
+        if ($zone) {
+            $this->client->addRequestData('zone', $this->iriConverter->getIriFromItem($zone));
+        }
     }
 
     /**
      * @When I make it applicable for the :taxCategory tax category
+     * @When I do not specify related tax category
      */
-    public function iMakeItApplicableForTheTaxCategory(TaxCategoryInterface $taxCategory): void
+    public function iMakeItApplicableForTheTaxCategory(?TaxCategoryInterface $taxCategory = null): void
     {
-        $this->client->addRequestData('category', $this->iriConverter->getIriFromItem($taxCategory));
+        if ($taxCategory) {
+            $this->client->addRequestData('category', $this->iriConverter->getIriFromItem($taxCategory));
+        }
     }
 
     /**
      * @When I specify its amount as :amount%
+     * @When I do not specify its amount
      */
-    public function iSpecifyItsAmountAs(int $amount): void
+    public function iSpecifyItsAmountAs(?string $amount = null): void
     {
-        $this->client->addRequestData('amount', $amount);
+        if ($amount) {
+            $this->client->addRequestData('amount', $amount);
+        }
     }
 
     /**
@@ -266,6 +281,103 @@ class ManagingTaxRateContext implements Context
             $this->responseChecker->getCollectionItemsWithValue($this->client->index(), 'code', $code),
             1,
             sprintf('There is more than one tax rate with code %s', $code)
+        );
+    }
+
+    /**
+     * @Then I should be notified that :element is required
+     */
+    public function iShouldBeNotifiedThatCodeIsRequired(string $element): void
+    {
+        Assert::contains(
+            $this->responseChecker->getError($this->client->getLastResponse()),
+            sprintf('%s: Please enter tax rate %s.', $element, $element)
+        );
+    }
+
+    /**
+     * @Then tax rate with :element :code should not be added
+     */
+    public function taxRateWithCodeShouldNotBeAdded(string $element, string $code): void
+    {
+        Assert::false($this->isItemOnIndex($element, $code), sprintf('Tax rate with %s %s exist', $element, $code));
+    }
+
+    private function isItemOnIndex(string $property, string $value): bool
+    {
+        return $this->responseChecker->hasItemWithValue($this->client->index(), $property, $value);
+    }
+
+    /**
+     * @Then I should be notified that zone has to be selected
+     */
+    public function iShouldBeNotifiedThatZoneHasToBeSelected(): void
+    {
+        Assert::contains(
+            $this->responseChecker->getError($this->client->getLastResponse()),
+            'zone: Please select tax zone.'
+        );
+    }
+
+    /**
+     * @Then I should be notified that category has to be selected
+     */
+    public function iShouldBeNotifiedThatCategoryHasToBeSelected(): void
+    {
+        Assert::contains(
+            $this->responseChecker->getError($this->client->getLastResponse()),
+            'category: Please select tax category.'
+        );
+    }
+
+    /**
+     * @When /^I want to modify (this tax rate)$/
+     */
+    public function iWantToModifyThisTaxRate(TaxRateInterface $taxRate): void
+    {
+        $this->client->buildUpdateRequest((string) $taxRate->getId());
+    }
+
+    /**
+     * @When I remove its amount
+     */
+    public function iRemoveItsAmount(): void
+    {
+        $this->client->addRequestData('amount', '');
+    }
+
+    /**
+     * @When I try to save my changes
+     */
+    public function iSaveMyChanges(): void
+    {
+        $this->client->update();
+    }
+
+    /**
+     * @Then /^(this tax rate) amount should still be ([^"]+)%$/
+     */
+    public function thisTaxRateAmountShouldStillBe(TaxRateInterface $taxRate, $taxRateAmount): void
+    {
+        Assert::true($taxRate->getAmount(), $taxRateAmount);
+    }
+
+    /**
+     * @When I remove its name
+     */
+    public function iRemoveItsName(): void
+    {
+        $this->client->addRequestData('name', '');
+    }
+
+    /**
+     * @Then /^(this tax rate) should still be named "([^"]+)"$/
+     */
+    public function thisTaxRateShouldStillBeNamed(TaxRateInterface $taxRate, string $taxRateName): void
+    {
+        Assert::true(
+            $this->responseChecker->hasValue($this->client->show((string) $taxRate->getId()), 'name', $taxRateName),
+            sprintf('Tax category rate is not %s', $taxRateName)
         );
     }
 }
