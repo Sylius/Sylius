ca512d0e757fba99eae0b3cce6dcfae0381ab373
Łukasz Chruściel <lchrusciel@gmail.com>
2020-10-09 19:06:23 +0200

    [Admin][Shop][AdminApi] Section resolvers implemented

---
+++ src/Sylius/Bundle/CoreBundle/SectionResolver/SectionCannotBeResolvedException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\SectionResolver;
+
+final class SectionCannotBeResolvedException extends \RuntimeException
+{
+}
--- src/Sylius/Bundle/ShopBundle/EventListener/CustomerEmailUpdaterListener.php
+++ src/Sylius/Bundle/ShopBundle/EventListener/CustomerEmailUpdaterListener.php
@@ -13,7 +13,8 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\ShopBundle\EventListener;
 
-use Sylius\Bundle\UserBundle\EventListener\PasswordUpdaterListener as BasePasswordUpdaterListener;
+use Sylius\Bundle\CoreBundle\SectionResolver\SectionProviderInterface;
+use Sylius\Bundle\ShopBundle\SectionResolver\ShopSection;
 use Sylius\Bundle\UserBundle\UserEvents;
 use Sylius\Component\Channel\Context\ChannelContextInterface;
 use Sylius\Component\Core\Model\ChannelInterface;
@@ -30,45 +31,58 @@ final class CustomerEmailUpdaterListener
 {
     /** @var GeneratorInterface */
     private $tokenGenerator;
+
     /** @var ChannelContextInterface */
     private $channelContext;
+
     /** @var EventDispatcherInterface */
     private $eventDispatcher;
+
     /** @var SessionInterface */
     private $session;
+
+    /** @var SectionProviderInterface */
+    private $uriBasedSectionContext;
 
     public function __construct(
         GeneratorInterface $tokenGenerator,
         ChannelContextInterface $channelContext,
         EventDispatcherInterface $eventDispatcher,
-        SessionInterface $session
+        SessionInterface $session,
+        SectionProviderInterface $uriBasedSectionContext
     ) {
         $this->tokenGenerator = $tokenGenerator;
         $this->channelContext = $channelContext;
         $this->eventDispatcher = $eventDispatcher;
         $this->session = $session;
+        $this->uriBasedSectionContext = $uriBasedSectionContext;
     }
 
     public function eraseVerification(GenericEvent $event): void
     {
+        if (!$this->uriBasedSectionContext->getSection() instanceof ShopSection) {
+            return;
+        }
+
         $customer = $event->getSubject();
 
         /** @var CustomerInterface $customer */
         Assert::isInstanceOf($customer, CustomerInterface::class);
 
-        /** @var ShopUserInterface $user */
+        /** @var ShopUserInterface|null $user */
         $user = $customer->getUser();
-        if ($customer->getEmail() !== $user->getUsername()) {
-            $user->setEmail($customer->getEmail());
-            $user->setVerifiedAt(null);
+        Assert::isInstanceOf($user, ShopUserInterface::class);
 
-            $token = $this->tokenGenerator->generate();
-            $user->setEmailVerificationToken($token);
+        if ($customer->getEmail() !== $user->getUsername()) {
+            $user->setVerifiedAt(null);
 
             /** @var ChannelInterface $channel */
             $channel = $this->channelContext->getChannel();
 
             if ($channel->isAccountVerificationRequired()) {
+                $token = $this->tokenGenerator->generate();
+                $user->setEmailVerificationToken($token);
+
                 $user->setEnabled(false);
             }
         }
@@ -76,10 +90,7 @@ final class CustomerEmailUpdaterListener
 
     public function sendVerificationEmail(GenericEvent $event): void
     {
-        /** @var ChannelInterface $channel */
-        $channel = $this->channelContext->getChannel();
-
-        if (!$channel->isAccountVerificationRequired()) {
+        if (!$this->uriBasedSectionContext->getSection() instanceof ShopSection) {
             return;
         }
 
@@ -90,17 +101,21 @@ final class CustomerEmailUpdaterListener
 
         /** @var ShopUserInterface $user */
         $user = $customer->getUser();
+        Assert::isInstanceOf($user, ShopUserInterface::class);
+
+        /** @var ChannelInterface $channel */
+        $channel = $this->channelContext->getChannel();
+
+        if (!$channel->isAccountVerificationRequired()) {
+            return;
+        }
 
         if (!$user->isEnabled() && !$user->isVerified() && null !== $user->getEmailVerificationToken()) {
             $this->eventDispatcher->dispatch(UserEvents::REQUEST_VERIFICATION_TOKEN, new GenericEvent($user));
-            $this->addFlash('success', 'sylius.user.verify_email_request');
-        }
-    }
 
-    private function addFlash(string $type, string $message): void
-    {
-        /** @var FlashBagInterface $flashBag */
-        $flashBag = $this->session->getBag('flashes');
-        $flashBag->add($type, $message);
+            /** @var FlashBagInterface $flashBag */
+            $flashBag = $this->session->getBag('flashes');
+            $flashBag->add('success', 'sylius.user.verify_email_request');
+        }
     }
 }
