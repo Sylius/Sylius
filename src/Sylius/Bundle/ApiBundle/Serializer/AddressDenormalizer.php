395eed0f80dce49ea8189721d5d8a7fecd815824
Grzegorz Sadowski <sadowskigp@gmail.com>
2020-10-21 13:21:22 +0200

    [API][Address] Revert changes in address denormalizer

--- src/Sylius/Bundle/ApiBundle/Serializer/AddressDenormalizer.php
+++ src/Sylius/Bundle/ApiBundle/Serializer/AddressDenormalizer.php
@@ -13,11 +13,6 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\ApiBundle\Serializer;
 
-use Sylius\Bundle\ApiBundle\Provider\CustomerProviderInterface;
-use Sylius\Component\Core\Model\AddressInterface;
-use Sylius\Component\Core\Model\ShopUserInterface;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Core\Exception\TokenNotFoundException;
 use Symfony\Component\Serializer\Normalizer\ContextAwareDenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 
@@ -27,9 +22,6 @@ final class AddressDenormalizer implements ContextAwareDenormalizerInterface
     /** @var DenormalizerInterface */
     private $objectNormalizer;
 
-    /** @var TokenStorageInterface */
-    private $tokenStorage;
-
     /** @var string */
     private $classType;
 
@@ -38,11 +30,9 @@ final class AddressDenormalizer implements ContextAwareDenormalizerInterface
 
     public function __construct(
         DenormalizerInterface $objectNormalizer,
-        TokenStorageInterface $tokenStorage,
         string $classType,
         string $interfaceType
     ) {
-        $this->tokenStorage = $tokenStorage;
         $this->objectNormalizer = $objectNormalizer;
         $this->classType = $classType;
         $this->interfaceType = $interfaceType;
@@ -50,35 +40,16 @@ final class AddressDenormalizer implements ContextAwareDenormalizerInterface
 
     public function denormalize($data, string $type, string $format = null, array $context = [])
     {
-        /** @var AddressInterface $address */
-        $address = $this->objectNormalizer->denormalize(
+        return $this->objectNormalizer->denormalize(
             $data,
             $this->classType,
             $format,
             $context
         );
-
-        $token = $this->tokenStorage->getToken();
-        if ($token === null) {
-            throw new TokenNotFoundException();
-        }
-
-        /** @var ShopUserInterface $loggedUser */
-        $loggedUser = $token->getUser();
-
-        if ($loggedUser instanceof ShopUserInterface) {
-            $customer = $loggedUser->getCustomer();
-
-            $address->setCustomer($customer);
-
-            $customer->setDefaultAddress($address);
-        }
-
-        return $address;
     }
 
     public function supportsDenormalization($data, string $type, string $format = null, array $context = [])
     {
-        return $type === $this->interfaceType || $type === $this->classType;
+        return $type === $this->interfaceType;
     }
 }
