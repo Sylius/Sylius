665bac744717d2d3d46224eefccd4e484af1e1bd
Tomanhez <tomek.sut@gmail.com>
2020-11-04 12:14:12 +0100

    Add toogle on admin attribute crud

--- src/Sylius/Bundle/CoreBundle/Migrations/Version20201104085538.php
+++ src/Sylius/Bundle/CoreBundle/Migrations/Version20201104085538.php
@@ -7,15 +7,17 @@ namespace Sylius\Bundle\CoreBundle\Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20201103195231 extends AbstractMigration
+final class Version20201104085538 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE sylius_product_attribute ADD translatable TINYINT(1) DEFAULT \'1\' NOT NULL');
+        $this->addSql('ALTER TABLE sylius_product_attribute_value CHANGE locale_code locale_code VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('ALTER TABLE sylius_product_attribute DROP translatable');
+        $this->addSql('ALTER TABLE sylius_product_attribute_value CHANGE locale_code locale_code VARCHAR(255) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`');
     }
 }
