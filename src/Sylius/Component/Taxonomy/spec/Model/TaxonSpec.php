a4e5d6da3d2cff96b37cd80329fdb8ab90d507d4
jb cr <51637606+jbcr@users.noreply.github.com>
2020-11-27 09:24:09 +0100

    fix tests

--- src/Sylius/Component/Taxonomy/spec/Model/TaxonSpec.php
+++ src/Sylius/Component/Taxonomy/spec/Model/TaxonSpec.php
@@ -130,7 +130,8 @@ final class TaxonSpec extends ObjectBehavior
 
     function its_fullname_prepends_with_parents_fullname(TaxonInterface $root): void
     {
-        $root->getFullname()->willReturn('Category');
+        $root->getFullname()->withArguments([' / '])->willReturn('Category');
+        $root->getFullname()->withArguments([' -- '])->willReturn('Category');
         $this->setName('T-shirts');
 
         $root->addChild($this)->shouldBeCalled();
