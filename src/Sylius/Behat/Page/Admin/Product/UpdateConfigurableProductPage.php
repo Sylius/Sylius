b8fbdfdb679014ec1f26ad82414b6685bbfa48cb
Kamil Kokot <kamil@kokot.me>
2020-12-07 14:15:01 +0100

    Fix checking whether an image exists

--- src/Sylius/Behat/Page/Admin/Product/UpdateConfigurableProductPage.php
+++ src/Sylius/Behat/Page/Admin/Product/UpdateConfigurableProductPage.php
@@ -79,10 +79,10 @@ class UpdateConfigurableProductPage extends BaseUpdatePage implements UpdateConf
         }
 
         $this->getDriver()->visit($imageUrl);
-        $pageText = $this->getDocument()->getText();
+        $statusCode = $this->getDriver()->getStatusCode();
         $this->getDriver()->back();
 
-        return false === stripos($pageText, '404 Not Found');
+        return strpos((string) $statusCode, '2') === 0;
     }
 
     public function attachImage(string $path, string $type = null): void
