799118fc9bec6d76c1db6d2e59587ee3b60919fe
SirDomin <dominik.garbulski@sylius.com>
2021-02-25 09:02:31 +0100

    change serializer to item

--- src/Sylius/Bundle/ApiBundle/Serializer/ProductVariantSerializer.php
+++ src/Sylius/Bundle/ApiBundle/Serializer/ProductVariantSerializer.php
@@ -8,7 +8,6 @@ use Sylius\Component\Channel\Context\ChannelContextInterface;
 use Sylius\Component\Core\Calculator\ProductVariantPriceCalculatorInterface;
 use Sylius\Component\Core\Model\ProductVariantInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-use Symfony\Component\Serializer\SerializerInterface;
 use Webmozart\Assert\Assert;
 
 final class ProductVariantSerializer implements NormalizerInterface
@@ -37,38 +36,13 @@ final class ProductVariantSerializer implements NormalizerInterface
         Assert::isInstanceOf($object, ProductVariantInterface::class);
 
         $data = $this->objectNormalizer->normalize($object, $format, $context);
-        if (isset($data['product'])) {
-            $data['product'] = $data['product']['@id'];
-            $data['translations'] = $this->serializeTranslation($data['translations']);
-        }
         $data['price'] = $this->priceCalculator->calculate($object, ['channel' => $this->channelContext->getChannel()]);
 
         return $data;
     }
 
-    public function supportsNormalization($data, $format = null, $context = []): bool
+    public function supportsNormalization($data, $format = null): bool
     {
-        if(
-            $data instanceof ProductVariantInterface &&
-            $context['operation_type'] !== 'subresource' &&
-            array_key_exists('groups', $context) &&
-            $context['groups'] != 'shop:cart:read' &&
-            $context['groups'] != 'admin:order_item_unit:read' &&
-            $context['groups'] != 'admin:product:read'
-        ) {
-            return true;
-        }
-        return false;
-    }
-
-    private function serializeTranslation($translations): array
-    {
-        $serializedTranslations = [];
-
-        foreach ($translations['hydra:member'] as $translation) {
-            $serializedTranslations[$translation['locale']] = $translation;
-        }
-
-        return $serializedTranslations;
+        return $data instanceof ProductVariantInterface;
     }
 }
