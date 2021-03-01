e9c3fc86f16c2519af1153f1840e32a461595057
Stefan Doorn <stefan@efectos.nl>
2021-03-01 15:51:37 +0100

    Do not fail when mb_detect_encoding returns false

--- src/Sylius/Component/User/Canonicalizer/Canonicalizer.php
+++ src/Sylius/Component/User/Canonicalizer/Canonicalizer.php
@@ -17,6 +17,6 @@ final class Canonicalizer implements CanonicalizerInterface
 {
     public function canonicalize(?string $string): ?string
     {
-        return null === $string ? null : mb_convert_case($string, \MB_CASE_LOWER, mb_detect_encoding($string));
+        return null === $string ? null : mb_convert_case($string, \MB_CASE_LOWER, mb_detect_encoding($string) ?: null);
     }
 }
