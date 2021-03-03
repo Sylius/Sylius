f807ba23580773987075d677d84d201410e18d09
arti0090 <arti0090@gmail.com>
2021-03-03 12:30:31 +0100

    add data provider spec

--- src/Sylius/Bundle/ApiBundle/spec/DataProvider/ResetPasswordItemDataProviderSpec.php
+++ src/Sylius/Bundle/ApiBundle/spec/DataProvider/ResetPasswordItemDataProviderSpec.php
@@ -2,9 +2,25 @@
 
 declare(strict_types=1);
 
-namespace Sylius\Bundle\ApiBundle\spec\DataProvider;
+namespace spec\Sylius\Bundle\ApiBundle\DataProvider;
 
-class ResetPasswordItemDataProviderspec
+use PhpSpec\ObjectBehavior;
+use Sylius\Bundle\ApiBundle\Command\ResetPassword;
+use Sylius\Component\Core\Model\AddressInterface;
+
+final class ResetPasswordItemDataProviderSpec extends ObjectBehavior
 {
+    function it_supports_only_reset_password(): void
+    {
+        $this->supports(ResetPassword::class, 'post')->shouldReturn(true);
+        $this->supports(AddressInterface::class, 'post')->shouldReturn(false);
+    }
 
+    function it_provides_reset_password_class(): void
+    {
+        $this
+            ->getItem(ResetPassword::class, 'resetToken')
+            ->shouldBeLike(new ResetPassword('resetToken'))
+        ;
+    }
 }
