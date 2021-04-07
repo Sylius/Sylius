87e13c5088fb1128d06b4760dce422d122e7819d
Grzegorz Sadowski <sadowskigp@gmail.com>
2021-04-07 12:12:37 +0200

    [API] Overwrite CartBlamerListener in ApiBundle

---
+++ src/Sylius/Bundle/ApiBundle/EventListener/CartBlamerListener.php
@@ -0,0 +1,107 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\ApiBundle\EventListener;
+
+use Doctrine\Persistence\ObjectManager;
+use Sylius\Bundle\ApiBundle\SectionResolver\ShopApiSection;
+use Sylius\Bundle\CoreBundle\SectionResolver\SectionProviderInterface;
+use Sylius\Bundle\ShopBundle\SectionResolver\ShopSection;
+use Sylius\Bundle\UserBundle\Event\UserEvent;
+use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\Model\ShopUserInterface;
+use Sylius\Component\Order\Context\CartContextInterface;
+use Sylius\Component\Order\Context\CartNotFoundException;
+use Sylius\Component\Resource\Exception\UnexpectedTypeException;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+
+final class CartBlamerListener
+{
+    /** @var ObjectManager */
+    private $cartManager;
+
+    /** @var CartContextInterface */
+    private $cartContext;
+
+    /** @var SectionProviderInterface */
+    private $uriBasedSectionContext;
+
+    public function __construct(
+        ObjectManager $cartManager,
+        CartContextInterface $cartContext,
+        SectionProviderInterface $uriBasedSectionContext
+    ) {
+        $this->cartManager = $cartManager;
+        $this->cartContext = $cartContext;
+        $this->uriBasedSectionContext = $uriBasedSectionContext;
+    }
+
+    public function onImplicitLogin(UserEvent $userEvent): void
+    {
+        if (!$this->uriBasedSectionContext->getSection() instanceof ShopSection) {
+            return;
+        }
+
+        $user = $userEvent->getUser();
+        if (!$user instanceof ShopUserInterface) {
+            return;
+        }
+
+        $this->blame($user);
+    }
+
+    public function onInteractiveLogin(InteractiveLoginEvent $interactiveLoginEvent): void
+    {
+        $section = $this->uriBasedSectionContext->getSection();
+        if (!$section instanceof ShopSection && !$section instanceof ShopApiSection) {
+            return;
+        }
+
+        $user = $interactiveLoginEvent->getAuthenticationToken()->getUser();
+        if (!$user instanceof ShopUserInterface) {
+            return;
+        }
+
+        $this->blame($user);
+    }
+
+    private function blame(ShopUserInterface $user): void
+    {
+        $cart = $this->getCart();
+        if (null === $cart) {
+            return;
+        }
+
+        $cart->setCustomer($user->getCustomer());
+        $this->cartManager->persist($cart);
+        $this->cartManager->flush();
+    }
+
+    /**
+     * @throws UnexpectedTypeException
+     */
+    private function getCart(): ?OrderInterface
+    {
+        try {
+            $cart = $this->cartContext->getCart();
+        } catch (CartNotFoundException $exception) {
+            return null;
+        }
+
+        if (!$cart instanceof OrderInterface) {
+            throw new UnexpectedTypeException($cart, OrderInterface::class);
+        }
+
+        return $cart;
+    }
+}
