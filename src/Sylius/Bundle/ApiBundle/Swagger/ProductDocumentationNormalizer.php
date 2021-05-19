1639cb0e5cb22653e691a4370681be2faa76d95d
Grzegorz Sadowski <sadowskigp@gmail.com>
2021-05-19 11:57:57 +0200

    [API][Product] Add defaultVariant field to the Swagger schema

---
+++ src/Sylius/Bundle/ApiBundle/Swagger/ProductDocumentationNormalizer.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\ApiBundle\Swagger;
+
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+/** @experimental */
+final class ProductDocumentationNormalizer implements NormalizerInterface
+{
+    /** @var NormalizerInterface */
+    private $decoratedNormalizer;
+
+    public function __construct(NormalizerInterface $decoratedNormalizer)
+    {
+        $this->decoratedNormalizer = $decoratedNormalizer;
+    }
+
+    public function supportsNormalization($data, $format = null)
+    {
+        return $this->decoratedNormalizer->supportsNormalization($data, $format);
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        $docs = $this->decoratedNormalizer->normalize($object, $format, $context);
+
+        $defaultVariantSchema = [
+            'type' => 'string',
+            'format' => 'iri-reference',
+            'nullable' => true,
+            'readOnly' => true,
+        ];
+
+        $docs['components']['schemas']['Product.jsonld-admin.product.read']['properties']['defaultVariant'] = $defaultVariantSchema;
+        $docs['components']['schemas']['Product.jsonld-shop.product.read']['properties']['defaultVariant'] = $defaultVariantSchema;
+
+        return $docs;
+    }
+}
