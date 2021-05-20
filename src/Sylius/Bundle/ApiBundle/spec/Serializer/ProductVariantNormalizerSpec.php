b48f9f44c5544a4145837220ee22403fc3caf613
Grzegorz Sadowski <sadowskigp@gmail.com>
2021-05-20 07:18:44 +0200

    [API][ProductVariant] Add additional specs for product variant normalizer

--- src/Sylius/Bundle/ApiBundle/spec/Serializer/ProductVariantNormalizerSpec.php
+++ src/Sylius/Bundle/ApiBundle/spec/Serializer/ProductVariantNormalizerSpec.php
@@ -17,9 +17,8 @@ use PhpSpec\ObjectBehavior;
 use Sylius\Component\Channel\Context\ChannelContextInterface;
 use Sylius\Component\Core\Calculator\ProductVariantPricesCalculatorInterface;
 use Sylius\Component\Core\Model\ChannelInterface;
-use Sylius\Component\Core\Model\ProductVariant;
+use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Core\Model\ProductVariantInterface;
-use Sylius\Component\Order\Model\Order;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 final class ProductVariantNormalizerSpec extends ObjectBehavior
@@ -31,15 +30,23 @@ final class ProductVariantNormalizerSpec extends ObjectBehavior
         $this->beConstructedWith($pricesCalculator, $channelContext);
     }
 
-    function it_supports_only_product_variant_interface(): void
+    function it_supports_only_product_variant_interface(ProductVariantInterface $variant, OrderInterface $order): void
     {
-        $this->supportsNormalization(new ProductVariant())->shouldReturn(true);
-        $this->supportsNormalization(new Order())->shouldReturn(false);
+        $this->supportsNormalization($variant)->shouldReturn(true);
+        $this->supportsNormalization($order)->shouldReturn(false);
     }
 
-    function it_does_not_serialize_if_item_operation_name_is_admin_get(): void
+    function it_does_not_support_if_item_operation_name_is_admin_get(ProductVariantInterface $variant): void
     {
-        $this->supportsNormalization(new ProductVariant(), null, ['item_operation_name' => 'admin_get'])->shouldReturn(false);
+        $this->supportsNormalization($variant, null, ['item_operation_name' => 'admin_get'])->shouldReturn(false);
+    }
+
+    function it_does_not_support_if_the_normalizer_has_been_already_called(ProductVariantInterface $variant): void
+    {
+        $this
+            ->supportsNormalization($variant, null, ['product_variant_normalizer_already_called' => true])
+            ->shouldReturn(false)
+        ;
     }
 
     function it_serializes_product_variant_if_item_operation_name_is_different_that_admin_get(
@@ -57,5 +64,19 @@ final class ProductVariantNormalizerSpec extends ObjectBehavior
         $pricesCalculator->calculate($variant, ['channel' => $channel])->willReturn(1000);
 
         $this->normalize($variant, null, [])->shouldReturn(['price' => 1000]);
+    }
+
+    function it_throws_an_exception_if_the_normalizer_has_been_already_called(
+        NormalizerInterface $normalizer,
+        ProductVariantInterface $variant
+    ): void {
+        $this->setNormalizer($normalizer);
+
+        $normalizer->normalize($variant, null, ['product_variant_normalizer_already_called' => true])->shouldNotBeCalled();
+
+        $this
+            ->shouldThrow(\InvalidArgumentException::class)
+            ->during('normalize', [$variant, null, ['product_variant_normalizer_already_called' => true]])
+        ;
     }
 }
