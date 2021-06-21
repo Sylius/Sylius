7f6613f215caa1c834169150d9b2d6b608bc763d
Grzegorz Sadowski <sadowskigp@gmail.com>
2021-06-21 15:58:21 +0200

    [API][ShippingMethod] Fixes after PR review

--- src/Sylius/Bundle/ApiBundle/Swagger/ShippingMethodDocumentationNormalizer.php
+++ src/Sylius/Bundle/ApiBundle/Swagger/ShippingMethodDocumentationNormalizer.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace Sylius\Bundle\ApiBundle\Swagger;
@@ -17,7 +26,7 @@ final class ShippingMethodDocumentationNormalizer implements NormalizerInterface
         $this->decoratedNormalizer = $decoratedNormalizer;
     }
 
-    public function supportsNormalization($data, $format = null)
+    public function supportsNormalization($data, $format = null): bool
     {
         return $this->decoratedNormalizer->supportsNormalization($data, $format);
     }
