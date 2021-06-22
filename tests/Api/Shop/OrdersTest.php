aa1609b1f4679040350b1e8d1ec5b10a8a408f16
Grzegorz Sadowski <sadowskigp@gmail.com>
2021-06-22 11:51:51 +0200

    [API] Serialize only shipment id and method as shipment in order response

--- tests/Api/Shop/OrdersTest.php
+++ tests/Api/Shop/OrdersTest.php
@@ -15,6 +15,8 @@ namespace Sylius\Tests\Api\Shop;
 
 use Sylius\Bundle\ApiBundle\Command\Cart\AddItemToCart;
 use Sylius\Bundle\ApiBundle\Command\Cart\PickupCart;
+use Sylius\Bundle\ApiBundle\Command\Checkout\AddressOrder;
+use Sylius\Component\Core\Model\Address;
 use Sylius\Tests\Api\JsonApiTestCase;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
@@ -24,7 +26,13 @@ final class OrdersTest extends JsonApiTestCase
     /** @test */
     public function it_gets_an_order(): void
     {
-        $this->loadFixturesFromFiles(['cart.yaml', 'payment_method.yaml']);
+        $this->loadFixturesFromFiles([
+            'cart.yaml',
+            'country.yaml',
+            'zone.yaml',
+            'shipping_method.yaml',
+            'payment_method.yaml'
+        ]);
 
         $tokenValue = 'nAWw2jewpA';
 
@@ -38,6 +46,18 @@ final class OrdersTest extends JsonApiTestCase
         $addItemToCartCommand = new AddItemToCart('MUG_BLUE', 3);
         $addItemToCartCommand->setOrderTokenValue($tokenValue);
         $commandBus->dispatch($addItemToCartCommand);
+
+        $address = new Address();
+        $address->setFirstName('John');
+        $address->setLastName('Doe');
+        $address->setCity('New York');
+        $address->setStreet('Avenue');
+        $address->setCountryCode('US');
+        $address->setPostcode('90000');
+
+        $addressOrderCommand = new AddressOrder('sylius@example.com', $address);
+        $addressOrderCommand->setOrderTokenValue($tokenValue);
+        $commandBus->dispatch($addressOrderCommand);
 
         $this->client->request('GET', '/api/v2/shop/orders/nAWw2jewpA', [], [], self::CONTENT_TYPE_HEADER);
         $response = $this->client->getResponse();
