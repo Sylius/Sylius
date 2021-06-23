483fe79e11884daa0ad3e548f875c88c0db09c77
SirDomin <domin12wsx@gmail.com>
2021-06-23 12:40:04 +0200

    [API] handle shipping without addressing

--- src/Sylius/Behat/Context/Api/Shop/CheckoutContext.php
+++ src/Sylius/Behat/Context/Api/Shop/CheckoutContext.php
@@ -420,6 +420,18 @@ final class CheckoutContext implements Context
     }
 
     /**
+     * @Then I should be notified that the order should be addressed first
+     */
+    public function iShouldBeNotifiedThatTheOrderShouldBeAddressedFirst()
+    {
+        Assert::same($this->ordersClient->getLastResponse()->getStatusCode(), Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        $content = json_decode($this->ordersClient->getLastResponse()->getContent(), true);
+
+        Assert::same($content['message'], 'Order should be addressed first.');
+    }
+
+    /**
      * @Then I should be informed that shipping method with code :code does not exist
      */
     public function iShouldBeInformedThatShippingMethodWithCodeDoesNotExist(string $code): void
