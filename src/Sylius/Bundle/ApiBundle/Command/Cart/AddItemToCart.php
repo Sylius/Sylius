8d4e281720ca853db3c741b09e8909e3c5d42e32
Tomanhez <tomek.sut@gmail.com>
2021-07-01 23:06:07 +0200

    Add CommandFieldItemIriToIdentifierAwareInterface

--- src/Sylius/Bundle/ApiBundle/Command/Cart/AddItemToCart.php
+++ src/Sylius/Bundle/ApiBundle/Command/Cart/AddItemToCart.php
@@ -13,10 +13,11 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\ApiBundle\Command\Cart;
 
+use Sylius\Bundle\ApiBundle\Command\CommandFieldItemIriToIdentifierAwareInterface;
 use Sylius\Bundle\ApiBundle\Command\OrderTokenValueAwareInterface;
 
 /** @experimental */
-class AddItemToCart implements OrderTokenValueAwareInterface
+class AddItemToCart implements OrderTokenValueAwareInterface, CommandFieldItemIriToIdentifierAwareInterface
 {
     /** @var string|null */
     public $orderTokenValue;
