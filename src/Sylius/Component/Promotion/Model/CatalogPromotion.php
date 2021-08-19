b0dd613a6dfc7e7552ad71c26c6324382484ef5c
Arkadiusz Krakowiak <arkadiusz.k.e@gmail.com>
2021-08-19 22:56:26 +0200

    [CatalogPromotion] Configure base entity + api

---
+++ src/Sylius/Component/Promotion/Model/CatalogPromotion.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Component\Promotion\Model;
+
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+class CatalogPromotion implements ResourceInterface
+{
+    protected int $id;
+    protected string $name;
+    protected string $code;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getCode(): string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): void
+    {
+        $this->code = $code;
+    }
+}
