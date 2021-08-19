f930a9f7c4df685ca3024ec3620069240dde0a31
Arkadiusz Krakowiak <arkadiusz.k.e@gmail.com>
2021-08-19 17:20:16 +0200

    [CatalogPromotion] Creating simple catalog promotion

---
+++ features/promotion/managing_catalog_promotions/managing_catalog_promotions.feature
@@ -0,0 +1,15 @@
+@managing_catalog_promotions
+Feature: Managing catalog promotions
+    In order to set up a catalog promotion for some special occasions
+    As an Administrator
+    I want to have an option to configure such a promotion
+
+    Background:
+        Given the store operates on a single channel in "United States"
+        And I am logged in as an administrator
+
+    @api
+    Scenario: Creating simple catalog promotion only with code and name
+        When I create a new catalog promotion with "winter_sale" code and "Winter sale" name
+        Then there should be 1 new catalog promotion on the list
+        And it should have "winter_sale" code and "Winter sale" name
---
+++ src/Sylius/Behat/Context/Api/Admin/ManagingCatalogPromotionsContext.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Behat\Context\Api\Admin;
+
+use Behat\Behat\Context\Context;
+use Sylius\Behat\Client\ApiClientInterface;
+use Sylius\Behat\Client\ResponseCheckerInterface;
+use Webmozart\Assert\Assert;
+
+final class ManagingCatalogPromotionsContext implements Context
+{
+    private ApiClientInterface $client;
+    private ResponseCheckerInterface $responseChecker;
+
+    public function __construct(ApiClientInterface $client, ResponseCheckerInterface $responseChecker)
+    {
+        $this->client = $client;
+        $this->responseChecker = $responseChecker;
+    }
+
+    /**
+     * @When I create a new catalog promotion with :code code and :name name
+     */
+    public function iCreateANewCatalogPromotionWithCodeAndName(string $code, string $name): void
+    {
+        $this->client->buildCreateRequest();
+        $this->client->addRequestData('code', $code);
+        $this->client->addRequestData('name', $name);
+        $this->client->create();
+    }
+
+    /**
+     * @Then there should be :amount new catalog promotion on the list
+     */
+    public function thereShouldBeNewCatalogPromotionOnTheList(int $amount): void
+    {
+        Assert::count($this->responseChecker->getCollection($this->client->index()), $amount);
+    }
+
+    /**
+     * @Then it should have :code code and :name name
+     */
+    public function itShouldHaveCodeAndName(string $code, string $name): void
+    {
+        $expectedValues = ['code' => $code, 'name' => $name];
+        Assert::true(
+            $this->responseChecker->hasItemWithValues($this->client->index(), $expectedValues),
+            sprintf('Cannot find catalog promotions with %s in the list', implode(',', $expectedValues))
+        );
+    }
+}
