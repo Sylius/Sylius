3fafecac8dd87009a289575998a67691bd74f34a
arti0090 <arti0090@gmail.com>
2021-08-23 11:38:23 +0200

    GET, PUT, DELETE for Catalog Promotions

--- tests/Api/Admin/CatalogPromotionsTest.php
+++ tests/Api/Admin/CatalogPromotionsTest.php
@@ -43,4 +43,74 @@ final class CatalogPromotionsTest extends JsonApiTestCase
 
         $this->assertResponse($response, 'admin/get_catalog_promotions_admin_response', Response::HTTP_OK);
     }
+
+    /** @test */
+    public function it_allows_admin_to_update_catalog_promotion(): void
+    {
+        $this->loadFixturesFromFile('authentication/api_administrator.yaml');
+        $this->loadFixturesFromFile('catalog_promotion.yaml');
+
+        $token = $this->logInAdminUser('api@example.com');
+        $authorizationHeader = self::$container->getParameter('sylius.api.authorization_header');
+        $header['HTTP_' . $authorizationHeader] = 'Bearer ' . $token;
+        $header = array_merge($header, self::CONTENT_TYPE_HEADER);
+
+        // Should be replaced with fixture_catalog_promotion['id'];
+        $id = $this->getId($header);
+
+        $this->client->request(
+            'PUT',
+            sprintf('/api/v2/admin/catalog-promotions/%s', $id),
+            [],
+            [],
+            $header,
+            json_encode(["name" => "new_promotion", "code" => "new_code"], JSON_THROW_ON_ERROR)
+        );
+
+        $response = $this->client->getResponse();
+
+        $this->assertResponse($response, 'admin/put_catalog_promotion_admin_response', Response::HTTP_OK);
+    }
+
+    /** @test */
+    public function it_allows_admin_to_delete_catalog_promotion(): void
+    {
+        $this->loadFixturesFromFile('authentication/api_administrator.yaml');
+        $catalogPromotion = $this->loadFixturesFromFile('catalog_promotion.yaml');
+
+        $token = $this->logInAdminUser('api@example.com');
+        $authorizationHeader = self::$container->getParameter('sylius.api.authorization_header');
+        $header['HTTP_' . $authorizationHeader] = 'Bearer ' . $token;
+        $header = array_merge($header, self::CONTENT_TYPE_HEADER);
+
+        // Should be replaced with $catalogPromotion['id'];
+        $id = $this->getId($header);
+
+        $this->client->request(
+            'DELETE',
+            sprintf('/api/v2/admin/catalog-promotions/%s', $id),
+            [],
+            [],
+            $header
+        );
+
+        $response = $this->client->getResponse();
+
+        $this->assertResponseCode($response, Response::HTTP_NO_CONTENT);
+    }
+
+    private function getId(array $header): string
+    {
+        $this->client->request(
+            'GET',
+            sprintf('/api/v2/admin/catalog-promotions'),
+            [],
+            [],
+            $header
+        );
+
+        $response = $this->client->getResponse();
+        $content = json_decode($response->getContent(), true);
+        return (string) $content['hydra:member'][0]['id'];
+    }
 }
