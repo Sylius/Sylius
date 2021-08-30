0f4d7c9b766714199479b941cdc32de0defc8bb9
Mateusz Zalewski <zaleslaw@gmail.com>
2021-08-30 16:47:18 +0200

    [Behat] Viewing applied catalog promotions on variants index

--- src/Sylius/Behat/Context/Api/Shop/ProductVariantContext.php
+++ src/Sylius/Behat/Context/Api/Shop/ProductVariantContext.php
@@ -16,6 +16,7 @@ namespace Sylius\Behat\Context\Api\Shop;
 use Behat\Behat\Context\Context;
 use Sylius\Behat\Client\ApiClientInterface;
 use Sylius\Behat\Client\ResponseCheckerInterface;
+use Sylius\Component\Core\Model\ProductInterface;
 use Sylius\Component\Core\Model\ProductVariantInterface;
 use Webmozart\Assert\Assert;
 
@@ -42,6 +43,14 @@ final class ProductVariantContext implements Context
     }
 
     /**
+     * @When I view variants
+     */
+    public function iViewVariants(): void
+    {
+        $this->client->index();
+    }
+
+    /**
      * @Then /^the product variant price should be ("[^"]+")$/
      */
     public function theProductVariantPriceShouldBe(int $price): void
@@ -59,5 +68,41 @@ final class ProductVariantContext implements Context
         $response = $this->responseChecker->getResponseContent($this->client->getLastResponse());
 
         Assert::same($response['originalPrice'], $originalPrice);
+    }
+
+    /**
+     * @Then /^I should see ("[^"]+" variant) is discounted from ("[^"]+") to ("[^"]+") with "([^"]+)" promotion$/
+     */
+    public function iShouldSeeVariantIsDiscountedFromToWithPromotion(
+        ProductVariantInterface $targetVariant,
+        int $originalPrice,
+        int $price,
+        string $promotionName
+    ): void {
+        $response = $this->responseChecker->getResponseContent($this->client->getLastResponse());
+
+        foreach ($response['hydra:member'] as $variant) {
+            if ($variant['code'] !== $targetVariant->getCode()) {
+                continue;
+            }
+
+            Assert::same($variant['price'], $price);
+            Assert::same($variant['originalPrice'], $originalPrice);
+            Assert::inArray(['name' => $promotionName], $variant['appliedPromotions']);
+        }
+    }
+
+    /**
+     * @Then I should see :firstVariant variant and :secondVariant variant are not discounted
+     */
+    public function iShouldSeeVariantsAreNotDiscounted(string ...$variantsNames): void
+    {
+        $response = $this->responseChecker->getResponseContent($this->client->getLastResponse());
+
+        foreach ($response['hydra:member'] as $variant) {
+            if (in_array($variant['translations']['en_US']['name'], $variantsNames)) {
+                Assert::keyNotExists($variant, 'appliedPromotions');
+            }
+        }
     }
 }
