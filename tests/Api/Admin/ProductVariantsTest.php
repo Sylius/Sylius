a87aa958f39dad5a3c3b481a9f316df609c8e03b
Grzegorz Sadowski <sadowskigp@gmail.com>
2021-09-21 13:52:26 +0200

    [API][ProductVariant] Add updating channel pricings of variants

--- tests/Api/Admin/ProductVariantsTest.php
+++ tests/Api/Admin/ProductVariantsTest.php
@@ -13,6 +13,8 @@ declare(strict_types=1);
 
 namespace Sylius\Tests\Api\Admin;
 
+use Sylius\Component\Core\Model\ChannelInterface;
+use Sylius\Component\Core\Model\ProductVariantInterface;
 use Sylius\Tests\Api\JsonApiTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -21,7 +23,7 @@ final class ProductVariantsTest extends JsonApiTestCase
     /** @test */
     public function it_denies_access_to_a_products_list_for_not_authenticated_user(): void
     {
-        $this->loadFixturesFromFile('product_variant.yaml');
+        $this->loadFixturesFromFiles(['channel.yaml', 'product_variant.yaml']);
 
         $this->client->request('GET', '/api/v2/admin/product-variants/MUG');
 
@@ -32,7 +34,7 @@ final class ProductVariantsTest extends JsonApiTestCase
     /** @test */
     public function it_gets_all_product_variants(): void
     {
-        $this->loadFixturesFromFiles(['product_variant.yaml', 'authentication/api_administrator.yaml']);
+        $this->loadFixturesFromFiles(['channel.yaml', 'product_variant.yaml', 'authentication/api_administrator.yaml']);
 
         $this->client->request(
             'POST',
@@ -58,5 +60,51 @@ final class ProductVariantsTest extends JsonApiTestCase
         $response = $this->client->getResponse();
 
         $this->assertResponse($response, 'admin/get_product_variants_response', Response::HTTP_OK);
+    }
+
+    /** @test */
+    public function it_updates_channel_pricing_of_product_variant(): void
+    {
+        $fixtures = $this->loadFixturesFromFiles(['channel.yaml', 'product_variant.yaml', 'authentication/api_administrator.yaml']);
+
+        $this->client->request(
+            'POST',
+            '/api/v2/admin/authentication-token',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json', 'HTTP_ACCEPT' => 'application/json'],
+            json_encode(['email' => 'api@example.com', 'password' => 'sylius'])
+        );
+
+        $token = json_decode($this->client->getResponse()->getContent(), true)['token'];
+        $authorizationHeader = self::$container->getParameter('sylius.api.authorization_header');
+
+        $header['HTTP_' . $authorizationHeader] = 'Bearer ' . $token;
+
+        /** @var ProductVariantInterface $productVariant */
+        $productVariant = $fixtures['product_variant'];
+        /** @var ChannelInterface $channel */
+        $channel = $fixtures['channel_web'];
+
+        $this->client->request(
+            'PUT',
+            sprintf('/api/v2/admin/product-variants/%s', $productVariant->getCode()),
+            [],
+            [],
+            array_merge($header, self::CONTENT_TYPE_HEADER),
+            json_encode([
+                'channelPricings' => ['WEB' => [
+                    '@id' => sprintf('/api/v2/admin/channel-pricings/%s', $productVariant->getChannelPricingForChannel($channel)->getId()),
+                    'price' => 3000,
+                    'originalPrice' => 4000,
+                ]]
+            ], JSON_THROW_ON_ERROR)
+        );
+
+        $this->assertResponse(
+            $this->client->getResponse(),
+            'admin/put_product_variant_response',
+            Response::HTTP_OK
+        );
     }
 }
