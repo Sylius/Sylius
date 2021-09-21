691b700a11b6688d7296a1693efb47bbef6141cf
Łukasz Chruściel <lchrusciel@gmail.com>
2021-09-21 14:40:20 +0200

    [Maintenace] Test existence of new cache headers

---
+++ src/Sylius/Bundle/ShopBundle/Tests/EventListener/AdminSectionCacheControlSubscriberTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\AdminBundle\Tests\EventListener;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class AdminSectionCacheControlSubscriberTest extends WebTestCase
+{
+    /**
+     * @test
+     */
+    public function it_returns_proper_cache_headers_for_customer_account_endpoints(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/en_US/account/');
+
+        $this->assertResponseHeaderSame('Cache-Control', 'max-age=0, must-revalidate, no-cache, no-store, private');
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_normal_headers_otherwise(): void
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/en_US/');
+
+        $this->assertResponseHeaderSame('Cache-Control', 'max-age=0, must-revalidate, private');
+    }
+}
