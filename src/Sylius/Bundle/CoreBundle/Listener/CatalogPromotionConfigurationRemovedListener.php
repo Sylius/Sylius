074f306d4e71f8963b46d36d6b2873991575a4c9
arti0090 <arti0090@gmail.com>
2021-09-22 11:56:31 +0200

    Remove apply from removing listener

--- src/Sylius/Bundle/CoreBundle/Listener/CatalogPromotionConfigurationRemovedListener.php
+++ src/Sylius/Bundle/CoreBundle/Listener/CatalogPromotionConfigurationRemovedListener.php
@@ -14,34 +14,20 @@ declare(strict_types=1);
 namespace Sylius\Bundle\CoreBundle\Listener;
 
 use Sylius\Bundle\CoreBundle\Processor\CatalogPromotionClearerInterface;
-use Sylius\Bundle\CoreBundle\Processor\CatalogPromotionProcessorInterface;
 use Sylius\Component\Promotion\Event\CatalogPromotionConfigurationRemoved;
-use Sylius\Component\Resource\Repository\RepositoryInterface;
 
 final class CatalogPromotionConfigurationRemovedListener
 {
     private CatalogPromotionClearerInterface $catalogPromotionClearer;
 
-    private CatalogPromotionProcessorInterface $catalogPromotionProcessor;
-
-    private RepositoryInterface $catalogPromotionRepository;
-
     public function __construct(
-        CatalogPromotionClearerInterface $catalogPromotionClearer,
-        CatalogPromotionProcessorInterface $catalogPromotionProcessor,
-        RepositoryInterface $catalogPromotionRepository
+        CatalogPromotionClearerInterface $catalogPromotionClearer
     ) {
         $this->catalogPromotionClearer = $catalogPromotionClearer;
-        $this->catalogPromotionProcessor = $catalogPromotionProcessor;
-        $this->catalogPromotionRepository = $catalogPromotionRepository;
     }
 
     public function __invoke(CatalogPromotionConfigurationRemoved $event): void
     {
         $this->catalogPromotionClearer->clear();
-
-        foreach ($this->catalogPromotionRepository->findAll() as $catalogPromotion) {
-            $this->catalogPromotionProcessor->process($catalogPromotion);
-        }
     }
 }
