919825c2b0941902f33d0d76ab7050d354d31480
Alexander Boehm <boehm@punkt.de>
2021-10-22 10:31:03 +0200

    BUGFIX: Sylius#12225 Update routing and refactor parameter name

--- src/Sylius/Bundle/AdminBundle/Action/ResendOrderConfirmationEmailAction.php
+++ src/Sylius/Bundle/AdminBundle/Action/ResendOrderConfirmationEmailAction.php
@@ -54,7 +54,7 @@ final class ResendOrderConfirmationEmailAction
 
     public function __invoke(Request $request): Response
     {
-        $orderId = $request->attributes->get('id');
+        $orderId = $request->attributes->get('orderId');
 
         if (!$this->csrfTokenManager->isTokenValid(new CsrfToken($orderId, (string) $request->query->get('_csrf_token')))) {
             throw new HttpException(Response::HTTP_FORBIDDEN, 'Invalid csrf token.');
