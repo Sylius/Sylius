ac0f191a797c60cd5057fb1796c7b2288234e5b3
Grzegorz Sadowski <sadowskigp@gmail.com>
2021-11-09 07:44:29 +0100

    [API] Remove temporarily conditions for tree left and right from taxon filter

--- src/Sylius/Bundle/ApiBundle/Filter/TaxonFilter.php
+++ src/Sylius/Bundle/ApiBundle/Filter/TaxonFilter.php
@@ -53,12 +53,8 @@ final class TaxonFilter extends AbstractContextAwareFilter
             ->addSelect('productTaxon')
             ->innerJoin(sprintf('%s.productTaxons', $alias), 'productTaxon')
             ->innerJoin('productTaxon.taxon', 'taxon')
-            ->andWhere('taxon.left >= :taxonLeft')
-            ->andWhere('taxon.right <= :taxonRight')
             ->andWhere('taxon.root = :taxonRoot')
             ->addOrderBy('productTaxon.position')
-            ->setParameter('taxonLeft', $taxon->getLeft())
-            ->setParameter('taxonRight', $taxon->getRight())
             ->setParameter('taxonRoot', $taxon->getRoot())
         ;
     }
