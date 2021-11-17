61291701a3687fdcc6f4b9b3097d11ae9519f145
SirDomin <domin12wsx@gmail.com>
2021-11-17 15:20:58 +0100

    [Promotions] add field to promotion entity

---
+++ src/Sylius/Bundle/CoreBundle/Migrations/Version20211117141834.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20211117141834 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sylius_promotion ADD applyOnDiscounted TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE sylius_catalog_promotion ADD applyOnDiscounted TINYINT(1) DEFAULT \'1\' NOT NULL');
+    }
+}
