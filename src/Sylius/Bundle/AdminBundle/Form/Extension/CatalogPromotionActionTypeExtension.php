ca72095d7e5eb5cc1b85429060a9969bc734c838
Grzegorz Sadowski <sadowskigp@gmail.com>
2021-11-24 07:53:29 +0100

    [CatalogPromotion] Add missing license docblocks

--- src/Sylius/Bundle/AdminBundle/Form/Extension/CatalogPromotionActionTypeExtension.php
+++ src/Sylius/Bundle/AdminBundle/Form/Extension/CatalogPromotionActionTypeExtension.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace Sylius\Bundle\AdminBundle\Form\Extension;
@@ -40,7 +49,7 @@ final class CatalogPromotionActionTypeExtension extends AbstractTypeExtension
                         'data-configuration' => $this->twig->render(
                             '@SyliusAdmin/CatalogPromotion/_action.html.twig',
                             ['field' => $builder->create(
-                                'configuration', 
+                                'configuration',
                                 $this->actionConfigurationTypes[$type],
                                 ['label' => false, 'csrf_protection' => false]
                             )->getForm()->createView()]
