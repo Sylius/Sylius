c594ae328f324f3af2b65cdbfd5ac3d4daedfdb8
SirDomin <domin12wsx@gmail.com>
2021-11-25 09:54:30 +0100

    add default option to doctrine

--- src/Sylius/Bundle/CoreBundle/Migrations/Version20211125085254.php
+++ src/Sylius/Bundle/CoreBundle/Migrations/Version20211125085254.php
@@ -10,22 +10,22 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211124211310 extends AbstractMigration
+final class Version20211125085254 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Set not null on minimum price';
+        return 'Set default value on minimum_price';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE sylius_channel_pricing CHANGE minimum_price minimum_price INT NOT NULL');
+        $this->addSql('ALTER TABLE sylius_channel_pricing CHANGE minimum_price minimum_price INT DEFAULT 0 NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE sylius_channel_pricing CHANGE minimum_price minimum_price INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE sylius_channel_pricing CHANGE minimum_price minimum_price INT NOT NULL');
     }
 }
