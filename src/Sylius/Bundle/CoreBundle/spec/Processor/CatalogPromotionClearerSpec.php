dbb88b00fb7669b6182503046be0d7aa1c3036da
Rafikooo <thecrazyrafik@gmail.com>
2021-12-02 23:08:31 +0100

    [CatalogPromotion][Fix] Clearer

--- src/Sylius/Bundle/CoreBundle/spec/Processor/CatalogPromotionClearerSpec.php
+++ src/Sylius/Bundle/CoreBundle/spec/Processor/CatalogPromotionClearerSpec.php
@@ -63,9 +63,7 @@ final class CatalogPromotionClearerSpec extends ObjectBehavior
         $secondChannelPricing->getAppliedPromotions()->willReturn(new ArrayCollection());
         $secondChannelPricing->getOriginalPrice()->shouldNotBeCalled();
         $secondChannelPricing->clearAppliedPromotions()->shouldNotBeCalled();
-
-        $catalogPromotion->getChannelPricings()->willReturn(new ArrayCollection([$firstChannelPricing->getWrappedObject(), $secondChannelPricing->getWrappedObject()]));
-
+        
         $stateMachine->get($catalogPromotion, CatalogPromotionTransitions::GRAPH)->willReturn($stateMachineInterface);
         $stateMachineInterface->can(CatalogPromotionTransitions::TRANSITION_DEACTIVATE)->willReturn(true);
         $stateMachineInterface->apply(CatalogPromotionTransitions::TRANSITION_DEACTIVATE)->shouldBeCalled();
