6e0f38273c6f75e10c519ad54436d42f2f7febc0
SirDomin <dominik.garbulski@gmail.com>
2021-12-09 13:20:25 +0100

    Revert "Remove swagger decorators when docs are disabled"

--- src/Sylius/Bundle/ApiBundle/DependencyInjection/SyliusApiExtension.php
+++ src/Sylius/Bundle/ApiBundle/DependencyInjection/SyliusApiExtension.php
@@ -15,7 +15,6 @@ namespace Sylius\Bundle\ApiBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Exception\ParameterNotFoundException;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
@@ -30,11 +29,5 @@ final class SyliusApiExtension extends Extension
         $container->setParameter('sylius_api.enabled', $config['enabled']);
 
         $loader->load('services.xml');
-
-        if (!$container->hasParameter('enable_swagger') || !$container->getParameter('enable_swagger')) {
-            return;
-        }
-
-        $loader->load('integrations/swagger.xml');
     }
 }
