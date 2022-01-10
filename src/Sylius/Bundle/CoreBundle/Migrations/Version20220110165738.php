03d28a4f1a4b00660feb561d351c25471b24c7f1
Łukasz Chruściel <lchrusciel@gmail.com>
2022-01-10 17:59:36 +0100

    [Maintenance] Migrate GatewayConfig config column type to json

---
+++ src/Sylius/Bundle/CoreBundle/Migrations/Version20220110165738.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220110165738 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Change GatewayConfig config type to json, to follow Payum support for doctrine/dbal v3';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE sylius_gateway_config CHANGE config config JSON NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE sylius_gateway_config CHANGE config config LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json_array)\'');
+    }
+}
