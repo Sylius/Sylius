cfd81a485cf32042321e3510c3c8e8c84fd51128
Łukasz Chruściel <lchrusciel@gmail.com>
2022-01-18 12:53:27 +0100

    [Minor] Deprecate shipment unit transition

--- src/Sylius/Component/Shipping/ShipmentUnitTransitions.php
+++ src/Sylius/Component/Shipping/ShipmentUnitTransitions.php
@@ -13,6 +13,9 @@ declare(strict_types=1);
 
 namespace Sylius\Component\Shipping;
 
+/**
+ * @deprecated Since Sylius v1.10 and will be removed in Sylius v2.0 as there is no such graph and therefore it was never used.
+ */
 class ShipmentUnitTransitions
 {
     public const GRAPH = 'sylius_shipment_unit';
