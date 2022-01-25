8d7142df4dd7df5b9e5f354c97fc88be264ba96a
Ernest Warwas <ernest.warwas96@gmail.com>
2022-01-25 16:08:51 +0100

    abortIf added to migration

--- src/Sylius/Bundle/CoreBundle/Migrations/Version20220114153236.php
+++ src/Sylius/Bundle/CoreBundle/Migrations/Version20220114153236.php
@@ -20,12 +20,16 @@ final class Version20220114153236 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
         $this->addSql('ALTER TABLE sylius_admin_user ADD timezone VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
         $this->addSql('ALTER TABLE sylius_admin_user DROP timezone');
     }
 }
