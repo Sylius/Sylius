bed032f77ec5f4e78597b0dec7b14c295d96b281
Loïc Frémont <lc.fremont@gmail.com>
2022-01-25 11:08:05 +0100

    Init data fixtures

---
+++ src/Sylius/Bundle/CoreBundle/DataFixtures/ShopConfigurationFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ShopConfigurationFixtures extends Fixture implements FixtureGroupInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+    }
+
+    public static function getGroups(): array
+    {
+        return ['shop_configuration'];
+    }
+}
