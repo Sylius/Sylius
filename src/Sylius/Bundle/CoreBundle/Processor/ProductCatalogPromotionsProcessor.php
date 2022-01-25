cae01a27de5abfa404a98c02b59b110d407f1097
Adam Kasperczak <adamkasperczak123@gmail.com>
2022-01-25 19:07:11 +0100

    refactor product processor to new logic

--- src/Sylius/Bundle/CoreBundle/Processor/ProductCatalogPromotionsProcessor.php
+++ src/Sylius/Bundle/CoreBundle/Processor/ProductCatalogPromotionsProcessor.php
@@ -13,34 +13,17 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\CoreBundle\Processor;
 
-use Sylius\Bundle\CoreBundle\Applicator\CatalogPromotionApplicatorInterface;
-use Sylius\Bundle\PromotionBundle\Provider\EligibleCatalogPromotionsProviderInterface;
-use Sylius\Component\Core\Model\CatalogPromotionInterface;
+use Sylius\Bundle\CoreBundle\Announcer\BatchedVariantsUpdateAnnouncerInterface;
 use Sylius\Component\Core\Model\ProductInterface;
 use Sylius\Component\Core\Model\ProductVariantInterface;
-use Sylius\Component\Core\Provider\CatalogPromotionVariantsProviderInterface;
-use Sylius\Component\Resource\Repository\RepositoryInterface;
+
 
 final class ProductCatalogPromotionsProcessor implements ProductCatalogPromotionsProcessorInterface
 {
-    private EligibleCatalogPromotionsProviderInterface $catalogPromotionsProvider;
-
-    private CatalogPromotionClearerInterface $catalogPromotionClearer;
-
-    private CatalogPromotionVariantsProviderInterface $catalogPromotionVariantsProvider;
-
-    private CatalogPromotionApplicatorInterface $catalogPromotionApplicator;
-
     public function __construct(
-        EligibleCatalogPromotionsProviderInterface $catalogPromotionsProvider,
-        CatalogPromotionClearerInterface $catalogPromotionClearer,
-        CatalogPromotionVariantsProviderInterface $catalogPromotionVariantsProvider,
-        CatalogPromotionApplicatorInterface $catalogPromotionApplicator
+        private CatalogPromotionClearerInterface $catalogPromotionClearer,
+        private BatchedVariantsUpdateAnnouncerInterface $announcer
     ) {
-        $this->catalogPromotionsProvider = $catalogPromotionsProvider;
-        $this->catalogPromotionClearer = $catalogPromotionClearer;
-        $this->catalogPromotionVariantsProvider = $catalogPromotionVariantsProvider;
-        $this->catalogPromotionApplicator = $catalogPromotionApplicator;
     }
 
     public function process(ProductInterface $product): void
@@ -48,35 +31,18 @@ final class ProductCatalogPromotionsProcessor implements ProductCatalogPromotion
         $variants = $product->getVariants()->toArray();
         $this->clearVariants($variants);
 
-        foreach ($this->catalogPromotionsProvider->provide() as $catalogPromotion) {
-            $this->processCatalogPromotionOnVariants($catalogPromotion, $variants);
-        }
+        $variantsCodes = array_map(
+            fn (ProductVariantInterface $variant): string => $variant->getCode(),
+            $variants
+        );
+
+        $this->announcer->dispatchVariantsUpdateCommand($variantsCodes);
     }
 
     private function clearVariants(array $variants): void
     {
         foreach ($variants as $variant) {
             $this->catalogPromotionClearer->clearVariant($variant);
-        }
-    }
-
-    private function processCatalogPromotionOnVariants(CatalogPromotionInterface $catalogPromotion, array $variants): void
-    {
-        $eligibleVariants = $this->catalogPromotionVariantsProvider->provideEligibleVariants($catalogPromotion);
-        $variantsToApplication = array_uintersect(
-            $variants,
-            $eligibleVariants,
-            function (ProductVariantInterface $firstVariant, ProductVariantInterface $secondVariant): int {
-                if ($firstVariant->getCode() === $secondVariant->getCode()) {
-                    return 0;
-                }
-
-                return 1;
-            }
-        );
-
-        foreach ($variantsToApplication as $variant) {
-            $this->catalogPromotionApplicator->applyOnVariant($variant, $catalogPromotion);
         }
     }
 }
--- src/Sylius/Bundle/CoreBundle/spec/Processor/ProductCatalogPromotionsProcessorSpec.php
+++ src/Sylius/Bundle/CoreBundle/spec/Processor/ProductCatalogPromotionsProcessorSpec.php
@@ -15,6 +15,7 @@ namespace spec\Sylius\Bundle\CoreBundle\Processor;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use PhpSpec\ObjectBehavior;
+use Sylius\Bundle\CoreBundle\Announcer\BatchedVariantsUpdateAnnouncerInterface;
 use Sylius\Bundle\CoreBundle\Applicator\CatalogPromotionApplicatorInterface;
 use Sylius\Bundle\CoreBundle\Processor\CatalogPromotionClearerInterface;
 use Sylius\Bundle\CoreBundle\Processor\ProductCatalogPromotionsProcessorInterface;
@@ -28,16 +29,12 @@ use Sylius\Component\Resource\Repository\RepositoryInterface;
 final class ProductCatalogPromotionsProcessorSpec extends ObjectBehavior
 {
     function let(
-        EligibleCatalogPromotionsProviderInterface $catalogPromotionsProvider,
         CatalogPromotionClearerInterface $catalogPromotionClearer,
-        CatalogPromotionVariantsProviderInterface $catalogPromotionVariantsProvider,
-        CatalogPromotionApplicatorInterface $catalogPromotionApplicator
+        BatchedVariantsUpdateAnnouncerInterface $announcer
     ): void {
         $this->beConstructedWith(
-            $catalogPromotionsProvider,
             $catalogPromotionClearer,
-            $catalogPromotionVariantsProvider,
-            $catalogPromotionApplicator
+            $announcer
         );
     }
 
@@ -46,15 +43,12 @@ final class ProductCatalogPromotionsProcessorSpec extends ObjectBehavior
         $this->shouldImplement(ProductCatalogPromotionsProcessorInterface::class);
     }
 
-    function it_reapplies_catalog_promotion_on_products_variants(
-        EligibleCatalogPromotionsProviderInterface $catalogPromotionsProvider,
+    function it_applies_catalog_promotion_on_products_variants(
         CatalogPromotionClearerInterface $catalogPromotionClearer,
-        CatalogPromotionVariantsProviderInterface $catalogPromotionVariantsProvider,
-        CatalogPromotionApplicatorInterface $catalogPromotionApplicator,
         ProductInterface $product,
         ProductVariantInterface $firstVariant,
         ProductVariantInterface $secondVariant,
-        CatalogPromotionInterface $catalogPromotion
+        BatchedVariantsUpdateAnnouncerInterface $announcer
     ): void {
         $product->getVariants()->willReturn(new ArrayCollection([
             $firstVariant->getWrappedObject(),
@@ -67,41 +61,7 @@ final class ProductCatalogPromotionsProcessorSpec extends ObjectBehavior
         $catalogPromotionClearer->clearVariant($firstVariant)->shouldBeCalled();
         $catalogPromotionClearer->clearVariant($secondVariant)->shouldBeCalled();
 
-        $catalogPromotionsProvider->provide()->willReturn([$catalogPromotion]);
-        $catalogPromotionVariantsProvider->provideEligibleVariants($catalogPromotion)->willReturn([$secondVariant]);
-
-        $catalogPromotionApplicator->applyOnVariant($firstVariant, $catalogPromotion)->shouldNotBeCalled();
-        $catalogPromotionApplicator->applyOnVariant($secondVariant, $catalogPromotion)->shouldBeCalled();
-
-        $this->process($product);
-    }
-
-    function it_does_not_apply_promotion_if_there_are_no_eligible_variants(
-        EligibleCatalogPromotionsProviderInterface $catalogPromotionsProvider,
-        CatalogPromotionClearerInterface $catalogPromotionClearer,
-        CatalogPromotionVariantsProviderInterface $catalogPromotionVariantsProvider,
-        CatalogPromotionApplicatorInterface $catalogPromotionApplicator,
-        ProductInterface $product,
-        ProductVariantInterface $firstVariant,
-        ProductVariantInterface $secondVariant,
-        CatalogPromotionInterface $catalogPromotion
-    ): void {
-        $product->getVariants()->willReturn(new ArrayCollection([
-            $firstVariant->getWrappedObject(),
-            $secondVariant->getWrappedObject(),
-        ]));
-
-        $firstVariant->getCode()->willReturn('PHP_MUG');
-        $secondVariant->getCode()->willReturn('SYMFONY_MUG');
-
-        $catalogPromotionClearer->clearVariant($firstVariant)->shouldBeCalled();
-        $catalogPromotionClearer->clearVariant($secondVariant)->shouldBeCalled();
-
-        $catalogPromotionsProvider->provide()->willReturn([$catalogPromotion]);
-        $catalogPromotionVariantsProvider->provideEligibleVariants($catalogPromotion)->willReturn([]);
-
-        $catalogPromotionApplicator->applyOnVariant($firstVariant, $catalogPromotion)->shouldNotBeCalled();
-        $catalogPromotionApplicator->applyOnVariant($secondVariant, $catalogPromotion)->shouldNotBeCalled();
+        $announcer->dispatchVariantsUpdateCommand(['PHP_MUG', 'SYMFONY_MUG'])->shouldBeCalled();
 
         $this->process($product);
     }
