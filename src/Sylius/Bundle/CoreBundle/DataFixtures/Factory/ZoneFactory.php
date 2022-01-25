467528bc9b31e731163c5f10e5df882ba477d51c
Loïc Frémont <lc.fremont@gmail.com>
2022-01-25 22:14:17 +0100

    Zone member factory

---
+++ src/Sylius/Bundle/CoreBundle/DataFixtures/Factory/ZoneFactory.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\DataFixtures\Factory;
+
+use Sylius\Component\Addressing\Model\Zone;
+use Sylius\Component\Addressing\Model\ZoneInterface;
+use Sylius\Component\Core\Formatter\StringInflector;
+use Sylius\Component\Resource\Factory\FactoryInterface;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @extends ModelFactory<ZoneInterface>
+ *
+ * @method static ZoneInterface|Proxy createOne(array $attributes = [])
+ * @method static ZoneInterface[]|Proxy[] createMany(int $number, array|callable $attributes = [])
+ * @method static ZoneInterface|Proxy find(object|array|mixed $criteria)
+ * @method static ZoneInterface|Proxy findOrCreate(array $attributes)
+ * @method static ZoneInterface|Proxy first(string $sortedField = 'id')
+ * @method static ZoneInterface|Proxy last(string $sortedField = 'id')
+ * @method static ZoneInterface|Proxy random(array $attributes = [])
+ * @method static ZoneInterface|Proxy randomOrCreate(array $attributes = [])
+ * @method static ZoneInterface[]|Proxy[] all()
+ * @method static ZoneInterface[]|Proxy[] findBy(array $attributes)
+ * @method static ZoneInterface[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static ZoneInterface[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method ZoneInterface|Proxy create(array|callable $attributes = [])
+ */
+final class ZoneFactory extends ModelFactory implements ZoneFactoryInterface
+{
+    public function __construct(private FactoryInterface $zoneFactory)
+    {
+        parent::__construct();
+    }
+
+    public function withCode(string $code): self
+    {
+        return $this->addState(['code' => $code]);
+    }
+
+    protected function getDefaults(): array
+    {
+        return [
+            'code' => null,
+            'name' => self::faker()->word(),
+            'type' => ZoneInterface::TYPE_ZONE,
+            'members' => [],
+        ];
+    }
+
+    protected function initialize(): self
+    {
+        return $this
+            ->beforeInstantiate(function (array $attributes): array {
+                $attributes['code'] = $attributes['code'] ?: StringInflector::nameToCode($attributes['name']);
+
+                return $attributes;
+            })
+            ->instantiateWith(function(array $attributes): ZoneInterface {
+                /** @var ZoneInterface $zone */
+                $zone = $this->zoneFactory->createNew();
+
+                $zone->setCode($attributes['code']);
+                $zone->setName($attributes['name']);
+                $zone->setType($attributes['type']);
+
+                foreach ($attributes['members'] as $member) {
+                    $zone->addMember($member);
+                }
+
+                return $zone;
+            })
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Zone::class;
+    }
+}
