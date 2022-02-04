82e4b96afe271ea0561de6c88646ea91a44351c6
Mateusz Zalewski <zaleslaw@gmail.com>
2022-02-04 09:07:44 +0100

    Enable tests that were not tagged as tests

--- src/Sylius/Bundle/CoreBundle/Tests/DependencyInjection/CircularDependencyBreakingErrorListenerPassTest.php
+++ src/Sylius/Bundle/CoreBundle/Tests/DependencyInjection/CircularDependencyBreakingErrorListenerPassTest.php
@@ -21,6 +21,7 @@ use Symfony\Component\DependencyInjection\Definition;
 
 final class CircularDependencyBreakingErrorListenerPassTest extends AbstractCompilerPassTestCase
 {
+    /** @test */
     public function it_register_circular_dependency_breaking_error_listener_when_exception_listener_is_registered(): void
     {
         $this->container->setDefinition('exception_listener', new Definition('ExceptionListener'));
@@ -30,6 +31,7 @@ final class CircularDependencyBreakingErrorListenerPassTest extends AbstractComp
         $this->assertContainerBuilderHasService(CircularDependencyBreakingErrorListener::class);
     }
 
+    /** @test */
     public function it_does_nothing_when_exception_listener_is_not_registered(): void
     {
         $this->compile();
