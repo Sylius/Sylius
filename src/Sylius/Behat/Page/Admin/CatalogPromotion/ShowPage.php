d6a5930e5951438b6b9d08a37ebc62a4bf04f140
Adam Kasperczak <adamkasperczak123@gmail.com>
2022-02-07 16:07:17 +0100

    minor fix to exclusive label

--- src/Sylius/Behat/Page/Admin/CatalogPromotion/ShowPage.php
+++ src/Sylius/Behat/Page/Admin/CatalogPromotion/ShowPage.php
@@ -95,14 +95,14 @@ class ShowPage extends SymfonyPage implements ShowPageInterface
 
     public function isExclusive(): bool
     {
-        return $this->hasElement('is_exclusive');
+        return $this->hasElement('exclusive');
     }
 
     protected function getDefinedElements(): array
     {
         return array_merge(parent::getDefinedElements(), [
             'end_date' => '[data-test-end-date]',
-            'is_exclusive' => '[data-test-is-exclusive]',
+            'exclusive' => '[data-test-exclusive]',
             'name' => '[data-test-name]',
             'priority' => '[data-test-priority]',
             'start_date' => '[data-test-start-date]',
