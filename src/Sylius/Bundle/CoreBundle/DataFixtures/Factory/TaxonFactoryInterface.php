31e63e0f6b7246040f2b316bc765ce1ed3ff773f
Loïc Frémont <lc.fremont@gmail.com>
2022-02-07 16:04:48 +0100

    Fix conflicts

---
+++ src/Sylius/Bundle/CoreBundle/DataFixtures/Factory/TaxonFactoryInterface.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\DataFixtures\Factory;
+
+use Sylius\Component\Core\Model\TaxonInterface;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @extends ModelFactory<TaxonInterface>
+ *
+ * @method static TaxonInterface|Proxy createOne(array $attributes = [])
+ * @method static TaxonInterface[]|Proxy[] createMany(int $number, array|callable $attributes = [])
+ * @method static TaxonInterface|Proxy find(object|array|mixed $criteria)
+ * @method static TaxonInterface|Proxy findOrCreate(array $attributes)
+ * @method static TaxonInterface|Proxy first(string $sortedField = 'id')
+ * @method static TaxonInterface|Proxy last(string $sortedField = 'id')
+ * @method static TaxonInterface|Proxy random(array $attributes = [])
+ * @method static TaxonInterface|Proxy randomOrCreate(array $attributes = [])
+ * @method static TaxonInterface[]|Proxy[] all()
+ * @method static TaxonInterface[]|Proxy[] findBy(array $attributes)
+ * @method static TaxonInterface[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static TaxonInterface[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method TaxonInterface|Proxy create(array|callable $attributes = [])
+ */
+interface TaxonFactoryInterface
+{
+    public function withCode(string $code): self;
+
+    public function withName(string $name): self;
+
+    public function withTranslations(array $translations): self;
+
+    public function withChildren(array $children): self;
+}
---
+++ tests/DataFixtures/Factory/TaxonFactoryTest.php
@@ -0,0 +1,117 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Tests\DataFixtures\Factory;
+
+use Sylius\Bundle\CoreBundle\DataFixtures\Factory\LocaleFactory;
+use Sylius\Bundle\CoreBundle\DataFixtures\Factory\TaxonFactory;
+use Sylius\Component\Core\Model\TaxonInterface;
+use Sylius\Component\Taxonomy\Model\TaxonTranslationInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Foundry\Test\Factories;
+
+final class TaxonFactoryTest extends KernelTestCase
+{
+    use Factories;
+
+    /** @test */
+    function it_creates_taxa(): void
+    {
+        $taxon = TaxonFactory::new()->withoutPersisting()->create();
+
+        $this->assertInstanceOf(TaxonInterface::class, $taxon->object());
+    }
+
+    /** @test */
+    function it_creates_taxa_with_codes(): void
+    {
+        $taxon = TaxonFactory::new()->withoutPersisting()->withCode('board-games')->create();
+
+        $this->assertEquals('board-games', $taxon->getCode());
+
+        $taxon = TaxonFactory::new()->withoutPersisting()->create();
+
+        $this->assertNotNull($taxon->getCode());
+    }
+
+    /** @test */
+    function it_creates_taxa_with_translations_for_each_locales(): void
+    {
+        LocaleFactory::randomOrCreate(['code' => 'en_US']);
+        LocaleFactory::randomOrCreate(['code' => 'fr_FR']);
+
+        $taxon = TaxonFactory::new()->withName('Board games')->withoutPersisting()->create();
+
+        $this->assertEquals(2, $taxon->getTranslations()->count());
+
+        $taxon->setCurrentLocale('en_US');
+        $taxon->setFallbackLocale('en_US');
+
+        $this->assertEquals('Board games', $taxon->getName());
+        $this->assertEquals('board-games', $taxon->getSlug());
+
+        $taxon->setCurrentLocale('fr_fr');
+        $taxon->setFallbackLocale('fr_FR');
+
+        $this->assertEquals('Board games', $taxon->getName());
+        $this->assertEquals('board-games', $taxon->getSlug());
+    }
+
+    /** @test */
+    function it_creates_taxa_with_custom_translations(): void
+    {
+        LocaleFactory::randomOrCreate(['code' => 'fr_FR']);
+        LocaleFactory::randomOrCreate(['code' => 'en_US']);
+
+        $taxon = TaxonFactory::new()->withTranslations([
+            'en_US' => [
+                'name' => 'Board games',
+            ],
+            'fr_FR' => [
+                'name' => 'Jeux de société',
+            ],
+        ])->withoutPersisting()->create();
+
+        $taxon->setCurrentLocale('en_US');
+        $taxon->setFallbackLocale('en_US');
+
+        $this->assertEquals('Board games', $taxon->getName());
+        $this->assertEquals('board-games', $taxon->getSlug());
+
+        $taxon->setCurrentLocale('fr_fr');
+        $taxon->setFallbackLocale('fr_FR');
+
+        $this->assertEquals('Jeux de société', $taxon->getName());
+        $this->assertEquals('jeux-de-societe', $taxon->getSlug());
+    }
+
+    /** @test */
+    function it_creates_taxa_with_children(): void
+    {
+        LocaleFactory::randomOrCreate(['code' => 'en_US']);
+
+        $taxon = TaxonFactory::new()->withCode('categories')->withChildren([
+            [
+                'name' => 'Jeans',
+            ],
+            [
+                'name' => 'Dresses',
+            ],
+        ])->withoutPersisting()->create();
+
+        /** @var TaxonInterface $firstTaxon */
+        $firstTaxon = $taxon->getChildren()->first();
+        $this->assertCount(2, $taxon->getChildren());
+        $this->assertEquals('Jeans', $firstTaxon->getName());
+    }
+}
