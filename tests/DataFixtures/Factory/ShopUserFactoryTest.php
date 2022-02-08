a3dda14e240d29343be246eae19a090d10149664
Loïc Frémont <lc.fremont@gmail.com>
2022-02-08 14:08:26 +0100

    Improve tests

--- tests/DataFixtures/Factory/ShopUserFactoryTest.php
+++ tests/DataFixtures/Factory/ShopUserFactoryTest.php
@@ -31,42 +31,38 @@ final class ShopUserFactoryTest extends KernelTestCase
         $shopUser = ShopUserFactory::new()->create();
 
         $this->assertInstanceOf(ShopUserInterface::class, $shopUser->object());
+        $this->assertNotNull($shopUser->getCustomer()->getEmail());
+        $this->assertNotNull($shopUser->getCustomer()->getFirstName());
+        $this->assertNotNull($shopUser->getCustomer()->getLastName());
+        $this->assertNotNull($shopUser->getCustomer()->getPhoneNumber());
+        $this->assertNotNull($shopUser->getCustomer()->getBirthday());
+        $this->assertNull($shopUser->getPlainPassword());
+        $this->assertNotNull($shopUser->getPassword());
+        $this->assertNotNull($shopUser->getCustomer()->getGroup());
     }
 
     /** @test */
-    function it_creates_customers_with_emails(): void
+    function it_creates_customers_with_custom_emails(): void
     {
         $shopUser = ShopUserFactory::new()->withEmail('shop@sylius.com')->create();
 
         $this->assertEquals('shop@sylius.com', $shopUser->getCustomer()->getEmail());
-
-        $shopUser = ShopUserFactory::new()->withoutPersisting()->create();
-
-        $this->assertNotNull($shopUser->getCustomer()->getEmail());
     }
 
     /** @test */
-    function it_creates_customers_with_first_names(): void
+    function it_creates_customers_with_custom_first_names(): void
     {
         $shopUser = ShopUserFactory::new()->withFirstName('Marty')->create();
 
         $this->assertEquals('Marty', $shopUser->getCustomer()->getFirstName());
-
-        $shopUser = ShopUserFactory::new()->create();
-
-        $this->assertNotNull($shopUser->getCustomer()->getFirstName());
     }
 
     /** @test */
-    function it_creates_customers_with_last_names(): void
+    function it_creates_customers_with_custom_last_names(): void
     {
         $shopUser = ShopUserFactory::new()->withLastName('McFly')->create();
 
         $this->assertEquals('McFly', $shopUser->getCustomer()->getLastName());
-
-        $shopUser = ShopUserFactory::new()->create();
-
-        $this->assertNotNull($shopUser->getCustomer()->getLastName());
     }
 
     /** @test */
@@ -86,67 +82,63 @@ final class ShopUserFactoryTest extends KernelTestCase
     }
 
     /** @test */
-    function it_creates_customers_with_phone_numbers(): void
+    function it_creates_customers_with_custom_phone_numbers(): void
     {
         $shopUser = ShopUserFactory::new()->withPhoneNumber('0102030405')->create();
 
         $this->assertEquals('0102030405', $shopUser->getCustomer()->getPhoneNumber());
-
-        $shopUser = ShopUserFactory::new()->create();
-
-        $this->assertNotNull($shopUser->getCustomer()->getPhoneNumber());
     }
 
     /** @test */
-    function it_creates_customers_with_birthdays(): void
+    function it_creates_customers_with_custom_birthdays(): void
     {
         $birthday = new \DateTimeImmutable('39 years ago');
 
         $shopUser = ShopUserFactory::new()->withBirthday($birthday)->create();
 
         $this->assertEquals($birthday, $shopUser->getCustomer()->getBirthday());
+    }
+
+    /** @test */
+    function it_creates_customers_with_custom_birthdays_via_relative_date(): void
+    {
+        $birthday = new \DateTimeImmutable('39 years ago');
 
         $shopUser = ShopUserFactory::new()->withBirthday('39 years ago')->create();
 
         $this->assertEquals($birthday->format('Y-m-d'), $shopUser->getCustomer()->getBirthday()->format('Y-m-d'));
-
-        $shopUser = ShopUserFactory::new()->create();
-
-        $this->assertNotNull($shopUser->getCustomer()->getBirthday());
     }
 
     /** @test */
-    function it_creates_customers_with_password(): void
+    function it_creates_customers_with_custom_passwords(): void
     {
         $shopUser = ShopUserFactory::new()->withoutPersisting()->withPassword('passw0rd')->create();
 
         $this->assertEquals('passw0rd', $shopUser->getPlainPassword());
-
-        $shopUser = ShopUserFactory::new()->withoutPersisting()->create();
-
-        $this->assertNotNull($shopUser->getPlainPassword());
     }
 
     /** @test */
-    function it_creates_customers_with_groups(): void
+    function it_creates_customers_with_new_custom_groups(): void
     {
-        $shopUser = ShopUserFactory::new()->create();
-
-        $this->assertNotNull($shopUser->getCustomer()->getGroup());
-
-        $shopUser = ShopUserFactory::new()->withoutPersisting()->withCustomerGroup('group_a')->create();
+        $shopUser = ShopUserFactory::new()->withCustomerGroup('group_a')->create();
 
         $this->assertEquals('group_a', $shopUser->getCustomer()->getGroup()->getCode());
+    }
 
-        // with proxy
-        $customerGroup = CustomerGroupFactory::new()->withoutPersisting()->withCode('group_a')->create();
-        $shopUser = ShopUserFactory::new()->withoutPersisting()->withCustomerGroup($customerGroup)->create();
+    /** @test */
+    function it_creates_customers_with_existing_proxy_custom_groups(): void
+    {
+        $customerGroup = CustomerGroupFactory::new()->withCode('group_a')->create();
+        $shopUser = ShopUserFactory::new()->withCustomerGroup($customerGroup)->create();
 
         $this->assertEquals($customerGroup->object(), $shopUser->getCustomer()->getGroup());
+    }
 
-        // without proxy
-        $customerGroup = CustomerGroupFactory::new()->withoutPersisting()->withCode('group_a')->create()->object();
-        $shopUser = ShopUserFactory::new()->withoutPersisting()->withCustomerGroup($customerGroup)->create();
+    /** @test */
+    function it_creates_customers_with_existing_custom_groups(): void
+    {
+        $customerGroup = CustomerGroupFactory::new()->withCode('group_a')->create()->object();
+        $shopUser = ShopUserFactory::new()->withCustomerGroup($customerGroup)->create();
 
         $this->assertEquals($customerGroup, $shopUser->getCustomer()->getGroup());
     }
