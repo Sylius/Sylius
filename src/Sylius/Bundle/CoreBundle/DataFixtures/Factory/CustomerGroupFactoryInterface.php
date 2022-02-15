1b038e7ebcde8d29a629760c3b6d33debf7076c6
Loïc Frémont <lc.fremont@gmail.com>
2022-02-15 17:55:18 +0100

    [Fixtures] Customer group factory (#13541)
    Add customer group factory

---
+++ src/Sylius/Bundle/CoreBundle/DataFixtures/Factory/CustomerGroupFactoryInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\DataFixtures\Factory;
+
+use Sylius\Component\Customer\Model\CustomerGroupInterface;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @extends ModelFactory<CustomerGroupInterface>
+ *
+ * @method static CustomerGroupInterface|Proxy createOne(array $attributes = [])
+ * @method static CustomerGroupInterface[]|Proxy[] createMany(int $number, array|callable $attributes = [])
+ * @method static CustomerGroupInterface|Proxy find(object|array|mixed $criteria)
+ * @method static CustomerGroupInterface|Proxy findOrCreate(array $attributes)
+ * @method static CustomerGroupInterface|Proxy first(string $sortedField = 'id')
+ * @method static CustomerGroupInterface|Proxy last(string $sortedField = 'id')
+ * @method static CustomerGroupInterface|Proxy random(array $attributes = [])
+ * @method static CustomerGroupInterface|Proxy randomOrCreate(array $attributes = [])
+ * @method static CustomerGroupInterface[]|Proxy[] all()
+ * @method static CustomerGroupInterface[]|Proxy[] findBy(array $attributes)
+ * @method static CustomerGroupInterface[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static CustomerGroupInterface[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method CustomerGroupInterface|Proxy create(array|callable $attributes = [])
+ */
+interface CustomerGroupFactoryInterface
+{
+    public function withCode(string $code): self;
+
+    public function withName(string $name): self;
+}
