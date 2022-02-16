e93eb786a7cecceffab0b36b4fccd5ed5582bd20
Loïc Frémont <lc.fremont@gmail.com>
2022-02-16 17:52:39 +0100

    Init address factory

---
+++ src/Sylius/Bundle/CoreBundle/DataFixtures/Factory/AddressFactory.php
@@ -0,0 +1,143 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\DataFixtures\Factory;
+
+use Sylius\Component\Core\Model\Address;
+use Sylius\Component\Core\Model\AddressInterface;
+use Sylius\Component\Core\Model\CustomerInterface;
+use Sylius\Component\Resource\Factory\FactoryInterface;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @extends ModelFactory<AddressInterface>
+ *
+ * @method static AddressInterface|Proxy createOne(array $attributes = [])
+ * @method static AddressInterface[]|Proxy[] createMany(int $number, array|callable $attributes = [])
+ * @method static AddressInterface|Proxy find(object|array|mixed $criteria)
+ * @method static AddressInterface|Proxy findOrCreate(array $attributes)
+ * @method static AddressInterface|Proxy first(string $sortedField = 'id')
+ * @method static AddressInterface|Proxy last(string $sortedField = 'id')
+ * @method static AddressInterface|Proxy random(array $attributes = [])
+ * @method static AddressInterface|Proxy randomOrCreate(array $attributes = [])
+ * @method static AddressInterface[]|Proxy[] all()
+ * @method static AddressInterface[]|Proxy[] findBy(array $attributes)
+ * @method static AddressInterface[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static AddressInterface[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method AddressInterface|Proxy create(array|callable $attributes = [])
+ */
+final class AddressFactory extends ModelFactory implements AddressFactoryInterface
+{
+    public function __construct(
+        private FactoryInterface $addressFactory,
+        private CountryFactoryInterface $countryFactory
+    ) {
+        parent::__construct();
+    }
+
+    public function withFirstName(string $firstName): self
+    {
+        return $this->addState(['first_name' => $firstName]);
+    }
+
+    public function withLastName(string $lastName): self
+    {
+        return $this->addState(['last_name' => $lastName]);
+    }
+
+    public function withPhoneNumber(string $phoneNumber): self
+    {
+        return $this->addState(['phone_number' => $phoneNumber]);
+    }
+
+    public function withCompany(string $company): self
+    {
+        return $this->addState(['company' => $company]);
+    }
+
+    public function withStreet(string $street): self
+    {
+        return $this->addState(['street' => $street]);
+    }
+
+    public function withCity(string $city): self
+    {
+        return $this->addState(['city' => $city]);
+    }
+
+    public function withPostcode(string $postcode): self
+    {
+        return $this->addState(['postcode' => $postcode]);
+    }
+
+    public function withCountryCode(string $countryCode): self
+    {
+        return $this->addState(['country_code' => $countryCode]);
+    }
+
+    public function withProvinceName(string $provinceName): self
+    {
+        return $this->addState(['province_name' => $provinceName]);
+    }
+
+    public function withProvinceCode(string $provinceCode): self
+    {
+        return $this->addState(['province_code' => $provinceCode]);
+    }
+
+    protected function getDefaults(): array
+    {
+        return [
+            'first_name' => self::faker()->firstName(),
+            'last_name' => self::faker()->lastName(),
+            'phone_number' => self::faker()->boolean() ? self::faker()->phoneNumber() : null,
+            'company' => self::faker()->boolean() ? self::faker()->company() : null,
+            'street' => self::faker()->streetAddress(),
+            'city' => self::faker()->city(),
+            'postcode' => self::faker()->postcode(),
+            'country_code' => $this->countryFactory::randomOrCreate()->getCode(),
+            'province_name' => null,
+            'province_code' => null,
+            'customer' => null,
+        ];
+    }
+
+    protected function initialize(): self
+    {
+        return $this
+            ->instantiateWith(function(array $attributes): AddressInterface {
+                /** @var AddressInterface $address */
+                $address = $this->addressFactory->createNew();
+                $address->setFirstName($attributes['first_name']);
+                $address->setLastName($attributes['last_name']);
+                $address->setPhoneNumber($attributes['phone_number']);
+                $address->setCompany($attributes['company']);
+                $address->setStreet($attributes['street']);
+                $address->setCity($attributes['city']);
+                $address->setPostcode($attributes['postcode']);
+                $address->setCountryCode($attributes['country_code']);
+                $address->setProvinceName($attributes['province_name']);
+                $address->setProvinceCode($attributes['province_code']);
+                $address->setCustomer($attributes['customer']);
+
+                return $address;
+            })
+        ;
+    }
+
+    protected static function getClass(): string
+    {
+        return Address::class;
+    }
+}
---
+++ tests/DataFixtures/Factory/AddressFactoryTest.php
@@ -0,0 +1,120 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Tests\DataFixtures\Factory;
+
+use Sylius\Bundle\CoreBundle\DataFixtures\Factory\AddressFactory;
+use Sylius\Component\Core\Model\AddressInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Foundry\Test\Factories;
+use Zenstruck\Foundry\Test\ResetDatabase;
+
+final class AddressFactoryTest extends KernelTestCase
+{
+    use ResetDatabase;
+    use Factories;
+
+    /** @test */
+    function it_creates_address_with_random_data(): void
+    {
+        $address = AddressFactory::createOne();
+
+        $this->assertInstanceOf(AddressInterface::class, $address->object());
+        $this->assertNotNull($address->getFirstName());
+        $this->assertNotNull($address->getLastName());
+        $this->assertNotNull($address->getStreet());
+        $this->assertNotNull($address->getCity());
+        $this->assertNotNull($address->getPostcode());
+        $this->assertNotNull($address->getCountryCode());
+    }
+
+    /** @test */
+    function it_creates_address_with_given_first_name(): void
+    {
+        $address = AddressFactory::new()->withFirstName('Marty')->create();
+
+        $this->assertEquals('Marty', $address->getFirstName());
+    }
+
+    /** @test */
+    function it_creates_address_with_given_last_name(): void
+    {
+        $address = AddressFactory::new()->withLastName('McFly')->create();
+
+        $this->assertEquals('McFly', $address->getLastName());
+    }
+
+    /** @test */
+    function it_creates_address_with_given_phone_number(): void
+    {
+        $address = AddressFactory::new()->withPhoneNumber('1955-1985-2015')->create();
+
+        $this->assertEquals('1955-1985-2015', $address->getPhoneNumber());
+    }
+
+    /** @test */
+    function it_creates_address_with_given_company(): void
+    {
+        $address = AddressFactory::new()->withCompany('Universal Pictures')->create();
+
+        $this->assertEquals('Universal Pictures', $address->getCompany());
+    }
+
+    /** @test */
+    function it_creates_address_with_given_street(): void
+    {
+        $address = AddressFactory::new()->withStreet('9303 Lyon Drive, Lyon Estates')->create();
+
+        $this->assertEquals('9303 Lyon Drive, Lyon Estates', $address->getStreet());
+    }
+
+    /** @test */
+    function it_creates_address_with_given_city(): void
+    {
+        $address = AddressFactory::new()->withCity('Hill Valley')->create();
+
+        $this->assertEquals('Hill Valley', $address->getCity());
+    }
+
+    /** @test */
+    function it_creates_address_with_given_post_code(): void
+    {
+        $address = AddressFactory::new()->withPostcode('95420')->create();
+
+        $this->assertEquals('95420', $address->getPostcode());
+    }
+
+    /** @test */
+    function it_creates_address_with_given_country_code(): void
+    {
+        $address = AddressFactory::new()->withCountryCode('US')->create();
+
+        $this->assertEquals('US', $address->getCountryCode());
+    }
+
+    /** @test */
+    function it_creates_address_with_given_province_name(): void
+    {
+        $address = AddressFactory::new()->withProvinceName('California')->create();
+
+        $this->assertEquals('California', $address->getProvinceName());
+    }
+
+    /** @test */
+    function it_creates_address_with_given_province_code(): void
+    {
+        $address = AddressFactory::new()->withProvinceCode('CA')->create();
+
+        $this->assertEquals('CA', $address->getProvinceCode());
+    }
+}
