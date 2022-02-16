016fb11ed7aeb48cf7b00bc230ad763b3afaaa1b
Loïc Frémont <lc.fremont@gmail.com>
2022-02-16 10:26:23 +0100

    Fix getClass method

--- src/Sylius/Bundle/CoreBundle/DataFixtures/Factory/ProductAssociationTypeFactory.php
+++ src/Sylius/Bundle/CoreBundle/DataFixtures/Factory/ProductAssociationTypeFactory.php
@@ -13,9 +13,9 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\CoreBundle\DataFixtures\Factory;
 
-use Sylius\Bundle\ProductBundle\Form\Type\ProductAssociationType;
 use Sylius\Component\Core\Formatter\StringInflector;
 use Sylius\Component\Locale\Model\LocaleInterface;
+use Sylius\Component\Product\Model\ProductAssociationType;
 use Sylius\Component\Product\Model\ProductAssociationTypeInterface;
 use Sylius\Component\Resource\Factory\FactoryInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
