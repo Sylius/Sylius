f79b210bffbbda80cdb79e481fd71a858c6d483d
mamazu <14860264+mamazu@users.noreply.github.com>
2022-02-18 23:41:48 +0100

    Moving the ThemeContext to the shop bundle

--- src/Sylius/Bundle/ShopBundle/Form/Extension/ChannelTypeExtension.php
+++ src/Sylius/Bundle/ShopBundle/Form/Extension/ChannelTypeExtension.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Sylius\Bundle\ShopBundle\Form\Extension;
 
 use Sylius\Bundle\ChannelBundle\Form\Type\ChannelType;
@@ -7,7 +18,7 @@ use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\FormBuilderInterface;
 use Sylius\Bundle\ThemeBundle\Form\Type\ThemeNameChoiceType;
 
-class ChannelTypeExtension extends AbstractTypeExtension
+final class ChannelTypeExtension extends AbstractTypeExtension
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
