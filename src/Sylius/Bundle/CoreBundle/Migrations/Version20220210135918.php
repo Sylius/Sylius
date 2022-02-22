314cd103e61cb347d770fd30bab7d4a51b34a1a7
Grzegorz Sadowski <sadowskigp@gmail.com>
2022-02-22 08:14:26 +0100

    [Migrations] Add missing mysql check for migrations

--- src/Sylius/Bundle/CoreBundle/Migrations/Version20220210135918.php
+++ src/Sylius/Bundle/CoreBundle/Migrations/Version20220210135918.php
@@ -19,12 +19,16 @@ final class Version20220210135918 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
         $this->addSql('ALTER TABLE sylius_order ADD by_guest TINYINT(1) DEFAULT \'1\' NOT NULL');
         $this->addSql('UPDATE sylius_order o SET o.by_guest = 0 WHERE o.customer_id IN (SELECT customer_id FROM sylius_shop_user)');
     }
 
     public function down(Schema $schema): void
     {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
         $this->addSql('ALTER TABLE sylius_order DROP by_guest');
     }
 }
