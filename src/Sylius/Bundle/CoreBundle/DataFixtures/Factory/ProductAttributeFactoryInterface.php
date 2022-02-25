c0c3c3fd4f44207a5bb2c6c29a94655b6ae3fcfd
Loïc Frémont <lc.fremont@gmail.com>
2022-02-25 19:04:51 +0100

    [Fixtures] Product attribute factory (#13618)
    Product attribute factory

---
+++ src/Sylius/Bundle/CoreBundle/DataFixtures/Factory/ProductAttributeFactoryInterface.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\DataFixtures\Factory;
+
+use Sylius\Component\Product\Model\ProductAttributeInterface;
+use Zenstruck\Foundry\ModelFactory;
+use Zenstruck\Foundry\Proxy;
+
+/**
+ * @extends ModelFactory<ProductAttributeInterface>
+ *
+ * @method static ProductAttributeInterface|Proxy createOne(array $attributes = [])
+ * @method static ProductAttributeInterface[]|Proxy[] createMany(int $number, array|callable $attributes = [])
+ * @method static ProductAttributeInterface|Proxy find(object|array|mixed $criteria)
+ * @method static ProductAttributeInterface|Proxy findOrCreate(array $attributes)
+ * @method static ProductAttributeInterface|Proxy first(string $sortedField = 'id')
+ * @method static ProductAttributeInterface|Proxy last(string $sortedField = 'id')
+ * @method static ProductAttributeInterface|Proxy random(array $attributes = [])
+ * @method static ProductAttributeInterface|Proxy randomOrCreate(array $attributes = [])
+ * @method static ProductAttributeInterface[]|Proxy[] all()
+ * @method static ProductAttributeInterface[]|Proxy[] findBy(array $attributes)
+ * @method static ProductAttributeInterface[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static ProductAttributeInterface[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method ProductAttributeInterface|Proxy create(array|callable $attributes = [])
+ */
+interface ProductAttributeFactoryInterface
+{
+    public function withCode(string $code): self;
+
+    public function withType(string $type): self;
+
+    public function withName(string $name): self;
+
+    public function translatable(): self;
+
+    public function untranslatable(): self;
+
+    public function withConfiguration(array $configuration): self;
+}
