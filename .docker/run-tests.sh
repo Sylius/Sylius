e62e6b32a7a1763381005b4dbb6bd94d19f0cdef
Ferror <zmalcherczyk@gmail.com>
2022-03-03 12:59:11 +0100

    add else echo

--- .docker/run-tests.sh
+++ .docker/run-tests.sh
@@ -4,8 +4,10 @@ CURRENT_NODE_VERSION=$(docker run -i sylius node --version)
 EXPECTED_NODE_VERSION="v14.19.0"
 
 if [[ $CURRENT_NODE_VERSION != "$EXPECTED_NODE_VERSION" ]]; then
-  echo "Invalid NODE version got $NODE_VERSION expected $EXPECTED_NODE_VERSION"
-  exit 1
+    echo "Invalid NODE version got $CURRENT_NODE_VERSION expected $EXPECTED_NODE_VERSION"
+    exit 1
+else
+    echo "NODE version: $CURRENT_NODE_VERSION"
 fi
 
 exit 0
