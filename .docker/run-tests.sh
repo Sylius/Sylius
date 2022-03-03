021485719ec2207dd765541e4dbefdf312e66337
Ferror <zmalcherczyk@gmail.com>
2022-03-03 12:35:15 +0100

    run tests

---
+++ .docker/run-tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CURRENT_NODE_VERSION=$(docker run -i sylius node --version)
+EXPECTED_NODE_VERSION="v14.19.0"
+
+if [[ $CURRENT_NODE_VERSION != "$EXPECTED_NODE_VERSION" ]]; then
+  echo "Invalid NODE version got $NODE_VERSION expected $EXPECTED_NODE_VERSION"
+  exit 1
+fi
