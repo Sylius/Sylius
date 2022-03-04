c3889a8aa5f5f01678525f89abdd6caf67d43b90
Loïc Frémont <lc.fremont@gmail.com>
2022-03-04 09:38:56 +0100

    Purge database trait (#13717)

--- tests/DataFixtures/Factory/CustomerGroupFactoryTest.php
+++ tests/DataFixtures/Factory/CustomerGroupFactoryTest.php
@@ -15,13 +15,13 @@ namespace Sylius\Tests\DataFixtures\Factory;
 
 use Sylius\Bundle\CoreBundle\DataFixtures\Factory\CustomerGroupFactory;
 use Sylius\Component\Customer\Model\CustomerGroupInterface;
+use Sylius\Tests\PurgeDatabaseTrait;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Zenstruck\Foundry\Test\Factories;
-use Zenstruck\Foundry\Test\ResetDatabase;
 
 final class CustomerGroupFactoryTest extends KernelTestCase
 {
-    use ResetDatabase;
+    use PurgeDatabaseTrait;
     use Factories;
 
     /** @test */
--- tests/DataFixtures/Factory/ZoneMemberFactoryTest.php
+++ tests/DataFixtures/Factory/ZoneMemberFactoryTest.php
@@ -16,13 +16,13 @@ namespace Sylius\Tests\DataFixtures\Factory;
 use Sylius\Bundle\CoreBundle\DataFixtures\Factory\ZoneFactory;
 use Sylius\Bundle\CoreBundle\DataFixtures\Factory\ZoneMemberFactory;
 use Sylius\Component\Addressing\Model\ZoneMemberInterface;
+use Sylius\Tests\PurgeDatabaseTrait;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Zenstruck\Foundry\Test\Factories;
-use Zenstruck\Foundry\Test\ResetDatabase;
 
 final class ZoneMemberFactoryTest extends KernelTestCase
 {
-    use ResetDatabase;
+    use PurgeDatabaseTrait;
     use Factories;
 
     /** @test */
