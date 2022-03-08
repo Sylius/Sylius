b084d62a9c8ed18f7dd88bc5f04f343b29c31fa6
Ferror <zmalcherczyk@gmail.com>
2022-03-08 09:31:37 +0100

    Check for php version

--- .docker/run-tests.sh
+++ .docker/run-tests.sh
@@ -10,4 +10,14 @@ else
     echo "NODE version: $CURRENT_NODE_VERSION"
 fi
 
+CURRENT_PHP_VERSION=$(docker run -i sylius php -r 'echo phpversion();')
+EXPECTED_PHP_VERSION="8.0.16"
+
+if [[ $CURRENT_PHP_VERSION != "$EXPECTED_PHP_VERSION" ]]; then
+    echo "Invalid PHP version got $CURRENT_PHP_VERSION expected $EXPECTED_PHP_VERSION"
+    exit 1
+else
+    echo "PHP version: $CURRENT_PHP_VERSION"
+fi
+
 exit 0
