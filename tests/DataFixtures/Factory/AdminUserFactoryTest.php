875cd606581571321065351d316d89f8fbb8cf0b
Loïc Frémont <lc.fremont@gmail.com>
2022-03-12 20:14:45 +0100

    Use Purge database trait

--- tests/DataFixtures/Factory/AdminUserFactoryTest.php
+++ tests/DataFixtures/Factory/AdminUserFactoryTest.php
@@ -15,13 +15,13 @@ namespace Sylius\Tests\DataFixtures\Factory;
 
 use Sylius\Bundle\CoreBundle\DataFixtures\Factory\AdminUserFactory;
 use Sylius\Component\Core\Model\AdminUserInterface;
+use Sylius\Tests\PurgeDatabaseTrait;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Zenstruck\Foundry\Test\Factories;
-use Zenstruck\Foundry\Test\ResetDatabase;
 
 final class AdminUserFactoryTest extends KernelTestCase
 {
-    use ResetDatabase;
+    use PurgeDatabaseTrait;
     use Factories;
 
     /** @test */
