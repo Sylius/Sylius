1380d09420469a0cc2af6b7120d9bb6eb53d121f
Ferror <zmalcherczyk@gmail.com>
2022-03-15 18:04:14 +0100

    Check the state

--- src/Sylius/Component/Core/StateGuard/CompleteStepGuard.php
+++ src/Sylius/Component/Core/StateGuard/CompleteStepGuard.php
@@ -13,15 +13,33 @@ declare(strict_types=1);
 
 namespace Sylius\Component\Core\StateGuard;
 
+use SM\Factory\FactoryInterface;
 use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\OrderCheckoutStates;
+use Sylius\Component\Core\OrderCheckoutTransitions;
 
 class CompleteStepGuard
 {
+    public function __construct(
+        private FactoryInterface $stateMachineFactory,
+    ) {
+    }
+
     /**
      * You can skip shipping step and payment step when order is free AND not require shipping.
      */
     public function isSatisfiedBy(OrderInterface $order): bool
     {
-        return $order->isShippingRequired() === false && $order->getTotal() === 0;
+        $stateMachine = $this->stateMachineFactory->get($order, OrderCheckoutTransitions::GRAPH);
+
+        return $order->isShippingRequired() === false
+            && $order->getTotal() === 0
+            && in_array(
+                $stateMachine->getState(),
+                [
+                    OrderCheckoutStates::STATE_ADDRESSED,
+                    OrderCheckoutStates::STATE_PAYMENT_SELECTED,
+                ]
+            );
     }
 }
