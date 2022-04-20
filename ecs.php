20f38df1b568a21eb38c0c936e02c05072393532
Ferror <zmalcherczyk@gmail.com>
2022-04-20 16:38:53 +0200

    [maintenance]Fix ECS HeaderCommentFixer

--- ecs.php
+++ ecs.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use PhpCsFixer\Fixer\ClassNotation\VisibilityRequiredFixer;
 use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
 use SlevomatCodingStandard\Sniffs\Commenting\InlineDocCommentDeclarationSniff;
@@ -9,16 +18,22 @@ use Symplify\EasyCodingStandard\ValueObject\Option;
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import('vendor/sylius-labs/coding-standard/ecs.php');
 
-//    $containerConfigurator->services()->set(HeaderCommentFixer::class)->call('configure', [
-//        'location' => 'after_open',
-//        'header' =>
-//'This file is part of the Sylius package.
-//
-//(c) Paweł Jędrzejewski
-//
-//For the full copyright and license information, please view the LICENSE
-//file that was distributed with this source code.',
-//    ]);
+    $containerConfigurator->services()
+        ->set(HeaderCommentFixer::class)
+        ->call('configure', [
+            [
+                'location' => 'after_open',
+                'comment_type' => HeaderCommentFixer::HEADER_COMMENT,
+                'header' => <<<TEXT
+This file is part of the Sylius package.
+
+(c) Paweł Jędrzejewski
+
+For the full copyright and license information, please view the LICENSE
+file that was distributed with this source code.
+TEXT
+            ]
+        ]);
 
     $containerConfigurator->parameters()->set(Option::SKIP, [
         InlineDocCommentDeclarationSniff::class . '.MissingVariable',
