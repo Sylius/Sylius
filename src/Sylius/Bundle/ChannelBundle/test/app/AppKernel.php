d41bc32bea9a19b85af6db3b5626544658aa9c61
Loïc Frémont <lc.fremont@gmail.com>
2022-05-13 13:49:01 +0200

    Fix Channel bundle test app

--- src/Sylius/Bundle/ChannelBundle/test/app/AppKernel.php
+++ src/Sylius/Bundle/ChannelBundle/test/app/AppKernel.php
@@ -32,7 +32,7 @@ class AppKernel extends Kernel
         ];
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
+    public function registerContainerConfiguration(LoaderInterface $loader): void
     {
         $loader->load(__DIR__ . '/config/config.yml');
     }
