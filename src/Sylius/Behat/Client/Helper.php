13cb7e78c15f77999a2851b18c877535b3f9cb63
Ferror <zmalcherczyk@gmail.com>
2022-05-30 12:27:03 +0200

    minor change

--- src/Sylius/Behat/Client/Helper.php
+++ src/Sylius/Behat/Client/Helper.php
@@ -14,12 +14,14 @@ declare(strict_types=1);
 namespace Sylius\Behat\Client;
 
 use Sylius\Behat\Service\SharedStorageInterface;
+use Sylius\Behat\Service\Storage\RequestBuilderStorageInterface;
 use Webmozart\Assert\Assert;
 
 final class Helper
 {
     public function __construct(
         private SharedStorageInterface $sharedStorage,
+        private RequestBuilderStorageInterface $requestBuilderStorage,
         private RequestBuilderFactoryInterface $requestBuilderFactory,
         private ApiClientInterface $apiClient,
         private string $authorizationHeader,
@@ -29,28 +31,28 @@ final class Helper
     public function update(callable $closure): void
     {
         /** @var RequestBuilder $requestBuilder */
-        $requestBuilder = $this->sharedStorage->get('request_builder');
+        $requestBuilder = $this->requestBuilderStorage->get();
 
         $requestBuilder = $closure($requestBuilder);
 
-        $this->sharedStorage->set('request_builder', $requestBuilder);
+        $this->requestBuilderStorage->set($requestBuilder);
     }
 
     public function authorize(RequestBuilder $requestBuilder): RequestBuilder
     {
         return $requestBuilder->withHeader(
             sprintf('HTTP_%s', $this->authorizationHeader),
-            sprintf('Bearer %s',$this->sharedStorage->get('token'))
+            sprintf('Bearer %s', $this->sharedStorage->get('token'))
         );
     }
 
-    public function preparePut(string $section, string $resource, string $id): RequestBuilder
+    public function preparePut(string ...$resources): RequestBuilder
     {
-        $showRequest = $this->requestBuilderFactory->get($section, $resource, $id);
+        $showRequest = $this->requestBuilderFactory->get(...$resources);
         $showRequest = $this->authorize($showRequest);
         $showResponse = $this->apiClient->executeCustomRequest($showRequest->build());
 
-        $updateRequestBuilder = $this->requestBuilderFactory->put($section, $resource, $id);
+        $updateRequestBuilder = $this->requestBuilderFactory->put(...$resources);
         $updateRequestBuilder = $this->authorize($updateRequestBuilder);
 
         Assert::notFalse($showResponse->getContent());
