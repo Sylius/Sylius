623c91c20474bf99879721a85621cbfcddba73a9
Loïc Frémont <lc.fremont@gmail.com>
2022-06-02 10:20:15 +0200

    [Rector] Configure PHP version up to 8.0

--- rector.php
+++ rector.php
@@ -5,13 +5,12 @@ declare(strict_types=1);
 use Rector\Core\Configuration\Option;
 use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
-use Rector\Set\ValueObject\SetList;
+use Rector\Set\ValueObject\LevelSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void
 {
-    $containerConfigurator->import(SetList::PHP_74);
-    $containerConfigurator->import(SetList::PHP_80);
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::AUTO_IMPORT_NAMES, true);
