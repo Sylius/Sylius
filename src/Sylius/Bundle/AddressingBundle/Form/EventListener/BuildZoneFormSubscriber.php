71d72ec7fd19c6be502664a3deb04e5130b6fc2e
Kevin Kaniaburka <kevin.kaniaburka@gmail.com>
2022-06-07 08:09:56 +0200

    [Addressing] Add BuildZoneFormSubscriber

---
+++ src/Sylius/Bundle/AddressingBundle/Form/EventListener/BuildZoneFormSubscriber.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\AddressingBundle\Form\EventListener;
+
+use Sylius\Component\Addressing\Model\ZoneInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Webmozart\Assert\Assert;
+
+/** @internal */
+final class BuildZoneFormSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            FormEvents::PRE_SUBMIT => 'preSubmit',
+        ];
+    }
+
+    public function preSubmit(FormEvent $event): void
+    {
+        $data = $event->getData();
+
+        if (!isset($data['members'])) {
+            return;
+        }
+
+        /** @var ZoneInterface $zone */
+        $zone = $event->getForm()->getData();
+
+        Assert::isInstanceOf($zone, ZoneInterface::class);
+
+        $membersCodes = $zone->getMembers()
+            ->map(fn ($member): string => $member->getCode())
+            ->getValues()
+        ;
+
+        $members = [];
+        $newlyAddedMembers = [];
+
+        foreach ($data['members'] as $member) {
+            if (!isset($member['code'])) {
+                continue;
+            }
+
+            $existingMemberIndex = array_search($member['code'], $membersCodes, true);
+
+            if (false === $existingMemberIndex) {
+                $newlyAddedMembers[] = $member;
+
+                continue;
+            }
+
+            $members[$existingMemberIndex] = $member;
+        }
+
+        array_push($members, ...$newlyAddedMembers);
+        $data['members'] = $members;
+
+        $event->setData($data);
+    }
+}
---
+++ src/Sylius/Bundle/AddressingBundle/spec/Form/EventListener/BuildZoneFormSubscriberSpec.php
@@ -0,0 +1,173 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace spec\Sylius\Bundle\AddressingBundle\Form\EventListener;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Sylius\Bundle\AddressingBundle\Form\EventListener\BuildZoneFormSubscriber;
+use Sylius\Component\Addressing\Model\ZoneInterface;
+use Sylius\Component\Addressing\Model\ZoneMemberInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
+
+final class BuildZoneFormSubscriberSpec extends ObjectBehavior
+{
+    function it_is_initializable(): void
+    {
+        $this->shouldHaveType(BuildZoneFormSubscriber::class);
+    }
+
+    function it_is_a_subscriber(): void
+    {
+        $this->shouldImplement(EventSubscriberInterface::class);
+    }
+
+    function it_subscribes_to_event(): void
+    {
+        $this::getSubscribedEvents()->shouldReturn([
+            FormEvents::PRE_SUBMIT => 'preSubmit',
+        ]);
+    }
+
+    function it_fixes_members_keys_on_pre_submit(
+        FormEvent $event,
+        FormInterface $form,
+        ZoneInterface $zone,
+        ZoneMemberInterface $belgiumZone,
+        ZoneMemberInterface $germanyZone,
+        ZoneMemberInterface $franceZone,
+    ): void {
+        $event->getData()->willReturn([
+            'name' => 'Europe',
+            'code' => 'EU',
+            'members' => [
+                0 => ['code' => 'EU-BE'],
+                3 => ['code' => 'EU-PL'],
+                4 => ['code' => 'EU-FR'],
+            ],
+        ]);
+
+        $event->getForm()->willReturn($form);
+        $form->getData()->willReturn($zone);
+
+        $belgiumZone->getCode()->willReturn('EU-BE');
+        $germanyZone->getCode()->willReturn('EU-DE');
+        $franceZone->getCode()->willReturn('EU-FR');
+
+        $zone->getMembers()->willReturn(new ArrayCollection([
+            $belgiumZone->getWrappedObject(),
+            $germanyZone->getWrappedObject(),
+            $franceZone->getWrappedObject(),
+        ]));
+
+        $event
+            ->setData([
+                'name' => 'Europe',
+                'code' => 'EU',
+                'members' => [
+                    0 => ['code' => 'EU-BE'],
+                    2 => ['code' => 'EU-FR'],
+                    3 => ['code' => 'EU-PL'],
+                ],
+            ])
+            ->shouldBeCalled()
+        ;
+
+        $this->preSubmit($event);
+    }
+
+    function it_does_nothing_if_there_are_no_members_in_the_form_data(FormEvent $event): void
+    {
+        $event->getData()->willReturn([
+            'name' => 'Europe',
+            'code' => 'EU',
+        ]);
+
+        $event->setData(Argument::any())->shouldNotBeCalled();
+
+        $this->preSubmit($event);
+    }
+
+    function it_ignores_members_with_no_code(
+        FormEvent $event,
+        FormInterface $form,
+        ZoneInterface $zone,
+        ZoneMemberInterface $belgiumZone,
+        ZoneMemberInterface $germanyZone,
+        ZoneMemberInterface $franceZone,
+    ): void {
+        $event->getData()->willReturn([
+            'name' => 'Europe',
+            'code' => 'EU',
+            'members' => [
+                0 => ['code' => 'EU-BE'],
+                3 => [],
+                4 => ['code' => 'EU-FR'],
+            ],
+        ]);
+
+        $event->getForm()->willReturn($form);
+        $form->getData()->willReturn($zone);
+
+        $belgiumZone->getCode()->willReturn('EU-BE');
+        $germanyZone->getCode()->willReturn('EU-DE');
+        $franceZone->getCode()->willReturn('EU-FR');
+
+        $zone->getMembers()->willReturn(new ArrayCollection([
+            $belgiumZone->getWrappedObject(),
+            $germanyZone->getWrappedObject(),
+            $franceZone->getWrappedObject(),
+        ]));
+
+        $event
+            ->setData([
+                'name' => 'Europe',
+                'code' => 'EU',
+                'members' => [
+                    0 => ['code' => 'EU-BE'],
+                    2 => ['code' => 'EU-FR'],
+                ],
+            ])
+            ->shouldBeCalled()
+        ;
+
+        $this->preSubmit($event);
+    }
+
+    public function it_throws_an_exception_if_from_data_is_not_a_zone(
+        FormEvent $event,
+        FormInterface $form
+    ): void {
+        $event->getData()->willReturn([
+            'name' => 'Europe',
+            'code' => 'EU',
+            'members' => [
+                0 => ['code' => 'EU-BE'],
+                3 => ['code' => 'EU-PL'],
+                4 => ['code' => 'EU-FR'],
+            ],
+        ]);
+
+        $event->getForm()->willReturn($form);
+        $form->getData()->willReturn(null);
+
+        $this
+            ->shouldThrow(\InvalidArgumentException::class)
+            ->during('preSubmit', [$event])
+        ;
+    }
+}
