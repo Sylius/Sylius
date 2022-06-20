a5c76bd110050410d691312e13d030ad041ce389
Grzegorz Sadowski <sadowskigp@gmail.com>
2022-06-20 07:43:35 +0200

    [Fixtures] Add a fixture listener for removing images

---
+++ src/Sylius/Bundle/CoreBundle/spec/Fixture/Listener/ImagesPurgerListenerSpec.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace spec\Sylius\Bundle\CoreBundle\Fixture\Listener;
+
+use PhpSpec\ObjectBehavior;
+use Sylius\Bundle\FixturesBundle\Listener\SuiteEvent;
+use Sylius\Bundle\FixturesBundle\Suite\SuiteInterface;
+use Symfony\Component\Filesystem\Filesystem;
+
+final class ImagesPurgerListenerSpec extends ObjectBehavior
+{
+    public function let(Filesystem $filesystem): void
+    {
+        $this->beConstructedWith($filesystem, '/media');
+    }
+
+    public function it_removes_images_before_fixture_suite(Filesystem $filesystem, SuiteInterface $suite): void
+    {
+        $filesystem->remove('/media')->shouldBeCalled();
+
+        $this->beforeSuite(new SuiteEvent($suite->getWrappedObject()), []);
+    }
+}
