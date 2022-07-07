c5ecc80b45c9313153eae31b4c3c1b8a9984b671
Loïc Frémont <lc.fremont@gmail.com>
2022-07-07 18:02:09 +0200

    Fix some deprecations

--- src/Sylius/Bundle/CoreBundle/Tests/TestKernel.php
+++ src/Sylius/Bundle/CoreBundle/Tests/TestKernel.php
@@ -135,9 +135,7 @@ final class TestKernel extends BaseKernel
 
         $containerBuilder->loadFromExtension('security', [
             'firewalls' => [
-                'main' => [
-                    'anonymous' => true,
-                ],
+                'main' => [],
             ],
         ]);
 
@@ -147,15 +145,6 @@ final class TestKernel extends BaseKernel
                 'server_version' => '5.7',
                 'charset' => 'UTF8',
                 'url' => 'sqlite:///%kernel.project_dir%/var/data.db',
-            ],
-        ]);
-
-        $containerBuilder->loadFromExtension('swiftmailer', [
-            'disable_delivery' => true,
-            'logging' => true,
-            'spool' => [
-                'type' => 'file',
-                'path' => '%kernel.cache_dir%/spool',
             ],
         ]);
 
