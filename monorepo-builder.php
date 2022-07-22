5892b65a0b800cdc7c63c4d543f8e17309a283a2
Loïc Frémont <lc.fremont@gmail.com>
2022-07-22 11:28:33 +0200

    Checking package versions

---
+++ monorepo-builder.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\ComposerJsonManipulator\ValueObject\ComposerJsonSection;
+use Symplify\MonorepoBuilder\Config\MBConfig;
+use Symplify\MonorepoBuilder\ValueObject\Option;
+
+return static function (MBConfig $config): void {
+    $config->packageDirectories([
+        // default value
+        __DIR__ . '/src/Sylius',
+    ]);
+};
