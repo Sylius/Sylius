39ad298f8a3a7a8872cf4e3dbdb929a5d7762f44
Jan Goralski <jan.wojciech.goralski@gmail.com>
2022-08-22 13:12:28 +0200

    [Admin][Order] Filter by autocompleted product/variant

--- src/Sylius/Component/Product/Repository/ProductRepositoryInterface.php
+++ src/Sylius/Component/Product/Repository/ProductRepositoryInterface.php
@@ -27,4 +27,6 @@ interface ProductRepositoryInterface extends RepositoryInterface
      * @return array|ProductInterface[]
      */
     public function findByNamePart(string $phrase, string $locale, ?int $limit = null): array;
+
+    public function findByPhrase(string $phrase, string $locale, ?int $limit = null): iterable;
 }
