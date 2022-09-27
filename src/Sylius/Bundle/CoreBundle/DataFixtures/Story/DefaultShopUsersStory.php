ec29edfb13d0b15860078df67b25961f80464f8c
Loïc Frémont <lc.fremont@gmail.com>
2022-09-28 00:46:21 +0200

    [Fixtures] Default shop users story

---
+++ src/Sylius/Bundle/CoreBundle/DataFixtures/Story/DefaultShopUsersStory.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\DataFixtures\Story;
+
+use Sylius\Bundle\CoreBundle\DataFixtures\Factory\ChannelFactoryInterface;
+use Sylius\Bundle\CoreBundle\DataFixtures\Factory\CustomerGroupFactoryInterface;
+use Sylius\Bundle\CoreBundle\DataFixtures\Factory\PaymentMethodFactoryInterface;
+use Sylius\Bundle\CoreBundle\DataFixtures\Factory\ShopUserFactoryInterface;
+use Zenstruck\Foundry\Story;
+
+final class DefaultShopUsersStory extends Story implements DefaultShopUsersStoryInterface
+{
+    public function __construct(private ShopUserFactoryInterface $shopUserFactory)
+    {
+    }
+
+    public function build(): void
+    {
+        $this->shopUserFactory::new()
+            ->withEmail('shop@example.com')
+            ->withFirstName('John')
+            ->withLastName('Doe')
+            ->withPassword('sylius')
+            ->create()
+        ;
+    }
+}
---
+++ tests/DataFixtures/Story/DefaultShopUsersStoryTest.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Tests\DataFixtures\Story;
+
+use Sylius\Bundle\CoreBundle\DataFixtures\Story\DefaultCustomerGroupsStoryInterface;
+use Sylius\Bundle\CoreBundle\DataFixtures\Story\DefaultShopUsersStoryInterface;
+use Sylius\Component\Core\Model\ShippingMethodInterface;
+use Sylius\Component\Core\Model\ShopUserInterface;
+use Sylius\Component\Core\Repository\ShippingMethodRepositoryInterface;
+use Sylius\Component\Customer\Model\CustomerGroupInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Sylius\Component\User\Repository\UserRepositoryInterface;
+use Sylius\Tests\PurgeDatabaseTrait;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Foundry\Test\Factories;
+
+final class DefaultShopUsersStoryTest extends KernelTestCase
+{
+    use PurgeDatabaseTrait;
+    use Factories;
+
+    /** @test */
+    public function it_creates_default_shop_users(): void
+    {
+        /** @var DefaultShopUsersStoryInterface $defaultShopUsersStory */
+        $defaultShopUsersStory = self::getContainer()->get('sylius.data_fixtures.story.default_shop_users');
+
+        $defaultShopUsersStory->build();
+
+        $shopUser = $this->getCustomerGroupByCode('shop@example.com');
+        $this->assertNotNull($shopUser, sprintf('Shop user "%s" was not found but it should.', 'shop@example.com'));
+        $this->assertNotNull($shopUser->getCustomer());
+        $this->assertEquals('John', $shopUser->getCustomer()->getFirstName());
+        $this->assertEquals('Doe', $shopUser->getCustomer()->getLastName());
+        $this->assertNotNull($shopUser->getPassword());
+    }
+
+    private function getCustomerGroupByCode(string $email): ?ShopUserInterface
+    {
+        /** @var UserRepositoryInterface $shopUserRepository */
+        $shopUserRepository = self::getContainer()->get('sylius.repository.shop_user');
+
+        return $shopUserRepository->findOneByEmail($email);
+    }
+}
