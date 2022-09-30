4e105de5b4f4133ee373ddd575ef556ddc3ee94b
Loïc Frémont <lc.fremont@gmail.com>
2022-09-30 16:14:28 +0200

    Add decorations services for locale factory

---
+++ src/Sylius/Bundle/CoreBundle/DataFixtures/Updater/LocaleUpdaterInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\DataFixtures\Updater;
+
+use Sylius\Component\Locale\Model\LocaleInterface;
+
+interface LocaleUpdaterInterface
+{
+    public function update(LocaleInterface $locale, array $attributes): void;
+}
