1f43b43ce72d2ca776c17c1a3a0507044ac2dc13
Rafikooo <thecrazyrafik@gmail.com>
2022-10-04 14:18:37 +0200

    [Behat] Unused step implementation removed

--- src/Sylius/Behat/Context/Ui/Admin/ManagingCatalogPromotionsContext.php
+++ src/Sylius/Behat/Context/Ui/Admin/ManagingCatalogPromotionsContext.php
@@ -702,14 +702,6 @@ final class ManagingCatalogPromotionsContext implements Context
     }
 
     /**
-     * @Then I should be notified that a discount amount is not valid
-     */
-    public function iShouldBeNotifiedThatADiscountAmountIsNotValid(): void
-    {
-        Assert::same($this->formElement->getValidationMessage(), 'This value is not valid.');
-    }
-
-    /**
      * @Then I should be notified that a discount amount should be configured for at least one channel
      */
     public function iShouldBeNotifiedThatADiscountAmountShouldBeConfiguredForAtLeasOneChannel(): void
