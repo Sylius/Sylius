9f89500cd5a7a684f173e5c9189888f2352a7714
mamazu <14860264+mamazu@users.noreply.github.com>
2022-10-26 12:02:32 +0200

    Updating the RemoveExpiredCartCommand

--- src/Sylius/Bundle/OrderBundle/Command/RemoveExpiredCartsCommand.php
+++ src/Sylius/Bundle/OrderBundle/Command/RemoveExpiredCartsCommand.php
@@ -16,6 +16,7 @@ namespace Sylius\Bundle\OrderBundle\Command;
 use SyliusLabs\Polyfill\Symfony\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Sylius\Component\Order\Remover\ExpiredCartsRemoverInterface;
 
 /**
  * @final
@@ -23,6 +24,13 @@ use Symfony\Component\Console\Output\OutputInterface;
 class RemoveExpiredCartsCommand extends ContainerAwareCommand
 {
     protected static $defaultName = 'sylius:remove-expired-carts';
+
+    public function __construct(
+        private ?ExpiredCartsRemoverInterface $expiredCartsRemover = null,
+        private $expirationTime = null,
+    ) {
+        parent::__construct(null);
+    }
 
     protected function configure(): void
     {
@@ -33,15 +41,34 @@ class RemoveExpiredCartsCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $expirationTime = $this->getContainer()->getParameter('sylius_order.cart_expiration_period');
+        if ($this->expirationTime === null) {
+            trigger_deprecation(
+                'sylius/order-bundle',
+                '1.12',
+                sprintf('Not injecting the expiration time into %s', self::class),
+            );
+            $expirationTime = $this->getContainer()->getParameter('sylius_order.cart_expiration_period');
+        } else {
+            $expirationTime = $this->expirationTime;
+        }
+
         $output->writeln(sprintf(
             'Command will remove carts that have been idle for <info>%s</info>.',
             (string) $expirationTime,
         ));
 
-        $expiredCartsRemover = $this->getContainer()->get('sylius.expired_carts_remover');
-        $expiredCartsRemover->remove();
+        if ($this->expiredCartsRemover === null) {
+            trigger_deprecation(
+                'sylius/order-bundle',
+                '1.12',
+                sprintf('Not injecting the %s into the %s. Is deprecated', ExpiredCartsRemoverInterface::class, self::class)
+            );
+            $this->getContainer()->get('sylius.expired_carts_remover')->remove();
+        } else {
+            $this->expiredCartsRemover->remove();
+        }
 
         return 0;
     }
 }
+
