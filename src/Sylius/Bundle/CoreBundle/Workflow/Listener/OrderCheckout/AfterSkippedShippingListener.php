865ae5798c3dcfd5d4c2b6dfa70c8ad2e3a752b6
Loïc Frémont <lc.fremont@gmail.com>
2022-10-28 09:01:15 +0200

    Fix specifications

--- src/Sylius/Bundle/CoreBundle/Workflow/Listener/OrderCheckout/AfterSkippedShippingListener.php
+++ src/Sylius/Bundle/CoreBundle/Workflow/Listener/OrderCheckout/AfterSkippedShippingListener.php
@@ -7,12 +7,14 @@ namespace Sylius\Bundle\CoreBundle\Workflow\Listener\OrderCheckout;
 use Sylius\Bundle\CoreBundle\Workflow\Callback\OrderCheckout\AfterSkippedShippingCallbackInterface;
 use Sylius\Component\Core\Model\OrderInterface;
 use Symfony\Component\Workflow\Event\Event;
+use Webmozart\Assert\Assert;
 
 final class AfterSkippedShippingListener
 {
     /** @param AfterSkippedShippingCallbackInterface[] $callbacks */
     public function __construct(private iterable $callbacks)
     {
+        Assert::allIsInstanceOf($this->callbacks, AfterSkippedShippingCallbackInterface::class);
     }
 
     public function call(Event $event): void
--- src/Sylius/Bundle/CoreBundle/Workflow/Listener/Payment/AfterProcessedPaymentListener.php
+++ src/Sylius/Bundle/CoreBundle/Workflow/Listener/Payment/AfterProcessedPaymentListener.php
@@ -7,12 +7,14 @@ namespace Sylius\Bundle\CoreBundle\Workflow\Listener\Payment;
 use Sylius\Bundle\CoreBundle\Workflow\Callback\Payment\AfterProcessedPaymentCallbackInterface;
 use Sylius\Component\Core\Model\PaymentInterface;
 use Symfony\Component\Workflow\Event\Event;
+use Webmozart\Assert\Assert;
 
 final class AfterProcessedPaymentListener
 {
     /** @param AfterProcessedPaymentCallbackInterface[] $callbacks */
     public function __construct(private iterable $callbacks)
     {
+        Assert::allIsInstanceOf($this->callbacks, AfterProcessedPaymentCallbackInterface::class);
     }
 
     public function call(Event $event): void
