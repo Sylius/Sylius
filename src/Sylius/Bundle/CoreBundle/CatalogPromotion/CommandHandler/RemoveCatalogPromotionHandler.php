958b8d73a9dcdb2b27e375418dd5e2cc428ac1ff
Kevin Kaniaburka <kevin.kaniaburka@gmail.com>
2022-11-22 14:06:50 +0100

    [Catalog Promotions] Put catalog promotions into the processing state right after the delete request and keep this state until being removed

--- src/Sylius/Bundle/CoreBundle/CatalogPromotion/CommandHandler/RemoveCatalogPromotionHandler.php
+++ src/Sylius/Bundle/CoreBundle/CatalogPromotion/CommandHandler/RemoveCatalogPromotionHandler.php
@@ -14,32 +14,21 @@ declare(strict_types=1);
 namespace Sylius\Bundle\CoreBundle\CatalogPromotion\CommandHandler;
 
 use Doctrine\ORM\EntityManager;
-use Sylius\Bundle\CoreBundle\CatalogPromotion\Command\RemoveInactiveCatalogPromotion;
+use Sylius\Bundle\CoreBundle\CatalogPromotion\Command\RemoveCatalogPromotion;
 use Sylius\Component\Core\Model\CatalogPromotionInterface;
 use Sylius\Component\Promotion\Exception\InvalidCatalogPromotionStateException;
 use Sylius\Component\Promotion\Model\CatalogPromotionStates;
 use Sylius\Component\Promotion\Repository\CatalogPromotionRepositoryInterface;
 
-/** @deprecated since 1.12 and will be removed in Sylius 2.0. Use {@see RemoveCatalogPromotionHandler} instead. */
-final class RemoveInactiveCatalogPromotionHandler
+final class RemoveCatalogPromotionHandler
 {
     public function __construct(
         private CatalogPromotionRepositoryInterface $catalogPromotionRepository,
         private EntityManager $entityManager,
     ) {
-        trigger_deprecation(
-            'sylius/core-bundle',
-            '1.12',
-            sprintf(
-                'The "%s" class is deprecated since Sylius 1.12 and will be removed in 2.0. Use "%s" instead.',
-                self::class,
-                RemoveCatalogPromotionHandler::class,
-            )
-        );
     }
 
-    /** @psalm-suppress DeprecatedClass */
-    public function __invoke(RemoveInactiveCatalogPromotion $command): void
+    public function __invoke(RemoveCatalogPromotion $command): void
     {
         /** @var CatalogPromotionInterface|null $catalogPromotion */
         $catalogPromotion = $this->catalogPromotionRepository->findOneBy(['code' => $command->code]);
@@ -48,10 +37,10 @@ final class RemoveInactiveCatalogPromotionHandler
             return;
         }
 
-        if ($catalogPromotion->getState() !== CatalogPromotionStates::STATE_INACTIVE) {
+        if ($catalogPromotion->getState() !== CatalogPromotionStates::STATE_PROCESSING) {
             throw new InvalidCatalogPromotionStateException(
                 sprintf(
-                    'Catalog promotion with code "%s" cannot be removed as it is not in an inactive state.',
+                    'Catalog promotion with code "%s" cannot be removed as it is not in a processing state.',
                     $catalogPromotion->getCode(),
                 ),
             );
