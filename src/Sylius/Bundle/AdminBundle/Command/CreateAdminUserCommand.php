696e9892f7a8eca0bb462389c634b2ac136765e8
Rafikooo <thecrazyrafik@gmail.com>
2023-01-02 13:51:18 +0100

    [Feature][CreateAdminUserCommand] use MessageBus to handle admin user creation

--- src/Sylius/Bundle/AdminBundle/Command/CreateAdminUserCommand.php
+++ src/Sylius/Bundle/AdminBundle/Command/CreateAdminUserCommand.php
@@ -13,10 +13,8 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\AdminBundle\Command;
 
-use Sylius\Component\Core\Model\AdminUserInterface;
-use Sylius\Component\Resource\Factory\FactoryInterface;
-use Sylius\Component\User\Canonicalizer\CanonicalizerInterface;
-use Sylius\Component\User\Repository\UserRepositoryInterface;
+use Sylius\Bundle\AdminBundle\Message\CreateAdminUser;
+use Sylius\Bundle\AdminBundle\MessageHandler\CreateAdminUserResult;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -24,6 +22,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Intl\Locales;
+use Symfony\Component\Messenger\HandleTrait;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 #[AsCommand(
     name: 'sylius:admin-user:create',
@@ -31,13 +31,14 @@ use Symfony\Component\Intl\Locales;
 )]
 final class CreateAdminUserCommand extends Command
 {
+    use HandleTrait;
+
     private SymfonyStyle $io;
 
-    public function __construct(
-        private UserRepositoryInterface $adminUserRepository,
-        private FactoryInterface $adminUserFactory,
-        private CanonicalizerInterface $canonicalizer,
-    ) {
+    public function __construct(MessageBusInterface $messageBus, private string $defaultLocaleCode)
+    {
+        $this->messageBus = $messageBus;
+
         parent::__construct();
     }
 
@@ -56,31 +57,50 @@ final class CreateAdminUserCommand extends Command
 
         $this->io->title('Admin user creation');
 
-        $email = $this->io->askQuestion($this->createEmailQuestion());
+        $adminUserData = $this->askAdminUserData();
 
-        if ($this->adminUserExists($this->canonicalizer->canonicalize($email))) {
-            $this->io->error(sprintf('Admin user with email address %s already exists.', $email));
+        $this->showSummary($adminUserData);
+
+        if (!$this->adminCreationConfirmed()) {
+            $this->io->error('Admin user creation has been aborted.');
 
             return Command::INVALID;
         }
 
-        /** @var AdminUserInterface $adminUser */
-        $adminUser = $this->adminUserFactory->createNew();
+        /** @var CreateAdminUserResult $commandResult */
+        $commandResult = $this->handle(new CreateAdminUser(...array_values($adminUserData)));
 
-        $this->setAdminUserData($adminUser, $email);
-        $this->showSummary($adminUser);
+        if ($commandResult->hasViolations()) {
+            $this->io->error($commandResult->getViolationMessages());
 
-        if ($this->adminCreationConfirmed()) {
-            $this->adminUserRepository->add($adminUser);
-
-            $this->io->success('Admin user has been successfully created.');
-
-            return Command::SUCCESS;
+            return Command::INVALID;
         }
 
-        $this->io->error('Admin user creation has been aborted.');
+        $this->io->success('Admin user has been successfully created.');
 
-        return Command::INVALID;
+        return Command::SUCCESS;
+    }
+
+    private function askAdminUserData(): array
+    {
+        $adminUserData = [];
+
+        $adminUserData['email'] = $this->io->askQuestion($this->createEmailQuestion());
+        $adminUserData['username'] = $this->io->askQuestion(
+            $this->createQuestionWithNonBlankValidator('Username')
+        );
+        $adminUserData['first_name'] = $this->io->ask('First name');
+        $adminUserData['last_name'] = $this->io->ask('Last name');
+        $adminUserData['plain_password'] = $this->io->askQuestion(
+            $this->createQuestionWithNonBlankValidator('Password', true)
+        );
+
+        $localeCodes = Locales::getNames();
+        $adminUserData['locale_code'] = $this->io->choice('Locale code', $localeCodes, $this->defaultLocaleCode);
+
+        $adminUserData['enabled'] = $this->io->confirm('Do you want to enable this admin user?', true);
+
+        return $adminUserData;
     }
 
     private function createEmailQuestion(): Question
@@ -98,42 +118,27 @@ final class CreateAdminUserCommand extends Command
         return $question;
     }
 
-    private function adminUserExists(string $email): bool
+    private function createQuestionWithNonBlankValidator(string $askedQuestion, bool $hidden = false): Question
     {
-        return null !== $this->adminUserRepository->findOneByEmail($email);
+        $question = new Question($askedQuestion);
+        $question->setValidator(function (?string $value) {
+            if ($value === null) {
+                throw new \InvalidArgumentException('The value cannot be empty.');
+            }
+
+            return $value;
+        });
+        $question->setMaxAttempts(3);
+
+        if ($hidden) {
+            $question->setHidden(true);
+        }
+
+        return $question;
     }
 
-    private function setAdminUserData(AdminUserInterface $adminUser, string $email): void
+    private function showSummary(array $adminUserData): void
     {
-        $userName = $this->io->ask('Username');
-        $firstName = $this->io->ask('First name');
-        $lastName = $this->io->ask('Last name');
-        $password = $this->io->askHidden('Password');
-
-        $adminUser->setEmail($email);
-        $adminUser->setPlainPassword($password);
-        $adminUser->setUsername($userName);
-        $adminUser->setFirstName($firstName);
-        $adminUser->setLastName($lastName);
-
-        $locales = Locales::getNames();
-
-        $localeCode = $this->io->choice('Select the locale code', $locales, 'en_US');
-        $adminUser->setLocaleCode($localeCode);
-
-        $enabled = $this->io->confirm('Do you want to enable this admin user?', true);
-        $adminUser->setEnabled($enabled);
-    }
-
-    private function showSummary(AdminUserInterface $adminUser): void
-    {
-        /**
-         * @psalm-suppress UndefinedInterfaceMethod
-         *
-         * @phpstan-ignore-next-line
-         */
-        $username = $adminUser->getUsername();
-
         $this->io->writeln('The following admin user will be created:');
         $this->io->table(
             [
@@ -141,12 +146,12 @@ final class CreateAdminUserCommand extends Command
             ],
             [
                 [
-                    $adminUser->getEmail(),
-                    $username,
-                    $adminUser->getFirstName(),
-                    $adminUser->getLastName(),
-                    $adminUser->getLocaleCode(),
-                    $adminUser->isEnabled() ? 'Yes' : 'No',
+                    $adminUserData['email'],
+                    $adminUserData['username'],
+                    $adminUserData['first_name'],
+                    $adminUserData['last_name'],
+                    $adminUserData['locale_code'],
+                    $adminUserData['enabled'] ? 'Yes' : 'No',
                 ],
             ],
         );
