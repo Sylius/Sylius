90c2c85de6b30b65370963262e83ec44059ab6ab
Łukasz Chruściel <lchrusciel@gmail.com>
2023-03-06 21:24:04 +0100

    [Shipment] Reduce amount of queries during shipping eligibility checking

--- src/Sylius/Component/Shipping/Resolver/ShippingMethodsResolver.php
+++ src/Sylius/Component/Shipping/Resolver/ShippingMethodsResolver.php
@@ -15,21 +15,30 @@ namespace Sylius\Component\Shipping\Resolver;
 
 use Doctrine\Persistence\ObjectRepository;
 use Sylius\Component\Shipping\Checker\Eligibility\ShippingMethodEligibilityCheckerInterface;
+use Sylius\Component\Shipping\Model\ShippingMethodInterface;
 use Sylius\Component\Shipping\Model\ShippingSubjectInterface;
+use Sylius\Component\Shipping\Repository\ShippingMethodRepositoryInterface;
 
 final class ShippingMethodsResolver implements ShippingMethodsResolverInterface
 {
     public function __construct(
-        private ObjectRepository $shippingMethodRepository,
+        private ObjectRepository|ShippingMethodRepositoryInterface $shippingMethodRepository,
         private ShippingMethodEligibilityCheckerInterface $eligibilityChecker,
     ) {
+        if (!$this->shippingMethodRepository instanceof ShippingMethodRepositoryInterface) {
+            @trigger_error(sprintf(
+                'Not implementing "%s" in "%s" is deprecated since Sylius 1.13 and will be required in Sylius 2.0.',
+                ShippingMethodRepositoryInterface::class,
+                get_debug_type($this->shippingMethodRepository)
+            ), E_USER_DEPRECATED);
+        }
     }
 
     public function getSupportedMethods(ShippingSubjectInterface $subject): array
     {
         $methods = [];
 
-        foreach ($this->shippingMethodRepository->findBy(['enabled' => true]) as $shippingMethod) {
+        foreach ($this->getEnabledShippingMethods() as $shippingMethod) {
             if ($this->eligibilityChecker->isEligible($subject, $shippingMethod)) {
                 $methods[] = $shippingMethod;
             }
@@ -41,5 +50,17 @@ final class ShippingMethodsResolver implements ShippingMethodsResolverInterface
     public function supports(ShippingSubjectInterface $subject): bool
     {
         return true;
+    }
+
+    /**
+     * @return ShippingMethodInterface[]
+     */
+    private function getEnabledShippingMethods(): array
+    {
+        if ($this->shippingMethodRepository instanceof ShippingMethodRepositoryInterface) {
+            return $this->shippingMethodRepository->findEnabledWithRules();
+        }
+
+        return $this->shippingMethodRepository->findBy(['enabled' => true]);
     }
 }
