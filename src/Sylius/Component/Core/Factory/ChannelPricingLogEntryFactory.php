3824ba543694ac781bc454c6a4583f376074b85f
Rafikooo <thecrazyrafik@gmail.com>
2023-03-10 18:16:02 +0100

    [PriceHistory] Add ChannelPricingLogEntry factory

---
+++ src/Sylius/Component/Core/Factory/ChannelPricingLogEntryFactory.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Component\Core\Factory;
+
+use Sylius\Component\Core\Model\ChannelPricingInterface;
+use Sylius\Component\Core\Model\ChannelPricingLogEntryInterface;
+use Sylius\Component\Resource\Exception\UnsupportedMethodException;
+
+final class ChannelPricingLogEntryFactory implements ChannelPricingLogEntryFactoryInterface
+{
+    public function __construct(private string $className)
+    {
+        if (!is_a($className, ChannelPricingLogEntryInterface::class, true)) {
+            throw new \DomainException(sprintf(
+                'This factory requires %s or its descend to be used as resource',
+                ChannelPricingLogEntryInterface::class,
+            ));
+        }
+    }
+
+    /**
+     * @throws UnsupportedMethodException
+     */
+    public function createNew(): object
+    {
+        throw new UnsupportedMethodException('createNew');
+    }
+
+    public function create(
+        ChannelPricingInterface $channelPricing,
+        \DateTimeInterface $loggedAt,
+        int $price,
+        ?int $originalPrice = null,
+    ): ChannelPricingLogEntryInterface {
+        /** @var ChannelPricingLogEntryInterface $channelPricingLogEntry */
+        $channelPricingLogEntry = new $this->className(
+            $channelPricing,
+            $loggedAt,
+            $price,
+            $originalPrice,
+        );
+
+        return $channelPricingLogEntry;
+    }
+}
---
+++ src/Sylius/Component/Core/Factory/ChannelPricingLogEntryFactoryInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Component\Core\Factory;
+
+use Sylius\Component\Core\Model\ChannelPricingInterface;
+use Sylius\Component\Core\Model\ChannelPricingLogEntryInterface;
+use Sylius\Component\Resource\Factory\FactoryInterface;
+
+interface ChannelPricingLogEntryFactoryInterface extends FactoryInterface
+{
+    public function create(
+        ChannelPricingInterface $channelPricing,
+        \DateTimeInterface $loggedAt,
+        int $price,
+        ?int $originalPrice = null,
+    ): ChannelPricingLogEntryInterface;
+}
