ddcc17c4864f540b83266a42ccd948efa7ceaf56
Grzegorz Sadowski <sadowskigp@gmail.com>
2023-03-28 11:58:00 +0200

    Change application's version to v1.11.15

--- src/Sylius/Bundle/CoreBundle/Application/Kernel.php
+++ src/Sylius/Bundle/CoreBundle/Application/Kernel.php
@@ -31,7 +31,7 @@ use Webmozart\Assert\Assert;
 
 class Kernel extends HttpKernel
 {
-    public const VERSION = '1.11.15-DEV';
+    public const VERSION = '1.11.15';
 
     public const VERSION_ID = '11115';
 
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
 
     public const RELEASE_VERSION = '15';
 
-    public const EXTRA_VERSION = 'DEV';
+    public const EXTRA_VERSION = '';
 
     public function __construct(string $environment, bool $debug)
     {
