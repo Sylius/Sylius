7212ed57f4e96f612f54acf72cdb374f6726880a
Kevin Kaniaburka <kevin.kaniaburka@gmail.com>
2023-03-28 13:02:26 +0200

    [Channel] Add "order by" to the "find by" methods

--- src/Sylius/Bundle/ChannelBundle/Doctrine/ORM/ChannelRepository.php
+++ src/Sylius/Bundle/ChannelBundle/Doctrine/ORM/ChannelRepository.php
@@ -19,23 +19,25 @@ use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
 
 class ChannelRepository extends EntityRepository implements ChannelRepositoryInterface
 {
+    private const ORDER_BY = ['id' => 'ASC'];
+
     public function findOneByHostname(string $hostname): ?ChannelInterface
     {
-        return $this->findOneBy(['hostname' => $hostname]);
+        return $this->findOneBy(['hostname' => $hostname], self::ORDER_BY);
     }
 
     public function findOneEnabledByHostname(string $hostname): ?ChannelInterface
     {
-        return $this->findOneBy(['hostname' => $hostname, 'enabled' => true]);
+        return $this->findOneBy(['hostname' => $hostname, 'enabled' => true], self::ORDER_BY);
     }
 
     public function findOneByCode(string $code): ?ChannelInterface
     {
-        return $this->findOneBy(['code' => $code]);
+        return $this->findOneBy(['code' => $code], self::ORDER_BY);
     }
 
     public function findByName(string $name): iterable
     {
-        return $this->findBy(['name' => $name]);
+        return $this->findBy(['name' => $name], self::ORDER_BY);
     }
 }
