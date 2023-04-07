07aa4d0cd93891cdf7b4b4d84f088617d6365935
Rafikooo <thecrazyrafik@gmail.com>
2023-04-07 09:37:54 +0200

    [Behat][API] Introduce common ResponseContext service

---
+++ src/Sylius/Behat/Context/Api/Common/ResponseContext.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Behat\Context\Api\Common;
+
+use Behat\Behat\Context\Context;
+use Sylius\Behat\Client\ApiClientInterface;
+use Sylius\Behat\Client\ResponseCheckerInterface;
+use Webmozart\Assert\Assert;
+
+final class ResponseContext implements Context
+{
+    public function __construct(private ResponseCheckerInterface $responseChecker, private ApiClientInterface $client)
+    {
+    }
+
+    /**
+     * @Then I should be notified that it has been successfully edited
+     */
+    public function iShouldBeNotifiedThatItHasBeenSuccessfullyEdited(): void
+    {
+        Assert::true(
+            $this->responseChecker->isUpdateSuccessful($this->client->getLastResponse()),
+            sprintf(
+                'Resource could not be edited: %s',
+                $this->responseChecker->getError($this->client->getLastResponse()),
+            ),
+        );
+    }
+}
