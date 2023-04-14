7fc6734ca20c6edf164aedec9e325d31f95a5e49
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2023-04-14 10:42:06 +0200

    Add calculating OrderItemsSubtotal to the Order model

--- src/Sylius/Bundle/ShopBundle/Calculator/OrderItemsSubtotalCalculator.php
+++ src/Sylius/Bundle/ShopBundle/Calculator/OrderItemsSubtotalCalculator.php
@@ -13,8 +13,19 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\ShopBundle\Calculator;
 
+use Sylius\Component\Core\Model\Order;
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Core\Model\OrderItemInterface;
+
+trigger_deprecation(
+    'sylius/sylius',
+    '1.13',
+    sprintf(
+        'Class "%s" is deprecated and will be removed in Sylius 2.0. Items subtotal calculations is now available by using %s::getSubtotalItems method.',
+        OrderItemsSubtotalCalculator::class,
+        Order::class,
+    ),
+);
 
 final class OrderItemsSubtotalCalculator implements OrderItemsSubtotalCalculatorInterface
 {
