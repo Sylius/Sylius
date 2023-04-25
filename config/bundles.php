ab114457b3b8543f0309a1fcc882f51b5641cf53
Loïc Frémont <lc.fremont@gmail.com>
2023-04-25 14:12:58 +0200

    Enable doctrine fixtures on test cached env

--- config/bundles.php
+++ config/bundles.php
@@ -61,5 +61,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true, 'test_cached' => true],
 ];
