0b412d75b264e052bb8394604359204afbef5cc1
Rafikooo <thecrazyrafik@gmail.com>
2023-05-17 17:51:48 +0200

    [PostgreSQL][Installer] Handle Sylius installation with PostgreSQL

--- src/Sylius/Bundle/CoreBundle/Installer/Provider/DatabaseSetupCommandsProvider.php
+++ src/Sylius/Bundle/CoreBundle/Installer/Provider/DatabaseSetupCommandsProvider.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace Sylius\Bundle\CoreBundle\Installer\Provider;
 
 use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
 use Doctrine\DBAL\Schema\AbstractSchemaManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Helper\QuestionHelper;
@@ -25,93 +26,66 @@ use Webmozart\Assert\Assert;
 
 final class DatabaseSetupCommandsProvider implements DatabaseSetupCommandsProviderInterface
 {
+    private bool $isPostgreSQLPlatform;
+
     public function __construct(private Registry $doctrineRegistry)
     {
+        $this->isPostgreSQLPlatform = $this->isPostgreSQLPlatform();
     }
 
     public function getCommands(InputInterface $input, OutputInterface $output, QuestionHelper $questionHelper): array
     {
-        if (!$this->isDatabasePresent()) {
-            return [
-                'doctrine:database:create',
-                'doctrine:migrations:migrate' => ['--no-interaction' => true],
-            ];
-        }
+        $outputStyle = new SymfonyStyle($input, $output);
 
-        return array_merge($this->setupDatabase($input, $output, $questionHelper), [
-            'doctrine:migrations:version' => [
-                '--add' => true,
-                '--all' => true,
-                '--no-interaction' => true,
-            ],
-        ]);
-    }
+        if ($this->isSchemaHasAnyTable()) {
+            $outputStyle->writeln(sprintf('The database <info>%s</info> exists and it contains some tables.', $this->getDatabaseName()));
+            $outputStyle->writeln('<error>Warning! This action will erase your database.</error>');
 
-    /**
-     * @throws \Exception
-     */
-    private function isDatabasePresent(): bool
-    {
-        $databaseName = $this->getDatabaseName();
-
-        try {
-            $schemaManager = $this->getSchemaManager();
-
-            return in_array($databaseName, $schemaManager->listDatabases());
-        } catch (\Exception $exception) {
-            $message = $exception->getMessage();
-
-            $mysqlDatabaseError = str_contains($message, sprintf("Unknown database '%s'", $databaseName));
-            $postgresDatabaseError = str_contains($message, sprintf('database "%s" does not exist', $databaseName));
-
-            if ($mysqlDatabaseError || $postgresDatabaseError) {
-                return false;
+            $question = new ConfirmationQuestion('Do you want to drop all of them? (y/N) ', false);
+            if ($questionHelper->ask($input, $output, $question)) {
+                return $this->dropSchemaAndGetMigrateOrSchemaCreateCommands($outputStyle);
             }
 
-            throw $exception;
+            return [];
+        }
+
+        if ($this->isEmptyDatabasePresent()) {
+            $outputStyle->writeln(
+                sprintf('The database <info>%s</info> already exists and it has no tables.', $this->getDatabaseName()),
+            );
+
+            return $this->getCreateSchemaOrRunMigrationsCommand($outputStyle);
+        }
+
+        return $this->getCreateDatabaseWithSchemaCommands($outputStyle);
+    }
+
+    private function isEmptyDatabasePresent(): bool
+    {
+        try {
+            return 0 === count($this->getSchemaManager()->listTableNames());
+        } catch (\Exception) {
+            return false;
         }
     }
 
-    private function setupDatabase(InputInterface $input, OutputInterface $output, QuestionHelper $questionHelper): array
+    private function isSchemaHasAnyTable(): bool
     {
-        $outputStyle = new SymfonyStyle($input, $output);
-        $outputStyle->writeln('It appears that your database already exists.');
-        $outputStyle->writeln('<error>Warning! This action will erase your database.</error>');
-
-        $question = new ConfirmationQuestion('Would you like to reset it? (y/N) ', false);
-        if ($questionHelper->ask($input, $output, $question)) {
-            return [
-                'doctrine:database:drop' => ['--force' => true],
-                'doctrine:database:create',
-                'doctrine:migrations:migrate' => ['--no-interaction' => true],
-            ];
+        try {
+            return 0 !== count($this->getSchemaManager()->listTableNames());
+        } catch (\Exception) {
+            return false;
         }
-
-        if (!$this->isSchemaPresent()) {
-            return ['doctrine:migrations:migrate' => ['--no-interaction' => true]];
-        }
-
-        $outputStyle->writeln('Seems like your database contains schema.');
-        $outputStyle->writeln('<error>Warning! This action will erase your database.</error>');
-        $question = new ConfirmationQuestion('Do you want to reset it? (y/N) ', false);
-        if ($questionHelper->ask($input, $output, $question)) {
-            return [
-                'doctrine:schema:drop' => ['--force' => true],
-                'doctrine:migrations:migrate' => ['--no-interaction' => true],
-            ];
-        }
-
-        return [];
-    }
-
-    private function isSchemaPresent(): bool
-    {
-        return 0 !== count($this->getSchemaManager()->listTableNames());
     }
 
     private function getDatabaseName(): string
     {
         return $this->getEntityManager()->getConnection()->getDatabase();
+    }
+
+    private function isPostgreSQLPlatform(): bool
+    {
+        return $this->getEntityManager()->getConnection()->getDatabasePlatform() instanceof PostgreSQLPlatform;
     }
 
     private function getSchemaManager(): AbstractSchemaManager
@@ -136,5 +110,62 @@ final class DatabaseSetupCommandsProvider implements DatabaseSetupCommandsProvid
         Assert::isInstanceOf($objectManager, EntityManagerInterface::class);
 
         return $objectManager;
+    }
+
+    private function getCreateDatabaseWithSchemaCommands(SymfonyStyle $outputStyle): array
+    {
+        if ($this->isPostgreSQLPlatform) {
+            $outputStyle->writeln([
+                'As you\'re using PostgreSQL, we will create a database and schema instead of running migrations.',
+                'They will be available starting from Sylius 1.13.',
+            ]);
+
+            return [
+                'doctrine:database:create',
+                'doctrine:schema:create',
+            ];
+        }
+
+        return [
+            'doctrine:database:create',
+            'doctrine:migrations:migrate' => ['--no-interaction' => true],
+        ];
+    }
+
+    /** To refactor in Sylius 1.13 */
+    private function getCreateSchemaOrRunMigrationsCommand(SymfonyStyle $outputStyle): array
+    {
+        if ($this->isPostgreSQLPlatform) {
+            $outputStyle->writeln([
+                'As you\'re using PostgreSQL, we will create a schema instead of running migrations.',
+                'They will be available starting from Sylius 1.13.',
+            ]);
+
+            return ['doctrine:schema:create'];
+        }
+
+        return ['doctrine:migrations:migrate' => ['--no-interaction' => true]];
+    }
+
+    /** To refactor in Sylius 1.13 */
+    private function dropSchemaAndGetMigrateOrSchemaCreateCommands(SymfonyStyle $outputStyle): array
+    {
+        if ($this->isPostgreSQLPlatform) {
+            $outputStyle->writeln([
+                'As you\'re using PostgreSQL, we will drop and create a schema instead of running migrations.',
+                'They will be available starting from Sylius 1.13.',
+            ]);
+
+            return [
+                'doctrine:schema:drop' => ['--force' => true],
+                'doctrine:schema:create',
+            ];
+        }
+
+        return [
+            'doctrine:schema:drop' => ['--force' => true],
+            'doctrine:migrations:version' => ['--delete' => true, '--all' => true, '--no-interaction' => true],
+            'doctrine:migrations:migrate' => ['--no-interaction' => true],
+        ];
     }
 }
