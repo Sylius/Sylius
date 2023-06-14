b024c5680f92f00a3538d11cc38425fb66322398
Rafikooo <thecrazyrafik@gmail.com>
2023-06-14 21:53:36 +0200

    [Maintenance] Update the copyright block

--- app/migrations/Version20170913125128.php
+++ app/migrations/Version20170913125128.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Sylius package.
  *
- * (c) Paweł Jędrzejewski
+ * (c) Sylius Sp. z o.o.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -23,21 +23,21 @@ class Version20170913125128 extends AbstractMigration
         $this->addSql('ALTER TABLE sylius_order_item ADD product_name VARCHAR(255), ADD variant_name VARCHAR(255)');
 
         $this->addSql('
-                        UPDATE sylius_order_item 
-                        INNER JOIN sylius_order ON sylius_order_item.order_id = sylius_order.id 
-                        INNER JOIN sylius_product_variant ON sylius_order_item.variant_id = sylius_product_variant.id 
-                        INNER JOIN sylius_product ON sylius_product_variant.product_id = sylius_product.id  
+                        UPDATE sylius_order_item
+                        INNER JOIN sylius_order ON sylius_order_item.order_id = sylius_order.id
+                        INNER JOIN sylius_product_variant ON sylius_order_item.variant_id = sylius_product_variant.id
+                        INNER JOIN sylius_product ON sylius_product_variant.product_id = sylius_product.id
                         INNER JOIN sylius_product_translation ON sylius_product_translation.translatable_id = sylius_product.id
-                        SET sylius_order_item.product_name = sylius_product_translation.name 
+                        SET sylius_order_item.product_name = sylius_product_translation.name
                         WHERE sylius_product_translation.locale = sylius_order.locale_code
-       
+
         ');
         $this->addSql('
-                        UPDATE sylius_order_item 
-                        INNER JOIN sylius_order ON sylius_order_item.order_id = sylius_order.id 
-                        INNER JOIN sylius_product_variant ON sylius_order_item.variant_id = sylius_product_variant.id 
+                        UPDATE sylius_order_item
+                        INNER JOIN sylius_order ON sylius_order_item.order_id = sylius_order.id
+                        INNER JOIN sylius_product_variant ON sylius_order_item.variant_id = sylius_product_variant.id
                         INNER JOIN sylius_product_variant_translation ON sylius_product_variant_translation.translatable_id = sylius_product_variant.id
-                        SET sylius_order_item.variant_name = sylius_product_variant_translation.name 
+                        SET sylius_order_item.variant_name = sylius_product_variant_translation.name
                         WHERE sylius_product_variant_translation.locale = sylius_order.locale_code
         ');
 
