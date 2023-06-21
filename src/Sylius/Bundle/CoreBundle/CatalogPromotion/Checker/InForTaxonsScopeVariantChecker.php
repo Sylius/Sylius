8e38c77061c46b81fd66d5d2cc9eb25df1c53543
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2023-06-21 12:41:30 +0200

    Include children taxons while executing InForTaxonsScopeVariantChecker

--- src/Sylius/Bundle/CoreBundle/CatalogPromotion/Checker/InForTaxonsScopeVariantChecker.php
+++ src/Sylius/Bundle/CoreBundle/CatalogPromotion/Checker/InForTaxonsScopeVariantChecker.php
@@ -13,15 +13,23 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\CoreBundle\CatalogPromotion\Checker;
 
+use Sylius\Bundle\TaxonomyBundle\Repository\TaxonTreeRepositoryInterface;
 use Sylius\Component\Core\Model\ProductInterface;
 use Sylius\Component\Core\Model\ProductVariantInterface;
 use Sylius\Component\Core\Model\TaxonInterface;
 use Sylius\Component\Promotion\Model\CatalogPromotionScopeInterface;
+use Sylius\Component\Taxonomy\Repository\TaxonRepositoryInterface;
 use Webmozart\Assert\Assert;
 
 final class InForTaxonsScopeVariantChecker implements VariantInScopeCheckerInterface
 {
     public const TYPE = 'for_taxons';
+
+    public function __construct (
+        private TaxonRepositoryInterface $taxonRepository,
+        private TaxonTreeRepositoryInterface $taxonTreeRepository,
+    ) {
+    }
 
     public function inScope(CatalogPromotionScopeInterface $scope, ProductVariantInterface $productVariant): bool
     {
@@ -31,8 +39,29 @@ final class InForTaxonsScopeVariantChecker implements VariantInScopeCheckerInter
         /** @var ProductInterface $product */
         $product = $productVariant->getProduct();
 
+        $promotionTaxons = $configuration['taxons'];
+        foreach ($scope->getConfiguration()['taxons'] as $taxonCode) {
+            /** @var TaxonInterface $taxon */
+            $taxon = $this->taxonRepository->findOneBy(['code' => $taxonCode]);
+            Assert::notNull($taxon, sprintf('Taxon with code "%s" does not exist', $taxonCode));
+
+            $promotionTaxons = array_merge($promotionTaxons, $this->getTaxonChildrenCodes($taxon));
+        }
+
         return $product->getTaxons()->exists(
-            fn ($key, TaxonInterface $taxon): bool => \in_array($taxon->getCode(), $scope->getConfiguration()['taxons'], true),
+            fn ($key, TaxonInterface $taxon): bool => \in_array($taxon->getCode(), $promotionTaxons, true),
         );
+    }
+
+    /** @return array<string> */
+    private function getTaxonChildrenCodes(TaxonInterface $taxon): array
+    {
+        $childrenCodes = [];
+
+        foreach ($this->taxonTreeRepository->children($taxon) as $taxonChild) {
+            $childrenCodes[] = $taxonChild->getCode();
+        }
+
+        return $childrenCodes;
     }
 }
