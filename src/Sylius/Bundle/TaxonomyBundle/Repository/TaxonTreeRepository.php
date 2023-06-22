538fe976c4b11ed5220bc4c82c20fb5cdc60ae11
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2023-06-22 15:27:12 +0200

    Apply ECS fixes

--- src/Sylius/Bundle/TaxonomyBundle/Repository/TaxonTreeRepository.php
+++ src/Sylius/Bundle/TaxonomyBundle/Repository/TaxonTreeRepository.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Sylius package.
  *
- * (c) Paweł Jędrzejewski
+ * (c) Sylius Sp. z o.o.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -17,12 +17,12 @@ use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
 
 class TaxonTreeRepository implements TaxonTreeRepositoryInterface
 {
-    public function __construct (
+    public function __construct(
         private NestedTreeRepository $nestedTreeRepository,
     ) {
     }
 
-    public function children(?object $node = null, bool $direct = false, array|string|null $sortByField = null, array|string $direction = 'ASC', bool $includeNode = false,): array|null
+    public function children(?object $node = null, bool $direct = false, array|string|null $sortByField = null, array|string $direction = 'ASC', bool $includeNode = false): array|null
     {
         return $this->nestedTreeRepository->children($node, $direct, $sortByField, $direction, $includeNode);
     }
