cb3d6bb5ebe8c9d4460defc4cc9520d3883ce16a
Jan Goralski <jan.wojciech.goralski@gmail.com>
2023-06-30 10:35:17 +0200

    [Maintenance] Improve ProportionalIntegerDistributor to keep original index

--- src/Sylius/Component/Core/Distributor/ProportionalIntegerDistributor.php
+++ src/Sylius/Component/Core/Distributor/ProportionalIntegerDistributor.php
@@ -17,28 +17,57 @@ use Webmozart\Assert\Assert;
 
 final class ProportionalIntegerDistributor implements ProportionalIntegerDistributorInterface
 {
+    /**
+     * @param array<array-key, int> $integers
+     *
+     * @return array<array-key, int>
+     */
     public function distribute(array $integers, int $amount): array
     {
         Assert::allInteger($integers);
 
         $total = array_sum($integers);
-        $distributedAmounts = [];
 
-        foreach ($integers as $element) {
-            if ($element === 0) {
-                $distributedAmounts[] = 0;
-            } else {
-                $distributedAmounts[] = (int) round(($element * $amount) / $total, 0, \PHP_ROUND_HALF_DOWN);
+        $distributed = $this->distributeAmounts($integers, $amount, $total);
+        $remainder = $amount - array_sum($distributed);
+
+        if (0 === $remainder || $amount === $remainder) {
+            return $distributed;
+        }
+
+        return $this->distributeRemainder($distributed, $remainder);
+    }
+
+    /**
+     * @param array<array-key, int> $integers
+     *
+     * @return array<array-key, int>
+     */
+    private function distributeAmounts(array $integers, int $amount, int $total): array
+    {
+        return array_map(
+            fn (int $item) => 0 === $item ? 0 : (int) round(($item * $amount) / $total, 0, \PHP_ROUND_HALF_DOWN),
+            $integers,
+        );
+    }
+
+    /**
+     * @param array<array-key, int> $distributedAmounts
+     *
+     * @return array<array-key, int>
+     */
+    private function distributeRemainder(array $distributedAmounts, int $remainder): array
+    {
+        $iMax = abs($remainder);
+
+        $i = 0;
+        foreach ($distributedAmounts as $key => $distributedAmount) {
+            if ($i === $iMax) {
+                break;
             }
-        }
 
-        if (array_sum($distributedAmounts) === 0) {
-            return $distributedAmounts;
-        }
-
-        $missingAmount = $amount - array_sum($distributedAmounts);
-        for ($i = 0, $iMax = abs($missingAmount); $i < $iMax; ++$i) {
-            $distributedAmounts[$i] += $missingAmount >= 0 ? 1 : -1;
+            $distributedAmounts[$key] += $remainder >= 0 ? 1 : -1;
+            ++$i;
         }
 
         return $distributedAmounts;
