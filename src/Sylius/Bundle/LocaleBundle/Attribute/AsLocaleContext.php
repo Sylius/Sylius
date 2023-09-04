3137574d8212ceb2179c696501184e45e33e3b34
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2023-09-04 12:52:24 +0200

    Provide autoconfiguration with attributes for LocaleBundle
    Co-authored-by: Felix Fouillet <felix.fouillet@gmail.com>

---
+++ src/Sylius/Bundle/LocaleBundle/Attribute/AsLocaleContext.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\LocaleBundle\Attribute;
+
+#[\Attribute(\Attribute::TARGET_CLASS)]
+final class AsLocaleContext
+{
+    public const SERVICE_TAG = 'sylius.context.locale';
+
+    public function __construct(
+        private int $priority = 0,
+    ) {
+    }
+
+    public function getPriority(): int
+    {
+        return $this->priority;
+    }
+}
