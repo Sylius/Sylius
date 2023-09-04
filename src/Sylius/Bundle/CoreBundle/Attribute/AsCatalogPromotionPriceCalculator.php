b7f6a0397b8c80fb9121c4d80aa157e48bfa2774
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2023-09-04 12:16:04 +0200

    Provide autoconfiguration with attributes for CoreBundle
    Co-authored-by: Felix Fouillet <felix.fouillet@gmail.com>

---
+++ src/Sylius/Bundle/CoreBundle/Attribute/AsCatalogPromotionPriceCalculator.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\Attribute;
+
+#[\Attribute(\Attribute::TARGET_CLASS)]
+final class AsCatalogPromotionPriceCalculator
+{
+    public const SERVICE_TAG = 'sylius.catalog_promotion.price_calculator';
+
+    public function __construct(
+        private int $priority = 0,
+    ) {
+    }
+
+    public function getPriority(): int
+    {
+        return $this->priority;
+    }
+}
