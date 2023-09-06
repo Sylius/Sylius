8431d9bbdfff245c6bbd36eebdc8131187e3b78e
TheMilek <kam.grygierzec@gmail.com>
2023-09-06 14:06:21 +0200

    Prevent removing taxon if it is already used in promotion rule

--- src/Sylius/Behat/Context/Ui/Admin/RemovingTaxonContext.php
+++ src/Sylius/Behat/Context/Ui/Admin/RemovingTaxonContext.php
@@ -14,23 +14,52 @@ declare(strict_types=1);
 namespace Sylius\Behat\Context\Ui\Admin;
 
 use Behat\Behat\Context\Context;
+use Sylius\Behat\NotificationType;
 use Sylius\Behat\Page\Admin\Taxon\CreatePageInterface;
+use Sylius\Behat\Service\NotificationCheckerInterface;
+use Sylius\Behat\Service\SharedStorageInterface;
+use Sylius\Component\Core\Model\TaxonInterface;
+use Webmozart\Assert\Assert;
 
 final class RemovingTaxonContext implements Context
 {
     public function __construct(
         private CreatePageInterface $createPage,
+        private SharedStorageInterface $sharedStorage,
+        private NotificationCheckerInterface $notificationChecker,
     ) {
     }
 
     /**
-     * @When I remove taxon named :name
-     * @When I delete taxon named :name
-     * @When I try to delete taxon named :name
+     * @When I remove taxon named :taxon
+     * @When I delete taxon named :taxon
+     * @When I try to delete taxon named :taxon
      */
-    public function iRemoveTaxonNamed(string $name): void
+    public function iRemoveTaxonNamed(TaxonInterface $taxon): void
     {
         $this->createPage->open();
-        $this->createPage->deleteTaxonOnPageByName($name);
+        $this->createPage->deleteTaxonOnPageByName($taxon->getName());
+        $this->sharedStorage->set('taxon', $taxon);
+    }
+
+    /**
+     * @Then /^(this taxon) should still exist$/
+     */
+    public function theTaxonShouldStillExist(TaxonInterface $taxon): void
+    {
+        $this->createPage->open();
+
+        Assert::true($this->createPage->hasTaxonWithName($taxon->getName()));
+    }
+
+    /**
+     * @Then I should be notified that this taxon could not be deleted as it is in use by a promotion rule
+     */
+    public function iShouldBeNotifiedThatThisTaxonCouldNotBeDeleted(): void
+    {
+        $this->notificationChecker->checkNotification(
+            'Cannot delete a taxon that is in use by a promotion rule.',
+            NotificationType::failure(),
+        );
     }
 }
