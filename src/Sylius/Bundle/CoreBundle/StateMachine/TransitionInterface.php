21a9b6068eb202d9b5cb6436ba076b331de2385e
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2023-09-16 11:04:24 +0200

    Add a Symfony Workflow state machine adapter

--- src/Sylius/Bundle/CoreBundle/StateMachine/TransitionInterface.php
+++ src/Sylius/Bundle/CoreBundle/StateMachine/TransitionInterface.php
@@ -6,7 +6,13 @@ interface TransitionInterface
 {
     public function getName(): string;
 
+    /**
+     * @return array<string>|null
+     */
     public function getFroms(): ?array;
 
+    /**
+     * @return array<string>|null
+     */
     public function getTos(): ?array;
 }
---
+++ src/Sylius/Bundle/CoreBundle/spec/StateMachine/SymfonyWorkflowAdapterSpec.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace spec\Sylius\Bundle\CoreBundle\StateMachine;
+
+use PhpSpec\ObjectBehavior;
+use stdClass;
+use Sylius\Bundle\CoreBundle\StateMachine\Transition;
+use Symfony\Component\Workflow\Registry;
+use Symfony\Component\Workflow\Transition as SymfonyWorkflowTransition;
+use Symfony\Component\Workflow\Workflow;
+
+final class SymfonyWorkflowAdapterSpec extends ObjectBehavior
+{
+    function let(Registry $symfonyWorkflowRegistry): void
+    {
+        $this->beConstructedWith($symfonyWorkflowRegistry);
+    }
+
+    function it_returns_whether_a_transition_can_be_applied(
+        Registry $symfonyWorkflowRegistry,
+        Workflow $someWorkflow,
+        stdClass $subject,
+    ): void {
+        $symfonyWorkflowRegistry->get($subject, 'some_workflow')->willReturn($someWorkflow);
+        $someWorkflow->can($subject, 'transition')->shouldBeCalled()->willReturn(true);
+
+        $this->can($subject, 'some_workflow', 'transition')->shouldReturn(true);
+    }
+
+    function it_applies_a_transition(
+        Registry $symfonyWorkflowRegistry,
+        Workflow $someWorkflow,
+        stdClass $subject,
+    ): void {
+        $symfonyWorkflowRegistry->get($subject, 'some_workflow')->willReturn($someWorkflow);
+        $someWorkflow->apply($subject, 'transition', [])->shouldBeCalled();
+
+        $this->apply($subject, 'some_workflow', 'transition');
+    }
+
+    function it_returns_enabled_transitions(
+        Registry $symfonyWorkflowRegistry,
+        Workflow $someWorkflow,
+        stdClass $subject,
+    ): void {
+        $symfonyWorkflowRegistry->get($subject, 'some_workflow')->willReturn($someWorkflow);
+        $someWorkflow->getEnabledTransitions($subject)->shouldBeCalled()->willReturn([
+            new SymfonyWorkflowTransition('transition', 'from', 'to'),
+            new SymfonyWorkflowTransition('transition2', ['from'], ['to']),
+        ]);
+
+        $this->getEnabledTransition($subject, 'some_workflow')->shouldBeLike([
+            new Transition('transition', ['from'], ['to']),
+            new Transition('transition2', ['from'], ['to']),
+        ]);
+    }
+}
