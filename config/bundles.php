31481bb0b3507c1577f5a629ceb18f6c73c571db
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2023-09-17 10:01:34 +0200

    Require the symfony/ux-twig-component package

--- config/bundles.php
+++ config/bundles.php
@@ -59,4 +59,5 @@ return [
     SyliusLabs\DoctrineMigrationsExtraBundle\SyliusLabsDoctrineMigrationsExtraBundle::class => ['all' => true],
     SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
 ];
