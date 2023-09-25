f4afbad480bdb74c8411c220a872f40dafb63477
Grzegorz Sadowski <sadowskigp@gmail.com>
2023-09-25 12:08:34 +0200

    [API] Add possibility for optional message to ProductAttributeCannotBeRemoved exception

--- src/Sylius/Bundle/ApiBundle/Exception/ProductAttributeCannotBeRemoved.php
+++ src/Sylius/Bundle/ApiBundle/Exception/ProductAttributeCannotBeRemoved.php
@@ -7,8 +7,11 @@ namespace Sylius\Bundle\ApiBundle\Exception;
 /** @experimental */
 final class ProductAttributeCannotBeRemoved extends \RuntimeException
 {
-    public function __construct()
-    {
-        parent::__construct('Cannot delete, the product attribute is in use.');
+    public function __construct(
+        string $message = 'Cannot delete, the product attribute is in use.',
+        int $code = 0,
+        \Throwable $previous = null,
+    ) {
+        parent::__construct($message, $code, $previous);
     }
 }
