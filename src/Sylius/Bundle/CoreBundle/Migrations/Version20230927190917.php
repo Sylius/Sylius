d8899f1df181650234bdbb8da5aa43bfdf7479a5
Marcin Kukliński <senghe@gmail.com>
2023-09-27 21:14:04 +0200

    Add migration for archived_at field in promotions and catalog promotions

---
+++ src/Sylius/Bundle/CoreBundle/Migrations/Version20230927190917.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230927190917 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE sylius_catalog_promotion ADD archived_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE sylius_promotion ADD archived_at DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE sylius_catalog_promotion DROP archived_at');
+        $this->addSql('ALTER TABLE sylius_promotion DROP archived_at');
+    }
+}
