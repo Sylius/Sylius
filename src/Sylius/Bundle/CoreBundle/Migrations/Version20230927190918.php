1c14447ebf95a2e5323a9972d84a6fd6e0b2cad3
Marcin Kukliński <senghe@gmail.com>
2023-09-28 12:57:46 +0200

    Add PostgreSQL migration

---
+++ src/Sylius/Bundle/CoreBundle/Migrations/Version20230927190918.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Sylius\Bundle\CoreBundle\Doctrine\Migrations\AbstractPostgreSQLMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230927190918 extends AbstractPostgreSQLMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add archived_at field on promotion tables';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE sylius_catalog_promotion ADD archived_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
+        $this->addSql('ALTER TABLE sylius_promotion ADD archived_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE sylius_catalog_promotion DROP archived_at');
+        $this->addSql('ALTER TABLE sylius_promotion DROP archived_at');
+    }
+}
