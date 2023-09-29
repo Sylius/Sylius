49c59617e7c0a717204625ea8767a898613442fd
Jan Goralski <jan.wojciech.goralski@gmail.com>
2023-09-29 14:26:53 +0200

    [Locale] Remove deprecations

--- src/Sylius/Bundle/LocaleBundle/spec/Templating/Helper/LocaleHelperSpec.php
+++ src/Sylius/Bundle/LocaleBundle/spec/Templating/Helper/LocaleHelperSpec.php
@@ -22,9 +22,11 @@ use Symfony\Component\Templating\Helper\Helper;
 
 final class LocaleHelperSpec extends ObjectBehavior
 {
-    function let(LocaleConverterInterface $localeConverter): void
-    {
-        $this->beConstructedWith($localeConverter);
+    function let(
+        LocaleConverterInterface $localeConverter,
+        LocaleContextInterface $localeContext,
+    ): void {
+        $this->beConstructedWith($localeConverter, $localeContext);
     }
 
     function it_is_a_helper(): void
@@ -37,49 +39,52 @@ final class LocaleHelperSpec extends ObjectBehavior
         $this->shouldImplement(LocaleHelperInterface::class);
     }
 
-    function it_converts_locales_code_to_name(LocaleConverterInterface $localeConverter): void
-    {
+    function it_converts_locales_code_to_name_using_default_locale_when_context_has_none(
+        LocaleConverterInterface $localeConverter,
+        LocaleContextInterface $localeContext,
+    ): void {
+        $localeContext->getLocaleCode()->willThrow(LocaleNotFoundException::class);
         $localeConverter->convertCodeToName('fr_FR', null)->willReturn('French (France)');
 
         $this->convertCodeToName('fr_FR')->shouldReturn('French (France)');
     }
 
-    function it_converts_locales_code_to_name_using_given_locale(LocaleConverterInterface $localeConverter): void
-    {
+    function it_converts_locales_code_to_name_using_locale_from_context(
+        LocaleConverterInterface $localeConverter,
+        LocaleContextInterface $localeContext,
+    ): void {
+        $localeContext->getLocaleCode()->willReturn('en');
+        $localeConverter->convertCodeToName('fr_FR', 'en')->willReturn('French (France)');
+
+        $this->convertCodeToName('fr_FR')->shouldReturn('French (France)');
+    }
+
+    function it_converts_locales_code_to_name_using_given_locale(
+        LocaleConverterInterface $localeConverter,
+        LocaleContextInterface $localeContext,
+    ): void {
+        $localeContext->getLocaleCode()->shouldNotBeCalled();
         $localeConverter->convertCodeToName('en', 'pl')->willReturn('angielski');
 
         $this->convertCodeToName('en', 'pl')->shouldReturn('angielski');
     }
 
-    function it_converts_locales_code_to_name_using_locale_from_the_context(
+    function it_fallbacks_to_the_code_if_the_name_is_not_in_the_database_and_no_locale_is_in_context(
         LocaleConverterInterface $localeConverter,
         LocaleContextInterface $localeContext,
     ): void {
-        $this->beConstructedWith($localeConverter, $localeContext);
-
-        $localeContext->getLocaleCode()->willReturn('pl');
-
-        $localeConverter->convertCodeToName('en', 'pl')->willReturn('angielski');
-
-        $this->convertCodeToName('en')->shouldReturn('angielski');
-    }
-
-    function it_converts_locale_code_to_name_using_default_locale_if_passed_locale_context_throws_an_exception(
-        LocaleConverterInterface $localeConverter,
-        LocaleContextInterface $localeContext,
-    ): void {
-        $this->beConstructedWith($localeConverter, $localeContext);
-
         $localeContext->getLocaleCode()->willThrow(LocaleNotFoundException::class);
+        $localeConverter->convertCodeToName('en_DG', null)->willThrow(new \InvalidArgumentException());
 
-        $localeConverter->convertCodeToName('en', null)->willReturn('English');
-
-        $this->convertCodeToName('en')->shouldReturn('English');
+        $this->convertCodeToName('en_DG')->shouldReturn('en_DG');
     }
 
-    function it_fallbacks_to_the_code_if_the_name_is_not_in_the_database(LocaleConverterInterface $localeConverter): void
-    {
-        $localeConverter->convertCodeToName('en_DG', null)->willThrow(new \InvalidArgumentException());
+    function it_fallbacks_to_the_code_if_the_name_is_not_in_the_database_and_a_locale_is_in_context(
+        LocaleConverterInterface $localeConverter,
+        LocaleContextInterface $localeContext,
+    ): void {
+        $localeContext->getLocaleCode()->willReturn('en');
+        $localeConverter->convertCodeToName('en_DG', 'en')->willThrow(new \InvalidArgumentException());
 
         $this->convertCodeToName('en_DG')->shouldReturn('en_DG');
     }
