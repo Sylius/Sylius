3be1ccc153319a0f38230f83d2b8ed4f13dcc5c7
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2023-10-01 07:22:09 +0200

    Remove accidentally changed files

--- rector.php
+++ rector.php
@@ -2,12 +2,21 @@
 
 declare(strict_types=1);
 
-use Rector\Config\RectorConfig;
+use Rector\Core\Configuration\Option;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (RectorConfig $config): void
+return static function (ContainerConfigurator $containerConfigurator): void
 {
-    $config->sets([
-        LevelSetList::UP_TO_PHP_82
-    ]);
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+    $parameters->set(Option::IMPORT_SHORT_CLASSES, false);
+
+    $services = $containerConfigurator->services();
+    $services->set(TypedPropertyRector::class);
+    $services->set(ClosureToArrowFunctionRector::class);
 };
