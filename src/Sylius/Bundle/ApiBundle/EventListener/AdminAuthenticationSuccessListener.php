7ef27dbd6cc5182b1de8fb94ee48800f2cf05033
Rafikooo <thecrazyrafik@gmail.com>
2023-10-02 18:46:02 +0200

    [ApiBundle] Upgrade AdminUser auth to API Platform 3

--- src/Sylius/Bundle/ApiBundle/EventListener/AdminAuthenticationSuccessListener.php
+++ src/Sylius/Bundle/ApiBundle/EventListener/AdminAuthenticationSuccessListener.php
@@ -13,11 +13,11 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\ApiBundle\EventListener;
 
-use ApiPlatform\Core\Api\IriConverterInterface;
+use ApiPlatform\Api\IriConverterInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
 use Sylius\Component\Core\Model\AdminUserInterface;
 
-final class AdminAuthenticationSuccessListener
+final readonly class AdminAuthenticationSuccessListener
 {
     public function __construct(private IriConverterInterface $iriConverter)
     {
@@ -32,7 +32,7 @@ final class AdminAuthenticationSuccessListener
             return;
         }
 
-        $data['adminUser'] = $this->iriConverter->getIriFromItem($adminUser);
+        $data['adminUser'] = $this->iriConverter->getIriFromResource($adminUser);
 
         $event->setData($data);
     }
