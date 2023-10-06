33153a0f7cce3b03c52d0292fb1dec94ce5cf938
Wojdylak <karolwojdyla37@gmail.com>
2023-10-06 12:19:54 +0200

    Fix copyright block

--- disabledTests/Api/Shop/CurrenciesTest.php
+++ disabledTests/Api/Shop/CurrenciesTest.php
@@ -1,10 +1,13 @@
 <?php
 
 /*
- * This file was created by developers working at BitBag
- * Do you need more information about us and what we do? Visit our https://bitbag.io website!
- * We are hiring developers from all over the world. Join us and start your new, exciting adventure and become part of us: https://bitbag.io/career
-*/
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 declare(strict_types=1);
 
