ef6946fa30cb38fff034e11f4c62ed1568d103b2
Rafikooo <thecrazyrafik@gmail.com>
2023-10-11 13:48:06 +0200

    [CoreBundle] Update the PostgreSQLDefaultSchemaListener class

--- src/Sylius/Bundle/CoreBundle/EventListener/PostgreSQLDefaultSchemaListener.php
+++ src/Sylius/Bundle/CoreBundle/EventListener/PostgreSQLDefaultSchemaListener.php
@@ -13,8 +13,6 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\CoreBundle\EventListener;
 
-use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\Schema\AbstractSchemaManager;
 use Doctrine\DBAL\Schema\PostgreSQLSchemaManager;
 use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
 
@@ -22,9 +20,7 @@ final class PostgreSQLDefaultSchemaListener
 {
     public function postGenerateSchema(GenerateSchemaEventArgs $args): void
     {
-        $connection = $args->getEntityManager()->getConnection();
-
-        $schemaManager = $this->createSchemaManager($connection);
+        $schemaManager = $args->getEntityManager()->getConnection()->createSchemaManager();
 
         if (!is_a($schemaManager, PostgreSQLSchemaManager::class)) {
             return;
@@ -37,19 +33,5 @@ final class PostgreSQLDefaultSchemaListener
                 $schema->createNamespace($namespace);
             }
         }
-    }
-
-    private function createSchemaManager(Connection $connection): AbstractSchemaManager
-    {
-        if (method_exists($connection, 'createSchemaManager')) {
-            return $connection->createSchemaManager();
-        }
-
-        if (method_exists($connection, 'getSchemaManager')) {
-            /** @psalm-suppress DeprecatedMethod */
-            return $connection->getSchemaManager();
-        }
-
-        throw new \RuntimeException('Cannot create schema manager');
     }
 }
