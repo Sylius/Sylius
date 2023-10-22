d9d8195cab577a5160ee886b311767f6e67aba93
Jibé Barth <barth.jib@gmail.com>
2023-10-22 16:55:08 +0200

    Use Property promotion constructor, remove EventSubscriberInterface

--- src/Sylius/Bundle/ShopBundle/EventListener/AlreadyLoggedInUserRegistrationListener.php
+++ src/Sylius/Bundle/ShopBundle/EventListener/AlreadyLoggedInUserRegistrationListener.php
@@ -14,41 +14,23 @@ declare(strict_types=1);
 namespace Sylius\Bundle\ShopBundle\EventListener;
 
 use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
-final class AlreadyLoggedInUserRegistrationListener implements EventSubscriberInterface
+final class AlreadyLoggedInUserRegistrationListener
 {
-    private AuthorizationCheckerInterface $authorizationChecker;
-
-    private RequestStack $requestStack;
-
-    private RouterInterface $router;
-
     public function __construct(
-        AuthorizationCheckerInterface $authorizationChecker,
-        RequestStack $requestStack,
-        RouterInterface $router
+        private AuthorizationCheckerInterface $authorizationChecker,
+        private RequestStack $requestStack,
+        private RouterInterface $router
     ) {
-        $this->authorizationChecker = $authorizationChecker;
-        $this->requestStack = $requestStack;
-        $this->router = $router;
-    }
-
-    public static function getSubscribedEvents(): array
-    {
-        return [
-            'sylius.customer.initialize_register' => 'handleAlreadyConnectedUser',
-        ];
     }
 
     public function handleAlreadyConnectedUser(ResourceControllerEvent $event): void
     {
         if (!$this->authorizationChecker->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            // Not authenticated, nothing to do here
             return;
         }
 
