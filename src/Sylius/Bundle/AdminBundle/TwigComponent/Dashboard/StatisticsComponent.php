075fb7c5a3382cdda13615bf72b3ee230f3ccd4e
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2023-10-24 18:07:29 +0200

    Improve the Dashboard view

--- src/Sylius/Bundle/AdminBundle/TwigComponent/Dashboard/StatisticsComponent.php
+++ src/Sylius/Bundle/AdminBundle/TwigComponent/Dashboard/StatisticsComponent.php
@@ -14,34 +14,78 @@ declare(strict_types=1);
 namespace Sylius\Bundle\AdminBundle\TwigComponent\Dashboard;
 
 use Sylius\Bundle\AdminBundle\Provider\StatisticsDataProviderInterface;
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
 use Sylius\Component\Core\Model\ChannelInterface;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\LiveArg;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\ComponentToolsTrait;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
 use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
 
+#[AsLiveComponent]
 final class StatisticsComponent
 {
-    /** @var array<string, mixed> */
-    public array $context;
+    use ComponentToolsTrait;
+    use DefaultActionTrait;
 
-    public ChannelInterface $channel;
+    #[ExposeInTemplate]
+    public readonly string $eventName;
 
+    #[LiveProp]
+    public string $channelCode;
+
+    #[LiveProp]
+    #[ExposeInTemplate]
+    public string $startDate = 'first day of january this year';
+
+    #[LiveProp]
+    #[ExposeInTemplate]
+    public string $endDate = 'first day of january next year';
+
+    #[LiveProp]
+    #[ExposeInTemplate]
+    public string $range = 'month';
+
+    #[LiveProp]
+    #[ExposeInTemplate]
+    public string $rangeName = 'year';
+
+    /**
+     * @param ChannelRepositoryInterface<ChannelInterface> $channelRepository
+     */
     public function __construct (
+        private readonly ChannelRepositoryInterface $channelRepository,
         private readonly StatisticsDataProviderInterface $statisticsDataProvider,
     ) {
+        $this->eventName = 'sylius.admin.dashboard.statistics';
     }
 
     #[ExposeInTemplate]
-    public function getContext(): array
+    public function getStatistics(): array
     {
-        return array_merge(
-            $this->context,
-            [
-                ...$this->statisticsDataProvider->getRawData(
-                    $this->channel,
-                    new \DateTime('-30 days'),
-                    new \DateTime(),
-                    'day',
-                ),
-            ]
+        /** @var ChannelInterface $channel */
+        $channel = $this->channelRepository->findOneByCode($this->channelCode);
+
+        return $this->statisticsDataProvider->getRawData(
+            $channel,
+            new \DateTime($this->startDate),
+            new \DateTime($this->endDate),
+            $this->range,
         );
+    }
+
+    #[LiveAction]
+    public function changeRange(
+        #[LiveArg] string $name,
+        #[LiveArg] string $range,
+        #[LiveArg] string $startDate,
+        #[LiveArg] string $endDate
+    ): void {
+        $this->rangeName = $name;
+        $this->range = $range;
+        $this->startDate = date('Y-m-d', strtotime($startDate));
+        $this->endDate = date('Y-m-d', strtotime($endDate));
     }
 }
