e42cea730acf6bad50f2dcdaf1957d6ca9aa9dd8
Rafikooo <thecrazyrafik@gmail.com>
2023-10-26 13:14:55 +0200

    [AdminBundle] Remove Swiftmailer

--- src/Sylius/Bundle/AdminBundle/test/config/bundles.php
+++ src/Sylius/Bundle/AdminBundle/test/config/bundles.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['test_with_swiftmailer' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
