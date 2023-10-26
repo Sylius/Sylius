8efdd9f0a3dfb5bad83f11ad6344073c87a0c8ae
Kamil Grygierzec <kam.grygierzec@gmail.com>
2023-10-26 18:24:48 +0200

    Fix order workflow tests

--- src/Sylius/Bundle/CoreBundle/Tests/Functional/StateMachine/OrderWorkflowTest.php
+++ src/Sylius/Bundle/CoreBundle/Tests/Functional/StateMachine/OrderWorkflowTest.php
@@ -17,7 +17,8 @@ use Sylius\Bundle\CoreBundle\StateMachine\StateMachineInterface;
 use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Component\Core\Model\Customer;
 use Sylius\Component\Core\Model\Order;
-use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Order\Model\OrderInterface;
+use Sylius\Component\Order\OrderTransitions;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
@@ -25,12 +26,21 @@ final class OrderWorkflowTest extends KernelTestCase
 {
     private OrderInterface $order;
 
-    public function setup(): void
+    public function setUp(): void
     {
         parent::setUp();
 
+        $sequenceRepository = $this->createMock(RepositoryInterface::class);
+        $sequenceRepository
+            ->method('findOneBy')
+            ->willReturn(null)
+        ;
+
+        self::getContainer()->set('sylius.repository.order_sequence', $sequenceRepository);
+
         $channel = $this->createMock(ChannelInterface::class);
         $customer = $this->createMock(Customer::class);
+
         $order = new Order();
         $order->setChannel($channel);
         $order->setCustomer($customer);
@@ -38,41 +48,29 @@ final class OrderWorkflowTest extends KernelTestCase
         $this->order = $order;
     }
 
-    /** @test */
-    public function it_applies_new_state_for_order_cart_state(): void
-    {
-        $stateMachine = $this->getStateMachine();
-        $order = $this->order;
-
-        $this->assertSame('cart', $order->getState());
-
-        $stateMachine->apply($order, 'sylius_order', 'create');
-
-        $this->assertSame('new', $order->getState());
-    }
-
     /**
      * @test
      *
-     * @dataProvider availableTransitionsForNewState
+     * @dataProvider availableTransitionsForOrder
      */
-    public function it_applies_all_available_transitions_for_order_new_state(
+    public function it_applies_all_available_transitions_for_order(
+        string $initialState,
         string $transition,
         string $expectedState,
     ): void {
         $stateMachine = $this->getStateMachine();
         $order = $this->order;
 
-        $stateMachine->apply($order, 'sylius_order', 'create');
-        $stateMachine->apply($order, 'sylius_order', $transition);
+        $stateMachine->apply($order, OrderTransitions::GRAPH, $initialState);
+        $stateMachine->apply($order, OrderTransitions::GRAPH, $transition);
 
         $this->assertSame($expectedState, $order->getState());
     }
 
-    public function availableTransitionsForNewState(): iterable
+    public function availableTransitionsForOrder(): iterable
     {
-        yield ['cancel', 'cancelled'];
-        yield ['fulfill', 'fulfilled'];
+        yield [OrderTransitions::TRANSITION_CREATE, OrderTransitions::TRANSITION_CANCEL, OrderInterface::STATE_CANCELLED];
+        yield [OrderTransitions::TRANSITION_CREATE, OrderTransitions::TRANSITION_FULFILL, OrderInterface::STATE_FULFILLED];
     }
 
     private function getStateMachine(): StateMachineInterface
