8e0fdd8fefcd567aec0ee29c2695987830421746
Rafikooo <thecrazyrafik@gmail.com>
2023-10-26 13:15:36 +0200

    [ApiBundle] Remove Swiftmailer

--- src/Sylius/Bundle/ApiBundle/Tests/MessageHandler/Admin/SendResetPasswordEmailHandlerTest.php
+++ src/Sylius/Bundle/ApiBundle/Tests/MessageHandler/Admin/SendResetPasswordEmailHandlerTest.php
@@ -29,10 +29,6 @@ final class SendResetPasswordEmailHandlerTest extends KernelTestCase
     /** @test */
     public function it_sends_password_reset_token_email(): void
     {
-        if ($this->isItSwiftmailerTestEnv()) {
-            $this->markTestSkipped('Test is relevant only for the environment without swiftmailer');
-        }
-
         $container = self::getContainer();
 
         /** @var TranslatorInterface $translator */
@@ -65,12 +61,5 @@ final class SendResetPasswordEmailHandlerTest extends KernelTestCase
             $email,
             $translator->trans('sylius.email.admin_password_reset.to_reset_your_password_token', [], null, 'en_US'),
         );
-    }
-
-    private function isItSwiftmailerTestEnv(): bool
-    {
-        $env = self::getContainer()->getParameter('kernel.environment');
-
-        return $env === 'test_with_swiftmailer';
     }
 }
