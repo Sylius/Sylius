09e626608b610cb8b7a3bfbb2fb470fa3490186e
Rafikooo <thecrazyrafik@gmail.com>
2023-11-16 21:26:22 +0100

    [Unit] Add tests for shipping address of placed order

--- tests/Api/Admin/OrdersTest.php
+++ tests/Api/Admin/OrdersTest.php
@@ -72,14 +72,48 @@ final class OrdersTest extends JsonApiTestCase
     }
 
     /** @test */
-    public function it_updates_an_order_billing_address(): void
+    public function it_gets_a_billing_address_of_placed_order(): void
     {
         $fixtures = $this->loadFixturesFromFiles([
             'authentication/api_administrator.yaml',
             'channel.yaml',
-            'customer.yaml',
-            'customer_order.yaml',
+            'order/order.yaml',
+            'order/customer.yaml',
         ]);
+
+        $headers = $this
+            ->headerBuilder()
+            ->withJsonLdAccept()
+            ->withAdminUserAuthorization('api@example.com')
+            ->build()
+        ;
+
+        /** @var AddressInterface $billingAddress */
+        $billingAddress = $fixtures['billing_address'];
+
+        $this->client->request(
+            method: 'GET',
+            uri: '/api/v2/admin/addresses/' . $billingAddress->getId(),
+            server: $headers,
+        );
+
+        $this->assertResponse(
+            $this->client->getResponse(),
+            'admin/order/get_billing_address_of_placed_order_response',
+            Response::HTTP_OK,
+        );
+    }
+
+    /** @test */
+    public function it_updates_a_billing_address_of_placed_order(): void
+    {
+        $fixtures = $this->loadFixturesFromFiles([
+            'authentication/api_administrator.yaml',
+            'channel.yaml',
+            'order/customer.yaml',
+            'order/order.yaml',
+        ]);
+
 
         $headers = $this
             ->headerBuilder()
@@ -97,32 +131,34 @@ final class OrdersTest extends JsonApiTestCase
             uri: '/api/v2/admin/addresses/' . $billingAddress->getId(),
             server: $headers,
             content: json_encode([
-                'firstName' => 'Adam',
-                'lastName' => 'Handley',
-                'company'=> 'FMŻ',
-                'street' => 'Kościuszki 21',
-                'countryCode' => 'FR',
-                'city' => 'Bordeaux',
-                'postcode' => '99-999',
-                'phoneNumber' => '911213969',
+                'firstName' => 'Updated: Adam',
+                'lastName' => 'Updated: Handley',
+                'company'=> 'Updated: FMŻ',
+                'street' => 'Updated: Kościuszki 21',
+                'countryCode' => 'Updated: FR',
+                'city' => 'Updated: Bordeaux',
+                'postcode' => 'Updated: 99-999',
+                'phoneNumber' => 'Updated: 911213969',
+                'provinceCode' => 'Updated: PL-WP',
+                'provinceName' => 'Updated: wielkopolskie'
             ]),
         );
 
         $this->assertResponse(
             $this->client->getResponse(),
-            'admin/order/get_order_billing_data_response',
+            'admin/order/put_billing_address_of_placed_order_response',
             Response::HTTP_OK,
         );
     }
 
     /** @test */
-    public function it_does_not_allow_to_update_customer_of_already_placed_order(): void
+    public function it_prevents_customer_update_in_billing_address_of_placed_order(): void
     {
         $fixtures = $this->loadFixturesFromFiles([
             'authentication/api_administrator.yaml',
             'channel.yaml',
-            'customer.yaml',
-            'customer_order.yaml',
+            'order/order.yaml',
+            'order/customer.yaml',
         ]);
 
         $headers = $this
@@ -156,6 +192,135 @@ final class OrdersTest extends JsonApiTestCase
         $this->client->request(
             method: 'GET',
             uri: '/api/v2/admin/addresses/' . $billingAddress->getId(),
+            server: $headers,
+        );
+
+        $this->assertSame(
+            '/api/v2/admin/customers/' . $customerTony->getId(),
+            json_decode($this->client->getResponse()->getContent())->customer
+        );
+    }
+
+    /** @test */
+    public function it_gets_a_shipping_address_of_placed_order(): void
+    {
+        $fixtures = $this->loadFixturesFromFiles([
+            'authentication/api_administrator.yaml',
+            'channel.yaml',
+            'order/customer.yaml',
+            'order/order.yaml',
+        ]);
+
+        $headers = $this
+            ->headerBuilder()
+            ->withJsonLdAccept()
+            ->withAdminUserAuthorization('api@example.com')
+            ->build()
+        ;
+
+        /** @var AddressInterface $shippingAddress */
+        $shippingAddress = $fixtures['shipping_address'];
+
+        $this->client->request(
+            method: 'GET',
+            uri: '/api/v2/admin/addresses/' . $shippingAddress->getId(),
+            server: $headers,
+        );
+
+        $this->assertResponse(
+            $this->client->getResponse(),
+            'admin/order/get_shipping_address_of_placed_order_response',
+            Response::HTTP_OK,
+        );
+    }
+
+    /** @test */
+    public function it_updated_a_shipping_address_of_placed_order(): void
+    {
+        $fixtures = $this->loadFixturesFromFiles([
+            'authentication/api_administrator.yaml',
+            'channel.yaml',
+            'order/customer.yaml',
+            'order/order.yaml',
+        ]);
+
+        $headers = $this
+            ->headerBuilder()
+            ->withJsonLdAccept()
+            ->withJsonLdContentType()
+            ->withAdminUserAuthorization('api@example.com')
+            ->build()
+        ;
+
+        /** @var AddressInterface $shippingAddress */
+        $shippingAddress = $fixtures['shipping_address'];
+
+        $this->client->request(
+            method: 'PUT',
+            uri: '/api/v2/admin/addresses/' . $shippingAddress->getId(),
+            server: $headers,
+            content: json_encode([
+                'firstName' => 'Updated: Julia',
+                'lastName' => 'Updated: Kowalska',
+                'company' => 'Updated: Błysk',
+                'street' => 'Updated: Marszałkowska 10',
+                'countryCode' => 'Updated: PL',
+                'city' => 'Updated: Warszawa',
+                'postcode' => 'Updated: 00-001',
+                'phoneNumber' => 'Updated: 48222333444',
+                'provinceCode' => 'Updated: PL-MA',
+                'provinceName' => 'Updated: mazowieckie'
+            ]),
+        );
+
+        $this->assertResponse(
+            $this->client->getResponse(),
+            'admin/order/put_shipping_address_of_placed_order_response',
+            Response::HTTP_OK,
+        );
+    }
+
+    /** @test */
+    public function it_prevents_customer_update_in_shipping_address_of_placed_order(): void
+    {
+        $fixtures = $this->loadFixturesFromFiles([
+            'authentication/api_administrator.yaml',
+            'channel.yaml',
+            'order/customer.yaml',
+            'order/order.yaml',
+        ]);
+
+        $headers = $this
+            ->headerBuilder()
+            ->withJsonLdAccept()
+            ->withJsonLdContentType()
+            ->withAdminUserAuthorization('api@example.com')
+            ->build()
+        ;
+
+        /** @var AddressInterface $shippingAddress */
+        $shippingAddress = $fixtures['shipping_address'];
+
+        /** @var CustomerInterface $customerTony */
+        $customerTony = $fixtures['customer_tony'];
+
+        /** @var CustomerInterface $customerDave */
+        $customerDave = $fixtures['customer_dave'];
+
+        $this->client->request(
+            method: 'PUT',
+            uri: '/api/v2/admin/addresses/' . $shippingAddress->getId(),
+            server: $headers,
+            content: json_encode([
+                'customer' => '/api/v2/admin/customers/' . $customerDave->getId(),
+            ]),
+        );
+
+        $this->assertResponseCode($this->client->getResponse(), Response::HTTP_OK);
+
+        $this->client->request(
+            method: 'GET',
+            uri: '/api/v2/admin/addresses/' . $shippingAddress->getId(),
             server: $headers,
         );
 
