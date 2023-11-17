a67c34b16d94cd1c8511458101e9a6c2fd284ca6
Francis Hilaire <Prometee@users.noreply.github.com>
2023-11-17 13:03:04 +0100

    Add API resource

---
+++ src/Sylius/Bundle/ApiBundle/Command/Payment/AddPaymentRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\ApiBundle\Command\Payment;
+
+use Sylius\Bundle\ApiBundle\Command\CustomerEmailAwareInterface;
+use Sylius\Bundle\ApiBundle\Command\IriToIdentifierConversionAwareInterface;
+use Sylius\Bundle\ApiBundle\Command\PaymentMethodCodeAwareInterface;
+
+/** @experimental */
+class AddPaymentRequest implements IriToIdentifierConversionAwareInterface
+{
+    public function __construct(
+        public string $type,
+        public mixed $data,
+        public ?string $paymentId,
+        public ?string $paymentMethodCode,
+    ) {
+    }
+}
