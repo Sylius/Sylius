a8b9c54e2cef44f6d49cb1e915d850fc40086aca
Rafikooo <thecrazyrafik@gmail.com>
2023-12-04 17:03:23 +0100

    [PHPStan] Specify return types in the StatisticsDataProviderInterface service

--- src/Sylius/Bundle/AdminBundle/Provider/StatisticsDataProvider.php
+++ src/Sylius/Bundle/AdminBundle/Provider/StatisticsDataProvider.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 namespace Sylius\Bundle\AdminBundle\Provider;
 
 use Sylius\Bundle\MoneyBundle\Formatter\MoneyFormatterInterface;
-use Sylius\Component\Core\Dashboard\DashboardStatistics;
 use Sylius\Component\Core\Dashboard\DashboardStatisticsProviderInterface;
 use Sylius\Component\Core\Dashboard\Interval;
 use Sylius\Component\Core\Dashboard\SalesDataProviderInterface;
@@ -31,7 +30,6 @@ class StatisticsDataProvider implements StatisticsDataProviderInterface
 
     public function getRawData(ChannelInterface $channel, \DateTimeInterface $startDate, \DateTimeInterface $endDate, string $interval): array
     {
-        /** @var DashboardStatistics $statistics */
         $statistics = $this->statisticsProvider->getStatisticsForChannelInPeriod($channel, $startDate, $endDate);
 
         $salesSummary = $this->salesDataProvider->getSalesSummary(
