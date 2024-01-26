41af1bcb4c619c63d4581a51cc55faf34b6b6a45
Grzegorz Sadowski <sadowskigp@gmail.com>
2024-01-26 15:04:25 +0100

    [Shop] Deprecate ContactEmailManager and its interface

--- src/Sylius/Bundle/ShopBundle/Controller/ContactController.php
+++ src/Sylius/Bundle/ShopBundle/Controller/ContactController.php
@@ -40,6 +40,16 @@ final class ContactController
         private LocaleContextInterface $localeContext,
         private DeprecatedContactEmailManagerInterface|ContactEmailManagerInterface $contactEmailManager,
     ) {
+        if ($this->contactEmailManager instanceof DeprecatedContactEmailManagerInterface) {
+            trigger_deprecation(
+                'sylius/admin-bundle',
+                '1.13',
+                'Passing an instance of %s as constructor argument for %s is deprecated and will be prohibited in Sylius 2.0. Pass an instance of %s instead.',
+                DeprecatedContactEmailManagerInterface::class,
+                self::class,
+                ContactEmailManagerInterface::class,
+            );
+        }
     }
 
     public function requestAction(Request $request): Response
