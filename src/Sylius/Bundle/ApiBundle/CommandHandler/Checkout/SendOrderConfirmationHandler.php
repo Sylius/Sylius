5e66b40007a0bdb314e9bfae7ef4d21b073fdf34
Rafikooo <thecrazyrafik@gmail.com>
2024-01-31 17:53:26 +0100

    Use OrderEmailManager in SendOrderConfirmationHandler

--- src/Sylius/Bundle/ApiBundle/CommandHandler/Checkout/SendOrderConfirmationHandler.php
+++ src/Sylius/Bundle/ApiBundle/CommandHandler/Checkout/SendOrderConfirmationHandler.php
@@ -14,10 +14,9 @@ declare(strict_types=1);
 namespace Sylius\Bundle\ApiBundle\CommandHandler\Checkout;
 
 use Sylius\Bundle\ApiBundle\Command\Checkout\SendOrderConfirmation;
-use Sylius\Bundle\CoreBundle\Mailer\Emails;
+use Sylius\Bundle\CoreBundle\Mailer\OrderEmailManagerInterface;
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Core\Repository\OrderRepositoryInterface;
-use Sylius\Component\Mailer\Sender\SenderInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Webmozart\Assert\Assert;
 
@@ -25,7 +24,7 @@ use Webmozart\Assert\Assert;
 final class SendOrderConfirmationHandler implements MessageHandlerInterface
 {
     public function __construct(
-        private SenderInterface $emailSender,
+        private OrderEmailManagerInterface $orderEmailManager,
         private OrderRepositoryInterface $orderRepository,
     ) {
     }
@@ -37,14 +36,6 @@ final class SendOrderConfirmationHandler implements MessageHandlerInterface
         $email = $order->getCustomer()->getEmail();
         Assert::notNull($email);
 
-        $this->emailSender->send(
-            Emails::ORDER_CONFIRMATION,
-            [$email],
-            [
-                'order' => $order,
-                'channel' => $order->getChannel(),
-                'localeCode' => $order->getLocaleCode(),
-            ],
-        );
+        $this->orderEmailManager->sendConfirmationEmail($order);
     }
 }
--- src/Sylius/Bundle/ApiBundle/spec/CommandHandler/Checkout/SendOrderConfirmationHandlerSpec.php
+++ src/Sylius/Bundle/ApiBundle/spec/CommandHandler/Checkout/SendOrderConfirmationHandlerSpec.php
@@ -15,19 +15,17 @@ namespace spec\Sylius\Bundle\ApiBundle\CommandHandler\Checkout;
 
 use PhpSpec\ObjectBehavior;
 use Sylius\Bundle\ApiBundle\Command\Checkout\SendOrderConfirmation;
-use Sylius\Bundle\CoreBundle\Mailer\Emails;
-use Sylius\Component\Core\Model\ChannelInterface;
+use Sylius\Bundle\CoreBundle\Mailer\OrderEmailManagerInterface;
 use Sylius\Component\Core\Model\CustomerInterface;
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Core\Repository\OrderRepositoryInterface;
-use Sylius\Component\Mailer\Sender\SenderInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 final class SendOrderConfirmationHandlerSpec extends ObjectBehavior
 {
-    function let(SenderInterface $sender, OrderRepositoryInterface $orderRepository): void
+    function let(OrderEmailManagerInterface $orderEmailManager, OrderRepositoryInterface $orderRepository): void
     {
-        $this->beConstructedWith($sender, $orderRepository);
+        $this->beConstructedWith($orderEmailManager, $orderRepository);
     }
 
     function it_is_a_message_handler(): void
@@ -36,32 +34,19 @@ final class SendOrderConfirmationHandlerSpec extends ObjectBehavior
     }
 
     function it_sends_order_confirmation_message(
+        OrderEmailManagerInterface $orderEmailManager,
         OrderInterface $order,
-        SendOrderConfirmation $sendOrderConfirmation,
-        SenderInterface $sender,
         CustomerInterface $customer,
-        ChannelInterface $channel,
         OrderRepositoryInterface $orderRepository,
     ): void {
-        $sendOrderConfirmation->orderToken()->willReturn('TOKEN');
-
         $orderRepository->findOneByTokenValue('TOKEN')->willReturn($order);
 
-        $order->getChannel()->willReturn($channel);
         $order->getLocaleCode()->willReturn('pl_PL');
 
         $order->getCustomer()->willReturn($customer);
         $customer->getEmail()->willReturn('johnny.bravo@email.com');
 
-        $sender->send(
-            Emails::ORDER_CONFIRMATION,
-            ['johnny.bravo@email.com'],
-            [
-                'order' => $order->getWrappedObject(),
-                'channel' => $channel->getWrappedObject(),
-                'localeCode' => 'pl_PL',
-            ],
-        )->shouldBeCalled();
+        $orderEmailManager->sendConfirmationEmail($order)->shouldBeCalled();
 
         $this(new SendOrderConfirmation('TOKEN'));
     }
