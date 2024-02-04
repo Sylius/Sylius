358f9836956b278d439d2e4750cea38be0cdcaa8
Francis Hilaire <Prometee@users.noreply.github.com>
2024-02-04 18:12:50 +0100

    Refactor to use CoreBundle instead of SyliusPay(um|ment)Bundle

---
+++ src/Sylius/Bundle/CoreBundle/PaymentRequest/Payum/Factory/PayumTokenFactory.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\PaymentRequest\Payum\Factory;
+
+use Payum\Core\Payum;
+use Payum\Core\Security\TokenInterface;
+use Sylius\Component\Core\Model\PaymentMethodInterface;
+use Sylius\Component\Payment\Model\PaymentRequestInterface;
+use Webmozart\Assert\Assert;
+
+final class PayumTokenFactory implements PayumTokenFactoryInterface
+{
+    public function __construct(private Payum $payum)
+    {
+    }
+
+    public function createNew(PaymentRequestInterface $paymentRequest): TokenInterface
+    {
+        $payment = $paymentRequest->getPayment();
+        Assert::notNull($payment);
+
+        /** @var PaymentMethodInterface|null $paymentMethod */
+        $paymentMethod = $paymentRequest->getMethod();
+        Assert::notNull($paymentMethod);
+
+        $gatewayConfig = $paymentMethod->getGatewayConfig();
+        Assert::notNull($gatewayConfig);
+
+        $gatewayName = $gatewayConfig->getGatewayName();
+
+        /** @var array{
+         *     'target_path'?: string,
+         *     'target_path_parameters'?: array<string, string>,
+         *     'after_path'?: string,
+         *     'after_path_parameters'?: array<string, string>,
+         * }|null $payload
+         */
+        $payload = $paymentRequest->getRequestPayload();
+        Assert::notNull($payload, 'The request payload need to be not null!');
+
+        $targetPath = $payload['target_path'] ?? null;
+        Assert::notNull($targetPath, 'The request payload must have a "target_path" field!');
+
+        $targetPathParameters = $payload['target_path_parameters'] ?? [];
+
+        $afterPath = $payload['after_path'] ?? null;
+        Assert::notNull($afterPath, 'The request payload must have an "after_path" field!');
+
+        $afterPathParameters = $payload['after_path_parameters'] ?? [];
+
+        return $this->payum->getTokenFactory()->createToken(
+            $gatewayName,
+            $payment,
+            $targetPath,
+            $targetPathParameters,
+            $afterPath,
+            $afterPathParameters,
+        );
+    }
+}
---
+++ src/Sylius/Bundle/CoreBundle/PaymentRequest/Payum/PaymentRequestContext.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\CoreBundle\PaymentRequest\Payum;
+
+use Sylius\Component\Payment\Model\PaymentRequestInterface;
+
+final class PaymentRequestContext implements PaymentRequestContextInterface
+{
+    private ?PaymentRequestInterface $paymentRequest = null;
+
+    public function isEnabled(): bool
+    {
+        return null !== $this->paymentRequest;
+    }
+
+    public function enable(PaymentRequestInterface $paymentRequest): void
+    {
+        $this->paymentRequest = $paymentRequest;
+    }
+
+    public function getPaymentRequest(): ?PaymentRequestInterface
+    {
+        return $this->paymentRequest;
+    }
+
+    public function disable(): void
+    {
+        $this->paymentRequest = null;
+    }
+}
