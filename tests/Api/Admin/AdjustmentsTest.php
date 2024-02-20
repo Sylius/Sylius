56f7b2b0dde4100ad8fb7022ec07e567d79c7089
Wojdylak <karolwojdyla37@gmail.com>
2024-02-20 13:56:58 +0100

    [Api] Remove collection operation of adjustments

--- tests/Api/Admin/AdjustmentsTest.php
+++ tests/Api/Admin/AdjustmentsTest.php
@@ -29,33 +29,6 @@ final class AdjustmentsTest extends JsonApiTestCase
     }
 
     /** @test */
-    public function it_gets_adjustments(): void
-    {
-        $this->loadFixturesFromFiles([
-            'authentication/api_administrator.yaml',
-            'channel.yaml',
-            'cart.yaml', 'country.yaml',
-            'shipping_method.yaml',
-            'payment_method.yaml'
-        ]);
-
-        $this->placeOrder('token');
-        $this->placeOrder('token2');
-
-        $this->client->request(
-            method: 'GET',
-            uri: '/api/v2/admin/adjustments',
-            server: $this->headerBuilder()->withJsonLdAccept()->withAdminUserAuthorization('api@example.com')->build(),
-        );
-
-        $this->assertResponse(
-            $this->client->getResponse(),
-            'admin/adjustments/get_adjustments',
-            Response::HTTP_OK,
-        );
-    }
-
-    /** @test */
     public function it_gets_adjustment_by_id(): void
     {
         $this->loadFixturesFromFiles([
