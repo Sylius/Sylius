3bb95721bea0da8a2b0ab7616bbc423a8df30b36
Francis Hilaire <Prometee@users.noreply.github.com>
2024-02-22 18:31:05 +0100

    Add a trait to avoid code duplicates

--- src/Sylius/Bundle/CoreBundle/PaymentRequest/Command/Offline/CapturePaymentRequest.php
+++ src/Sylius/Bundle/CoreBundle/PaymentRequest/Command/Offline/CapturePaymentRequest.php
@@ -14,21 +14,14 @@ declare(strict_types=1);
 namespace Sylius\Bundle\CoreBundle\PaymentRequest\Command\Offline;
 
 use Sylius\Bundle\CoreBundle\PaymentRequest\Command\PaymentRequestHashAwareInterface;
+use Sylius\Bundle\CoreBundle\PaymentRequest\Command\PaymentRequestHashAwareTrait;
 
 class CapturePaymentRequest implements PaymentRequestHashAwareInterface
 {
+    use PaymentRequestHashAwareTrait;
+
     public function __construct(
-        protected string $hash,
+        protected ?string $hash,
     ) {
-    }
-
-    public function getHash(): string
-    {
-        return $this->hash;
-    }
-
-    public function setHash(string $hash): void
-    {
-        $this->hash = $hash;
     }
 }
