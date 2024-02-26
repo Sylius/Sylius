3af6637a64cbc87a75d8eca6ca4ec82ead07f9aa
Wojdylak <karolwojdyla37@gmail.com>
2024-02-26 12:47:37 +0100

    [API] Add DoctrineCollectionArrayValuesNormalizer class

---
+++ src/Sylius/Bundle/ApiBundle/spec/Serializer/DoctrineCollectionArrayValuesNormalizerSpec.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace spec\Sylius\Bundle\ApiBundle\Serializer;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use PhpSpec\ObjectBehavior;
+use Sylius\Component\Core\Model\OrderInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+final class DoctrineCollectionArrayValuesNormalizerSpec extends ObjectBehavior
+{
+    function it_supports_only_doctrine_collection_with_normalization_context_key(
+        OrderInterface $order,
+        Collection $collection,
+    ): void {
+        $this->supportsNormalization($order)->shouldReturn(false);
+        $this->supportsNormalization($order, null, ['collection_array_values' => false])->shouldReturn(false);
+        $this->supportsNormalization($order, null, ['collection_array_values' => true])->shouldReturn(false);
+
+        $this->supportsNormalization($collection)->shouldReturn(false);
+        $this->supportsNormalization($collection, null, ['collection_array_values' => false])->shouldReturn(false);
+        $this->supportsNormalization($collection, null, ['collection_array_values' => true])->shouldReturn(true);
+    }
+
+    function it_normalizes_collection_with_array_values(
+        NormalizerInterface $normalizer,
+    ): void {
+        $this->setNormalizer($normalizer);
+
+        $collection = new ArrayCollection(['1' => ['id' => 1], '2' => ['id' => 2]]);
+
+        $this->normalize($collection, null, ['collection_array_values' => true]);
+
+        $normalizer->normalize([['id' => 1], ['id' => 2]], null, ['collection_array_values' => true])->shouldHaveBeenCalled();
+    }
+}
