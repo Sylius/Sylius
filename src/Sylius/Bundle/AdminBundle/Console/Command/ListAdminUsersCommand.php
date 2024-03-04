cfd5505986fb1b684bd1b33d14004e97ad294e94
Simon Krull <krull@punkt.de>
2024-03-04 17:53:57 +0100

    FEATURE: Implement option to also select single admin user by username

--- src/Sylius/Bundle/AdminBundle/Console/Command/ListAdminUsersCommand.php
+++ src/Sylius/Bundle/AdminBundle/Console/Command/ListAdminUsersCommand.php
@@ -15,6 +15,7 @@ namespace Sylius\Bundle\AdminBundle\Console\Command;
 
 use Sylius\Component\Core\Model\AdminUser;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Sylius\Component\Core\Model\AdminUserInterface;
@@ -23,8 +24,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Sylius\Component\User\Repository\UserRepositoryInterface;
 
 #[AsCommand(
-    name: 'sylius:admin-users:list',
-    description: 'List all admin users',
+    name: 'sylius:admin-user:list',
+    description: 'Displays all admin users with information in a list. Optionally, also one user can be displayed if the username is specified as an option.',
 )]
 final class ListAdminUsersCommand extends Command
 {
@@ -51,6 +52,24 @@ final class ListAdminUsersCommand extends Command
     {
         $this->io->title('List available admin users');
 
+        $username = $input->getOption('username');
+        if (isset($username)) {
+            $user = $this->userRepositoryInterface->findOneBy(['username' => $username]);
+            if ($user === null)
+            {
+                $this->io->error(sprintf('The user with the username "%s" has not been found', $username));
+                return Command::FAILURE;
+            }
+            $this->listSingleAdminUser($user);
+        } else {
+            $this->listAllAdminUsers();
+        }
+
+        return Command::SUCCESS;
+    }
+
+    private function listAllAdminUsers(): void
+    {
         $adminUsers = $this->userRepositoryInterface->findAll();
         /** @var AdminUser $adminUser */
         foreach ($adminUsers as $adminUser) {
@@ -71,6 +90,31 @@ final class ListAdminUsersCommand extends Command
                 ],
             );
         }
-        return Command::SUCCESS;
+    }
+
+    private function listSingleAdminUser(AdminUserInterface $adminUser): void
+    {
+        /** @var AdminUser $adminUser */
+        $this->io->table(
+            [
+                'ID', 'E-Mail', 'Username', 'First name', 'Last name', 'Locale code', 'Enabled',
+            ],
+            [
+                [
+                    $adminUser->getId(),
+                    $adminUser->getEmail(),
+                    $adminUser->getUsername(),
+                    $adminUser->getFirstname() ?? 'No Firstname Set',
+                    $adminUser->getLastName() ?? 'No Lastname Set',
+                    $adminUser->getLocaleCode(),
+                    $adminUser->isEnabled() ? 'Enabled' : 'Disabled',
+                ],
+            ],
+        );
+    }
+
+    protected function configure(): void
+    {
+        $this->addOption('username', null, InputOption::VALUE_OPTIONAL, 'List one user by the given username');
     }
 }
