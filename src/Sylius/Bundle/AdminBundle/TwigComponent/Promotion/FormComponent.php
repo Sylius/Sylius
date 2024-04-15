ef9d5a8f9606491b740188455c5b10f819175616
Grzegorz Sadowski <sadowskigp@gmail.com>
2024-04-15 11:08:55 +0200

    [Admin] Apply snake_case to promotion templates names

--- src/Sylius/Bundle/AdminBundle/TwigComponent/Promotion/FormComponent.php
+++ src/Sylius/Bundle/AdminBundle/TwigComponent/Promotion/FormComponent.php
@@ -25,7 +25,7 @@ use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 use Symfony\UX\LiveComponent\LiveCollectionTrait;
 
-#[AsLiveComponent(name: 'sylius_admin:promotion:form', template: '@SyliusAdmin/Promotion/form.html.twig')]
+#[AsLiveComponent(name: 'sylius_admin:promotion:form', template: '@SyliusAdmin/promotion/form.html.twig')]
 final class FormComponent
 {
     use DefaultActionTrait;
