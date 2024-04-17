58c1aae02f3295307340bca725841027cd40334e
Wojdylak <karolwojdyla37@gmail.com>
2024-04-17 18:14:17 +0200

    [Admin][CatalogPromotion] Add extension for actions and scopes types

---
+++ src/Sylius/Bundle/AdminBundle/Form/Extension/CatalogPromotionScope/ForVariantsScopeConfigurationTypeExtension.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\AdminBundle\Form\Extension\CatalogPromotionScope;
+
+use Sylius\Bundle\AdminBundle\Form\Type\ProductVariantAutocompleteChoiceType;
+use Sylius\Bundle\CoreBundle\Form\Type\CatalogPromotionScope\ForVariantsScopeConfigurationType;
+use Sylius\Component\Core\Model\ProductVariantInterface;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class ForVariantsScopeConfigurationTypeExtension extends AbstractTypeExtension
+{
+    /** @param DataTransformerInterface<ProductVariantInterface, string|null> $productVariantsToCodesTransformer */
+    public function __construct(private readonly DataTransformerInterface $productVariantsToCodesTransformer)
+    {
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('variants', ProductVariantAutocompleteChoiceType::class, [
+            'label' => 'sylius.ui.variants',
+            'multiple' => true,
+            'required' => false,
+            ])
+            ->get('variants')->addModelTransformer($this->productVariantsToCodesTransformer)
+        ;
+    }
+
+    public static function getExtendedTypes(): iterable
+    {
+        yield  ForVariantsScopeConfigurationType::class;
+    }
+}
---
+++ src/Sylius/Bundle/AdminBundle/Form/Type/ProductVariantAutocompleteChoiceType.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\AdminBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\UX\Autocomplete\Form\AsEntityAutocompleteField;
+use Symfony\UX\Autocomplete\Form\BaseEntityAutocompleteType;
+
+#[AsEntityAutocompleteField(route: 'sylius_admin_entity_autocomplete_admin')]
+final class ProductVariantAutocompleteChoiceType extends AbstractType
+{
+    public function __construct(
+        private readonly string $productVariantClass,
+    ) {
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'class' => $this->productVariantClass,
+            'choice_name' => 'descriptor',
+            'choice_value' => 'code',
+            'searchable_fields' => ['code'],
+        ]);
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'sylius_admin_product_variant_autocomplete_choice';
+    }
+
+    public function getParent(): string
+    {
+        return BaseEntityAutocompleteType::class;
+    }
+}
