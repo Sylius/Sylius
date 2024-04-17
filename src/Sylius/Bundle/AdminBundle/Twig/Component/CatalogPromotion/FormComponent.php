02392c4aa9d802aece57a031bbe712aa854e047b
Wojdylak <karolwojdyla37@gmail.com>
2024-04-17 18:13:05 +0200

    [Admin][CatalogPromotion] Add FormComponent

---
+++ src/Sylius/Bundle/AdminBundle/Twig/Component/CatalogPromotion/FormComponent.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\AdminBundle\Twig\Component\CatalogPromotion;
+
+use Sylius\Bundle\AdminBundle\TwigComponent\HookableComponentTrait;
+use Sylius\Component\Core\Model\CatalogPromotion;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\LiveArg;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\LiveCollectionTrait;
+
+#[AsLiveComponent(name: 'sylius_admin:catalog_promotion:form', template: '@SyliusAdmin/catalog_promotion/form.html.twig')]
+class FormComponent
+{
+    use DefaultActionTrait;
+    use HookableComponentTrait;
+    use LiveCollectionTrait;
+
+    #[LiveProp(fieldName: 'resource')]
+    public ?CatalogPromotion $resource = null;
+
+    /** @param class-string $formClass */
+    public function __construct(
+        private readonly FormFactoryInterface $formFactory,
+        private readonly string $formClass,
+    ) {
+    }
+
+    #[LiveAction]
+    public function addCollectionItem(PropertyAccessorInterface $propertyAccessor, #[LiveArg] string $name, #[LiveArg] string $type): void
+    {
+        $propertyPath = $this->fieldNameToPropertyPath($name, $this->formName);
+        $data = $propertyAccessor->getValue($this->formValues, $propertyPath);
+
+        if (!\is_array($data)) {
+            $propertyAccessor->setValue($this->formValues, $propertyPath, []);
+            $data = [];
+        }
+
+        $index = [] !== $data ? max(array_keys($data)) + 1 : 0;
+        $propertyAccessor->setValue($this->formValues, $propertyPath . "[$index]", ['type' => $type]);
+    }
+
+    protected function instantiateForm(): FormInterface
+    {
+        return $this->formFactory->create($this->formClass, $this->resource);
+    }
+}
