019719e45bd7b9c4c1756631eaa02a157e0c1685
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2024-04-23 21:30:13 +0200

    Start using Twig Hooks autoprefixing in the Catalog Promotion templates

--- src/Sylius/Bundle/AdminBundle/Twig/Component/CatalogPromotion/FormComponent.php
+++ src/Sylius/Bundle/AdminBundle/Twig/Component/CatalogPromotion/FormComponent.php
@@ -13,8 +13,8 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\AdminBundle\Twig\Component\CatalogPromotion;
 
-use Sylius\Bundle\AdminBundle\TwigComponent\HookableComponentTrait;
 use Sylius\Component\Core\Model\CatalogPromotion;
+use Sylius\TwigHooks\Twig\Component\HookableComponentTrait;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
