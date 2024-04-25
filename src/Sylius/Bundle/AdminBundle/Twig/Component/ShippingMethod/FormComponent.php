1fcf340de9b400e956a71400a48db435d5ac7afa
Wojdylak <karolwojdyla37@gmail.com>
2024-04-25 15:48:27 +0200

    [Admin][ShippingMethod] Change to adding rule per types

---
+++ src/Sylius/Bundle/AdminBundle/Twig/Component/ShippingMethod/FormComponent.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\AdminBundle\Twig\Component\ShippingMethod;
+
+use Sylius\Bundle\AdminBundle\TwigComponent\HookableComponentTrait;
+use Sylius\Component\Core\Model\ShippingMethod;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveAction;
+use Symfony\UX\LiveComponent\Attribute\LiveArg;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\LiveCollectionTrait;
+
+#[AsLiveComponent(name: 'sylius_admin:shipping_method:form', template: '@SyliusAdmin/shipping_method/form.html.twig')]
+final class FormComponent
+{
+    use DefaultActionTrait;
+    use HookableComponentTrait;
+    use LiveCollectionTrait;
+
+    #[LiveProp(fieldName: 'resource')]
+    public ?ShippingMethod $resource = null;
+
+    public function __construct(
+        private readonly FormFactoryInterface $formFactory,
+        private readonly string $shippingMethodTypeClass,
+    ) {
+    }
+
+    #[LiveAction]
+    public function addCollectionItem(PropertyAccessorInterface $propertyAccessor, #[LiveArg] string $name, #[LiveArg] string $type): void
+    {
+        $propertyPath = $this->fieldNameToPropertyPath($name, $this->formName);
+        $data = $propertyAccessor->getValue($this->formValues, $propertyPath);
+
+        if (!\is_array($data)) {
+            $propertyAccessor->setValue($this->formValues, $propertyPath, []);
+            $data = [];
+        }
+
+        $propertyAccessor->setValue(
+            $this->formValues,
+            sprintf('%s[%s]', $propertyPath, $this->resolveItemIndex($data)),
+            ['type' => $type],
+        );
+    }
+
+    protected function instantiateForm(): FormInterface
+    {
+        return $this->formFactory->create($this->shippingMethodTypeClass, $this->resource);
+    }
+
+    /** @param array<array-key, array<string, mixed>> $data */
+    private function resolveItemIndex(array $data): int
+    {
+        return [] !== $data ? max(array_keys($data)) + 1 : 0;
+    }
+}
