c5b44c7e2b26ab905b8a5751e042b7649042dcde
Wojdylak <karolwojdyla37@gmail.com>
2024-04-25 15:48:27 +0200

    [Admin][ShippingMethod] Change to adding rule per types

--- src/Sylius/Bundle/AdminBundle/Form/Extension/ShippingMethodTypeExtension.php
+++ src/Sylius/Bundle/AdminBundle/Form/Extension/ShippingMethodTypeExtension.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\AdminBundle\Form\Extension;
 
+use Sylius\Bundle\AdminBundle\Form\Type\AddButtonType;
 use Sylius\Bundle\ShippingBundle\Form\Type\ShippingMethodRuleType;
 use Sylius\Bundle\ShippingBundle\Form\Type\ShippingMethodType;
 use Symfony\Component\Form\AbstractTypeExtension;
@@ -21,30 +22,40 @@ use Symfony\UX\LiveComponent\Form\Type\LiveCollectionType;
 
 final class ShippingMethodTypeExtension extends AbstractTypeExtension
 {
+    /** @param array<string, string> $types */
+    public function __construct(private readonly array $types)
+    {
+    }
+
     /**
      * @param array<string, mixed> $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->remove('rules');
         $builder
             ->add('rules', LiveCollectionType::class, [
                 'entry_type' => ShippingMethodRuleType::class,
+                'entry_options' => [
+                    'types' => $this->types,
+                ],
                 'allow_add' => true,
                 'allow_delete' => true,
                 'by_reference' => false,
+                'button_add_type' => AddButtonType::class,
                 'button_add_options' => [
                     'label' => 'sylius.ui.add_rule',
+                    'types' => $this->types,
+                ],
+                'button_delete_options' => [
+                    'label' => false,
                 ],
             ])
         ;
     }
 
-    /**
-     * @return iterable<class-string>
-     */
+    /** @return iterable<class-string> */
     public static function getExtendedTypes(): iterable
     {
-        return [ShippingMethodType::class];
+        yield ShippingMethodType::class;
     }
 }
