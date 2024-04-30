1c98544ff174000586978352dd7378c4d0e9a182
Rafikooo <thecrazyrafik@gmail.com>
2024-04-30 17:12:20 +0200

    [Admin] Update twig hooks configuration

--- src/Sylius/Bundle/AdminBundle/Twig/Component/PaymentMethod/FormComponent.php
+++ src/Sylius/Bundle/AdminBundle/Twig/Component/PaymentMethod/FormComponent.php
@@ -13,8 +13,8 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\AdminBundle\Twig\Component\PaymentMethod;
 
-use Sylius\Bundle\AdminBundle\TwigComponent\HookableComponentTrait;
 use Sylius\Component\Core\Model\PaymentMethod;
+use Sylius\TwigHooks\Twig\Component\HookableComponentTrait;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
