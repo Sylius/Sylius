bd20b880ae483f65aaa8c61db2b9ebcfd4cfdaf0
Kamil Grygierzec <kam.grygierzec@gmail.com>
2024-04-30 13:26:07 +0200

    Add zone's form

---
+++ src/Sylius/Bundle/AdminBundle/Form/Extension/ZoneTypeExtension.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\AdminBundle\Form\Extension;
+
+use Sylius\Bundle\AddressingBundle\Form\Type\ZoneMemberType;
+use Sylius\Bundle\AddressingBundle\Form\Type\ZoneType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\UX\LiveComponent\Form\Type\LiveCollectionType;
+
+final class ZoneTypeExtension extends AbstractTypeExtension
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event): void {
+            $entryOptions = $event->getForm()->get('members')->getConfig()->getOptions()['entry_options'];
+
+            $event->getForm()->add('members', LiveCollectionType::class, [
+                'entry_type' => ZoneMemberType::class,
+                'entry_options' => $entryOptions,
+                'button_add_options' => [
+                    'label' => 'sylius.form.zone.add_member',
+                ],
+                'allow_add' => true,
+                'allow_delete' => true,
+                'by_reference' => false,
+                'delete_empty' => true,
+            ]);
+        });
+    }
+
+    public static function getExtendedTypes(): iterable
+    {
+        return [ZoneType::class];
+    }
+}
---
+++ src/Sylius/Bundle/AdminBundle/TwigComponent/Zone/FormComponent.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\AdminBundle\TwigComponent\Zone;
+
+use Sylius\Bundle\AdminBundle\TwigComponent\HookableComponentTrait;
+use Sylius\Component\Addressing\Model\Zone;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+use Symfony\UX\LiveComponent\LiveCollectionTrait;
+
+#[AsLiveComponent(name: 'sylius_admin:zone:form', template: '@SyliusAdmin/zone/form.html.twig')]
+final class FormComponent
+{
+    use DefaultActionTrait;
+    use HookableComponentTrait;
+    use LiveCollectionTrait;
+
+    #[LiveProp(fieldName: 'resource')]
+    public ?Zone $zone = null;
+
+    #[LiveProp(fieldName: 'type')]
+    public ?string $type = null;
+
+    /** @param class-string $formClass */
+    public function __construct(
+        private readonly FormFactoryInterface $formFactory,
+        private readonly string $formClass,
+    ) {
+    }
+
+    protected function instantiateForm(): FormInterface
+    {
+        $this->zone->setType($this->type);
+
+        return $this->formFactory->create($this->formClass, $this->zone, ['add_build_zone_form_subscriber' => false]);
+    }
+}
