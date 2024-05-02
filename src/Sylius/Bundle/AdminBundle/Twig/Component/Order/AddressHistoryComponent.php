b3587a3336264f1911b327913f596661c4f9f7b3
Jan Goralski <jan.wojciech.goralski@gmail.com>
2024-05-02 15:35:34 +0200

    [Admin] Change AddressHistoryComponent to be a simple component

--- src/Sylius/Bundle/AdminBundle/Twig/Component/Order/AddressHistoryComponent.php
+++ src/Sylius/Bundle/AdminBundle/Twig/Component/Order/AddressHistoryComponent.php
@@ -15,28 +15,14 @@ namespace Sylius\Bundle\AdminBundle\Twig\Component\Order;
 
 use Sylius\Component\Addressing\Model\AddressLogEntry;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
-use Sylius\TwigHooks\LiveComponent\HookableLiveComponentTrait;
-use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
-use Symfony\UX\LiveComponent\Attribute\LiveProp;
-use Symfony\UX\LiveComponent\DefaultActionTrait;
 use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
 
-#[AsLiveComponent(
-    name: 'sylius_admin:order:history:address',
-    template: '@SyliusAdmin/Order/history/content/sections/addresses/address.html.twig',
-)]
 final class AddressHistoryComponent
 {
-    use DefaultActionTrait;
-    use HookableLiveComponentTrait;
-
-    #[LiveProp]
     public ?string $addressId = null;
 
-    #[LiveProp]
     public ?string $header = null;
 
-    #[LiveProp]
     public ?string $sort = 'desc';
 
     /** @param RepositoryInterface<AddressLogEntry> $addressLogRepository */
