1601e03da5f37b9567b5862657964365a9a2f746
Kamil Grygierzec <kam.grygierzec@gmail.com>
2024-05-13 14:54:44 +0200

    Extract admin-only ChannelType

--- src/Sylius/Bundle/AdminBundle/Form/Type/ChannelType.php
+++ src/Sylius/Bundle/AdminBundle/Form/Type/ChannelType.php
@@ -15,12 +15,22 @@ namespace Sylius\Bundle\AdminBundle\Form\Type;
 
 use Sylius\Bundle\ChannelBundle\Form\Type\ChannelType as BaseChannelType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
 
 final class ChannelType extends AbstractType
 {
-    public function getBlockPrefix(): string
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        return 'sylius_admin_channel';
+        $builder
+            ->add('menuTaxon', TaxonAutocompleteType::class, [
+                'label' => 'sylius.form.channel.menu_taxon',
+                'multiple' => false,
+            ])
+            ->add('channelPriceHistoryConfig', ChannelPriceHistoryConfigType::class, [
+                'label' => false,
+                'required' => false,
+            ])
+        ;
     }
 
     public function getParent(): string
