52c56583173e5b0255b3038badbe7ca9f1e049ba
Jan Goralski <jan.wojciech.goralski@gmail.com>
2024-05-23 18:16:50 +0200

    [Admin][Product] Add filtration by main taxon

--- src/Sylius/Behat/Page/Admin/Product/IndexPageInterface.php
+++ src/Sylius/Behat/Page/Admin/Product/IndexPageInterface.php
@@ -19,6 +19,8 @@ interface IndexPageInterface extends CrudIndexPageInterface
 {
     public function filterByTaxon(string $taxonName): void;
 
+    public function filterByMainTaxon(string $taxonName): void;
+
     public function setFilterSearch(string $phrase): void;
 
     public function hasProductAccessibleImage(string $productCode): bool;
