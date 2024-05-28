6b3e3e3e3745bff96b18e78e7425acbdc93f5105
Grzegorz Sadowski <sadowskigp@gmail.com>
2024-05-28 11:22:39 +0200

    [Behat][Admin] Extract waitForFormUpdate method to the CRUD index page

--- src/Sylius/Behat/Page/Admin/Inventory/IndexPage.php
+++ src/Sylius/Behat/Page/Admin/Inventory/IndexPage.php
@@ -59,13 +59,4 @@ class IndexPage extends BaseIndexPage implements IndexPageInterface
             'product_filter' => '#criteria_product',
         ]);
     }
-
-    private function waitForFormUpdate(): void
-    {
-        $form = $this->getElement('filters_form');
-        sleep(1); // we need to sleep, as sometimes the check below is executed faster than the form sets the busy attribute
-        $form->waitFor(1500, function () use ($form) {
-            return !$form->hasAttribute('busy');
-        });
-    }
 }
