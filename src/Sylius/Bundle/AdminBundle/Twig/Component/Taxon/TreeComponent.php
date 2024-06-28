d7e5383bab6b3975e8b0dc84a5255233b986c829
Wojdylak <karolwojdyla37@gmail.com>
2024-06-28 18:12:09 +0200

    [Admin] Taxon index

--- src/Sylius/Bundle/AdminBundle/Twig/Component/Taxon/TreeComponent.php
+++ src/Sylius/Bundle/AdminBundle/Twig/Component/Taxon/TreeComponent.php
@@ -14,14 +14,13 @@ declare(strict_types=1);
 namespace Sylius\Bundle\AdminBundle\Twig\Component\Taxon;
 
 use Doctrine\Persistence\ObjectManager;
-use Sylius\Component\Core\Model\TaxonInterface;
+use Sylius\Bundle\AdminBundle\Doctrine\Query\Taxon\AllTaxonsInterface;
 use Sylius\Component\Taxonomy\Repository\TaxonRepositoryInterface;
 use Sylius\TwigHooks\LiveComponent\HookableLiveComponentTrait;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\Attribute\LiveArg;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
-use Symfony\UX\TwigComponent\Attribute\ExposeInTemplate;
 
 #[AsLiveComponent]
 class TreeComponent
@@ -29,22 +28,17 @@ class TreeComponent
     use DefaultActionTrait;
     use HookableLiveComponentTrait;
 
-    /**
-     * @param TaxonRepositoryInterface<TaxonInterface> $taxonRepository
-     */
     public function __construct(
+        private readonly AllTaxonsInterface $allTaxons,
         private readonly TaxonRepositoryInterface $taxonRepository,
         private readonly ObjectManager $taxonManager,
     ) {
     }
 
-    /**
-     * @return array<TaxonInterface>
-     */
-    #[ExposeInTemplate(name: 'root_nodes')]
-    public function getRootNodes(): array
+    /** @return array<array-key, mixed> */
+    public function getTree(): array
     {
-        return $this->taxonRepository->findHydratedRootNodes();
+        return $this->buildTree($this->allTaxons->getArrayResult());
     }
 
     #[LiveAction]
@@ -67,10 +61,30 @@ class TreeComponent
         $this->taxonManager->flush();
     }
 
-    #[LiveAction]
-    public function delete(#[LiveArg] int $taxonId): void
+    /**
+     * @param array<array-key, mixed> $taxons
+     *
+     * @return array<array-key, mixed>
+     */
+    private function buildTree(array $taxons): array
     {
-        $taxon = $this->taxonRepository->find($taxonId);
-        $this->taxonRepository->remove($taxon);
+        $tree = [];
+        $children = [];
+
+        foreach ($taxons as $taxon) {
+            $treeChild = [
+                'id' => $taxon['id'],
+                'name' => $taxon['name'],
+                'children' => $children[$taxon['id']] ?? [],
+            ];
+
+            if (null !== $taxon['parent_id']) {
+                $children[$taxon['parent_id']][] = $treeChild;
+            } else {
+                $tree[] = $treeChild;
+            }
+        }
+
+        return $tree;
     }
 }
