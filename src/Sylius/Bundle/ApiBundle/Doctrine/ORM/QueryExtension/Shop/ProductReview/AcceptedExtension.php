02e0a5471dc77bf4c5e0a27b69bcbad0648e8b41
Jan Goralski <jan.wojciech.goralski@gmail.com>
2024-07-12 16:40:27 +0200

    [API][QueryExtension] Further restructuring

---
+++ src/Sylius/Bundle/ApiBundle/Doctrine/ORM/QueryExtension/Shop/ProductReview/AcceptedExtension.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\ApiBundle\Doctrine\ORM\QueryExtension\Shop\ProductReview;
+
+use ApiPlatform\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
+use Doctrine\ORM\QueryBuilder;
+use Sylius\Component\Review\Model\ReviewInterface;
+
+final readonly class AcceptedExtension implements QueryCollectionExtensionInterface
+{
+    public function __construct(private string $productReviewClass)
+    {
+    }
+
+    /**
+     * @param array<array-key, mixed> $context
+     */
+    public function applyToCollection(
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        string $resourceClass,
+        ?Operation $operation = null,
+        array $context = [],
+    ): void {
+        if ($this->productReviewClass !== $resourceClass || null === $operation) {
+            return;
+        }
+
+        if ($operation->getName() !== 'shop_get') {
+            return;
+        }
+
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+
+        $statusParameterName = $queryNameGenerator->generateParameterName('status');
+
+        $queryBuilder
+            ->andWhere(sprintf('%s.status = :%s', $rootAlias, $statusParameterName))
+            ->setParameter($statusParameterName, ReviewInterface::STATUS_ACCEPTED)
+        ;
+    }
+}
---
+++ src/Sylius/Bundle/ApiBundle/spec/Doctrine/ORM/QueryExtension/Shop/Locale/ChannelBasedExtensionSpec.php
@@ -0,0 +1,117 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace spec\Sylius\Bundle\ApiBundle\Doctrine\ORM\QueryExtension\Shop\Locale;
+
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Get;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\QueryBuilder;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Sylius\Bundle\ApiBundle\SectionResolver\AdminApiSection;
+use Sylius\Bundle\ApiBundle\SectionResolver\ShopApiSection;
+use Sylius\Bundle\ApiBundle\Serializer\ContextKeys;
+use Sylius\Bundle\CoreBundle\SectionResolver\SectionProviderInterface;
+use Sylius\Component\Core\Model\ChannelInterface;
+use Sylius\Component\Locale\Model\LocaleInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+final class ChannelBasedExtensionSpec extends ObjectBehavior
+{
+    function let(SectionProviderInterface $sectionProvider): void
+    {
+        $this->beConstructedWith($sectionProvider);
+    }
+
+    public function it_does_not_apply_conditions_to_collection_for_unsupported_resource(
+        SectionProviderInterface $sectionProvider,
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+    ): void {
+        $sectionProvider->getSection()->shouldNotBeCalled();
+        $queryBuilder->getRootAliases()->shouldNotBeCalled();
+        $queryBuilder->andWhere(Argument::any())->shouldNotBeCalled();
+
+        $this->applyToCollection($queryBuilder, $queryNameGenerator, \stdClass::class);
+    }
+
+    function it_does_not_apply_conditions_for_non_shop_api_section(
+        SectionProviderInterface $sectionProvider,
+        AdminApiSection $adminApiSection,
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        ChannelInterface $channel,
+    ): void {
+        $sectionProvider->getSection()->willReturn($adminApiSection);
+
+        $queryBuilder->getRootAliases()->shouldNotBeCalled();
+        $queryBuilder->andWhere(Argument::any())->shouldNotBeCalled();
+
+        $this->applyToCollection(
+            $queryBuilder,
+            $queryNameGenerator,
+            LocaleInterface::class,
+            new Get(),
+            [
+                ContextKeys::CHANNEL => $channel->getWrappedObject(),
+                ContextKeys::HTTP_REQUEST_METHOD_TYPE => Request::METHOD_GET,
+            ],
+        );
+    }
+
+    function it_applies_conditions_for_shop_api_section(
+        SectionProviderInterface $sectionProvider,
+        ShopApiSection $shopApiSection,
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+        ChannelInterface $channel,
+        LocaleInterface $locale,
+    ): void {
+        $sectionProvider->getSection()->willReturn($shopApiSection);
+
+        $queryNameGenerator->generateParameterName('locales')->shouldBeCalled()->willReturn('locales');
+
+        $locales = new ArrayCollection([$locale->getWrappedObject()]);
+        $channel->getLocales()->shouldBeCalled()->willReturn($locales);
+
+        $queryBuilder->getRootAliases()->willReturn(['o']);
+        $queryBuilder->andWhere('o.id in (:locales)')->shouldBeCalled()->willReturn($queryBuilder->getWrappedObject());
+        $queryBuilder->setParameter('locales', $locales)->shouldBeCalled()->willReturn($queryBuilder->getWrappedObject());
+
+        $this->applyToCollection(
+            $queryBuilder,
+            $queryNameGenerator,
+            LocaleInterface::class,
+            new Get(),
+            [
+                ContextKeys::CHANNEL => $channel->getWrappedObject(),
+                ContextKeys::HTTP_REQUEST_METHOD_TYPE => Request::METHOD_GET,
+            ],
+        );
+    }
+
+    function it_throws_an_exception_if_context_has_no_channel(
+        SectionProviderInterface $sectionProvider,
+        ShopApiSection $shopApiSection,
+        QueryBuilder $queryBuilder,
+        QueryNameGeneratorInterface $queryNameGenerator,
+    ): void {
+        $sectionProvider->getSection()->willReturn($shopApiSection);
+
+        $this
+            ->shouldThrow(\InvalidArgumentException::class)
+            ->during('applyToCollection', [$queryBuilder, $queryNameGenerator, LocaleInterface::class, new Get()])
+        ;
+    }
+}
