e8521a9c401fc77eeb2d6ab9385f78c29f34655a
Enrique Barbeito García <ebarbeito@gmail.com>
2024-07-24 11:16:07 +0200

    jQuery selector pptimization reducing DOM queries
    This stores the channelPricings selector to prevent to query the DOM several
    times.

--- src/Sylius/Bundle/AdminBundle/Resources/private/js/sylius-toggle-disabled-channel-pricings.js
+++ src/Sylius/Bundle/AdminBundle/Resources/private/js/sylius-toggle-disabled-channel-pricings.js
@@ -21,32 +21,35 @@ const toggleConfigurableProduct = (isChecked) => {
  * Toggle visibility within the simple product form.
  */
 const toggleSimpleProduct = (isChecked) => {
-  const $tabs = $('#sylius_product_variant_channelPricings div.ui.top.attached.tabular.menu a.item[data-disabled-channel-pricing]');
-  const $segments = $('#sylius_product_variant_channelPricings div.ui.bottom.attached.tab.segment[data-disabled-channel-pricing]');
-  if ($tabs.length !== $segments.length || $tabs.length === 0) {
-    return;
-  }
+  const $channelPricings = $('#sylius_product_variant_channelPricings');
+  const $tabs = $channelPricings.find('div.ui.top.attached.tabular.menu a.item[data-disabled-channel-pricing]');
+  const $segments = $channelPricings.find('div.ui.bottom.attached.tab.segment[data-disabled-channel-pricing]');
 
-  // Tabs visibility
+  if ($tabs.length !== $segments.length || $tabs.length === 0) return;
+
+  // Tabs and segments visibility
   $tabs.each(function () {
-    $(this).toggle(isChecked);
+    const $tab = $(this);
+    $tab.toggle(isChecked);
 
-    // Attached segment visibility symmetry
-    const thisChannelCode = $(this).data('tab');
-    $(`#sylius_product_variant_channelPricings div.ui.bottom.attached.tab.segment[data-disabled-channel-pricing][data-tab="${thisChannelCode}"]`)
-      .attr('style', $(this).attr('style'));
+    // Synchronize visibility styles with the corresponding segment
+    const channelCode = $tab.data('tab');
+    $channelPricings.find(`div.ui.bottom.attached.tab.segment[data-disabled-channel-pricing][data-tab="${channelCode}"]`)
+      .attr('style', $tab.attr('style'));
   });
 
-  // Active tab and segment after visibility update
-  const $tabActive = $('#sylius_product_variant_channelPricings a.item.active');
-  const $segmentActive = $('#sylius_product_variant_channelPricings div.ui.bottom.attached.tab.segment.active');
-  if ($tabActive.is(':hidden')) {
-    const $firstTabVisible = $('#sylius_product_variant_channelPricings a.item:visible').first();
+  // Update the active tab and segment
+  const $activeTab = $channelPricings.find('a.item.active');
+  const $activeSegment = $channelPricings.find('div.ui.bottom.attached.tab.segment.active');
 
-    $tabActive.removeClass('active');
-    $segmentActive.removeClass('active');
-    $firstTabVisible.addClass('active');
-    $(`#sylius_product_variant_channelPricings div.ui.bottom.attached.tab.segment[data-tab="${($firstTabVisible.data('tab'))}"]`)
+  if ($activeTab.is(':hidden')) {
+    const $firstVisibleTab = $channelPricings.find('a.item:visible').first();
+
+    $activeTab.removeClass('active');
+    $activeSegment.removeClass('active');
+
+    $firstVisibleTab.addClass('active');
+    $channelPricings.find(`div.ui.bottom.attached.tab.segment[data-tab="${$firstVisibleTab.data('tab')}"]`)
       .addClass('active');
   }
 };
@@ -57,13 +60,13 @@ $.fn.extend({
 
     if ($toggle.length === 0) return;
 
-    const toggleHandler = () => {
+    const toggleClicked = () => {
       const isChecked = $toggle.is(':checked');
       toggleConfigurableProduct(isChecked);
       toggleSimpleProduct(isChecked);
     };
 
-    toggleHandler();
-    $toggle.off('change').on('change', toggleHandler);
+    toggleClicked();
+    $toggle.off('change').on('change', toggleClicked);
   },
 });
