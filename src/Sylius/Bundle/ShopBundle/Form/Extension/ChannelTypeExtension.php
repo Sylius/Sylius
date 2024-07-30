d728702965050603411120c5559bae4825fbdff3
mamazu <14860264+mamazu@users.noreply.github.com>
2024-07-30 11:24:34 +0200

    Moving ChannelTypeExtension to the Shop Bundle

---
+++ src/Sylius/Bundle/ShopBundle/Form/Extension/ChannelTypeExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\ShopBundle\Form\Extension;
+
+use Sylius\Bundle\ThemeBundle\Form\Type\ThemeNameChoiceType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Sylius\Bundle\ChannelBundle\Form\Type\ChannelType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class ChannelTypeExtension extends AbstractTypeExtension
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        if (class_exists('\Sylius\Bundle\ThemeBundle\Form\Type\ThemeNameChoiceType')) {
+            $builder->add('themeName', ThemeNameChoiceType::class, [
+                'label' => 'sylius.form.channel.theme',
+                'required' => false,
+                'empty_data' => null,
+                'placeholder' => 'sylius.ui.no_theme',
+            ]);
+        }
+    }
+
+    public static function getExtendedTypes(): iterable
+    {
+        return [ChannelType::class];
+    }
+}
