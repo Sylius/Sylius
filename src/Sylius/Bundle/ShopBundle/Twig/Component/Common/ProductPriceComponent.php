9176c5c2dc307730c82ee6d070824c7400917f42
Kamil Grygierzec <kam.grygierzec@gmail.com>
2024-08-16 12:38:28 +0200

    Minor ecs fixes

--- src/Sylius/Bundle/ShopBundle/Twig/Component/Common/ProductPriceComponent.php
+++ src/Sylius/Bundle/ShopBundle/Twig/Component/Common/ProductPriceComponent.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace Sylius\Bundle\ShopBundle\Twig\Component\Common;
@@ -24,7 +33,7 @@ final class ProductPriceComponent
         private readonly ChannelContextInterface $channelContext,
         private readonly LocaleContextInterface $localeContext,
         private readonly CurrencyContextInterface $currencyContext,
-    ){
+    ) {
     }
 
     #[ExposeInTemplate(name: 'price')]
