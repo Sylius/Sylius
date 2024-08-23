dc0c2e080a14327c1893ce8dc25f5a70b92a0a2b
Michał Pysiak <mpysiak@gmail.com>
2024-08-23 06:49:38 +0200

    Fixes after CR

--- src/Sylius/Bundle/ShopBundle/Form/Type/AddToCartType.php
+++ src/Sylius/Bundle/ShopBundle/Form/Type/AddToCartType.php
@@ -14,31 +14,10 @@ declare(strict_types=1);
 namespace Sylius\Bundle\ShopBundle\Form\Type;
 
 use Sylius\Bundle\CoreBundle\Form\Type\Order\AddToCartType as BaseAddToCartType;
-use Sylius\Bundle\OrderBundle\Form\Type\CartItemType;
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
-use Sylius\Component\Core\Model\ProductInterface;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class AddToCartType extends AbstractResourceType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder->add('cartItem', CartItemType::class, ['product' => $options['product']]);
-    }
-
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        parent::configureOptions($resolver);
-
-        $resolver
-            ->setDefined([
-                'product',
-            ])
-            ->setAllowedTypes('product', ProductInterface::class)
-        ;
-    }
-
     public function getBlockPrefix(): string
     {
         return 'sylius_shop_add_to_cart';
