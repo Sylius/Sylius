310e49a5c5f4a148b59b8535808be4c42cff7f18
Grzegorz Sadowski <sadowskigp@gmail.com>
2024-08-23 12:52:51 +0200

    [Money] Deprecate unneeded templating helpers

--- src/Sylius/Bundle/MoneyBundle/Templating/Helper/ConvertMoneyHelperInterface.php
+++ src/Sylius/Bundle/MoneyBundle/Templating/Helper/ConvertMoneyHelperInterface.php
@@ -13,6 +13,17 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\MoneyBundle\Templating\Helper;
 
+use Sylius\Component\Currency\Converter\CurrencyConverterInterface;
+
+trigger_deprecation(
+    'sylius/money-bundle',
+    '1.14',
+    'The "%s" interface is deprecated, use "%s" instead.',
+    ConvertMoneyHelperInterface::class,
+    CurrencyConverterInterface::class,
+);
+
+/** @deprecated since Sylius 1.14 and will be removed in Sylius 2.0. Use {@see \Sylius\Component\Currency\Converter\CurrencyConverterInterface} instead. */
 interface ConvertMoneyHelperInterface
 {
     /**
