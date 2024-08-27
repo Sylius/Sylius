51ff2d535b3be8e8e687b927e8facc5036d1adea
Kamil Grygierzec <kam.grygierzec@gmail.com>
2024-08-27 15:13:44 +0200

    Make account update profile form a live component

--- src/Sylius/Bundle/ShopBundle/Twig/Component/Common/Account/FormComponent.php
+++ src/Sylius/Bundle/ShopBundle/Twig/Component/Common/Account/FormComponent.php
@@ -11,7 +11,7 @@
 
 declare(strict_types=1);
 
-namespace Sylius\Bundle\ShopBundle\Twig\Component\Register;
+namespace Sylius\Bundle\ShopBundle\Twig\Component\Common\Account;
 
 use Sylius\Component\Core\Model\Customer;
 use Sylius\TwigHooks\LiveComponent\HookableLiveComponentTrait;
