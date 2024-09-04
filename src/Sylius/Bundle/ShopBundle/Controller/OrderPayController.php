e01567955ac019c2761a9bccde98b30f1053499d
Francis Hilaire <Prometee@users.noreply.github.com>
2024-09-04 12:56:12 +0300

    Create new controller action to replace the payum after capture one

--- src/Sylius/Bundle/ShopBundle/Controller/OrderPayController.php
+++ src/Sylius/Bundle/ShopBundle/Controller/OrderPayController.php
@@ -14,26 +14,34 @@ declare(strict_types=1);
 namespace Sylius\Bundle\ShopBundle\Controller;
 
 use LogicException;
+use Payum\Core\Request\Generic;
+use Payum\Core\Request\GetStatusInterface;
 use Sylius\Bundle\ResourceBundle\Controller\RequestConfigurationFactoryInterface;
+use Sylius\Bundle\ShopBundle\Provider\AfterPayResponseProviderInterface;
 use Sylius\Bundle\ShopBundle\Provider\PayResponseProviderInterface;
 use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\Model\PaymentInterface;
 use Sylius\Component\Order\Repository\OrderRepositoryInterface;
 use Sylius\Component\Resource\Metadata\MetadataInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 final class OrderPayController
 {
     /**
-     * @param iterable<PayResponseProviderInterface> $payResponseProviders
      * @param OrderRepositoryInterface<OrderInterface> $orderRepository
+     * @param iterable<PayResponseProviderInterface> $payResponseProviders
+     * @param iterable<AfterPayResponseProviderInterface> $afterPayResponseProviders
      */
     public function __construct(
         private OrderRepositoryInterface $orderRepository,
         private MetadataInterface $orderMetadata,
         private RequestConfigurationFactoryInterface $requestConfigurationFactory,
         private iterable $payResponseProviders,
+        private iterable $afterPayResponseProviders,
     ) {
     }
 
@@ -57,5 +65,18 @@ final class OrderPayController
         }
 
         throw new LogicException(sprintf('No "pay response provider" available for order (id %s).',  $order->getId()));
+    }
+
+    public function afterPayAction(Request $request): Response
+    {
+        $configuration = $this->requestConfigurationFactory->create($this->orderMetadata, $request);
+
+        foreach ($this->afterPayResponseProviders as $provider) {
+            if ($provider->supports($configuration)) {
+                return $provider->getResponse($configuration);
+            }
+        }
+
+        throw new LogicException(sprintf('No "after pay response provider" available.'));
     }
 }
