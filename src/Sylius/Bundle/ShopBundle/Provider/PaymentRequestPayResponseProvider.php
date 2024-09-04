e7e162b7564544e1d8306e887a99b27bce9b0e41
Francis Hilaire <Prometee@users.noreply.github.com>
2024-09-04 12:57:11 +0300

    Change tokenValue to payment request hash

--- src/Sylius/Bundle/ShopBundle/Provider/PaymentRequestPayResponseProvider.php
+++ src/Sylius/Bundle/ShopBundle/Provider/PaymentRequestPayResponseProvider.php
@@ -19,6 +19,7 @@ use Sylius\Bundle\ResourceBundle\Controller\RequestConfiguration;
 use Sylius\Bundle\ShopBundle\Resolver\PaymentToPayResolverInterface;
 use Sylius\Component\Core\Model\OrderInterface;
 use Sylius\Component\Payment\Factory\PaymentRequestFactoryInterface;
+use Sylius\Component\Payment\Model\PaymentRequestInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
@@ -28,6 +29,9 @@ use function sprintf;
 
 final class PaymentRequestPayResponseProvider implements PayResponseProviderInterface
 {
+    /**
+     * @param PaymentRequestFactoryInterface<PaymentRequestInterface> $paymentRequestFactory
+     */
     public function __construct(
         private PaymentToPayResolverInterface $paymentToPayResolver,
         private PaymentRequestFactoryInterface $paymentRequestFactory,
@@ -56,7 +60,7 @@ final class PaymentRequestPayResponseProvider implements PayResponseProviderInte
         }
 
         $url = $this->router->generate('sylius_shop_order_after_pay', [
-            'tokenValue' => $order->getTokenValue(),
+            'hash' => $paymentRequest->getId(),
         ]);
 
         return new RedirectResponse($url);
