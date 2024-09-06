13536fcac7c50e794bbb41acd815aabd44e5de6d
Wojdylak <karolwojdyla37@gmail.com>
2024-09-06 11:26:18 +0200

    [Api] Move PaymentRequestUpdateDataProviderSpec.php to disabledSpec directory

--- src/Sylius/Bundle/ApiBundle/disabledSpec/DataProvider/PaymentRequestUpdateDataProviderSpec.php
+++ src/Sylius/Bundle/ApiBundle/disabledSpec/DataProvider/PaymentRequestUpdateDataProviderSpec.php
@@ -11,7 +11,7 @@
 
 declare(strict_types=1);
 
-namespace spec\Sylius\Bundle\ApiBundle\DataProvider;
+namespace Sylius\Bundle\ApiBundle\disabledSpec\DataProvider;
 
 use PhpSpec\ObjectBehavior;
 use Sylius\Bundle\ApiBundle\Checker\UpdatePaymentRequestEligibilityCheckerInterface;
