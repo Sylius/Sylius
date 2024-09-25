619bb4cfa69911ee52cb0ecea6021b86f3f4394b
Wojdylak <karolwojdyla37@gmail.com>
2024-09-25 13:57:11 +0200

    Fix AddPaymentRequest

--- src/Sylius/Bundle/ApiBundle/Command/Payment/AddPaymentRequest.php
+++ src/Sylius/Bundle/ApiBundle/Command/Payment/AddPaymentRequest.php
@@ -18,7 +18,7 @@ use Sylius\Bundle\ApiBundle\Command\IriToIdentifierConversionAwareInterface;
 class AddPaymentRequest implements IriToIdentifierConversionAwareInterface
 {
     public function __construct(
-        public readonly mixed $paymentId,
+        public readonly int|string $paymentId,
         public readonly string $paymentMethodCode,
         public readonly ?string $action = null,
         public readonly mixed $payload = null,
