688009eb146c08af4c0c997ccb34bb5faf5b9423
Grzegorz Sadowski <sadowskigp@gmail.com>
2024-10-02 08:47:51 +0200

    Move Payum-related exception to PayumBundle

--- src/Sylius/Bundle/PayumBundle/Exception/NonExistingPayumTokenException.php
+++ src/Sylius/Bundle/PayumBundle/Exception/NonExistingPayumTokenException.php
@@ -11,7 +11,7 @@
 
 declare(strict_types=1);
 
-namespace Sylius\Component\Payment\Exception;
+namespace Sylius\Bundle\PayumBundle\Exception;
 
 /** @experimental */
 final class NonExistingPayumTokenException extends \RuntimeException
