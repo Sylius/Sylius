0a1160ea7f283b8f97923f808fafb06b2390e72a
Francis Hilaire <Prometee@users.noreply.github.com>
2024-10-09 17:24:19 +0200

    Array definition

--- src/Sylius/Bundle/PaymentBundle/Wrapper/SymfonyRequestWrapperInterface.php
+++ src/Sylius/Bundle/PaymentBundle/Wrapper/SymfonyRequestWrapperInterface.php
@@ -6,6 +6,21 @@ namespace Sylius\Bundle\PaymentBundle\Wrapper;
 
 use Symfony\Component\HttpFoundation\Request;
 
-interface SymfonyRequestWrapperInterface {
+interface SymfonyRequestWrapperInterface
+{
+    /**
+     * @param Request $request
+     * @return array{
+     *      'http_request'?: array{
+     *          'uri'?: string,
+     *          'method'?: string,
+     *          'query'?: array<string, array<int, bool|float|int|string>|bool|float|int|string>,
+     *          'request'?: array<string, array<int, bool|float|int|string>|bool|float|int|string>,
+     *          'headers'?: array<string, array<int, string|null>>,
+     *          'content'?: string,
+     *          'client_ip'?: string,
+     *      },
+     *  }
+     */
     public function wrap(Request $request): array;
 }
