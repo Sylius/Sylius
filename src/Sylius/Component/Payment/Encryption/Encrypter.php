c559b258edff9c46bad593711a3b0b1fb6bdc8ed
Jan Goralski <jan.wojciech.goralski@gmail.com>
2024-10-11 14:03:09 +0200

    Create an encryption service

---
+++ src/Sylius/Component/Payment/Encryption/Encrypter.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Component\Payment\Encryption;
+
+use ParagonIE\Halite\Alerts\HaliteAlert;
+use ParagonIE\Halite\KeyFactory;
+use ParagonIE\Halite\Symmetric\Crypto;
+use ParagonIE\Halite\Symmetric\EncryptionKey;
+use ParagonIE\HiddenString\HiddenString;
+use Sylius\Component\Payment\Encryption\Exception\EncryptionException;
+
+final class Encrypter implements EncrypterInterface
+{
+    private const ENCRYPTION_SUFFIX = '#ENCRYPTED';
+
+    private const ENCRYPTION_SUFFIX_LENGTH = 10;
+
+    private ?EncryptionKey $key = null;
+
+    public function __construct(
+        #[\SensitiveParameter]
+        private readonly string $keyPhrase,
+        #[\SensitiveParameter]
+        private readonly string $keySalt,
+    ) {
+    }
+
+    public function encrypt(string $data): string
+    {
+        try {
+            return Crypto::encrypt(new HiddenString($data), $this->getKey()) . self::ENCRYPTION_SUFFIX;
+        } catch (\TypeError|\SodiumException|HaliteAlert $exception) {
+            throw EncryptionException::cannotEncrypt($exception);
+        }
+    }
+
+    public function decrypt(string $data): string
+    {
+        if (!str_ends_with($data, self::ENCRYPTION_SUFFIX)) {
+            return $data;
+        }
+
+        try {
+            $data = substr($data, 0, -self::ENCRYPTION_SUFFIX_LENGTH);
+
+            return Crypto::decrypt($data, $this->getKey())->getString();
+        } catch (\TypeError|\SodiumException|HaliteAlert $exception) {
+            throw EncryptionException::cannotDecrypt($exception);
+        }
+    }
+
+    private function getKey(): EncryptionKey
+    {
+        if (null === $this->key) {
+            $this->key = KeyFactory::deriveEncryptionKey(new HiddenString($this->keyPhrase), $this->keySalt);
+        }
+
+        return $this->key;
+    }
+}
