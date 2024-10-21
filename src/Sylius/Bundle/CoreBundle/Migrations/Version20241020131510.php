8a34c92e80f411a23cceb726f2677ebface5232d
Kamil Grygierzec <kam.grygierzec@gmail.com>
2024-10-21 13:20:51 +0200

    Resolve issues with images positions while upgrading the sylius database to 2.0

--- src/Sylius/Bundle/CoreBundle/Migrations/Version20241020131510.php
+++ src/Sylius/Bundle/CoreBundle/Migrations/Version20241020131510.php
@@ -20,12 +20,28 @@ final class Version20241020131510 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Add position field to ProductImage entity';
+        return 'Add position field to ProductImage entity and set positions for existing images.';
     }
 
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE sylius_product_image ADD position INT NOT NULL');
+
+        $this->addSql('SET @row_number = 0;');
+
+        $this->addSql('CREATE TEMPORARY TABLE IF NOT EXISTS image_count_per_product AS
+                        SELECT sylius_product_image.owner_id, sylius_product_image.id AS image_id
+                        FROM sylius_product_image
+                        ORDER BY sylius_product_image.owner_id, sylius_product_image.id');
+
+        $this->addSql('UPDATE sylius_product_image
+                        JOIN (
+                            SELECT owner_id, image_id, (@row_number := IF(@current_product = owner_id, @row_number + 1, 0)) AS position, @current_product := owner_id
+                            FROM image_count_per_product
+                        ) AS ranked_images ON ranked_images.image_id = sylius_product_image.id
+                        SET sylius_product_image.position = ranked_images.position');
+
+        $this->addSql('DROP TEMPORARY TABLE image_count_per_product;');
     }
 
     public function down(Schema $schema): void
