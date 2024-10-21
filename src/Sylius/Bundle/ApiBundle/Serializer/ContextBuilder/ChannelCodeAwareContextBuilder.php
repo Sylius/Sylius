b621a79950def5477b1466c04d2fb6636e73cf51
Wojdylak <karolwojdyla37@gmail.com>
2024-10-21 14:23:38 +0200

    [ApiBundle] Use ApiPlatform\State\SerializerContextBuilderInterface

--- src/Sylius/Bundle/ApiBundle/Serializer/ContextBuilder/ChannelCodeAwareContextBuilder.php
+++ src/Sylius/Bundle/ApiBundle/Serializer/ContextBuilder/ChannelCodeAwareContextBuilder.php
@@ -13,7 +13,7 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\ApiBundle\Serializer\ContextBuilder;
 
-use ApiPlatform\Serializer\SerializerContextBuilderInterface;
+use ApiPlatform\State\SerializerContextBuilderInterface;
 use Sylius\Component\Channel\Context\ChannelContextInterface;
 use Symfony\Component\HttpFoundation\Request;
 
