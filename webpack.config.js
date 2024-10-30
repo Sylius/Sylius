a055b96ff4aeaf448e3711bb5ae1e0063fecae41
Michał Pysiak <mpysiak@gmail.com>
2024-10-30 10:52:04 +0100

    WIP

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,15 @@ const SyliusAdmin = require('@sylius-ui/admin');
 const SyliusShop = require('@sylius-ui/shop');
 
 const adminConfig = SyliusAdmin.getWebpackConfig(path.resolve(__dirname));
+
+Encore
+  .setOutputPath('public/build/shop/')
+  .copyFiles({
+    from: 'src/Sylius/Bundle/UiBundle/Resources/private/img/',
+    to: 'images/[name].[ext]',
+    pattern: /\.(png|jpg|jpeg)$/
+  });
+
 const shopConfig = SyliusShop.getWebpackConfig(path.resolve(__dirname));
 
 module.exports = [adminConfig, shopConfig];
