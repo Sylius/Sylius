a67bb9aba6737f2785cc42563dfdf38a23c1ede7
Rafikooo <thecrazyrafik@gmail.com>
2025-03-26 11:45:23 +0100

    WIP

--- src/Sylius/Behat/Context/Ui/Shop/CartContext.php
+++ src/Sylius/Behat/Context/Ui/Shop/CartContext.php
@@ -305,6 +305,7 @@ final readonly class CartContext implements Context
      * @Given /^I have (product "[^"]+") added to the cart$/
      * @Given /^I have the (product "[^"]+") added to the cart$/
      * @Given he added product :product to the cart
+     * @When /^the customer adds ("[^"]+" product) to the cart$/
      * @When /^I add ("[^"]+" product) to the (cart)$/
      * @When /^the visitor adds ("[^"]+" product) to the cart$/
      * @When I add product :product to the cart
