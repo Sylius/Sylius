63793c1c4f8e65d4f13014d0de26115d4fd15c23
Loïc Frémont <lc.fremont@gmail.com>
2025-05-13 11:34:49 +0200

    Allow ORM 3 on Promotion bundle

--- src/Sylius/Bundle/PromotionBundle/test/src/Entity/PromotionSubject.php
+++ src/Sylius/Bundle/PromotionBundle/test/src/Entity/PromotionSubject.php
@@ -11,21 +11,33 @@
 
 declare(strict_types=1);
 
-namespace AppBundle\Entity;
+namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Promotion\Model\PromotionInterface;
 use Sylius\Component\Promotion\Model\PromotionSubjectInterface;
 use Sylius\Resource\Model\ResourceInterface;
 
+#[ORM\Entity]
+#[ORM\Table(name: 'app_promotion_subject')]
 class PromotionSubject implements ResourceInterface, PromotionSubjectInterface
 {
-    private int $id;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
 
     /** @var Collection<array-key, PromotionInterface> */
-    protected $promotions;
+    private Collection $promotions;
 
-    public function getId(): int
+    public function __construct()
+    {
+        $this->promotions = new ArrayCollection();
+    }
+
+    public function getId(): ?int
     {
         return $this->id;
     }
