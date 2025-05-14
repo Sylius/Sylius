248c512467e1edee1006a5da24ceb4871c465f8a
Loïc Frémont <lc.fremont@gmail.com>
2025-05-14 09:41:43 +0200

    Allow ORM 3 on Core bundle

--- src/Sylius/Bundle/ApiBundle/Tests/Application/src/Entity/Foo.php
+++ src/Sylius/Bundle/ApiBundle/Tests/Application/src/Entity/Foo.php
@@ -13,54 +13,65 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\ApiBundle\Application\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\AdminUserInterface;
 
+#[ORM\Entity]
+#[ORM\Table(name: 'foo')]
 class Foo
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
+    #[ORM\Column(type: 'string', length: 255)]
     private ?string $name = null;
 
+    #[ORM\ManyToOne(targetEntity: AdminUserInterface::class)]
+    #[ORM\JoinColumn(name: 'owner_id')]
     private ?AdminUserInterface $owner = null;
 
+    #[ORM\ManyToOne(targetEntity: FooSyliusResource::class)]
+    #[ORM\JoinColumn(name: 'foo_sylius_resource_id')]
     private ?FooSyliusResource $fooSyliusResource = null;
 
-    public function getId(): int
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function setId(int $id): void
+    public function setId(?int $id): void
     {
         $this->id = $id;
     }
 
-    public function getName(): string
+    public function getName(): ?string
     {
         return $this->name;
     }
 
-    public function setName(string $name): void
+    public function setName(?string $name): void
     {
         $this->name = $name;
     }
 
-    public function getOwner(): AdminUserInterface
+    public function getOwner(): ?AdminUserInterface
     {
         return $this->owner;
     }
 
-    public function setOwner(AdminUserInterface $owner): void
+    public function setOwner(?AdminUserInterface $owner): void
     {
         $this->owner = $owner;
     }
 
-    public function getFooSyliusResource(): FooSyliusResource
+    public function getFooSyliusResource(): ?FooSyliusResource
     {
         return $this->fooSyliusResource;
     }
 
-    public function setFooSyliusResource(FooSyliusResource $fooSyliusResource): void
+    public function setFooSyliusResource(?FooSyliusResource $fooSyliusResource): void
     {
         $this->fooSyliusResource = $fooSyliusResource;
     }
--- src/Sylius/Bundle/ApiBundle/Tests/Application/src/Entity/FooSyliusResource.php
+++ src/Sylius/Bundle/ApiBundle/Tests/Application/src/Entity/FooSyliusResource.php
@@ -13,30 +13,37 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\ApiBundle\Application\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 use Sylius\Resource\Model\ResourceInterface;
 
+#[ORM\Entity]
+#[ORM\Table(name: 'foo_sylius_resource')]
 class FooSyliusResource implements ResourceInterface
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
     private ?int $id = null;
 
+    #[ORM\Column(type: 'string', length: 255)]
     private ?string $name = null;
 
-    public function getId()
+    public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function setId(int $id): void
+    public function setId(?int $id): void
     {
         $this->id = $id;
     }
 
-    public function getName(): string
+    public function getName(): ?string
     {
         return $this->name;
     }
 
-    public function setName(string $name): void
+    public function setName(?string $name): void
     {
         $this->name = $name;
     }
