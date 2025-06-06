4c86065281fd3a027ffde7bf028625e326e324f4
mamazu <14860264+mamazu@users.noreply.github.com>
2025-06-06 20:00:51 +0200

    Fixing stuff

--- src/Sylius/Bundle/CoreBundle/Grid/Provider/OverrideGridProvider.php
+++ src/Sylius/Bundle/CoreBundle/Grid/Provider/OverrideGridProvider.php
@@ -14,30 +14,28 @@ declare(strict_types=1);
 namespace Sylius\Bundle\CoreBundle\Grid\Provider;
 
 use Sylius\Component\Grid\Definition\Grid;
-use Sylius\Component\Grid\Provider\GridProviderInterface;
-use Sylius\Component\Grid\Provider\ChainProvider;
-use Sylius\Component\Grid\Provider\ArrayGridProvider;
 use Sylius\Component\Grid\Exception\UndefinedGridException;
+use Sylius\Component\Grid\Provider\GridProviderInterface;
 
 /**
-* From 3.0 forward Sylius does not support the chain provider and only use Service grids. This is a BC layer for
-* allowing users to migrate on a per grid basis to the new configuration.
-*
-* @deprecated will be removed in 3.0
-*/
+ * From 3.0 forward Sylius does not support the chain provider and only use Service grids. This is a BC layer for
+ * allowing users to migrate on a per grid basis to the new configuration.
+ *
+ * @deprecated will be removed in 3.0
+ */
 final class OverrideGridProvider implements GridProviderInterface
 {
     /** @param array<string, mixed> $configuration */
     public function __construct(
         private array $configuration,
-        private ChainProvider $chainGridProvider,
-        private ArrayGridProvider $arrayGridProvider,
+        private GridProviderInterface $chainGridProvider,
+        private GridProviderInterface $arrayGridProvider,
     ) {
     }
 
     public function get(string $code): Grid
     {
-        $useYamlGrid = $this->configuration['grids'][$code]['use_legacy_yaml_config'] ?? true;
+        $useYamlGrid = $this->configuration['use_legacy_yaml_config'] ?? $this->configuration['grids'][$code]['use_legacy_yaml_config'] ?? true;
         if ($useYamlGrid) {
             try {
                 return $this->arrayGridProvider->get($code);
