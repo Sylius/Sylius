c1a5b9aa187d97a610199375d004c6add7794faa
Loïc Frémont <lc.fremont@gmail.com>
2025-07-16 20:03:43 +0200

    Apply suggestions from code review
    Co-authored-by: Jan Góralski <jan.wojciech.goralski@gmail.com>

--- src/Sylius/Bundle/CoreBundle/tests/Routing/RequestContextTest.php
+++ src/Sylius/Bundle/CoreBundle/tests/Routing/RequestContextTest.php
@@ -1,6 +1,17 @@
 <?php
 
-namespace Routing;
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Sylius\Bundle\CoreBundle\Routing;
 
 use PHPUnit\Framework\TestCase;
 use Sylius\Bundle\CoreBundle\Routing\RequestContext;
