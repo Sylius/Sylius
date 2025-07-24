e056ece8ff24b7d49193e761f19ad9f8050136d0
Loïc Frémont <lc.fremont@gmail.com>
2025-07-24 08:44:34 +0200

    [New routing] Admin product association types crud

---
+++ src/Sylius/Bundle/AdminBundle/Resources/config/app/sylius/resources/product_association_type.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+use Sylius\Bundle\AdminBundle\Form\Type\ProductAssociationTypeType;
+use Sylius\Resource\Metadata\BulkDelete;
+use Sylius\Resource\Metadata\Create;
+use Sylius\Resource\Metadata\Delete;
+use Sylius\Resource\Metadata\Index;
+use Sylius\Resource\Metadata\Operations;
+use Sylius\Resource\Metadata\ResourceMetadata;
+use Sylius\Resource\Metadata\Update;
+
+return (new ResourceMetadata())
+    ->withRoutePrefix('/%sylius_admin.path_name%')
+    ->withClass('%sylius.model.product_association_type.class%')
+    ->withFormType(ProductAssociationTypeType::class)
+    ->withSection('admin')
+    ->withTemplatesDir('@SyliusAdmin/shared/crud')
+    ->withRouteCondition("not context.isSyliusRoutingBcLayerEnabled('admin_product_association_type')")
+    ->withOperations(operations: new Operations(operations: [
+        new Create(
+            routeName: '_sylius_admin_product_association_type_create',
+            redirectToRoute: 'sylius_admin_product_association_type_update',
+        ),
+        new Update(
+            routeName: '_sylius_admin_product_association_type_update',
+            redirectToRoute: 'sylius_admin_product_association_type_update',
+        ),
+        new Delete(
+            routeName: '_sylius_admin_product_association_type_delete',
+            redirectToRoute: 'sylius_admin_product_association_type_index',
+        ),
+        new BulkDelete(
+            routeName: '_sylius_admin_product_association_type_bulk_delete',
+            redirectToRoute: 'sylius_admin_product_association_type_index',
+        ),
+        new Index(
+            routeName: '_sylius_admin_product_association_type_index',
+            grid: 'sylius_admin_product_association_type',
+        ),
+    ]))
+;
