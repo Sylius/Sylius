2f12b9c29f899ccfce3230c3780ada32093b1bd5
mamazu <14860264+mamazu@users.noreply.github.com>
2025-11-10 16:08:17 +0100

    Currency grid

---
+++ src/Sylius/Bundle/AdminBundle/Grid/CurrencyGrid.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\AdminBundle\Grid;
+
+use Sylius\Bundle\GridBundle\Builder\Action\CreateAction;
+use Sylius\Bundle\GridBundle\Builder\ActionGroup\MainActionGroup;
+use Sylius\Bundle\GridBundle\Builder\Field\StringField;
+use Sylius\Bundle\GridBundle\Builder\Field\TwigField;
+use Sylius\Bundle\GridBundle\Builder\Filter\Filter;
+use Sylius\Bundle\GridBundle\Builder\GridBuilderInterface;
+use Sylius\Bundle\GridBundle\Grid\AbstractGrid;
+use Sylius\Bundle\GridBundle\Grid\ResourceAwareGridInterface;
+
+final class CurrencyGrid extends AbstractGrid implements ResourceAwareGridInterface
+{
+    public function __construct(
+        private string $resourceClass,
+    ) {
+    }
+
+    public static function getName(): string
+    {
+        return 'sylius_admin_currency';
+    }
+
+    public function buildGrid(GridBuilderInterface $gridBuilder): void
+    {
+        $gridBuilder
+            ->setDriverOption('class', $this->resourceClass)
+            ->setLimits([10, 25, 50])
+            ->addOrderBy('code', 'asc')
+            ->addField(
+                TwigField::create('code', '@SyliusAdmin/shared/grid/field/code.html.twig')
+                    ->setLabel('sylius.ui.code')
+                    ->setSortable(true)
+                    ->withOptions([
+                        'vars' => [
+                            'th_class' => 'w-1 text-center',
+                        ],
+                    ]),
+            )
+            ->addField(
+                StringField::create('name')
+                    ->setLabel('sylius.ui.name'),
+            )
+            ->addFilter(
+                Filter::create('code', 'string')
+                    ->setLabel('sylius.ui.code'),
+            )
+            ->addActionGroup(
+                MainActionGroup::create(
+                    CreateAction::create(),
+                ),
+            )
+        ;
+    }
+
+    public function getResourceClass(): string
+    {
+        return $this->resourceClass;
+    }
+}
