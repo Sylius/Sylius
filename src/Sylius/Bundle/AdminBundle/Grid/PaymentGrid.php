175d7ab9cac9e8b0d7a12ca499e6f589ef076483
mamazu <14860264+mamazu@users.noreply.github.com>
2025-11-11 16:22:59 +0100

    Migrating PaymentGrid

---
+++ src/Sylius/Bundle/AdminBundle/Grid/PaymentGrid.php
@@ -0,0 +1,150 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Bundle\AdminBundle\Grid;
+
+use Sylius\Bundle\GridBundle\Builder\Action\Action;
+use Sylius\Bundle\GridBundle\Builder\ActionGroup\ItemActionGroup;
+use Sylius\Bundle\GridBundle\Builder\Field\TwigField;
+use Sylius\Bundle\GridBundle\Builder\Filter\Filter;
+use Sylius\Bundle\GridBundle\Builder\GridBuilderInterface;
+use Sylius\Bundle\GridBundle\Grid\AbstractGrid;
+use Sylius\Bundle\GridBundle\Grid\ResourceAwareGridInterface;
+
+final class PaymentGrid extends AbstractGrid implements ResourceAwareGridInterface
+{
+    public function __construct(
+        private readonly string $resourceClass,
+        private readonly string $channelClass,
+    ) {
+    }
+
+    public static function getName(): string
+    {
+        return 'sylius_admin_payment';
+    }
+
+    public function buildGrid(GridBuilderInterface $gridBuilder): void
+    {
+        $gridBuilder
+            ->setDriverOption('class', $this->resourceClass)
+            ->setRepositoryMethod('createListQueryBuilder')
+            ->addOrderBy('createdAt', 'desc')
+            ->setLimits([10, 25, 50])
+            ->addField(
+                TwigField::create('createdAt', '@SyliusAdmin/shared/grid/field/date.html.twig')
+                    ->setLabel('sylius.ui.date')
+                    ->setSortable(true)
+                    ->withOptions([
+                        'vars' => [
+                            'th_class' => 'w-1 text-center',
+                        ],
+                    ]),
+            )
+            ->addField(
+                TwigField::create('number', '@SyliusAdmin/shared/grid/field/order_number.html.twig')
+                    ->setLabel('sylius.ui.order')
+                    ->setPath('order')
+                    ->withOptions([
+                        'vars' => [
+                            'th_class' => 'w-1',
+                        ],
+                    ]),
+            )
+            ->addField(
+                TwigField::create('customer', '@SyliusAdmin/shared/grid/field/customer.html.twig')
+                    ->setLabel('sylius.ui.customer')
+                    ->setPath('order.customer')
+                    ->withOptions([
+                        'vars' => [
+                            'th_class' => 'w-100',
+                        ],
+                    ]),
+            )
+            ->addField(
+                TwigField::create('channel', '@SyliusAdmin/shared/grid/field/channel.html.twig')
+                    ->setLabel('sylius.ui.channel')
+                    ->setPath('order.channel'),
+            )
+            ->addField(
+                TwigField::create('state', '@SyliusAdmin/shared/grid/field/payment_state.html.twig')
+                    ->setLabel('sylius.ui.state')
+                    ->withOptions([
+                        'vars' => [
+                            'th_class' => 'w-1 text-center',
+                            'td_class' => 'text-center',
+                        ],
+                    ]),
+            )
+            ->addFilter(
+                Filter::create('state', 'select')
+                    ->setLabel('sylius.ui.state')
+                    ->setFormOptions([
+                        'choices' => [
+                            'sylius.ui.cancelled' => 'cancelled',
+                            'sylius.ui.completed' => 'completed',
+                            'sylius.ui.failed' => 'failed',
+                            'sylius.ui.new' => 'new',
+                            'sylius.ui.processing' => 'processing',
+                            'sylius.ui.refunded' => 'refunded',
+                        ],
+                    ]),
+            )
+            ->addFilter(
+                Filter::create('channel', 'entity')
+                    ->setLabel('sylius.ui.channel')
+                    ->setOptions([
+                        'fields' => [
+                            'order.channel',
+                        ],
+                    ])
+                    ->setFormOptions([
+                        'class' => $this->channelClass,
+                    ]),
+            )
+            ->addActionGroup(
+                ItemActionGroup::create(
+                    Action::create('list_payment_requests', 'show')
+                        ->setIcon('tabler:list-letters')
+                        ->setLabel('sylius.ui.list_payment_requests')
+                        ->setOptions([
+                            'link' => [
+                                'route' => 'sylius_admin_payment_request_index',
+                                'parameters' => [
+                                    'paymentId' => 'resource.id',
+                                ],
+                            ],
+                        ]),
+                    Action::create('complete', 'apply_transition')
+                        ->setLabel('sylius.ui.complete')
+                        ->setIcon('tabler:credit-card-pay')
+                        ->setOptions([
+                            'link' => [
+                                'route' => 'sylius_admin_payment_complete',
+                                'parameters' => [
+                                    'id' => 'resource.id',
+                                ],
+                            ],
+                            'class' => 'teal',
+                            'transition' => 'complete',
+                            'graph' => 'sylius_payment',
+                        ]),
+                ),
+            );
+    }
+
+    public function getResourceClass(): string
+    {
+        return $this->resourceClass;
+    }
+}
