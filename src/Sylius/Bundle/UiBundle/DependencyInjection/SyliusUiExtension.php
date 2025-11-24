00a3a55141734c71d0c3bfc54bc98eb253857010
Rafikooo <thecrazyrafik@gmail.com>
2025-11-24 16:57:21 +0100

    [UiBundle] Move UX Icons config to Extension with correct paths

--- src/Sylius/Bundle/UiBundle/DependencyInjection/SyliusUiExtension.php
+++ src/Sylius/Bundle/UiBundle/DependencyInjection/SyliusUiExtension.php
@@ -15,10 +15,11 @@ namespace Sylius\Bundle\UiBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-final class SyliusUiExtension extends Extension
+final class SyliusUiExtension extends Extension implements PrependExtensionInterface
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
@@ -30,5 +31,32 @@ final class SyliusUiExtension extends Extension
         $container->setParameter('sylius_ui.twig_ux.anonymous_component_template_prefixes', $config['twig_ux']['anonymous_component_template_prefixes'] ?? []);
         $container->setParameter('sylius_ui.twig_ux.live_component_tags', $config['twig_ux']['live_component_tags'] ?? []);
         $container->setParameter('sylius_ui.twig_ux.component_default_template', $config['twig_ux']['component_default_template']);
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $container->prependExtensionConfig('ux_icons', $this->getUxIconsConfiguration());
+    }
+
+    /** @return array<string, mixed> */
+    private function getUxIconsConfiguration(): array
+    {
+        return [
+            'icon_sets' => [
+                'tabler' => [
+                    'path' => __DIR__ . '/../Resources/assets/icons/tabler',
+                ],
+                'flagpack' => [
+                    'path' => __DIR__ . '/../Resources/assets/icons/flagpack',
+                ],
+            ],
+            'iconify' => [
+                'enabled' => true,
+            ],
+            'default_icon_attributes' => [
+                'class' => 'icon',
+            ],
+            'ignore_not_found' => true,
+        ];
     }
 }
