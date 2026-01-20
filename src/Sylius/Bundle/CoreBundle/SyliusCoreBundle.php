8cd3f79a255d54ffa9f90591edcbc20a8859bc1d
Grzegorz Sadowski <sadowskigp@gmail.com>
2026-01-20 12:49:05 +0100

    Change application's version to v2.1.11

--- src/Sylius/Bundle/CoreBundle/SyliusCoreBundle.php
+++ src/Sylius/Bundle/CoreBundle/SyliusCoreBundle.php
@@ -40,7 +40,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class SyliusCoreBundle extends AbstractResourceBundle
 {
-    public const VERSION = '2.1.11-DEV';
+    public const VERSION = '2.1.11';
 
     public const VERSION_ID = '20111';
 
@@ -50,7 +50,7 @@ final class SyliusCoreBundle extends AbstractResourceBundle
 
     public const RELEASE_VERSION = '11';
 
-    public const EXTRA_VERSION = 'DEV';
+    public const EXTRA_VERSION = '';
 
     /** @return string[] */
     public function getSupportedDrivers(): array
