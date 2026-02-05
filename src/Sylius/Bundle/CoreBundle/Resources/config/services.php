2c8f766062d4de50db0985b589f3b9a175f47854
Loïc Frémont <lc.fremont@gmail.com>
2026-02-05 17:24:02 +0100

    [New routing] Shop account address book

--- src/Sylius/Bundle/CoreBundle/Resources/config/services.php
+++ src/Sylius/Bundle/CoreBundle/Resources/config/services.php
@@ -496,6 +496,7 @@ return static function (ContainerConfigurator $container) {
         ->args([
             service('sylius.context.shopper'),
         ])
+        ->tag('sylius.form_variables')
         ->tag('sylius.metadata_variables')
         ->tag('sylius.resource_factory_variables')
         ->tag('sylius.repository_variables')
