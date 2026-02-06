dfd560c6570c390317bb688bacb068c8ffd8cfcb
TheMilek <kam.grygierzec@gmail.com>
2026-02-06 15:35:43 +0100

    [OrderBundle] fix formatting after xml to php migration

--- src/Sylius/Bundle/OrderBundle/Resources/config/services/form.php
+++ src/Sylius/Bundle/OrderBundle/Resources/config/services/form.php
@@ -21,46 +21,56 @@ use Sylius\Bundle\OrderBundle\Form\Type\OrderType;
 use Symfony\Component\Form\Extension\Core\DataMapper\DataMapper;
 
 return static function (ContainerConfigurator $container) {
-    $services = $container->services();
     $parameters = $container->parameters();
+    $services = $container->services();
     $parameters->set('sylius.form.type.order.validation_groups', ['sylius']);
     $parameters->set('sylius.form.type.order_item.validation_groups', ['sylius']);
     $parameters->set('sylius.form.type.cart.validation_groups', ['sylius']);
     $parameters->set('sylius.form.type.cart_item.validation_groups', ['sylius']);
 
-    $services->set('sylius.form.data_mapper.order_item_quantity', OrderItemQuantityDataMapper::class)
+    $services
+        ->set('sylius.form.data_mapper.order_item_quantity', OrderItemQuantityDataMapper::class)
         ->args([
             service('sylius.modifier.order_item_quantity'),
             inline_service(DataMapper::class),
-        ]);
+        ])
+    ;
 
-    $services->set('sylius.form.type.order', OrderType::class)
+    $services
+        ->set('sylius.form.type.order', OrderType::class)
         ->args([
             '%sylius.model.order.class%',
             '%sylius.form.type.order.validation_groups%',
         ])
-        ->tag('form.type');
+        ->tag('form.type')
+    ;
 
-    $services->set('sylius.form.type.order_item', OrderItemType::class)
+    $services
+        ->set('sylius.form.type.order_item', OrderItemType::class)
         ->args([
             '%sylius.model.order_item.class%',
             '%sylius.form.type.order_item.validation_groups%',
             service('sylius.form.data_mapper.order_item_quantity'),
         ])
-        ->tag('form.type');
+        ->tag('form.type')
+    ;
 
-    $services->set('sylius.form.type.cart', CartType::class)
+    $services
+        ->set('sylius.form.type.cart', CartType::class)
         ->args([
             '%sylius.model.order.class%',
             '%sylius.form.type.cart.validation_groups%',
         ])
-        ->tag('form.type');
+        ->tag('form.type')
+    ;
 
-    $services->set('sylius.form.type.cart_item', CartItemType::class)
+    $services
+        ->set('sylius.form.type.cart_item', CartItemType::class)
         ->args([
             '%sylius.model.order_item.class%',
             '%sylius.form.type.cart_item.validation_groups%',
             service('sylius.form.data_mapper.order_item_quantity'),
         ])
-        ->tag('form.type');
+        ->tag('form.type')
+    ;
 };
