2f6116e3de77699c11bdc5be044511afd68b2f6b
TheMilek <kam.grygierzec@gmail.com>
2026-02-06 11:51:01 +0100

    Fixes

--- src/Sylius/Bundle/ChannelBundle/Resources/config/services/integrations/debug.php
+++ src/Sylius/Bundle/ChannelBundle/Resources/config/services/integrations/debug.php
@@ -1,21 +1,35 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-return static function(ContainerConfigurator $container) {
+use Sylius\Bundle\ChannelBundle\Context\FakeChannel\FakeChannelCodeProvider;
+use Sylius\Bundle\ChannelBundle\Context\FakeChannel\FakeChannelCodeProviderInterface;
+use Sylius\Bundle\ChannelBundle\Context\FakeChannel\FakeChannelContext;
+use Sylius\Bundle\ChannelBundle\Context\FakeChannel\FakeChannelPersister;
+
+return static function (ContainerConfigurator $container) {
     $services = $container->services();
 
-    $services->set('sylius.context.channel.fake_channel.persister', 'Sylius\Bundle\ChannelBundle\Context\FakeChannel\FakeChannelPersister')
+    $services->set('sylius.context.channel.fake_channel.persister', FakeChannelPersister::class)
         ->args([service('sylius.context.channel.fake_channel.code_provider')])
         ->tag('kernel.event_listener', ['event' => 'kernel.response', 'method' => 'onKernelResponse', 'priority' => -8192]);
 
-    $services->set('sylius.context.channel.fake_channel.code_provider', 'Sylius\Bundle\ChannelBundle\Context\FakeChannel\FakeChannelCodeProvider')
-        ->private();
+    $services->set('sylius.context.channel.fake_channel.code_provider', FakeChannelCodeProvider::class);
 
-    $services->alias('Sylius\Bundle\ChannelBundle\Context\FakeChannel\FakeChannelCodeProviderInterface', 'sylius.context.channel.fake_channel.code_provider');
+    $services->alias(FakeChannelCodeProviderInterface::class, 'sylius.context.channel.fake_channel.code_provider');
 
-    $services->set('sylius.context.channel.fake_channel.context', 'Sylius\Bundle\ChannelBundle\Context\FakeChannel\FakeChannelContext')
-        ->private()
+    $services->set('sylius.context.channel.fake_channel.context', FakeChannelContext::class)
         ->args([
             service('sylius.context.channel.fake_channel.code_provider'),
             service('sylius.repository.channel'),
--- src/Sylius/Bundle/ChannelBundle/Resources/config/services/integrations/doctrine/orm.php
+++ src/Sylius/Bundle/ChannelBundle/Resources/config/services/integrations/doctrine/orm.php
@@ -1,8 +1,21 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-return static function(ContainerConfigurator $container) {
+use Sylius\Bundle\ChannelBundle\Doctrine\ORM\ChannelRepository;
+
+return static function (ContainerConfigurator $container) {
     $parameters = $container->parameters();
-    $parameters->set('sylius.repository.channel.class', 'Sylius\Bundle\ChannelBundle\Doctrine\ORM\ChannelRepository');
+    $parameters->set('sylius.repository.channel.class', ChannelRepository::class);
 };
