da505dba11a71ac09c3082abffb387be4b9525d5
TheMilek <kam.grygierzec@gmail.com>
2026-02-06 15:32:51 +0100

    [AttributeBundle] fix formatting after xml to php migration

--- src/Sylius/Bundle/AttributeBundle/Resources/config/services.php
+++ src/Sylius/Bundle/AttributeBundle/Resources/config/services.php
@@ -24,40 +24,55 @@ use Sylius\Component\Attribute\AttributeType\AttributeTypeInterface;
 use Sylius\Component\Registry\ServiceRegistry;
 
 return static function (ContainerConfigurator $container) {
-    $services = $container->services();
-    $parameters = $container->parameters();
     $container->import('services/attribute_types.php');
+
+    $parameters = $container->parameters();
+    $services = $container->services();
 
     $parameters->set('sylius.model.attribute.interface', AttributeTypeInterface::class);
 
-    $services->set('sylius.doctrine.orm.event_subscriber.load_metadata.attribute', LoadMetadataSubscriber::class)
-        ->public()
+    $services
+        ->set('sylius.doctrine.orm.event_subscriber.load_metadata.attribute', LoadMetadataSubscriber::class)
         ->args(['%sylius.attribute.subjects%'])
-        ->tag('doctrine.event_subscriber');
+        ->public()
+        ->tag('doctrine.event_subscriber')
+    ;
 
-    $services->set('sylius.registry.attribute_type', ServiceRegistry::class)
+    $services
+        ->set('sylius.registry.attribute_type', ServiceRegistry::class)
         ->args([
             '%sylius.model.attribute.interface%',
             'attribute type',
-        ]);
+        ])
+    ;
 
     $services->set('sylius.form_registry.attribute_type', FormTypeRegistry::class);
 
-    $services->set('sylius.form.type.attribute_type_choice', AttributeTypeChoiceType::class)
+    $services
+        ->set('sylius.form.type.attribute_type_choice', AttributeTypeChoiceType::class)
         ->args(['%sylius.attribute.attribute_types%'])
-        ->tag('form.type');
+        ->tag('form.type')
+    ;
 
-    $services->set('sylius.validator.attribute_type', AttributeTypeValidator::class)
+    $services
+        ->set('sylius.validator.attribute_type', AttributeTypeValidator::class)
         ->args([service('sylius.registry.attribute_type')])
-        ->tag('validator.constraint_validator', ['alias' => 'sylius_attribute_type_validator']);
+        ->tag('validator.constraint_validator', ['alias' => 'sylius_attribute_type_validator'])
+    ;
 
-    $services->set('sylius.validator.valid_attribute_value', ValidAttributeValueValidator::class)
+    $services
+        ->set('sylius.validator.valid_attribute_value', ValidAttributeValueValidator::class)
         ->args([service('sylius.registry.attribute_type')])
-        ->tag('validator.constraint_validator', ['alias' => 'sylius_valid_attribute_value_validator']);
+        ->tag('validator.constraint_validator', ['alias' => 'sylius_valid_attribute_value_validator'])
+    ;
 
-    $services->set('sylius.validator.valid_text_attribute_configuration', ValidTextAttributeConfigurationValidator::class)
-        ->tag('validator.constraint_validator', ['alias' => 'sylius_valid_text_attribute_validator']);
+    $services
+        ->set('sylius.validator.valid_text_attribute_configuration', ValidTextAttributeConfigurationValidator::class)
+        ->tag('validator.constraint_validator', ['alias' => 'sylius_valid_text_attribute_validator'])
+    ;
 
-    $services->set('sylius.validator.valid_select_attribute_configuration', ValidSelectAttributeConfigurationValidator::class)
-        ->tag('validator.constraint_validator', ['alias' => 'sylius_valid_select_attribute_validator']);
+    $services
+        ->set('sylius.validator.valid_select_attribute_configuration', ValidSelectAttributeConfigurationValidator::class)
+        ->tag('validator.constraint_validator', ['alias' => 'sylius_valid_select_attribute_validator'])
+    ;
 };
