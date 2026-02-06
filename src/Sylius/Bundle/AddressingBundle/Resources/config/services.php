7638c0fe2e46da6ab17e5751bd9b2d4357bf4b74
TheMilek <kam.grygierzec@gmail.com>
2026-02-06 10:43:41 +0100

    Minor fixes

--- src/Sylius/Bundle/AddressingBundle/Resources/config/services.php
+++ src/Sylius/Bundle/AddressingBundle/Resources/config/services.php
@@ -1,69 +1,90 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-return static function(ContainerConfigurator $container) {
+use Sylius\Bundle\AddressingBundle\Twig\CountryNameExtension;
+use Sylius\Bundle\AddressingBundle\Twig\ProvinceNamingExtension;
+use Sylius\Bundle\AddressingBundle\Validator\Constraints\ProvinceAddressConstraintValidator;
+use Sylius\Bundle\AddressingBundle\Validator\Constraints\UniqueProvinceCollectionValidator;
+use Sylius\Bundle\AddressingBundle\Validator\Constraints\ZoneCannotContainItselfValidator;
+use Sylius\Bundle\AddressingBundle\Validator\Constraints\ZoneMemberGroupValidator;
+use Sylius\Component\Addressing\Comparator\AddressComparator;
+use Sylius\Component\Addressing\Comparator\AddressComparatorInterface;
+use Sylius\Component\Addressing\Converter\CountryNameConverter;
+use Sylius\Component\Addressing\Converter\CountryNameConverterInterface;
+use Sylius\Component\Addressing\Factory\ZoneFactory;
+use Sylius\Component\Addressing\Factory\ZoneFactoryInterface;
+use Sylius\Component\Addressing\Matcher\ZoneMatcher;
+use Sylius\Component\Addressing\Matcher\ZoneMatcherInterface;
+use Sylius\Component\Addressing\Provider\ProvinceNamingProvider;
+use Sylius\Component\Addressing\Provider\ProvinceNamingProviderInterface;
+
+return static function (ContainerConfigurator $container) {
     $services = $container->services();
-    $container->import('services/checkers.php');
-    $container->import('services/form.php');
-    $container->import('services/listeners.php');
+    $container->import('services/*.php');
 
-
-    $services->set('sylius.custom_factory.zone', 'Sylius\Component\Addressing\Factory\ZoneFactory')
+    $services->set('sylius.custom_factory.zone', ZoneFactory::class)
         ->decorate('sylius.factory.zone', null, 256)
         ->args([
             service('sylius.custom_factory.zone.inner'),
             service('sylius.factory.zone_member'),
         ]);
 
-    $services->alias('Sylius\Component\Addressing\Factory\ZoneFactoryInterface', 'sylius.custom_factory.zone');
+    $services->alias(ZoneFactoryInterface::class, 'sylius.custom_factory.zone');
 
-    $services->set('sylius.provider.province_naming', 'Sylius\Component\Addressing\Provider\ProvinceNamingProvider')
+    $services->set('sylius.provider.province_naming', ProvinceNamingProvider::class)
         ->lazy()
         ->args([service('sylius.repository.province')]);
 
-    $services->alias('Sylius\Component\Addressing\Provider\ProvinceNamingProviderInterface', 'sylius.provider.province_naming');
+    $services->alias(ProvinceNamingProviderInterface::class, 'sylius.provider.province_naming');
 
-    $services->set('sylius.matcher.zone', 'Sylius\Component\Addressing\Matcher\ZoneMatcher')
+    $services->set('sylius.matcher.zone', ZoneMatcher::class)
         ->public()
         ->args([service('sylius.repository.zone')]);
 
-    $services->alias('Sylius\Component\Addressing\Matcher\ZoneMatcherInterface', 'sylius.matcher.zone')
+    $services->alias(ZoneMatcherInterface::class, 'sylius.matcher.zone')
         ->public();
 
-    $services->set('sylius.converter.country_name', 'Sylius\Component\Addressing\Converter\CountryNameConverter');
+    $services->set('sylius.converter.country_name', CountryNameConverter::class);
 
-    $services->alias('Sylius\Component\Addressing\Converter\CountryNameConverterInterface', 'sylius.converter.country_name');
+    $services->alias(CountryNameConverterInterface::class, 'sylius.converter.country_name');
 
-    $services->set('sylius.comparator.address', 'Sylius\Component\Addressing\Comparator\AddressComparator');
+    $services->set('sylius.comparator.address', AddressComparator::class);
 
-    $services->alias('Sylius\Component\Addressing\Comparator\AddressComparatorInterface', 'sylius.comparator.address');
+    $services->alias(AddressComparatorInterface::class, 'sylius.comparator.address');
 
-    $services->set('sylius.twig.extension.country_name', 'Sylius\Bundle\AddressingBundle\Twig\CountryNameExtension')
-        ->args([''])
+    $services->set('sylius.twig.extension.country_name', CountryNameExtension::class)
         ->tag('twig.extension');
 
-    $services->set('sylius.twig.extension.province_naming', 'Sylius\Bundle\AddressingBundle\Twig\ProvinceNamingExtension')
-        ->args([
-            service('sylius.provider.province_naming'),
-            '',
-        ])
+    $services->set('sylius.twig.extension.province_naming', ProvinceNamingExtension::class)
+        ->args([service('sylius.provider.province_naming')])
         ->tag('twig.extension');
 
-    $services->set('sylius.validator.valid_province_address', 'Sylius\Bundle\AddressingBundle\Validator\Constraints\ProvinceAddressConstraintValidator')
+    $services->set('sylius.validator.valid_province_address', ProvinceAddressConstraintValidator::class)
         ->args([
             service('sylius.repository.country'),
             service('sylius.repository.province'),
         ])
         ->tag('validator.constraint_validator', ['alias' => 'sylius_province_address_validator']);
 
-    $services->set('sylius.validator.zone_cannot_contain_itself', 'Sylius\Bundle\AddressingBundle\Validator\Constraints\ZoneCannotContainItselfValidator')
+    $services->set('sylius.validator.zone_cannot_contain_itself', ZoneCannotContainItselfValidator::class)
         ->tag('validator.constraint_validator', ['alias' => 'sylius_zone_cannot_contain_itself_validator']);
 
-    $services->set('sylius.validator.unique_province_collection', 'Sylius\Bundle\AddressingBundle\Validator\Constraints\UniqueProvinceCollectionValidator')
+    $services->set('sylius.validator.unique_province_collection', UniqueProvinceCollectionValidator::class)
         ->tag('validator.constraint_validator', ['alias' => 'sylius_unique_province_collection_validator']);
 
-    $services->set('sylius.validator.zone_member_group', 'Sylius\Bundle\AddressingBundle\Validator\Constraints\ZoneMemberGroupValidator')
+    $services->set('sylius.validator.zone_member_group', ZoneMemberGroupValidator::class)
         ->args(['%sylius.addressing.zone_member.validation_groups%'])
         ->tag('validator.constraint_validator', ['alias' => 'sylius_zone_member_group']);
 };
--- src/Sylius/Bundle/AddressingBundle/Resources/config/services/listeners.php
+++ src/Sylius/Bundle/AddressingBundle/Resources/config/services/listeners.php
@@ -1,14 +1,27 @@
 <?php
 
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-return static function(ContainerConfigurator $container) {
+use Sylius\Bundle\AddressingBundle\EventListener\ZoneMemberIntegrityListener;
+
+return static function (ContainerConfigurator $container) {
     $services = $container->services();
 
     $services->defaults()
         ->public();
 
-    $services->set('sylius.listener.zone_member_integrity', 'Sylius\Bundle\AddressingBundle\EventListener\ZoneMemberIntegrityListener')
+    $services->set('sylius.listener.zone_member_integrity', ZoneMemberIntegrityListener::class)
         ->args([
             service('request_stack'),
             service('sylius.checker.zone_deletion'),
