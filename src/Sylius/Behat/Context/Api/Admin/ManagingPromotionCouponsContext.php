d54e6394bb62063aa31a6df0a8fd228c0420ac34
Mateusz <mateusz@rustle.pl>
2026-03-04 13:54:06 +0100

    Fixed build errors

--- src/Sylius/Behat/Context/Api/Admin/ManagingPromotionCouponsContext.php
+++ src/Sylius/Behat/Context/Api/Admin/ManagingPromotionCouponsContext.php
@@ -173,7 +173,9 @@ final readonly class ManagingPromotionCouponsContext implements Context
      */
     public function iSpecifyTheirCodeLengthAs(?int $codeLength = null): void
     {
-        $this->client->updateRequestData(['codeLength' => $codeLength]);
+        if (null !== $codeLength) {
+            $this->client->updateRequestData(['codeLength' => $codeLength]);
+        }
     }
 
     /**
