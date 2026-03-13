90fadaab0bc74684df56d465d3d8bec996873ec7
TheMilek <kam.grygierzec@gmail.com>
2026-03-13 11:38:22 +0100

    Change application's version to v1.12.25-dev

--- src/Sylius/Bundle/CoreBundle/SyliusCoreBundle.php
+++ src/Sylius/Bundle/CoreBundle/SyliusCoreBundle.php
@@ -40,17 +40,17 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class SyliusCoreBundle extends AbstractResourceBundle
 {
-    public const VERSION = '1.12.24';
+    public const VERSION = '1.12.25-DEV';
 
-    public const VERSION_ID = '11224';
+    public const VERSION_ID = '11225';
 
     public const MAJOR_VERSION = '1';
 
     public const MINOR_VERSION = '12';
 
-    public const RELEASE_VERSION = '24';
+    public const RELEASE_VERSION = '25';
 
-    public const EXTRA_VERSION = '';
+    public const EXTRA_VERSION = 'DEV';
 
     public function getSupportedDrivers(): array
     {
