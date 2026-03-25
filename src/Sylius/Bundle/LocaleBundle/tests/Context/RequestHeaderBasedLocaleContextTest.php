df0704ba6df1250a59bde2c3b7c2b937d88087f2
Marek Rzytki <rzytki7@gmail.com>
2026-03-25 19:43:40 +0100

    Refactor locale resolution tests to use data providers

--- src/Sylius/Bundle/LocaleBundle/tests/Context/RequestHeaderBasedLocaleContextTest.php
+++ src/Sylius/Bundle/LocaleBundle/tests/Context/RequestHeaderBasedLocaleContextTest.php
@@ -54,169 +54,113 @@ final class RequestHeaderBasedLocaleContextTest extends TestCase
         $this->requestHeaderBasedLocaleContext->getLocaleCode();
     }
 
-    public function testThrowsLocaleNotFoundExceptionIfLocaleFromMainRequestPreferredLanguageCannotBeResolved(): void
+    /**
+     * @dataProvider provideResolvingCases
+     *
+     * @param list<string> $availableLocales
+     */
+    public function testResolvesLocaleCode(string $acceptLanguage, string $default, array $availableLocales, string $expected): void
     {
         $request = new Request();
-        $request->headers->set('Accept-Language', 'fr_FR');
+        $request->headers->set('Accept-Language', $acceptLanguage);
 
         $this->requestStack->expects(self::once())->method('getMainRequest')->willReturn($request);
 
         $this->localeProvider->expects(self::once())
             ->method('getDefaultLocaleCode')
-            ->willReturn('pl_PL');
+            ->willReturn($default);
 
         $this->localeProvider->expects(self::once())
             ->method('getAvailableLocalesCodes')
-            ->willReturn(['pl_PL', 'de_DE']);
+            ->willReturn($availableLocales);
+
+        self::assertSame($expected, $this->requestHeaderBasedLocaleContext->getLocaleCode());
+    }
+
+    /**
+     * @dataProvider provideNotFoundCases
+     *
+     * @param list<string> $availableLocales
+     */
+    public function testThrowsLocaleNotFoundExceptionForRequest(string $acceptLanguage, string $default, array $availableLocales): void
+    {
+        $request = new Request();
+        $request->headers->set('Accept-Language', $acceptLanguage);
+
+        $this->requestStack->expects(self::once())->method('getMainRequest')->willReturn($request);
+
+        $this->localeProvider->expects(self::once())
+            ->method('getDefaultLocaleCode')
+            ->willReturn($default);
+
+        $this->localeProvider->expects(self::once())
+            ->method('getAvailableLocalesCodes')
+            ->willReturn($availableLocales);
 
         self::expectException(LocaleNotFoundException::class);
 
         $this->requestHeaderBasedLocaleContext->getLocaleCode();
     }
 
-    public function testThrowsLocaleNotFoundExceptionWhenLanguagePrefixHasNoMatchingLocale(): void
+    /** @return iterable<string, array{acceptLanguage: string, default: string, availableLocales: list<string>, expected: string}> */
+    public static function provideResolvingCases(): iterable
     {
-        $request = new Request();
-        $request->headers->set('Accept-Language', 'en');
-
-        $this->requestStack->expects(self::once())->method('getMainRequest')->willReturn($request);
-
-        $this->localeProvider->expects(self::once())
-            ->method('getDefaultLocaleCode')
-            ->willReturn('it_IT');
-
-        $this->localeProvider->expects(self::once())
-            ->method('getAvailableLocalesCodes')
-            ->willReturn(['it_IT']);
-
-        self::expectException(LocaleNotFoundException::class);
-
-        $this->requestHeaderBasedLocaleContext->getLocaleCode();
+        yield 'locale syntax' => [
+            'acceptLanguage' => 'de_DE',
+            'default' => 'pl_PL',
+            'availableLocales' => ['pl_PL', 'de_DE'],
+            'expected' => 'de_DE',
+        ];
+        yield 'mixed-cased language syntax' => [
+            'acceptLanguage' => 'dE-De',
+            'default' => 'pl_PL',
+            'availableLocales' => ['pl_PL', 'de_DE'],
+            'expected' => 'de_DE',
+        ];
+        yield 'upper-cased language syntax' => [
+            'acceptLanguage' => 'DE-DE',
+            'default' => 'pl_PL',
+            'availableLocales' => ['pl_PL', 'de_DE'],
+            'expected' => 'de_DE',
+        ];
+        yield 'lower-cased language syntax' => [
+            'acceptLanguage' => 'de-de',
+            'default' => 'pl_PL',
+            'availableLocales' => ['pl_PL', 'de_DE'],
+            'expected' => 'de_DE',
+        ];
+        yield 'language prefix for macro region locale' => [
+            'acceptLanguage' => 'en',
+            'default' => 'en_150',
+            'availableLocales' => ['en_150'],
+            'expected' => 'en_150',
+        ];
+        yield 'language prefix for Latin America macro region locale' => [
+            'acceptLanguage' => 'es',
+            'default' => 'es_419',
+            'availableLocales' => ['es_419'],
+            'expected' => 'es_419',
+        ];
+        yield 'first locale by language prefix when multiple macro region locales available' => [
+            'acceptLanguage' => 'en',
+            'default' => 'en_001',
+            'availableLocales' => ['en_001', 'en_150'],
+            'expected' => 'en_001',
+        ];
     }
 
-    public function testResolvesLocaleFromMainRequestPreferredLanguageInLocaleSyntax(): void
+    /** @return iterable<string, array{acceptLanguage: string, default: string, availableLocales: list<string>}> */
+    public static function provideNotFoundCases(): iterable
     {
-        $request = new Request();
-        $request->headers->set('Accept-Language', 'de_DE');
-
-        $this->requestStack->expects(self::once())->method('getMainRequest')->willReturn($request);
-
-        $this->localeProvider->expects(self::once())
-            ->method('getDefaultLocaleCode')
-            ->willReturn('pl_PL');
-
-        $this->localeProvider->expects(self::once())
-            ->method('getAvailableLocalesCodes')
-            ->willReturn(['pl_PL', 'de_DE']);
-
-        self::assertSame('de_DE', $this->requestHeaderBasedLocaleContext->getLocaleCode());
-    }
-
-    public function testResolvesLocaleFromMainRequestPreferredLanguageInMixedCasedLanguageSyntax(): void
-    {
-        $request = new Request();
-        $request->headers->set('Accept-Language', 'dE-De');
-
-        $this->requestStack->expects(self::once())->method('getMainRequest')->willReturn($request);
-
-        $this->localeProvider->expects(self::once())
-            ->method('getDefaultLocaleCode')
-            ->willReturn('pl_PL');
-
-        $this->localeProvider->expects(self::once())
-            ->method('getAvailableLocalesCodes')
-            ->willReturn(['pl_PL', 'de_DE']);
-
-        self::assertSame('de_DE', $this->requestHeaderBasedLocaleContext->getLocaleCode());
-    }
-
-    public function testResolvesLocaleFromMainRequestPreferredLanguageInUpperCasedLanguageSyntax(): void
-    {
-        $request = new Request();
-        $request->headers->set('Accept-Language', 'DE-DE');
-
-        $this->requestStack->expects(self::once())->method('getMainRequest')->willReturn($request);
-
-        $this->localeProvider->expects(self::once())
-            ->method('getDefaultLocaleCode')
-            ->willReturn('pl_PL');
-
-        $this->localeProvider->expects(self::once())
-            ->method('getAvailableLocalesCodes')
-            ->willReturn(['pl_PL', 'de_DE']);
-
-        self::assertSame('de_DE', $this->requestHeaderBasedLocaleContext->getLocaleCode());
-    }
-
-    public function testResolvesLocaleFromMainRequestPreferredLanguageInLowerCasedLanguageSyntax(): void
-    {
-        $request = new Request();
-        $request->headers->set('Accept-Language', 'de-de');
-
-        $this->requestStack->expects(self::once())->method('getMainRequest')->willReturn($request);
-
-        $this->localeProvider->expects(self::once())
-            ->method('getDefaultLocaleCode')
-            ->willReturn('pl_PL');
-
-        $this->localeProvider->expects(self::once())
-            ->method('getAvailableLocalesCodes')
-            ->willReturn(['pl_PL', 'de_DE']);
-
-        self::assertSame('de_DE', $this->requestHeaderBasedLocaleContext->getLocaleCode());
-    }
-
-    public function testResolvesLocaleByLanguagePrefixForMacroRegionLocale(): void
-    {
-        $request = new Request();
-        $request->headers->set('Accept-Language', 'en');
-
-        $this->requestStack->expects(self::once())->method('getMainRequest')->willReturn($request);
-
-        $this->localeProvider->expects(self::once())
-            ->method('getDefaultLocaleCode')
-            ->willReturn('en_150');
-
-        $this->localeProvider->expects(self::once())
-            ->method('getAvailableLocalesCodes')
-            ->willReturn(['en_150']);
-
-        self::assertSame('en_150', $this->requestHeaderBasedLocaleContext->getLocaleCode());
-    }
-
-    public function testResolvesLocaleByLanguagePrefixForLatinAmericaMacroRegionLocale(): void
-    {
-        $request = new Request();
-        $request->headers->set('Accept-Language', 'es');
-
-        $this->requestStack->expects(self::once())->method('getMainRequest')->willReturn($request);
-
-        $this->localeProvider->expects(self::once())
-            ->method('getDefaultLocaleCode')
-            ->willReturn('es_419');
-
-        $this->localeProvider->expects(self::once())
-            ->method('getAvailableLocalesCodes')
-            ->willReturn(['es_419']);
-
-        self::assertSame('es_419', $this->requestHeaderBasedLocaleContext->getLocaleCode());
-    }
-
-    public function testResolvesFirstLocaleByLanguagePrefixWhenMultipleMacroRegionLocalesAvailable(): void
-    {
-        $request = new Request();
-        $request->headers->set('Accept-Language', 'en');
-
-        $this->requestStack->expects(self::once())->method('getMainRequest')->willReturn($request);
-
-        $this->localeProvider->expects(self::once())
-            ->method('getDefaultLocaleCode')
-            ->willReturn('en_001');
-
-        $this->localeProvider->expects(self::once())
-            ->method('getAvailableLocalesCodes')
-            ->willReturn(['en_001', 'en_150']);
-
-        self::assertSame('en_001', $this->requestHeaderBasedLocaleContext->getLocaleCode());
+        yield 'locale not available' => [
+            'acceptLanguage' => 'fr_FR',
+            'default' => 'pl_PL',
+            'availableLocales' => ['pl_PL', 'de_DE'],
+        ];
+        yield 'language prefix with no matching locale' => [
+            'acceptLanguage' => 'en',
+            'default' => 'it_IT',
+            'availableLocales' => ['it_IT'],
+        ];
     }
 }
