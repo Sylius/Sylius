a46c8f031dbeb1645e7799081ee43bff11f96ca4
TheMilek <kam.grygierzec@gmail.com>
2026-04-10 08:51:54 +0200

    [Taxonomy] Unify Tests directory with tests

--- src/Sylius/Bundle/TaxonomyBundle/tests/Validator/Constraints/TaxonParentRelationValidatorTest.php
+++ src/Sylius/Bundle/TaxonomyBundle/tests/Validator/Constraints/TaxonParentRelationValidatorTest.php
@@ -11,7 +11,7 @@
 
 declare(strict_types=1);
 
-namespace Sylius\Bundle\TaxonomyBundle\Tests\Validator\Constraints;
+namespace Tests\Sylius\Bundle\TaxonomyBundle\Validator\Constraints;
 
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
