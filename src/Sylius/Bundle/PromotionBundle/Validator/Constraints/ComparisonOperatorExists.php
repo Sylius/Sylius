c11032b89880bbf3ba05f46e98f5a7535f0f1c18
Francis Hilaire <Prometee@users.noreply.github.com>
2026-08-05 16:07:26 +0200

    [PromotionBundle] Simplify ComparisonOperatorExists constraint constructor

--- src/Sylius/Bundle/PromotionBundle/Validator/Constraints/ComparisonOperatorExists.php
+++ src/Sylius/Bundle/PromotionBundle/Validator/Constraints/ComparisonOperatorExists.php
@@ -19,28 +19,12 @@ use Symfony\Component\Validator\Constraint;
 #[\Attribute]
 final class ComparisonOperatorExists extends Constraint
 {
-    /**
-     * @param array<string, mixed>|null $options
-     */
     #[HasNamedArguments]
     public function __construct(
-        ?array $options = null,
         public string $message = 'sylius.promotion_rule.comparison_operator.invalid',
         ?array $groups = null,
         mixed $payload = null,
     ) {
-        if (\is_array($options)) {
-            trigger_deprecation(
-                'sylius/promotion-bundle',
-                '2.3',
-                'Passing an array of options to configure the "%s" constraint is deprecated and will be removed in Sylius 3.0, use named arguments instead.',
-                static::class,
-            );
-
-            $this->message = $options['message'] ?? $this->message;
-            $groups ??= $options['groups'] ?? null;
-            $payload ??= $options['payload'] ?? null;
-        }
 
         parent::__construct(groups: $groups, payload: $payload);
     }
